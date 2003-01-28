c24238b25f21296518a80d4d6df67c6fa6162e2e
dudleyc <dudleyc@bar.>
2003-01-28 20:03:01 +0000

    pubsub/xarversion.php: 	#  change pubsub category

--- pubsub/xarversion.php
+++ pubsub/xarversion.php
@@ -3,11 +3,6 @@ $modversion['name'] = 'Pubsub';
 $modversion['id'] = '181';
 $modversion['version'] = '1.0';
 $modversion['description'] = 'Allow users to subscribe to updates to events';
-$modversion['credits'] = 'xardocs/credits.txt';
-$modversion['help'] = 'xardocs/help.txt';
-$modversion['changelog'] = 'xardocs/changelog.txt';
-$modversion['license'] = 'xardocs/license.txt';
-$modversion['coding'] = 'xardocs/coding.txt';
 $modversion['official'] = 1;
 $modversion['author'] = 'Chris Dudley';
 $modversion['contact'] = 'miko@xaraya.com';
@@ -15,5 +10,5 @@ $modversion['admin'] = 1;
 $modversion['user'] = 1;
 $modversion['securityschema'] = array('Pubsub::' => 'PubsubID:EventID:HandlingID');
 $modversion['class'] = 'Utility';
-$modversion['category'] = 'Content';
+$modversion['category'] = 'Global';
 ?>
