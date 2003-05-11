98d843ded3ae828a62876b82d04721e723699b9f
John.Cox <John.Cox@mcnabb.>
2003-05-11 20:03:05 +0000

    release/xartemplates/user-main.xd: 	#  release module work release/xartemplates/user-viewids.xd: 	#  release module work release/xaruser.php: 	#  release module work release/xartemplates/user-rssviewids.xd: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/xaraya-modules/release/xartemplates/user-rssviewids.xd

--- release/xaruser.php
+++ release/xaruser.php
@@ -14,9 +14,7 @@ function release_user_main()
     // Security Check
     if(!xarSecurityCheck('OverviewRelease')) return;
     
-    xarResponseRedirect(xarModURL('release',
-                                  'user',
-                                  'viewnotes'));
+    return array();
 
 }
 
@@ -174,6 +172,90 @@ function release_user_viewids()
                                            'user',
                                            'countdocs',
                                            array('rid' => $item['rid']));
+    }
+
+    // Add the array of items to the template variables
+    $data['items'] = $items;
+    return $data;
+
+}
+
+function release_user_rssviewids()
+{
+    // Security Check
+    if(!xarSecurityCheck('OverviewRelease')) return;
+
+    $phase = xarVarCleanFromInput('phase');
+
+    if (empty($phase)){
+        $phase = 'all';
+    }
+
+    $data = array();
+
+    switch(strtolower($phase)) {
+
+        case 'all':
+        default:
+
+            // The user API function is called. 
+            $items = xarModAPIFunc('release',
+                                   'user',
+                                   'getallids');
+            break;
+
+        case 'themes':
+
+            // The user API function is called. 
+            $items = xarModAPIFunc('release',
+                                   'user',
+                                   'getthemeids');
+            break;
+
+        case 'modules':
+
+            // The user API function is called. 
+            $items = xarModAPIFunc('release',
+                                   'user',
+                                   'getmoduleids');
+            break;
+    }
+
+
+
+    if (empty($items)) {
+        $msg = xarML('There are no items to display in the release module');
+        xarExceptionSet(XAR_USER_EXCEPTION, 'MISSING_DATA', new DefaultUserException($msg));
+        return;
+    }
+
+    // Check individual permissions for Edit / Delete
+    for ($i = 0; $i < count($items); $i++) {
+        $item = $items[$i];
+
+        // Basic Information
+        $items[$i]['rid'] = xarVarPrepForDisplay($item['rid']);
+        $items[$i]['name'] = xarVarPrepForDisplay($item['name']);
+        $items[$i]['desc'] = xarVarPrepForDisplay($item['desc']);
+
+        $getuser = xarModAPIFunc('roles',
+                                 'user',
+                                 'get',
+                                  array('uid' => $item['uid']));
+
+        // Author Name and Contact URL
+        $items[$i]['author'] = $getuser['name'];
+        $items[$i]['contacturl'] = xarModURL('roles',
+                                             'user',
+                                             'display',
+                                              array('uid' => $item['uid']));
+        
+        // InfoURL
+        $items[$i]['infourl'] = xarModURL('release',
+                                          'user',
+                                          'display',
+                                          array('rid' => $item['rid']));
+
     }
 
     // Add the array of items to the template variables
