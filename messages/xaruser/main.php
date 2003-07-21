8998a125ab49fd250cc6c270894608fe5afa3cc5
rabbitt <rabbitt@shiva.nioka.org>
2003-07-21 20:03:07 +0000

    messages/xartemplates/user-main.xd: 	#  point main --> display messages/xaruser/main.php: 	#  point main --> display

--- messages/xaruser/main.php
+++ messages/xaruser/main.php
@@ -1,25 +1,8 @@
 <?php
 function messages_user_main() {
 
-    if (!xarSecurityCheck('ViewMessages', 0)) {
-        return $data['error'] = xarML('You are not permitted to view messages.');
-    }
+    xarResponseRedirect(xarModURL('messages','user','display'));
 
-    xarTplSetPageTitle( 'Messages :: Splash Page' );
-
-    // Initialize the statusmessage
-    $statusmsg = xarSessionGetVar( 'messages_statusmsg' );
-    if ( isset( $statusmsg ) ) {
-        xarSessionDelVar( 'messages_statusmsg' );
-        $common['statusmsg'] = $statusmsg;
-    }
-
-    $common['menu'] = array();
-    $common['pagetitle'] = 'Splash Page';
-
-    $data['common'] = $common;
-
-    return $data;
 }
 
 ?>
