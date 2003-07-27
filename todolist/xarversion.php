c289cebce644e34e58ba4839f0cec980b308c760
voll <voll@yahoo.com>
2003-07-27 20:03:07 +0000

    todolist/xarversion.php: 	#  change module id to 67

--- todolist/xarversion.php
+++ todolist/xarversion.php
@@ -1,6 +1,6 @@
 <?php // $Id: s.xarversion.php 1.5 02/12/01 14:28:40+01:00 marcel@hsdev.com $
 $modversion['name'] = 'Todolist';
-$modversion['id'] = '69';
+$modversion['id'] = '67';
 $modversion['version'] = '0.9.13';
 $modversion['description'] = 'Todolist';
 $modversion['credits'] = 'docs/credits.txt';
@@ -9,7 +9,7 @@ $modversion['changelog'] = 'docs/changelog.txt';
 $modversion['license'] = 'docs/license.txt';
 $modversion['official'] = 1;
 $modversion['author'] = 'Jörg Menke/Volodymyr Metenchuk';
-$modversion['contact'] = 'http://postnuke.solidno.ru/';
+$modversion['contact'] = 'http://www.postnuke.ru/';
 $modversion['admin'] = 1;
 $modversion['user'] = 1;
 $modversion['securityschema'] = array('todolist::Item' => 'item name::item ID');
