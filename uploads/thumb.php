f1c1f01ca2120ec3784292e5c2b1a87585e65813
mcortez <mcortez@ariel2.fullcoll.edu>
2003-08-26 20:03:08 +0000

    uploads/thumb.php: 	#  BitKeeper file C:/cygwin/home/mcortez/xaraya-modules/uploads/thumb.php uploads/xaradmin/importpictures.php: 	#  BitKeeper file C:/cygwin/home/mcortez/xaraya-modules/uploads/xaradmin/importpictures.php

---
+++ uploads/thumb.php
@@ -0,0 +1,179 @@
+<?php
+
+function ImageCreateFrom($file)
+{
+	$IMAGE_PROPERTIES = GetImageSize($file);
+
+	switch( $IMAGE_PROPERTIES[2] )
+	{
+		case 1:
+		    $im = @imagecreatefromgif ($file); /* Attempt to open */
+			break;
+		case 2:
+		    $im = @imagecreatefromjpeg ($file); /* Attempt to open */
+			break;
+		case 3:
+		    $im = @imagecreatefrompng ($file); /* Attempt to open */
+			break;
+		default:
+			$im = false;
+	}
+    if (!$im) { /* See if it failed */
+        $im = imagecreate (150, 30); /* Create a blank image */
+        $bgc = imagecolorallocate ($im, 255, 255, 255);
+        $tc = imagecolorallocate ($im, 0, 0, 0);
+        imagefilledrectangle ($im, 0, 0, 150, 30, $bgc);
+        /* Output an errmsg */
+        imagestring ($im, 1, 5, 5, "Error loading $imgname", $tc);
+    }
+    return $im;
+}
+
+
+function createthumb($IMAGE_SOURCE,$THUMB_X,$THUMB_Y,$OUTPUT_FILE){
+
+  $BACKUP_FILE = $OUTPUT_FILE . "_backup.jpg";
+  copy($IMAGE_SOURCE,$BACKUP_FILE);
+
+
+    $SRC_IMAGE = ImageCreateFrom($BACKUP_FILE);
+    $SRC_X = ImageSX($SRC_IMAGE);
+    $SRC_Y = ImageSY($SRC_IMAGE);
+
+    if (($THUMB_Y == "0") AND ($THUMB_X == "0")) {
+
+      return(0);
+    } elseif ($THUMB_Y == "0") {
+      $SCALEX = $THUMB_X/($SRC_X-1);
+      $THUMB_Y = $SRC_Y*$SCALEX;
+    } elseif ($THUMB_X == "0") {
+      $SCALEY = $THUMB_Y/($SRC_Y-1);
+      $THUMB_X = $SRC_X*$SCALEY;
+    }
+
+    $THUMB_X = (int)($THUMB_X);
+    $THUMB_Y = (int)($THUMB_Y);
+
+    $DEST_IMAGE = imagecreatetruecolor($THUMB_X,$THUMB_Y);
+
+
+    unlink($BACKUP_FILE);
+    if (!ImageCopyResampleBicubic($DEST_IMAGE, $SRC_IMAGE, 0, 0, 0, 0, $THUMB_X, $THUMB_Y, $SRC_X, $SRC_Y)) {
+
+      imagedestroy($SRC_IMAGE);
+      imagedestroy($DEST_IMAGE);
+      return(0);
+    } else {
+      imagedestroy($SRC_IMAGE);
+      if (ImageJPEG($DEST_IMAGE,$OUTPUT_FILE)) {
+        imagedestroy($DEST_IMAGE);
+        return(1);
+      }
+      imagedestroy($DEST_IMAGE);
+    }
+    return(0);
+
+
+} # end createthumb
+
+
+function LoadJpeg ($imgname) {
+    $im = @imagecreatefromjpeg ($imgname); /* Attempt to open */
+    if (!$im) { /* See if it failed */
+        $im  = imagecreate (150, 30); /* Create a blank image */
+        $bgc = imagecolorallocate ($im, 255, 255, 255);
+        $tc  = imagecolorallocate ($im, 0, 0, 0);
+        imagefilledrectangle ($im, 0, 0, 150, 30, $bgc);
+        /* Output an errmsg */
+        imagestring ($im, 1, 5, 5, "Error loading $imgname", $tc);
+    }
+    return $im;
+}
+/*
+function ImageCopyResampleBicubic (&$dst_img, &$src_img, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h) {
+  ImagePaletteCopy ($dst_img, $src_img);
+  $rX = $src_w / $dst_w;
+  $rY = $src_h / $dst_h;
+  $w = 0;
+  for ($y = $dst_y; $y < $dst_h; $y++) {
+    $ow = $w; $w = round(($y + 1) * $rY);
+    $t = 0;
+    for ($x = $dst_x; $x < $dst_w; $x++) {
+      $r = $g = $b = 0; $a = 0;
+      $ot = $t; $t = round(($x + 1) * $rX);
+      for ($u = 0; $u < ($w - $ow); $u++) {
+        for ($p = 0; $p < ($t - $ot); $p++) {
+          $c = ImageColorsForIndex ($src_img, ImageColorAt ($src_img, $ot + $p, $ow + $u));
+          $r += $c['red'];
+          $g += $c['green'];
+          $b += $c['blue'];
+          $a++;
+        }
+      }
+      ImageSetPixel ($dst_img, $x, $y, ImageColorClosest ($dst_img, $r / $a, $g / $a, $b / $a)); 
+    }
+  }
+}*/
+
+function ImageCopyResampleBicubic (&$dst_img, &$src_img, $dst_x,
+    $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h)
+    /*
+    port to PHP by John Jensen July 10 2001 (updated June 13, 2002 by tim@smoothdeity.com) -- 
+    original code (in C, for the PHP GD Module) by jernberg@fairytale.se
+    */
+    {        
+        $palsize = ImageColorsTotal ($src_img);
+        for ($i = 0; $i < $palsize; $i++) 
+            {  // get palette.
+            $colors = ImageColorsForIndex ($src_img, $i);
+            ImageColorAllocate ($dst_img, $colors['red'], $colors['green'], $colors['blue']);
+            }
+    
+        $scaleX = ($src_w - 1) / $dst_w;
+        $scaleY = ($src_h - 1) / $dst_h;
+    
+        $scaleX2 = (int) ($scaleX / 2);
+        $scaleY2 = (int) ($scaleY / 2);
+        
+        $dstSizeX = imagesx( $dst_img );
+        $dstSizeY = imagesy( $dst_img );
+        $srcSizeX = imagesx( $src_img );
+        $srcSizeY = imagesy( $src_img );
+
+    
+        for ($j = 0; $j < ($dst_h - $dst_y); $j++) {
+            $sY = (int) ($j * $scaleY) + $src_y;
+            $y13 = $sY + $scaleY2;
+            
+            $dY = $j + $dst_y;
+            
+            if (($sY > $srcSizeY) or ($dY > $dstSizeY))
+                break 1;
+            
+    
+            for ($i = 0; $i < ($dst_w - $dst_x); $i++) {
+                $sX = (int) ($i * $scaleX) + $src_x;
+                $x34 = $sX + $scaleX2;
+                
+                $dX = $i + $dst_x;
+                
+                if (($sX > $srcSizeX) or ($dX > $dstSizeX))
+                    break 1;
+    
+                $color1 = ImageColorsForIndex ($src_img, ImageColorAt ($src_img, $sX, $y13));
+                $color2 = ImageColorsForIndex ($src_img, ImageColorAt ($src_img, $sX, $sY));
+                $color3 = ImageColorsForIndex ($src_img, ImageColorAt ($src_img, $x34, $y13));
+                $color4 = ImageColorsForIndex ($src_img, ImageColorAt ($src_img, $x34, $sY));
+    
+                $red = ($color1['red'] + $color2['red'] + $color3['red'] + $color4['red']) / 4;
+                $green = ($color1['green'] + $color2['green'] + $color3['green'] + $color4['green']) / 4;
+                $blue = ($color1['blue'] + $color2['blue'] + $color3['blue'] + $color4['blue']) / 4;
+    
+                ImageSetPixel ($dst_img, $dX, $dY,
+                    ImageColorClosest ($dst_img, $red, $green, $blue)); 
+            }
+        }
+		return true;
+    } 
+
+?>
---
+++ uploads/xaradmin/importpictures.php
@@ -0,0 +1,189 @@
+<?php
+function uploads_admin_importpictures( $args )
+{
+//	global $dd_26;
+	//$dd_26 = 'http://epicsaga.com/what_do_you_know?';
+
+	// Can use this or the dd_26 one
+	global $uploads_var_overide;
+
+	//Config
+	$image_import_dir 			 = '/home/epicsaga/public_html/var/uploads/images';
+	$Picture_Publication_Type_ID = 5;
+
+	echo "Import Pictures here<br>";
+	
+	// Kick mod available
+	echo "Checking mod avaliable (dynamicdata): ";
+	$avail = xarModIsAvailable("dynamicdata");
+	if( $avail )
+	{
+		echo "yes<br>";
+	} else {
+		echo "no<br>";
+	}
+	
+	// Get files to import 
+	$FilesInDir = getFileList( $image_import_dir );
+	
+	// Prune out dupes, and ones already in the system
+	$prunedFiles = pruneFiles( $FilesInDir );
+
+
+	// Setup Article Defaults
+	$title   = '';
+	$summary = '';
+	$notes   = '';
+	$pubdate = time();
+	$status  = 2;		//Default to approved
+	$ptid    = $Picture_Publication_Type_ID;
+	$cids = array();
+	
+	$pubtypeid = $Picture_Publication_Type_ID;
+	$authorid  = xarSessionGetVar('uid');
+	$aid       = 0;
+
+    $article = array('title' => $title,
+                     'summary' => $summary,
+                     'body' => $body,
+                     'notes' => $notes,
+                     'pubdate' => $pubdate,
+                     'status' => $status,
+                     'ptid' => $ptid,
+                     'cids' => $cids,
+                  // for preview
+                     'pubtypeid' => $ptid,
+                     'authorid' => $authorid,
+                     'aid' => 0
+					 );
+
+	// Loop through files and import
+	foreach ($prunedFiles as $filename) 
+	{
+		echo "File: ".$filename."<br>";
+		$lastSlash = strlen($filename) - strpos( strrev( $filename ), '/' );
+		$title = ucwords(str_replace( "_", " ", substr ($filename, $lastSlash, strpos( $filename, '.')-1 ) ));
+
+		// import file into Uploads
+		$filepath = $image_import_dir.$filename;
+		
+		if (is_file($filepath))
+		{
+			$data = array('ulfile'   => $filename
+						 ,'filepath' => $filepath
+						 ,'utype'    => 'file'
+						 ,'mod'      => 'uploads'
+						 ,'modid'    => 0
+						 ,'filesize' => filesize($filepath)
+						 ,'type'     => '');
+
+			echo "About to store<br>";	
+			$info = xarModAPIFunc('uploads','user','store',$data);
+			echo "Stored<br>";	
+
+		}		
+		
+		// Setup file specific title
+		$article['title'] = $title;
+		
+		// Setup var to overide the uploads dd property when dd hook is called to place correct link
+		$uploads_var_overide = $info['link'];
+//		$dd_26 				 = $info['link'];
+
+		// Create Picture Article
+		echo "Creating Article<br>";
+		$aid = xarModAPIFunc('articles','admin','create',$article);
+		
+	
+		echo "Article Created :: ID :: $aid<br>";
+	}
+	exit();
+}
+
+
+
+function getFileList( $import_directory )
+{
+	// Recurse through import directories, getting files
+	$DirectoriesToScan = array($import_directory);
+	$DirectoriesScanned = array();
+	while (count($DirectoriesToScan) > 0) 
+	{
+	 foreach ($DirectoriesToScan as $DirectoryKey => $startingdir) {
+	   if ($dir = @opendir($startingdir)) {
+		 while (($file = readdir($dir)) !== false) {
+		   if (($file != '.') && ($file != '..')) {
+		   $RealPathName = realpath($startingdir.'/'.$file);
+			 if (is_dir($RealPathName)) {
+			   if (!in_array($RealPathName, $DirectoriesScanned) && !in_array($RealPathName, $DirectoriesToScan)) {
+				 $DirectoriesToScan[] = $RealPathName;
+			   }
+			 } elseif (is_file($RealPathName)) {
+			   $FilesInDir[] = substr($RealPathName,strlen($import_directory));
+			 }
+		   }
+		 }
+		closedir($dir);
+	   }
+	   $DirectoriesScanned[] = $startingdir;
+	  unset($DirectoriesToScan[$DirectoryKey]);
+	 }
+	}
+	
+	return $FilesInDir;
+}
+
+function pruneFiles( $FilesInDir )
+{
+	// Now check to see if any of those files are already in the system
+	if( isset($FilesInDir) )
+	{	
+	
+		// Get database setup
+		list($dbconn) = xarDBGetConn();
+		$xartable = xarDBGetTables();
+			
+		// table and column definitions
+		$uploadstable = $xartable['uploads'];
+	
+		// Remove dupes and sort
+		$FilesInDir = array_unique($FilesInDir);
+		sort($FilesInDir);
+
+		$prunedFiles = array();
+		foreach ($FilesInDir as $filename) 
+		{
+			// Get items
+			$sql = "SELECT  xar_ulid,
+							xar_ulfile,
+							xar_ulhash,
+							xar_ulapp
+					FROM $uploadstable
+					WHERE xar_ulfile = '$filename' OR xar_ulhash = '$filename' ;";
+			$result = $dbconn->Execute($sql);
+			
+			// Check for an error with the database code, and if so set an appropriate
+			// error message and return
+			if ($dbconn->ErrorNo() != 0) {
+				$msg = xarMLByKey('DATABASE_ERROR', $sql);
+				xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'DATABASE_ERROR',
+							   new SystemException(__FILE__.'('.__LINE__.'): '.$msg));
+				return;
+			}
+			
+			// Check for no rows found, and if so, add file to pruned list
+			if ($result->EOF) 
+			{
+				$insystem = 'No';
+				$prunedFiles[] = $filename;
+				
+			}
+			//close the result set
+			$result->Close();
+		
+		}
+	}
+	return $prunedFiles;
+}
+
+?>
