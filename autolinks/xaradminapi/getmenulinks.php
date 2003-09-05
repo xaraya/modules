8cd410a7cd3f89cdb34ae033fb8efd4dc2705a90
Jason.Judge <Jason.Judge@acad-ajp.>
2003-09-05 20:03:09 +0000

    autolinks/xaradminapi/getmenulinks.php: 	#  New menu options. Never realised this didn't get pushed - should have gone in ages ago.

--- autolinks/xaradminapi/getmenulinks.php
+++ autolinks/xaradminapi/getmenulinks.php
@@ -9,28 +9,48 @@
  */
 function autolinks_adminapi_getmenulinks()
 {
-
-// Security Check
-    if (xarSecurityCheck('AddAutolinks',0)) {
-
-        $menulinks[] = Array('url'   => xarModURL('autolinks',
-                                                   'admin',
-                                                   'new'),
-                              'title' => xarML('Add a new Autolink into the system'),
-                              'label' => xarML('Add'));
-    }
-
-// Security Check
+    // Security Check
     if (xarSecurityCheck('EditAutolinks',0)) {
 
         $menulinks[] = Array('url'   => xarModURL('autolinks',
                                                    'admin',
                                                    'view'),
                               'title' => xarML('View and Edit Autolinks'),
-                              'label' => xarML('View'));
+                              'label' => xarML('View Links'));
     }
 
-// Security Check
+    // Security Check
+    if (xarSecurityCheck('AddAutolinks',0)) {
+
+        $menulinks[] = Array('url'   => xarModURL('autolinks',
+                                                   'admin',
+                                                   'new'),
+                              'title' => xarML('Add a new Autolink into the system'),
+                              'label' => xarML('Add Link'));
+    }
+
+    // Security Check
+    if (xarSecurityCheck('EditAutolinks',0)) {
+
+        $menulinks[] = Array('url'   => xarModURL('autolinks',
+                                                   'admin',
+                                                   'viewtype'),
+                              'title' => xarML('View and Edit Autolink Types'),
+                              'label' => xarML('View Types'));
+    }
+
+    // TODO: AddAutolinksTypes ?
+    // Security Check
+    if (xarSecurityCheck('AddAutolinks',0)) {
+
+        $menulinks[] = Array('url'   => xarModURL('autolinks',
+                                                   'admin',
+                                                   'newtype'),
+                              'title' => xarML('Add a new Autolink Type into the system'),
+                              'label' => xarML('Add Type'));
+    }
+
+    // Security Check
     if (xarSecurityCheck('AdminAutolinks',0)) {
         $menulinks[] = Array('url'   => xarModURL('autolinks',
                                                    'admin',
