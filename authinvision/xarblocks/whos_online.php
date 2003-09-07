a35de9e0097684acf0acd73226ad1f2ef8e206ba
XarayaGeek <XarayaGeek@bugsjr.(none)>
2003-09-07 20:03:09 +0000

    authinvision/xarblocks/whos_online.php: 	#  BitKeeper file /usr/local/repositories/core/modules/authinvision/xarblocks/whos_online.php authinvision/xartemplates/blocks/whos_online.xd: 	#  BitKeeper file /usr/local/repositories/core/modules/authinvision/xartemplates/blocks/whos_online.xd

---
+++ authinvision/xarblocks/whos_online.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * File: $Id: s.online.php 1.25 03/06/10 20:10:43+02:00 marc@marclaptop. $
+ *
+ * Online Block
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2002 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ *
+ * @subpackage roles module
+ * @author Jim McDonald, Greg Allan, John Cox
+*/
+
+/**
+ * initialise block
+ */
+function authinvision_whos_onlineblock_init()
+{
+    return true;
+}
+
+/**
+ * get information on block
+ */
+function authinvision_whos_onlineblock_info()
+{
+    return array('text_type' => 'Online',
+                 'module' => 'roles',
+                 'text_type_long' => 'Display who is online');
+}
+
+/**
+ * Display func.
+ * @param $blockinfo array containing title,content
+ */
+function authinvision_whos_onlineblock_display($blockinfo)
+{
+    // Security check
+    if (!xarSecurityCheck('ViewRoles',0,'Block',"All:" . $blockinfo['title'] . ":All", 'All')) return;
+
+    // Get variables from content block
+    $vars = unserialize($blockinfo['content']);
+
+    // Database setup
+    list($dbconn) = xarDBGetConn();
+    $xartable = xarDBGetTables();
+    $sessioninfotable = $xartable['session_info'];
+    $activetime = time() - (xarConfigGetVar('Site.Session.Duration') * 60);
+    $sql = "SELECT COUNT(1)
+            FROM $sessioninfotable
+            WHERE xar_lastused > $activetime AND xar_uid > 2
+            GROUP BY xar_uid
+            ";
+    $result = $dbconn->Execute($sql);
+
+    if ($dbconn->ErrorNo() != 0) {
+        return false;
+    }
+    $args['numusers'] = $result->RecordCount();
+    $result->Close();
+
+   $query2 = "SELECT count( 1 )
+             FROM $sessioninfotable
+              WHERE xar_lastused > $activetime AND xar_uid = '2'
+              GROUP BY xar_ipaddr
+             ";
+   $result2 = $dbconn->Execute($query2);
+   $args['numguests'] = $result2->RecordCount();
+   $result2->Close();
+
+       // Pluralise
+
+   if ($args['numguests'] == 1) {
+       $args['guests'] = xarML('guest');
+   } else {
+       $args['guests'] = xarML('guests');
+   }
+
+   if ($args['numusers'] == 1) {
+       $args['users'] = xarML('user');
+   } else {
+       $args['users'] = xarML('users');
+   }
+   $args['blockid'] = $blockinfo['bid'];
+    // Block formatting
+    if (empty($blockinfo['title'])) {
+        $blockinfo['title'] = xarML('Online');
+    }
+
+  $lastuser = xarModGetVar('roles', 'lastuser');
+
+    // Make sure we have a lastuser
+    if (!empty($lastuser)) {
+        $status = xarModAPIFunc('authinvision',
+                                'user',
+                                'getlast',
+                                array('lastuser' => $lastuser));
+   $args['lastuser'] = $status['name'];
+//    var_dump($status);
+        // Check return
+        if ($status)
+            $args['lastuser'] = $status;
+    }
+
+    $args['blockid'] = $blockinfo['bid'];
+    if (empty($blockinfo['template'])) {
+        $template = 'whos_online';
+    } else {
+        $template = $blockinfo['template'];
+    }
+    $blockinfo['content'] = xarTplBlock('authinvision', $template, $args);
+    return $blockinfo;
+}
+
+?>
