e5247ef8589040be4df7885bb330d0e847cb4fe6
Jason.Judge <Jason.Judge@acad-ajp.>
2003-09-15 20:03:09 +0000

    autolinks/xaradmin/view.php: 	#  Provide autolink type selection in link type. autolinks/xartemplates/admin-new.xd: 	#  Provide autolink type selection in link type. autolinks/xartemplates/admin-view.xd: 	#  Provide autolink type selection in link type.

--- autolinks/xaradmin/view.php
+++ autolinks/xaradmin/view.php
@@ -8,6 +8,7 @@ function autolinks_admin_view()
 {
     // Get parameters from whatever input we need
     xarVarFetch('startnumitem', 'id', $startnumitem, NULL, XARVAR_NOT_REQUIRED);
+    xarVarFetch('tid', 'int', $tid, NULL, XARVAR_NOT_REQUIRED);
 
     $data['items'] = array();
     $data['authid'] = xarSecGenAuthKey();
@@ -20,20 +21,40 @@ function autolinks_admin_view()
                                     xarModGetVar('autolinks', 'itemsperpage'));
 
     // Security Check
-    if(!xarSecurityCheck('EditAutolinks')) return;
+    if(!xarSecurityCheck('EditAutolinks')) {return;}
 
-    // The user API function is called
-    $links = xarModAPIFunc(
-        'autolinks', 'user', 'getall',
-        array(
-            'startnum' => $startnumitem,
-            'numitems' => xarModGetVar('autolinks', 'itemsperpage')
-        )
+    // Multi-page selection.
+    $select_order = array(
+        'startnum' => $startnumitem,
+        'numitems' => xarModGetVar('autolinks', 'itemsperpage')
     );
 
-    // Check individual permissions for Edit/Delete/Enable
+    // Get previous type restriction if there was one.
+    if (!isset($tid) && xarModGetUserVar('autolinks', 'viewlinks_select_tid')) {
+        $tid = xarModGetUserVar('autolinks', 'viewlinks_select_tid');
+    }
+
+    // Limit list by autolink type if required.
+    if (!empty($tid)) {
+        $select_order['tid'] = $tid;
+        // Hack in place until bug 1232 is fixed: create a dummy module var if necessary.
+        if (xarModGetUserVar('autolinks', 'viewlinks_select_tid') === NULL) {
+            xarModSetVar('autolinks', 'viewlinks_select_tid', '');
+        }
+        // Set the user option for next time.
+        xarModSetUserVar('autolinks', 'viewlinks_select_tid', $tid);
+    } else {
+        // No longer need select flag.
+        xarModDelUserVar('autolinks', 'viewlinks_select_tid');
+    }
+
+    // The user API function is called
+    $links = xarModAPIFunc('autolinks', 'user', 'getall', $select_order);
+
+    // Auto key needed for enable/disable toggle.
     $authid = xarSecGenAuthKey();
 
+    // Check individual permissions for Edit/Delete/Enable
     if (is_array($links)) {
         $counttypes = xarModAPIfunc('autolinks', 'user', 'counttypes');
         foreach($links as $lid => $link) {
@@ -97,6 +118,18 @@ function autolinks_admin_view()
         }
         // Add the array of items to the template variables
         $data['items'] = $links;
+    }
+
+    // Get the list of types to display.
+    $types = xarModAPIfunc('autolinks', 'user', 'getalltypes');
+    if (is_array($types)) {
+        $data['types'] = $types;
+    }
+
+    if (!empty($tid)) {
+        $data['tid'] = $tid;
+    } else {
+        $data['tid'] = 0;
     }
 
     // Return the template variables defined in this function
