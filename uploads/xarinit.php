a6da6f7f1df8b9d8baa5e2f3c6c609bb2a6d1df0
BRUMDall <BRUMDall@brumdall2m.bru.fsc.net>
2003-09-17 20:03:09 +0000

    uploads/xarinit.php: 	#  fix uploads

--- uploads/xarinit.php
+++ uploads/xarinit.php
@@ -17,28 +17,28 @@
 function uploads_init()
 {
 
-	if( isset( $_SERVER['PATH_TRANSLATED'] ) )
-	{
-		$uploads_directory = dirname(realpath($_SERVER['PATH_TRANSLATED'])) . '/var/uploads/';
-	} elseif( isset( $_SERVER['SCRIPT_FILENAME'] ) ) {
-		$uploads_directory = dirname(realpath($_SERVER['SCRIPT_FILENAME'])) . '/var/uploads/';
-	} else {
-		$uploads_directory = 'var/uploads/';
-	}
+    if( isset( $_SERVER['PATH_TRANSLATED'] ) )
+    {
+        $uploads_directory = dirname(realpath($_SERVER['PATH_TRANSLATED'])) . '/var/uploads/';
+    } elseif( isset( $_SERVER['SCRIPT_FILENAME'] ) ) {
+        $uploads_directory = dirname(realpath($_SERVER['SCRIPT_FILENAME'])) . '/var/uploads/';
+    } else {
+        $uploads_directory = 'var/uploads/';
+    }
     xarModSetVar('uploads', 'uploads_directory', $uploads_directory);
     xarModSetVar('uploads', 'maximum_upload_size', '100000');
     xarModSetVar('uploads', 'allowed_types', 'gif;jpg;zip;tar.gz;tgz');
-	xarModSetVar('uploads', 'confirm_delete', '1');
+    xarModSetVar('uploads', 'confirm_delete', '1');
 
     xarModSetVar('uploads', 'max_image_width', '600');
     xarModSetVar('uploads', 'max_image_height', '800');
-	xarModSetVar('uploads', 'thumbnail_setting', '0');
-	xarModSetVar('uploads', 'thumbnail_path', '');
-	xarModSetVar('uploads', 'netpbm_path', '');
+    xarModSetVar('uploads', 'thumbnail_setting', '0');
+    xarModSetVar('uploads', 'thumbnail_path', '');
+    xarModSetVar('uploads', 'netpbm_path', '');
 
     xarModSetVar('uploads', 'import_directory',  '');
     xarModSetVar('uploads', 'obfuscate_imports', '0');
-		
+        
     // Get datbase setup
     list($dbconn) = xarDBGetConn();
     $xartable = xarDBGetTables();
@@ -51,29 +51,29 @@ function uploads_init()
 
     // Define the table structure in this associative array
     /*CREATE TABLE `xar_uploads` (
-	`xar_ulid` INT( 32 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-	`xar_ulmod` VARCHAR( 32 ) NOT NULL ,
-	`xar_ulmodid` INT NOT NULL ,
-	`xar_uluid` INT( 32 ) UNSIGNED NOT NULL ,
-	`xar_ulfile` VARCHAR( 254 ) NOT NULL ,
-	`xar_ulhash` VARCHAR( 254 ) NOT NULL ,
-	`xar_ulapp` TINYINT( 3 ) DEFAULT '0' NOT NULL ,
-	`xar_ulbid` INT( 32 ) UNSIGNED DEFAULT '0' NOT NULL ,
-	`xar_ultype` CHAR( 1 ) NOT NULL, 
-	INDEX ( `xar_ulmod` ) 
+    `xar_ulid` INT( 32 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `xar_ulmod` VARCHAR( 32 ) NOT NULL ,
+    `xar_ulmodid` INT NOT NULL ,
+    `xar_uluid` INT( 32 ) UNSIGNED NOT NULL ,
+    `xar_ulfile` VARCHAR( 254 ) NOT NULL ,
+    `xar_ulhash` VARCHAR( 254 ) NOT NULL ,
+    `xar_ulapp` TINYINT( 3 ) DEFAULT '0' NOT NULL ,
+    `xar_ulbid` INT( 32 ) UNSIGNED DEFAULT '0' NOT NULL ,
+    `xar_ultype` CHAR( 1 ) NOT NULL, 
+    INDEX ( `xar_ulmod` ) 
     );*/
     $uploadsfields = array(
         'xar_ulid'=>array('type'=>'integer','null'=>FALSE,'increment'=>TRUE,'primary_key'=>TRUE),
         'xar_ulmod'=>array('type'=>'varchar','size'=>32,'null'=>FALSE),
         'xar_ulmodid'=>array('type'=>'integer','size'=>'small','null'=>FALSE,'default'=>'0'),
-	'xar_uluid'=>array('type'=>'integer','size'=>32,'null'=>FALSE),
-	'xar_ulfile'=>array('type'=>'varchar','size'=>254,'null'=>FALSE),
-	'xar_ulhash'=>array('type'=>'varchar','size'=>254,'null'=>FALSE),
-	'xar_ulapp'=>array('type'=>'integer','size'=>3,'null'=>FALSE,'default'=>'0'),
-	'xar_ulbid'=>array('type'=>'integer','size'=>32,'null'=>FALSE,'default'=>'0'),
-	'xar_ultype'=>array('type'=>'char','size'=>1,'null'=>FALSE)
+    'xar_uluid'=>array('type'=>'integer','size'=>32,'null'=>FALSE),
+    'xar_ulfile'=>array('type'=>'varchar','size'=>254,'null'=>FALSE),
+    'xar_ulhash'=>array('type'=>'varchar','size'=>254,'null'=>FALSE),
+    'xar_ulapp'=>array('type'=>'integer','size'=>3,'null'=>FALSE,'default'=>'0'),
+    'xar_ulbid'=>array('type'=>'integer','size'=>32,'null'=>FALSE,'default'=>'0'),
+    'xar_ultype'=>array('type'=>'char','size'=>1,'null'=>FALSE)
     );
-		
+        
     // Create the Table - the function will return the SQL is successful or
     // raise an exception if it fails, in this case $sql is empty
     $query = xarDBCreateTable($uploadstable,$uploadsfields);
@@ -85,27 +85,27 @@ function uploads_init()
         `xar_ulbid` INT( 32 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
         `xar_ulid` INT( 32 ) UNSIGNED NOT NULL ,
         `xar_ulblob` BLOB NOT NULL ,
-	INDEX ( `xar_ulid` ) 
-	 );*/
+    INDEX ( `xar_ulid` ) 
+     );*/
 
     $blobsfields = array(
         'xar_ulbid'=>array('type'=>'integer','null'=>FALSE,'increment'=>TRUE,'primary_key'=>TRUE),
         'xar_ulid'=>array('type'=>'varchar','size'=>32,'null'=>FALSE),
         'xar_ulblob'=>array('type'=>'integer','size'=>'small','null'=>FALSE,'default'=>'0')
     );
-		
+        
     // Create the Table - the function will return the SQL is successful or
     // raise an exception if it fails, in this case $sql is empty
     $query = xarDBCreateTable($blobstable,$blobsfields);
     $result =& $dbconn->Execute($query);
     if (!$result) return;
     
-	
+    
     xarRegisterMask('ViewUploads','All','uploads','Upload','All','ACCESS_OVERVIEW');
     xarRegisterMask('ReadUploads','All','uploads','Upload','All','ACCESS_READ');
     xarRegisterMask('EditUploads','All','uploads','Upload','All','ACCESS_EDIT');
     xarRegisterMask('AdminUploads','All','uploads','Upload','All','ACCESS_ADMIN');
-	
+    
 
     /**
      * Register hooks
@@ -116,9 +116,9 @@ function uploads_init()
          $msg = xarML('Could not register hook');
          xarExceptionSet(XAR_USER_EXCEPTION, 'MISSING_DATA', new DefaultUserException($msg));
          return;
-	}
+    }
 
-	
+    
     return true;
 }
 
@@ -144,23 +144,23 @@ function articles_upgrade($oldversion)
 
             $result =& $dbconn->Execute($query);
             if (!$result) return;
-		case .03:
-			// Remove unused hooks
-			xarModUnregisterHook('item', 'new', 'GUI','uploads', 'admin', 'newhook'));
-			xarModUnregisterHook('item', 'create', 'API', 'uploads', 'admin', 'createhook'));
-			xarModUnregisterHook('item', 'display', 'GUI', 'uploads', 'user', 'formdisplay'));
-			
-			
-			// Had problems with unregister not working in beta testing... So forcefully removing these
-			list($dbconn) = xarDBGetConn();
-		
-			$hookstable = xarDBGetSiteTablePrefix() . '_hooks';
-			$query = "DELETE FROM $hookstable
-					  WHERE xar_tmodule='uploads' AND (xar_tfunc='formdisplay' OR xar_tfunc='createhook' OR xar_tfunc='newhook')";
-		
-			$result =& $dbconn->Execute($query);
-			if (!$result) return;
-			
+        case .03:
+            // Remove unused hooks
+            xarModUnregisterHook('item', 'new', 'GUI','uploads', 'admin', 'newhook');
+            xarModUnregisterHook('item', 'create', 'API', 'uploads', 'admin', 'createhook');
+            xarModUnregisterHook('item', 'display', 'GUI', 'uploads', 'user', 'formdisplay');
+            
+            
+            // Had problems with unregister not working in beta testing... So forcefully removing these
+            list($dbconn) = xarDBGetConn();
+        
+            $hookstable = xarDBGetSiteTablePrefix() . '_hooks';
+            $query = "DELETE FROM $hookstable
+                      WHERE xar_tmodule='uploads' AND (xar_tfunc='formdisplay' OR xar_tfunc='createhook' OR xar_tfunc='newhook')";
+        
+            $result =& $dbconn->Execute($query);
+            if (!$result) return;
+            
             break;
     }
     return true;
