42238c82d90ef61911c81c32b92263bc8ddcda51
mikespub <mikespub@fully.qualified.hostname>
2003-10-11 20:03:10 +0000

    sitetools/xarschedulerapi/backup.php: 	#  BitKeeper file D:/wintools/cygwin/home/modules/sitetools/xarschedulerapi/backup.php sitetools/xarschedulerapi/index.html: 	#  BitKeeper file D:/wintools/cygwin/home/modules/sitetools/xarschedulerapi/index.html sitetools/xarschedulerapi/optimize.php: 	#  BitKeeper file D:/wintools/cygwin/home/modules/sitetools/xarschedulerapi/optimize.php

---
+++ sitetools/xarschedulerapi/backup.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * take a backup of the database(s) (executed by the scheduler module)
+ * 
+ * @author jojodee <http://xaraya.athomeandabout.com >
+ * @access private
+ */
+function sitetools_schedulerapi_backup($args)
+{
+
+// TODO: get some configuration info here if necessary
+    // $whatever = xarModGetVar('sitetools','whatever');
+    // ...
+// TODO: we need some API function here (not a GUI function)
+//       It may return true (or some logging text) if it succeeds, and null if it fails
+    // return xarModAPIFunc('sitetools','admin','backup',
+    //                      array('whatever' => $whatever));
+
+    return true;
+}
+
+?>
---
+++ sitetools/xarschedulerapi/optimize.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * optimize the database (executed by the scheduler module)
+ * 
+ * @author jojodee <http://xaraya.athomeandabout.com >
+ * @access private
+ */
+function sitetools_schedulerapi_optimize($args)
+{
+
+// TODO: get some configuration info here if necessary
+    // $whatever = xarModGetVar('sitetools','whatever');
+    // ...
+// TODO: we need some API function here (not a GUI function)
+//       It may return true (or some logging text) if it succeeds, and null if it fails
+    // return xarModAPIFunc('sitetools','admin','optimize',
+    //                      array('whatever' => $whatever));
+
+    return true;
+}
+
+?>
