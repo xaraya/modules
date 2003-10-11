503919307a7227773b3d46254d3fc8035c046da9
jojodee <jojodee@hbwdev.net>
2003-10-11 20:03:10 +0000

    sitetools/xartemplates/includes/sitetools_admin_backup.js: 	#  BitKeeper file C:/servroot/home/jojodee/bk/xaraya-modules/sitetools/xartemplates/includes/sitetools_admin_backup.js

---
+++ sitetools/xartemplates/includes/sitetools_admin_backup.js
@@ -0,0 +1,6 @@
+function CheckAll(checkornot)
+{
+for (var i = 0; i < document.SelectedTablesForm.elements.length; i++) {
+  document.SelectedTablesForm.elements[i].checked = checkornot; 
+}                                                                     	
+}
