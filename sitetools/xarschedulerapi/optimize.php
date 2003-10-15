b7fd1118f84e5db9995296423be9c0d64d051ade
jojodee <jojodee@hbwdev.net>
2003-10-15 20:03:10 +0000

    sitetools/xarschedulerapi/optimize.php: 	#  Add optimize api function and finish off the schedulerap optimize.php file 	#  - now works nicely with scheduler :)

--- sitetools/xarschedulerapi/optimize.php
+++ sitetools/xarschedulerapi/optimize.php
@@ -6,17 +6,29 @@
  * @author jojodee <http://xaraya.athomeandabout.com >
  * @access private
  */
-function sitetools_schedulerapi_optimize($args)
+function sitetools_schedulerapi_optimize($dbname,$dbtype='')
 {
 
-// TODO: get some configuration info here if necessary
-    // $whatever = xarModGetVar('sitetools','whatever');
-    // ...
-// TODO: we need some API function here (not a GUI function)
-//       It may return true (or some logging text) if it succeeds, and null if it fails
-    // return xarModAPIFunc('sitetools','admin','optimize',
-    //                      array('whatever' => $whatever));
+    // DO LATER: get some configuration info here if necessary
+    // for now lets just use current database
+    if (($dbname='') || (empty($dbname))){
+        list($dbconn) = xarDBGetConn();
+            $dbname= xarDBGetName();
+    }
 
+    //       It may return true (or some logging text) if it succeeds, and null if it fails
+//     return
+     $tabledata=xarModAPIFunc('sitetools','admin','optimizedb',
+                      array('dbname' => $dbname));
+
+       $total_gain= $tabledata['total_gain'];
+       $total_gain = round ($total_gain,3);
+       //Add this new optimization record to the database
+       return xarModAPIFunc('sitetools',
+                              'admin',
+                              'create',
+                              array('totalgain' => $total_gain));
+                              
     return true;
 }
 
