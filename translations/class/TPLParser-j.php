680214428340875e85d278c5a38e024d00477148
voll <voll@yahoo.com>
2003-10-27 20:03:10 +0000

    translations/class/PHPParser-m.php: 	#  BitKeeper file C:/bk/modules/translations/class/PHPParser-m.php translations/class/TPLParser-j.php: 	#  BitKeeper file C:/bk/modules/translations/class/TPLParser-j.php translations/class/TPLParser-m.php: 	#  BitKeeper file C:/bk/modules/translations/class/TPLParser-m.php translations/class/TPLParser-v.php: 	#  BitKeeper file C:/bk/modules/translations/class/TPLParser-v.php translations/class/PHPParser.php: 	#  fix tpl parser, add other guy's parsers, add tests translations/class/TPLParser.php: 	#  fix tpl parser, add other guy's parsers, add tests

---
+++ translations/class/TPLParser-j.php
@@ -0,0 +1,183 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// Xaraya eXtensible Management System
+// Copyright (C) 2002 by the Xaraya Development Team.
+// http://www.xaraya.org
+// ----------------------------------------------------------------------
+// Original Author of file: Marco Canini
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+//
+
+class TPLParser
+{
+    var $transEntries = array();
+    var $transKeyEntries = array();
+    
+    var $_fd;
+    var $_offs;
+    var $_pos;
+    var $_len;
+    var $_buf;
+    var $_line;
+    var $_token;
+    var $_string;
+
+    function TPLParser()
+    {
+    }
+
+    function getTransEntries()
+    {
+        return $this->transEntries;
+    }
+
+    function getTransKeyEntries()
+    {
+        return $this->transKeyEntries;
+    }
+    
+    function _get_token($t_array, $right) {
+        $found = false;
+        while (!$found && !feof($this->_fd)) {
+            if ($this->_offs >= $this->_len) {
+                $this->_buf = fgets($this->_fd, 1024);
+                $this->_offs = 0;
+                $this->_len = strlen($this->_buf);
+                $this->_line++;
+            // printf("Getting line %d\n", $this->_line);
+            }
+            foreach ($t_array as $n => $t) {
+                if (($this->_pos = strpos($this->_buf, $t, $this->_offs)) !== false) {  // токен найден
+                    if ($right)
+                        $this->_string .= substr($this->_buf, $this->_offs, $this->_pos - $this->_offs);
+                    $this->_offs = $this->_pos + strlen($t);
+                    $this->_token = $n;
+                    $found = true;
+                    // printf("Found token %s[%d] at pos %d\n", $t, $n, $this->_pos);
+                    break;
+                }
+            }
+            if (!$found) {
+                if ($right)
+                $this->_string .= substr($this->_buf, $this->_offs);
+                $this->_offs = $this->_len;
+            }
+        }
+        return $found;
+    }
+
+    function parse($filename)
+    {
+        if (!file_exists($filename)) return;
+        $this->_fd = fopen($filename, 'r') or die("Cannot open file");
+
+        if (!$filesize = filesize($filename)) return;
+    
+    $this->_offs = 0;
+    $this->_len = 0;
+        
+        while (!feof($this->_fd)) {
+            
+            if ($this->_get_token(array("<xar:mlstring>", "<xar:mlkey>", "xarML(", "xarMLByKey("), false)) {
+            
+                $this->_string ='';
+                $line = $this->_line;
+                
+                switch ($this->_token) {
+                    case 0: 
+                        if ($this->_get_token(array("</xar:mlstring>"), true)) {
+                            $this->_string = trim($this->_string);
+                            if (!isset($this->transEntries[$this->_string])) {
+                                $this->transEntries[$this->_string] = array();
+                            }
+                            $this->transEntries[$this->_string][] = array('line' => $line, 'file' => $filename);
+                        }
+                        break;
+                    case 1:
+                        if ($this->_get_token(array("</xar:mlkey>"), true)) {
+                            $this->_string = trim($this->_string);
+                            if (!isset($this->transKeyEntries[$this->_string])) {
+                                $this->transKeyEntries[$this->_string] = array();
+                            }
+                            $this->transKeyEntries[$this->_string][] = array('line' => $line, 'file' => $filename);
+                        }
+                        break;
+                    case 2:
+                        if ($this->_get_token(array(")"), true)) {
+                            if ($string = $this->parseString($this->_string)) {
+                                if (!isset($this->transEntries[$string])) {
+                                    $this->transEntries[$string] = array();
+                                }
+                                $this->transEntries[$string][] = array('line' => $line, 'file' => $filename);
+                            }
+                        }
+                        break;
+                    case 3:
+                        if ($this->_get_token(array(")"), true)) {
+                            if ($string = $this->parseString($this->_string)) {
+                                if (!isset($this->transKeyEntries[$string])) {
+                                    $this->transKeyEntries[$string] = array();
+                                }
+                                $this->transKeyEntries[$string][] = array('line' => $line, 'file' => $filename);
+                            }
+                        }
+                        break;
+                    default :
+                        // internal error
+                    break;
+                }
+                if (defined('TPLPARSERDEBUG'))
+                    printf("Result: %s %s<br />\n", $string, $this->_string);
+            }
+        }
+        
+        fclose($this->_fd);
+    }
+
+    function parseString($buf)
+    {
+        $pos = 0;
+        $len = strlen($buf);
+        while ($pos < $len) {
+            $char = $buf{$pos++};
+            if ($char == "'" || $char == "'") {
+                $quote = $char;
+                break;
+            } elseif ($char != ' ') {
+                return;
+            }
+        }
+        if ($pos == $len) return;
+        $string = '';
+        while ($pos < $len) {
+            $char = $buf{$pos};
+            if ($char == "\\") {
+                if ($buf{$pos+1} == $quote) {
+                    $string .= $quote;
+                    $pos++;
+                } else {
+                    $string .= $char;
+                }
+            } else {
+                if ($char == $quote) {
+                    return $string;
+                }
+                $string .= $char;
+            }
+            $pos++;
+        }
+        return;
+    }
+
+}
+
+/*
+$p = new TPLParser();
+$p->parse('/home/marco/src/xaraya/html/modules/translations/xartemplates/admin-translate_subtype.xd');
+
+var_dump($p);
+*/
+?>
---
+++ translations/class/TPLParser-m.php
@@ -0,0 +1,215 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// Xaraya eXtensible Management System
+// Copyright (C) 2002 by the Xaraya Development Team.
+// http://www.xaraya.org
+// ----------------------------------------------------------------------
+// Original Author of file: Marco Canini
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+//
+
+class TPLParser
+{
+    var $transEntries = array();
+    var $transKeyEntries = array();
+
+    var $_fd;
+    var $_offs;
+    var $_pos;
+    var $_len;
+    var $_buf;
+    var $_line;
+    var $_token;
+    var $_string;
+    var $tokenarray;
+
+    function TPLParser()
+    {
+        $this->tokenarray = array("<xar:mlstring>", "<xar:mlkey>", "xarML('", "xarMLByKey('", 'xarML("', 'xarMLByKey("');
+    }
+
+    function getTransEntries()
+    {
+        return $this->transEntries;
+    }
+
+    function getTransKeyEntries()
+    {
+        return $this->transKeyEntries;
+    }
+
+    function _get_token($t_array, $right) {
+        foreach ($t_array as $n => $t) {
+            $found = false;
+//                     printf("Getting line %d\n"."for token %d " . $t, $this->_line, $n);
+            while (!$found) {
+                if (($this->_pos = strpos($this->_buf, $t, $this->_offs)) !== false) {  // токен найден
+                    if ($right)
+                        $this->_string .= substr($this->_buf, $this->_offs, $this->_pos - $this->_offs);
+                    $this->_offs = $this->_pos + strlen($t);
+                    $this->_token = $n;
+                    $found = true;
+//                        printf("Found token %s[%d] at pos %d\n"."<br />", $t, $n, $this->_pos);
+                    if(!$right) $this->parseclose();
+                }
+                else {
+                    $found = false;
+                    if ($right)
+                        $this->_string .= substr($this->_buf, $this->_offs);
+                    $this->_offs = $this->_len;
+                    $this->_offs = 0;
+                    break;
+                }
+            }
+        }
+        return $found;
+    }
+
+    function parseclose()
+    {
+        $this->_string ='';
+        $line = $this->_line;
+        switch ($this->_token) {
+            case 0:
+                if ($this->_get_token(array("</xar:mlstring>"), true)) {
+                    $this->_string = trim($this->_string);
+                    if (!isset($this->transEntries[$this->_string])) {
+                        $this->transEntries[$this->_string] = array();
+                    }
+                    $this->transEntries[$this->_string][] = array('line' => $line, 'file' => $this->filename);
+                }
+                break;
+            case 1:
+                if ($this->_get_token(array("</xar:mlkey>"), true)) {
+                    $this->_string = trim($this->_string);
+                    if (!isset($this->transKeyEntries[$this->_string])) {
+                        $this->transKeyEntries[$this->_string] = array();
+                    }
+                    $this->transKeyEntries[$this->_string][] = array('line' => $line, 'file' => $this->filename);
+                }
+                break;
+            case 2:
+                if ($this->_get_token(array("')"), true)) {
+                    if ($string = $this->parseString($this->_string)) {
+                        if (!isset($this->transEntries[$string])) {
+                            $this->transEntries[$string] = array();
+                        }
+                        $this->transEntries[$string][] = array('line' => $line, 'file' => $this->filename);
+                    }
+                }
+                break;
+            case 3:
+                if ($this->_get_token(array("')"), true)) {
+                    if ($string = $this->parseString($this->_string)) {
+                        if (!isset($this->transKeyEntries[$string])) {
+                            $this->transKeyEntries[$string] = array();
+                        }
+                        $this->transKeyEntries[$string][] = array('line' => $line, 'file' => $this->filename);
+                    }
+                }
+                break;
+            case 4:
+                if ($this->_get_token(array('")'), true)) {
+                    if ($string = $this->parseString($this->_string)) {
+                        if (!isset($this->transKeyEntries[$string])) {
+                            $this->transKeyEntries[$string] = array();
+                        }
+                        $this->transKeyEntries[$string][] = array('line' => $line, 'file' => $this->filename);
+                    }
+                }
+                break;
+            case 4:
+                if ($this->_get_token(array('")'), true)) {
+                    if ($string = $this->parseString($this->_string)) {
+                        if (!isset($this->transKeyEntries[$string])) {
+                            $this->transKeyEntries[$string] = array();
+                        }
+                        $this->transKeyEntries[$string][] = array('line' => $line, 'file' => $this->filename);
+                    }
+                }
+                break;
+            default :
+                // internal error
+            break;
+        }
+        if (defined('TPLPARSERDEBUG'))
+            printf("Result: %s %s<br />\n", $string, $this->_string);
+    }
+
+    function parse($filename)
+    {
+        if (!file_exists($filename)) return;
+        $this->filename = $filename;
+        $this->_fd = fopen($filename, 'r') or die("Cannot open file");
+
+        if (!$filesize = filesize($filename)) return;
+
+        $this->_offs = 0;
+        $this->_len = 0;
+
+        while (!feof($this->_fd)) {
+
+            $this->_buf = fgets($this->_fd, 1024);
+            $this->_len = strlen($this->_buf);
+            $this->_line++;
+            $this->_offs = 0;
+            if ($this->_get_token($this->tokenarray, false)) {
+
+
+//                if ($filename == "modules/base/xartemplates/blocks/menuAdmin.xd" || $line == 18){
+//                echo $this->_token;exit;
+//                }
+            }
+        }
+
+        fclose($this->_fd);
+    }
+
+    function parseString($buf)
+    {
+        return $buf;
+        $pos = 0;
+        $len = strlen($buf);
+        while ($pos < $len) {
+            $char = $buf{$pos++};
+            if ($char == "'" || $char == "'") {
+                $quote = $char;
+                break;
+            } elseif ($char != ' ') {
+                return;
+            }
+        }
+        if ($pos == $len) return;
+        $string = '';
+        while ($pos < $len) {
+            $char = $buf{$pos};
+            if ($char == "\\") {
+                if ($buf{$pos+1} == $quote) {
+                    $string .= $quote;
+                    $pos++;
+                } else {
+                    $string .= $char;
+                }
+            } else {
+                if ($char == $quote) {
+                    return $string;
+                }
+                $string .= $char;
+            }
+            $pos++;
+        }
+        return;
+    }
+
+}
+
+/*
+$p = new TPLParser();
+$p->parse('/home/marco/src/xaraya/html/modules/translations/xartemplates/admin-translate_subtype.xd');
+
+var_dump($p);
+*/
+?>
