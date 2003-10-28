e36ac2e764cdf805d643a401a24b0ba9946e66ec
ccorliss <ccorliss@lxwdev-1.schwabfoundation.org>
2003-10-28 20:03:10 +0000

    mime/xarincludes/mime.magic.php: 	#  updated to fix problem where it would, when unserializing the array, parse the octal numbers into real characters - which bombed out the unserialize when it saw things like double-quotes, etc. mime/xaruserapi/analyze_file.php: 	#  added checks for seeking past EOF and otherwise got the function working as expected

--- mime/xaruserapi/analyze_file.php
+++ mime/xaruserapi/analyze_file.php
@@ -27,31 +27,56 @@ function mime_userapi_analyze_file( $args )
 
     if (!($fp = @fopen($fileName, 'rb'))) {
         $msg = xarML('Unable to analyze file [#(1)]. Cannot open for reading!', $fileName);
-        xarExceptionSet(XAR_SYSETEM_EXCEPTION, 'FILE_NO_OPEN', new SystemException($msg));
+        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'FILE_NO_OPEN', new SystemException($msg));
         return FALSE;
     } else {
         foreach($mime_list as $mime_type => $mime_info) {
-
-            foreach ($mime_info['needles'] as $needle => $needle_info) {
-                if (!@fseek($fp, $needle_info['offset'], SEEK_SET)) {
-                    $msg = xarML('Unable to seek to offset [#(1)] within file: [#(2)]',$needle_info['offset'], $fileName);
-                    xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'FILE_NO_SEEK', new SystemException($msg));
-                    return FALSE;
-                }
             
+            // if this mime_type doesn't have a 
+            // magic string to check against, then
+            // go ahead and skip to the next one
+            if (!isset($mime_info['needles'])) {
+                continue;
+            }
+            
+            foreach ($mime_info['needles'] as $needle => $needle_info) {
+                
+                // if the offset is beyond the range of the file
+                // continue on to the next item
+                if ($needle_info['offset'] >= filesize($fileName)) {
+                    continue;
+                }
+
+                if ($needle_info['offset'] >= 0) {
+                    if (@fseek($fp, $needle_info['offset'], SEEK_SET)) {
+                        $msg = xarML('Unable to seek to offset [#(1)] within file: [#(2)]' ,
+                                      $needle_info['offset'], $fileName);
+                        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'FILE_NO_SEEK', new SystemException($msg));
+                        return FALSE;
+                    }
+                }
+               
                 if (!($value = @fread($fp, $needle_info['length']))) {
-                    $msg = xarML('Unable to read (#(1) bytes) from file: [#(2)]', $needle_info['length'], $fileName);
+                    $msg = xarML('Unable to read (#(1) bytes) from file: [#(2)].' , 
+                                 $needle_info['length'], $fileName);
                     xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'FILE_NO_READ', new SystemException($msg));
                     return FALSE;
                 }
-            
-                if ($needle == $value) {
+                
+                // FIXME: need to figure out how to get the stored, octal,
+                // representation of the needle back into ascii values easily
+                // and with minimal overhead - this works for now though
+                for ($i = 0, $check = ''; $i < strlen($value); $i++)
+                    $check .= "\\" . ord($value{$i});
+                } 
+                
+                if ($needle == $check) {
                     fclose($fp);
                     return $mime_type;
                 }
             }
         } 
-        
+       
         if (!rewind($fp)) {
             $msg = xarML('Unable to rewind to beginning of file: [#(1)]', $fileName);
             xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'FILE_NO_REWIND', new SystemException($msg));
@@ -63,7 +88,10 @@ function mime_userapi_analyze_file( $args )
             xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'FILE_NO_READ', new SystemException($msg));
             return FALSE;
         }
-
+        
+        // get rid of printable characters so we can 
+        // use ctype_print to check for printable characters
+        // which, in a binary file, there should'nt be any
         $value = str_replace(array("\n","\r","\t"), '', $value);
 
         if (ctype_print($value)) {
@@ -71,11 +99,12 @@ function mime_userapi_analyze_file( $args )
         } else {
             $mime_type = 'application/octet-stream';
         }
-        
+
+        if ($fp) 
+            fclose($fp);
+    
+        return $mime_type;
     }
-   
-    fclose($fp);
-    return $mime_type;
 }
 
 ?>
