191ece0374b0b3dd44d14f55eba85b160520e4a3
voll <voll@yahoo.com>
2003-10-28 20:03:10 +0000

    xproject/xartasks/modify.php: 	#  xproject: pnVarCensor delete xproject/xaruser/display.php: 	#  xproject: pnVarCensor delete

--- xproject/xartasks/modify.php
+++ xproject/xartasks/modify.php
@@ -8,24 +8,24 @@ function xproject_tasks_modify($args)
                                           'taskid',
                                           'objectid');
 
-	extract($args);
-	
+    extract($args);
+    
     if (!empty($objectid)) {
         $taskid = $objectid;
     }
-	
-	if (!xarModLoad('xproject', 'user')) return;
-	
-	$data = xarModAPIFunc('xproject','user','menu');
-	
-	$data['status'] = '';
-	
-	$task = xarModAPIFunc('xproject',
+    
+    if (!xarModLoad('xproject', 'user')) return;
+    
+    $data = xarModAPIFunc('xproject','user','menu');
+    
+    $data['status'] = '';
+    
+    $task = xarModAPIFunc('xproject',
                          'tasks',
                          'get',
                          array('taskid' => $taskid));
-	
-	if (!isset($task) && xarExceptionMajor() != XAR_NO_EXCEPTION) return;
+    
+    if (!isset($task) && xarExceptionMajor() != XAR_NO_EXCEPTION) return;
 
     $project = xarModAPIFunc('xproject',
                           'user',
@@ -33,14 +33,14 @@ function xproject_tasks_modify($args)
                           array('projectid' => $task['projectid']));
 
     if (isset($project['projectid']) && xarExceptionMajor() == XAR_NO_EXCEPTION) {
-		list($project['name']) = xarModCallHooks('item',
-											 'transform',
-											 $project['projectid'],
-											 array($project['name']));
-	
-		$data['name'] = xarVarCensor($project['name']);
-		$data['description'] = xarVarCensor($project['description']);
-	}
+        list($project['name']) = xarModCallHooks('item',
+                                             'transform',
+                                             $project['projectid'],
+                                             array($project['name']));
+    
+        $data['name'] = $project['name'];
+        $data['description'] = $project['description'];
+    }
 
     if (!xarSecAuthAction(0, 'xproject::Tasks', "$task[name]::$taskid", ACCESS_EDIT)) {
         $msg = xarML('Not authorized to modify #(1) item #(2)',
@@ -49,44 +49,44 @@ function xproject_tasks_modify($args)
                        new SystemException(__FILE__.'('.__LINE__.'): '.$msg));
         return;
     }
-	
-	if($task['parentid'] > 0) {
-		$parent = xarModAPIFunc('xproject',
-							  'tasks',
-							  'get',
-							  array('taskid' => $task['parentid']));
-	} else {
-		$data['roottask'] = xarML('project overview');
-	}
-	
-	if (isset($parent) && xarExceptionMajor() == XAR_NO_EXCEPTION) {
-		$data['taskparent'] = $parent['name'];
-		$data['taskparent_id'] = $parent['taskid'];
-	} else {
-		$data['taskparent'] = xarML('Project Top');
-		$data['taskparent_id'] = 0;
-	}
+    
+    if($task['parentid'] > 0) {
+        $parent = xarModAPIFunc('xproject',
+                              'tasks',
+                              'get',
+                              array('taskid' => $task['parentid']));
+    } else {
+        $data['roottask'] = xarML('project overview');
+    }
+    
+    if (isset($parent) && xarExceptionMajor() == XAR_NO_EXCEPTION) {
+        $data['taskparent'] = $parent['name'];
+        $data['taskparent_id'] = $parent['taskid'];
+    } else {
+        $data['taskparent'] = xarML('Project Top');
+        $data['taskparent_id'] = 0;
+    }
 
-	$data['projectid'] = $task['projectid'];
+    $data['projectid'] = $task['projectid'];
     $data['authid'] = xarSecGenAuthKey();
     $data['taskid'] = $taskid;
-	$data['parentid'] = $task['parentid'];
+    $data['parentid'] = $task['parentid'];
 
-	$data['taskname'] = $task['name'];
-	$data['description'] = $task['description'];
+    $data['taskname'] = $task['name'];
+    $data['description'] = $task['description'];
 
-	$statusoptions = array();    
-	$statusoptions[] = array('id'=>0,'name'=>xarML('Open'),'value'=>0);
-	$statusoptions[] = array('id'=>1,'name'=>xarML('Closed'),'value'=>1);
-	$data['statusoptions'] = $statusoptions;
-	$data['status'] = $task['status'];
+    $statusoptions = array();    
+    $statusoptions[] = array('id'=>0,'name'=>xarML('Open'),'value'=>0);
+    $statusoptions[] = array('id'=>1,'name'=>xarML('Closed'),'value'=>1);
+    $data['statusoptions'] = $statusoptions;
+    $data['status'] = $task['status'];
 
-	$data['priority'] = $task['priority'];
+    $data['priority'] = $task['priority'];
 
-	$data['updatebutton'] = xarVarPrepForDisplay(xarML('Modify'));
+    $data['updatebutton'] = xarVarPrepForDisplay(xarML('Modify'));
 
     $item = array();
-	$item['module'] = 'xproject';
+    $item['module'] = 'xproject';
     $hooks = xarModCallHooks('item','modify',$taskid,$item);
     if (empty($hooks)) {
         $data['hookoutput'] = '';
@@ -96,39 +96,39 @@ function xproject_tasks_modify($args)
         $data['hookoutput'] = $hooks;
     }
 
-	$data['tasks'] = array();
+    $data['tasks'] = array();
 
-	$tasks = xarModAPIFunc('xproject',
-							'tasks',
-							'getall',
-							array('startnum' => $startnum,
-								'projectid' => $task['projectid'],
-								'parentid' => $taskid));
-	if (isset($tasks) && is_array($tasks) && (xarExceptionMajor() == XAR_NO_EXCEPTION)) {
-		for ($i = 0; $i < count($tasks); $i++) {
-			$task = $tasks[$i];
-			if (xarSecAuthAction(0, 'xproject::Tasks', "$task[name]::$task[taskid]", ACCESS_EDIT)) {
-				$tasks[$i]['editurl'] = xarModURL('xproject',
-												   'tasks',
-												   'modify',
-												   array('taskid' => $task['taskid']));
-			} else {
-				$tasks[$i]['editurl'] = '';
-			}
-			if (xarSecAuthAction(0, 'xproject::Tasks', "$task[name]::$task[taskid]", ACCESS_DELETE)) {
-				$tasks[$i]['deleteurl'] = xarModURL('xproject',
-												   'tasks',
-												   'delete',
-												   array('taskid' => $task['taskid']));
-			} else {
-				$tasks[$i]['deleteurl'] = '';
-			}
-		}
-		$data['tasks'] = $tasks;
-		$data['numtasks'] = count($tasks);
-	}
+    $tasks = xarModAPIFunc('xproject',
+                            'tasks',
+                            'getall',
+                            array('startnum' => $startnum,
+                                'projectid' => $task['projectid'],
+                                'parentid' => $taskid));
+    if (isset($tasks) && is_array($tasks) && (xarExceptionMajor() == XAR_NO_EXCEPTION)) {
+        for ($i = 0; $i < count($tasks); $i++) {
+            $task = $tasks[$i];
+            if (xarSecAuthAction(0, 'xproject::Tasks', "$task[name]::$task[taskid]", ACCESS_EDIT)) {
+                $tasks[$i]['editurl'] = xarModURL('xproject',
+                                                   'tasks',
+                                                   'modify',
+                                                   array('taskid' => $task['taskid']));
+            } else {
+                $tasks[$i]['editurl'] = '';
+            }
+            if (xarSecAuthAction(0, 'xproject::Tasks', "$task[name]::$task[taskid]", ACCESS_DELETE)) {
+                $tasks[$i]['deleteurl'] = xarModURL('xproject',
+                                                   'tasks',
+                                                   'delete',
+                                                   array('taskid' => $task['taskid']));
+            } else {
+                $tasks[$i]['deleteurl'] = '';
+            }
+        }
+        $data['tasks'] = $tasks;
+        $data['numtasks'] = count($tasks);
+    }
 
-	return $data;
+    return $data;
 }
 
 ?>
