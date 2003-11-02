74736e059720d95f0a8d8167e2baa4b192ab1cd6
voll <voll@yahoo.com>
2003-11-02 20:03:11 +0000

    translations/class/PHPParser.php: 	#  PHPParser new version

--- translations/class/PHPParser.php
+++ translations/class/PHPParser.php
@@ -17,8 +17,29 @@ class PHPParser
     var $parsedFiles = array();
     var $notToParseFiles = array();
 
+    var $_fd;
+    var $_offs;
+    var $_pos;
+    var $_len;
+    var $_buf;
+    var $_line;
+    var $_token;
+    var $_right;
+    var $_string;
+    var $tokenarray;
+    var $lasttokenarray;
+    var $endtokenarray;
+    var $tokenarraytype;
+    var $iskeytokenarray;
+
     function PHPParser()
     {
+        $this->tokenarray = array("xarML('", "xarMLByKey('", 'xarML("', 'xarMLByKey("', '{ML_dont_parse', '{ML_include', '{ML_add_string', '{ML_add_key', 'include_once', 'include', 'require_once', 'require');
+        $this->endtokenarray = array(array("')","',"), array("')","',"), array('")','",'), array('")','",'), array('}'), array('}'), array('}'), array('}'), array(';'), array(';'), array(';'), array(';'));
+        $this->tokenarraytype = array('function', 'function', 'function', 'function', 'ML_dont_parse', 'ML_include', 'ML_add_string', 'ML_add_key', 'include', 'include', 'include', 'include');
+        $this->iskeytokenarray = array(0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+        $this->strlentokenarray = array(7, 12, 7, 12, 14, 11, 14, 11, 12, 7, 12, 7);
+        $this->strlenendtokenarray = array(2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1);
     }
 
     function getTransEntries()
@@ -31,8 +52,115 @@ class PHPParser
         return $this->transKeyEntries;
     }
 
+    function _get_token() {
+        $found = false;
+        if (defined('PHPPARSERDEBUG'))
+           printf("Getting line %d\n"."for %s token %d<br />\n", $this->_line, $this->_right?'end':'begin', $this->_token);
+        $this->_pos = -1;
+        foreach( $this->lasttokenarray as $n => $v )
+        {
+            $p = strpos( $this->_buf, $v, $this->_offs );
+            if ($p===FALSE)
+                continue;
+            if (($p<$this->_pos)||($this->_pos==-1)) {
+                $this->_pos = $p;
+                $this->_token = $n;
+            }
+        }
+        // list($this->_pos,$this->_token) = $this->findfirstof($this->_buf, $this->lasttokenarray, $this->_offs);
+        if ($this->_pos != -1) {
+            // if (defined('PHPPARSERDEBUG'))
+                // printf("Found %s token %s[%d] at pos %d<br />\n", $this->_right?'end':'begin', htmlspecialchars(substr($this->_buf, $this->_pos, strlen($this->tokenarray[$this->_token]))), $this->_token, $this->_pos);
+            if ($this->_right)
+                $this->_string .= substr($this->_buf, $this->_offs, $this->_pos - $this->_offs);
+            if ($this->_right)
+                $this->_offs = $this->_pos + $this->strlenendtokenarray[$this->_token];
+            else
+                $this->_offs = $this->_pos + $this->strlentokenarray[$this->_token];
+            if ($this->_offs > $this->_len) $this->_offs = $this->_len;
+            $found = true;
+            if(!$this->_right) {
+                // ParseClose
+                $this->_string ='';
+                $this->_right = true;
+                $this->lasttokenarray = $this->endtokenarray[$this->_token];
+                $token = $this->_token;
+                if ($this->_get_token()) {
+                    if (defined('PHPPARSERDEBUG'))
+                       printf("Result: %s<br />\n", $this->_string);
+                    switch ($this->tokenarraytype[$token]) {
+                    case 'function':
+                        $this->_string = trim($this->_string);
+                        if ($this->iskeytokenarray[$token]) {
+                            if (!isset($this->transKeyEntries[$this->_string])) {
+                                $this->transKeyEntries[$this->_string] = array();
+                            }
+                            $this->transKeyEntries[$this->_string][] = array('line' => $this->_line, 'file' => $this->filename);
+                        } else { 
+                            if (!isset($this->transEntries[$this->_string])) {
+                                $this->transEntries[$this->_string] = array();
+                            }
+                            $this->transEntries[$this->_string][] = array('line' => $this->_line, 'file' => $this->filename);
+                        }
+                        break;
+                    case 'ML_dont_parse':
+                        $this->_string = trim($this->_string, " \t'");
+                        $this->notToParseFiles[$this->_string] = true;
+                        break;
+                    case 'ML_include':
+                        $this->_string = trim($this->_string, " \t'");
+                        $this->includedFiles[$this->_string] = true;
+                        break;
+                    case 'ML_add_string':
+                        $this->_string = trim($this->_string, " \t'");
+                        if (!isset($this->transEntries[$this->_string])) {
+                            $this->transEntries[$this->_string] = array();
+                        }
+                        $this->transEntries[$this->_string][] = array('line' => $line, 'file' => $filename);
+                        break;
+                    case 'ML_add_key':
+                        $this->_string = trim($this->_string, " \t'");
+                        if (!isset($this->transKeyEntries[$this->_string])) {
+                            $this->transKeyEntries[$this->_string] = array();
+                        }
+                        $this->transKeyEntries[$this->_string][] = array('line' => $line, 'file' => $filename);
+                        break;
+                    case 'include':
+                        $this->_string = trim($this->_string, " \t'");
+                        //disregrd loggers
+                        if (!preg_match('/loggers/', $this->_string, $match)) {
+                            $this->includedFiles[$this->_string] = true;
+                        }
+                        break;
+                    }
+                    $this->lasttokenarray = $this->tokenarray;
+                    $this->_right = false;
+                    $this->_get_token();
+                }
+            }
+        } else {
+            $found = false;
+            if ($this->_right) {
+                $this->_string .= substr($this->_buf, $this->_offs);
+                while (!feof($this->_fd)) {
+                    $this->_buf = fgets($this->_fd, 1024);
+                    $this->_len = strlen($this->_buf);
+                    $this->_line++;
+                    $this->_offs = 0;
+                    if (!$this->_get_token()) continue;
+                    $found = true;
+                    break;
+                }
+            }
+            // $this->_offs = $this->_len;
+            $this->_offs = 0;
+        }
+        return $found;
+    }
+
     function parse($filename)
     {
+
         $this->parseFile($filename);
 
         $this->parsedFiles[$filename] = true;
@@ -50,108 +178,26 @@ class PHPParser
 
     function parseFile($filename)
     {
-        $line = 0;
-
         if (!file_exists($filename)) return;
-//        echo file_exists($filename)
-        $fd = fopen($filename, 'r');
-
+        $this->filename = $filename;
+        $this->_fd = fopen($filename, 'r') or die("Cannot open file");
         if (!$filesize = filesize($filename)) return;
 
-        while (!feof($fd)) {
-            $buf = fgets($fd, 1024);
-            $line++;
-            if (preg_match_all('/(xarML|xarMLByKey)\s*\(\s*\'(.*)\'[,|\)]/', $buf, $matches)) {
-                foreach ($matches[2] as $match) {
-//                    if ($string = $this->parseString($match)) {
-                        $string = $match;
-                        if (!isset($this->transEntries[$string])) {
-                            $this->transEntries[$string] = array();
-                        }
-                        $this->transEntries[$string][] = array('line' => $line, 'file' => $filename);
-                    }
-//                }
-            }
-            if (preg_match_all('/(xarML|xarMLByKey)\s*\(\s*\"(.*)\"[,|\)]/', $buf, $matches)) {
-                foreach ($matches[2] as $match) {
-//                    if ($string = $this->parseString($match)) {
-                      $string = $match;
-                        if (!isset($this->transEntries[$string])) {
-                            $this->transEntries[$string] = array();
-                        }
-                        $this->transEntries[$string][] = array('line' => $line, 'file' => $filename);
-//                    }
-                }
-            } elseif (preg_match('!^\s*//\s*\{(ML_dont_parse|ML_include|ML_add_string|ML_add_key)\s*(.*)\}!', $buf, $match)) {
-//                if ($string = $this->parseString($match[2])) {
-                    $string = $match[2];
-                    if ($match[1] == 'ML_dont_parse') {
-                        $this->notToParseFiles[$string] = true;
-                    } elseif ($match[1] == 'ML_include') {
-                        $this->includedFiles[$string] = true;
-                    } elseif ($match[1] == 'ML_add_string') {
-                        if (!isset($this->transEntries[$string])) {
-                            $this->transEntries[$string] = array();
-                        }
-                        $this->transEntries[$string][] = array('line' => $line, 'file' => $filename);
-                    } elseif ($match[1] == 'ML_add_key') {
-                        if (!isset($this->transKeyEntries[$string])) {
-                            $this->transKeyEntries[$string] = array();
-                        }
-                        $this->transKeyEntries[$string][] = array('line' => $line, 'file' => $filename);
-                    }
-//                }
-            }
-            if (preg_match('/(include_once|include|require_once|require)\s*\(?\s*(.*)/', $buf, $match)) {
-            //disregrd loggers
-                $string = $match[2];
-                if (!preg_match('/loggers/', $string, $match)) {
-                    if (($string = $this->parseString($string)) && strpos($string, '$') === false) {
-                        $this->includedFiles[$string] = true;
-                    }
-                }
-            }
-            if (defined('PHPPARSERDEBUG'))
-                printf("Result: %s<br />\n", $string);
+        $this->_offs = 0;
+        $this->_len = 0;
+        $this->_right = false;
+        $this->_line = 0;
+        $this->lasttokenarray = $this->tokenarray;
+
+        while (!feof($this->_fd)) {
+            $this->_buf = fgets($this->_fd, 1024);
+            $this->_len = strlen($this->_buf);
+            $this->_line++;
+            $this->_offs = 0;
+            $this->_get_token();
         }
 
-        fclose($fd);
+        fclose($this->_fd);
     }
-
-    function parseString($buf)
-    {
-        $pos = 0;
-        $len = strlen($buf);
-        while ($pos < $len) {
-            $char = $buf{$pos++};
-            if ($char == "'" || $char == "'") {
-                $quote = $char;
-                break;
-            } elseif ($char != ' ') {
-                return;
-            }
-        }
-        if ($pos == $len) return;
-        $string = '';
-        while ($pos < $len) {
-            $char = $buf{$pos};
-            if ($char == "\\") {
-                if ($buf{$pos+1} == $quote) {
-                    $string .= $quote;
-                    $pos++;
-                } else {
-                    $string .= $char;
-                }
-            } else {
-                if ($char == $quote) {
-                    return $string;
-                }
-                $string .= $char;
-            }
-            $pos++;
-        }
-        return;
-    }
-
 }
 ?>
