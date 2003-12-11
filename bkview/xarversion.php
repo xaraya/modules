938641ad1052a300c260f749935bb69e68ab91d9
marcel <marcel@hsdev.com>
2003-12-11 20:03:12 +0000

    bkview/xarversion.php: 	#  Making the version number 3 positions. No functional changes

--- bkview/xarversion.php
+++ bkview/xarversion.php
@@ -2,7 +2,7 @@
 // $Id: xarversion.php,v 1.2 2002/08/09 18:46:29 johnny Exp $
 $modversion['name'] = 'bkview';
 $modversion['id'] = '4702';
-$modversion['version'] = '1.1';
+$modversion['version'] = '1.1.0';
 $modversion['description'] = 'BitKeeper repository viewer';
 $modversion['credits'] = 'xardocs/credits.txt';
 $modversion['help'] = 'xardocs/help.txt';
