e08de32594983ce1b7ad2bca71bb565c5bb06e1f
jojodee <jojodee@hbwdev.net>
2003-12-14 20:03:12 +0000

    recommend/xaruserapi/rendersendtofriend.php: 	#  Add attribute to <xar:recommend-sendtofriend type="text|icon" /> 	#  Icon link by default, else 'text' will place "Send to friend" text link.

--- recommend/xaruserapi/rendersendtofriend.php
+++ recommend/xaruserapi/rendersendtofriend.php
@@ -1,25 +1,46 @@
 <?php
-
+/** 
+ * File: $Id$
+ *
+ * Handle Send to a friend tag
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Send To A Friend
+ * @author Jo Dalle Nogare <jojodee@xaraya.com>
+*/
 /**
  * Handle <xar:recommend-sendtofriend ...>  tags
- * Format : <xar:recommend-sendtofriend />
- * Typical usage is <xar:recommend-sendtofriend /> 
- * placed where you wish the 'send to friend' icon to appear
+ * Format : <xar:recommend-sendtofriend /> or
+ * <xar:recommend-sentofriend type="link|icon|somethingelse??" />
+ * Typical usage is <xar:recommend-sendtofriend type="text" /> will display the link
+ * or <xar:recommend-sendtofriend type="icon" /> will display the mail icon.
+ * The icon is displayed by default when no attributes provided.
+ * Placed in template  where you wish the 'send to friend' icon or text link to appear
  * @author jojodee
- * @param At this stage does not take attributes
+ * @param $args containing option for $type  - either "link" or "icon"
  * @returns string
  * @return the PHP code needed to invoke user_sendtofriend() in the BL template
  */
 function recommend_userapi_rendersendtofriend($args)
 {
     extract ($args);
-//   if(!xarVarFetch('aid', 'isset', $aid, NULL, XARVAR_DONT_SET)) {return;}
 
+    if (empty($type)) {$type = '';}
 
-        $alttext = xarML('Send this article to a friend');
-        $sendimg = xarTplGetImage('sendtofriend.gif', 'recommend');
+    //TODO: <jojodee> make easier with text configurable, along with text for email itself.
+    $linktext=xarML("Send to a friend");
+    $alttext = xarML('Send this article to a friend');
+
+    $sendimg = xarTplGetImage('sendtofriend.gif', 'recommend');
+    if ($type=='text') {
+        $out= 'echo "<a href=\"'.xarModURL('recommend','user','sendtofriend',array('aid'=>'$aid')).'\">'.$linktext.'</a>"';
+    } else {
         $out= 'echo "<a href=\"'.xarModURL('recommend','user','sendtofriend',array('aid'=>'$aid')).'\"><img src=\"'.$sendimg.'\" style=\"border:0;\" alt=\"'.$alttext.'\" /></a>"';
-
+    }
 return $out;
 }
 
