8366e264dd3d723cd08181bc1002e80345351df2
anon <anon@numenor.>
2003-12-16 20:03:12 +0000

    trackback/xarinit.php: 	#  standardize on x.x.x version format trackback/xarversion.php: 	#  standardize on x.x.x version format

--- trackback/xarinit.php
+++ trackback/xarinit.php
@@ -141,6 +141,7 @@ function trackback_upgrade($oldversion)
     // Upgrade dependent on old version number
     switch($oldversion) {
         case 1.0:
+        case '1.0':
             // Code to upgrade from version 1.0 goes here
             break;
         case 1.1:
--- trackback/xarversion.php
+++ trackback/xarversion.php
@@ -2,7 +2,7 @@
 // $Id: s.xarversion.php 1.6 03/01/25 20:48:07-05:00 John.Cox@mcnabb. $
 $modversion['name'] = 'trackback';
 $modversion['id'] = '183';
-$modversion['version'] = '1.0';
+$modversion['version'] = '1.0.0';
 $modversion['description'] = 'Implements the Trackback Specification for Xaraya';
 $modversion['credits'] = 'xardocs/credits.txt';
 $modversion['help'] = 'xardocs/help.txt';
