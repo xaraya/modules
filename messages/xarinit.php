7a743ba5c2e355e996dbc8c7ab6a8061f1f5c3a3
marcel <marcel@hsdev.com>
2003-12-16 20:03:12 +0000

    messages/xarinit.php: 	#  version 3 digit changes messages/xarversion.php: 	#  version 3 digit changes

--- messages/xarinit.php
+++ messages/xarinit.php
@@ -115,12 +115,14 @@ function messages_upgrade($oldversion)
 {
     // Upgrade dependent on old version number
     switch($oldversion) {
-        case 0.5:
+        case '0.5':
             break;
-        case 1.0:
+        case '1.0':
             // Code to upgrade from version 1.0 goes here
             break;
-        case 2.0:
+        case '1.8':
+            // compatability upgrade
+        case '2.0.0':
             // Code to upgrade from version 2.0 goes here
             break;
     }
--- messages/xarversion.php
+++ messages/xarversion.php
@@ -2,7 +2,7 @@
 // $Id: xarversion.php,v 1.8 2002/12/01 11:48:51 XarayaGeek Exp $
 $modversion['name'] = 'Xaraya Messages';
 $modversion['id'] = '6';
-$modversion['version'] = '1.8';
+$modversion['version'] = '1.8.0';
 $modversion['description'] = 'Xaraya Messages module';
 $modversion['credits'] = 'docs/credits.txt';
 $modversion['help'] = 'docs/help.txt';
