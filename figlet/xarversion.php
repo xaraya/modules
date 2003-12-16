7d07a26ca974362c1bf3725eb208ea21fe54292a
marcel <marcel@hsdev.com>
2003-12-16 20:03:12 +0000

    figlet/xarversion.php: 	#  version 3 digit changes

--- figlet/xarversion.php
+++ figlet/xarversion.php
@@ -2,7 +2,7 @@
 // $Id: s.xarversion.php 1.8 03/01/25 20:48:07-05:00 John.Cox@mcnabb. $
 $modversion['name'] = 'Figlet';
 $modversion['id'] = '740';
-$modversion['version'] = '0.1';
+$modversion['version'] = '0.1.0';
 $modversion['description'] = 'Transform text to ascii art';
 $modversion['credits'] = 'xardocs/faq.txt';
 $modversion['help'] = 'xardocs/faq.txt';
