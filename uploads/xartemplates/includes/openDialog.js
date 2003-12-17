40c00cf93f465664c7630c8dab2d9c02841b5294
ccorliss <ccorliss@lxwdev-1.schwabfoundation.org>
2003-12-17 20:03:12 +0000

    uploads/xartemplates/includes/local-import-form.xd: 	#  - changed imports -> incoming/trusted uploads/xartemplates/includes/attachment-list.xd: 	#  BitKeeper file /var/schwabfoundation/developers/rabbitt/uploads/uploads/xartemplates/includes/attachment-list.xd uploads/xartemplates/includes/openDialog.js: 	#  BitKeeper file /var/schwabfoundation/developers/rabbitt/uploads/uploads/xartemplates/includes/openDialog.js uploads/xartemplates/user-display_attachments.xd: 	#  BitKeeper file /var/schwabfoundation/developers/rabbitt/uploads/uploads/xartemplates/user-display_attachments.xd

---
+++ uploads/xartemplates/includes/openDialog.js
@@ -0,0 +1,14 @@
+function openDialog(sLocation, sTitle, iWidth, iHeight) {
+        
+    window.open(sLocation, 
+                sTitle, 
+                "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=" + iWidth + ",height=" + iHeight + ",left=20,top=20");
+}
+
+function PropertiesDialog(sLocation, sFileName) {
+    return openDialog(sLocation, sFileName, 500, 375);
+}
+
+function AttachmentsDialog(sLocation, sTitle) {
+    return openDialog(sLocation, sTitle, 500, 375);
+}
