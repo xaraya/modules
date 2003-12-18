5c40c0afdb733dadc224543ff4e902decff23102
shrockc <shrockc@inhs.org>
2003-12-18 20:03:12 +0000

    workflow/xaruser/run_activity.php: 	#  it is now possible to run multiple activities in one request...despite the large diff, only changed a include_once to include

--- workflow/xaruser/run_activity.php
+++ workflow/xaruser/run_activity.php
@@ -2,9 +2,9 @@
 
 /**
  * the run activity user function
- * 
+ *
  * @author mikespub
- * @access public 
+ * @access public
  */
 function workflow_user_run_activity()
 {
@@ -27,33 +27,33 @@ global $__comments;
 $__activity_completed = false;
 
 if ($feature_workflow != 'y') {
-	$tplData['msg'] =  xarML("This feature is disabled");
+  $tplData['msg'] =  xarML("This feature is disabled");
 
-	return xarTplModule('workflow', 'user', 'error', $tplData);
-	die;
+  return xarTplModule('workflow', 'user', 'error', $tplData);
+  die;
 }
 
 if (!isset($_REQUEST['auto'])) {
-	if ($tiki_p_use_workflow != 'y') {
-		$tplData['msg'] =  xarML("Permission denied");
+  if ($tiki_p_use_workflow != 'y') {
+    $tplData['msg'] =  xarML("Permission denied");
 
-		return xarTplModule('workflow', 'user', 'error', $tplData);
-		die;
-	}
+    return xarTplModule('workflow', 'user', 'error', $tplData);
+    die;
+  }
 }
 
 // Determine the activity using the activityId request
 // parameter and get the activity information
 // load then the compiled version of the activity
 if (!isset($_REQUEST['activityId'])) {
-	$tplData['msg'] =  xarML("No workflow activity indicated");
-	return xarTplModule('workflow', 'user', 'error', $tplData);
+  $tplData['msg'] =  xarML("No workflow activity indicated");
+  return xarTplModule('workflow', 'user', 'error', $tplData);
 }
 
 $activity = $baseActivity->getActivity($_REQUEST['activityId']);
 if (empty($activity)) {
-	$tplData['msg'] = xarML("Invalid workflow activity specified");
-	return xarTplModule('workflow', 'user', 'error', $tplData);
+  $tplData['msg'] = xarML("Invalid workflow activity specified");
+  return xarTplModule('workflow', 'user', 'error', $tplData);
 }
 $process->getProcess($activity->getProcessId());
 
@@ -66,27 +66,27 @@ $user_roles = $activity->getUserRoles($user);
 // Only check roles if this is an interactive
 // activity
 if ($activity->isInteractive() == 'y') {
-	if (!count(array_intersect($act_roles, $user_roles))) {
-		$tplData['msg'] =  xarML("You can't execute this activity");
+  if (!count(array_intersect($act_roles, $user_roles))) {
+    $tplData['msg'] =  xarML("You can't execute this activity");
 
-		return xarTplModule('workflow', 'user', 'error', $tplData);
-		die;
-	}
+    return xarTplModule('workflow', 'user', 'error', $tplData);
+    die;
+  }
 }
 
 $act_role_names = $activity->getActivityRoleNames($user);
 
 // FIXME: what's this for ?
 foreach ($act_role_names as $role) {
-	$name = 'tiki-role-' . $role['name'];
+  $name = 'tiki-role-' . $role['name'];
 
-	if (in_array($role['roleId'], $user_roles)) {
+  if (in_array($role['roleId'], $user_roles)) {
                 $tplData[$name] = 'y';
-		$$name = 'y';
-	} else {
+    $$name = 'y';
+  } else {
                 $tplData[$name] = 'n';
-		$$name = 'n';
-	}
+    $$name = 'n';
+  }
 }
 
 $source = GALAXIA_PROCESSES.'/' . $process->getNormalizedName(). '/compiled/' . $activity->getNormalizedName(). '.php';
@@ -99,25 +99,26 @@ $shared = GALAXIA_PROCESSES.'/' . $process->getNormalizedName(). '/code/shared.p
 include_once ($shared);
 
 // Now do whatever you have to do in the activity
-include_once ($source);
+// cls - this needs to be included each time, otherwise you can't run more than 1 activity per request
+include ($source);
 
 // Process comments
 if (isset($_REQUEST['__removecomment'])) {
-	$__comment = $instance->get_instance_comment($_REQUEST['__removecomment']);
+  $__comment = $instance->get_instance_comment($_REQUEST['__removecomment']);
 
-	if ($__comment['user'] == $user or $tiki_p_admin_workflow == 'y') {
-		$instance->remove_instance_comment($_REQUEST['__removecomment']);
-	}
+  if ($__comment['user'] == $user or $tiki_p_admin_workflow == 'y') {
+    $instance->remove_instance_comment($_REQUEST['__removecomment']);
+  }
 }
 
 $tplData['__comments'] =&  $__comments;
 
 if (!isset($_REQUEST['__cid']))
-	$_REQUEST['__cid'] = 0;
+  $_REQUEST['__cid'] = 0;
 
 if (isset($_REQUEST['__post'])) {
-	$instance->replace_instance_comment($_REQUEST['__cid'], $activity->getActivityId(), $activity->getName(),
-		$user, $_REQUEST['__title'], $_REQUEST['__comment']);
+  $instance->replace_instance_comment($_REQUEST['__cid'], $activity->getActivityId(), $activity->getName(),
+    $user, $_REQUEST['__title'], $_REQUEST['__comment']);
 }
 
 $__comments = $instance->get_instance_comments();
@@ -156,15 +157,15 @@ if (!isset($_REQUEST['auto']) && $activity->isInteractive() && $__activity_compl
     xarResponseRedirect($_REQUEST['return_url']);
     return true;
 } else {
-	if (!isset($_REQUEST['auto']) && $activity->isInteractive()) {
-		//$section = 'workflow';
-		//include_once ('tiki-section_options.php');
-		$template = $activity->getNormalizedName(). '.tpl';
-		$tplData['mid'] =  $process->getNormalizedName(). '/' . $template;
-	// not very clean way, but it works :)
+  if (!isset($_REQUEST['auto']) && $activity->isInteractive()) {
+    //$section = 'workflow';
+    //include_once ('tiki-section_options.php');
+    $template = $activity->getNormalizedName(). '.tpl';
+    $tplData['mid'] =  $process->getNormalizedName(). '/' . $template;
+  // not very clean way, but it works :)
                 $output = xarTpl__executeFromFile(GALAXIA_PROCESSES . '/' . $process->getNormalizedName(). '/code/templates/' . $template, $tplData);
                 $tplData['mid'] = $output;
-		$template = 'running';
+    $template = 'running';
 
                 // call display hooks if we have an instance
                 if (!empty($instance->instanceId)) {
@@ -183,9 +184,9 @@ if (!isset($_REQUEST['auto']) && $activity->isInteractive() && $__activity_compl
                                                          'iid' => $instance->getInstanceId()));
                     $tplData['hooks'] = xarModCallHooks('item','display',$instance->instanceId,$item);
                 }
-	} else {
-		$template = 'completed';
-	}
+  } else {
+    $template = 'completed';
+  }
 }
 
     $tplData['feature_help'] = $feature_help;
