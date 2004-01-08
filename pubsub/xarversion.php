662e987b55a8d98f05a1658b3816dfeefb97005c
garrett <garrett@mazrim.blacktower.com>
2004-01-08 20:04:01 +0000

    pubsub/xarversion.php: 	#  turned off the user menu since there is *no* user menu

--- pubsub/xarversion.php
+++ pubsub/xarversion.php
@@ -22,7 +22,7 @@ $modversion['official'] = 1;
 $modversion['author'] = 'Chris Dudley,Garrett Hunter';
 $modversion['contact'] = 'miko@xaraya.com,garrett@blacktower.com';
 $modversion['admin'] = 1;
-$modversion['user'] = 1;
+$modversion['user'] = 0;
 $modversion['securityschema'] = array('pubsub::item' => 'Pubsub ID:Event ID:Handling ID:Template ID');
 $modversion['class'] = 'Utility';
 $modversion['category'] = 'Global';
