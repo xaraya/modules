8f65cd6503e67cf20cb64cb497b445618113e268
rcave <rcave@thingtwo.rhetoric.com>
2004-01-11 20:04:01 +0000

    articles/xaruserapi/getstatusname.php: 	#  typo articles/xartemplates/admin-main.xd: 	#  grammar and typo changes articles/xartemplates/user-display.xd: 	#  grammar and typo changes articles/xartemplates/user-summary.xd: 	#  grammar and typo changes articles/xaruserapi/getstates.php: 	#  BitKeeper file /var/www/xar/modules/articles/xaruserapi/getstates.php articles/xaruserapi/getmonthcount.php: 	#  Create a custom query for each database to return the desired result: 	#    mymonth | count 	#  ---------+------- 	#   2003-12 |     2 	#   2004-01 |     1

--- articles/xaruserapi/getmonthcount.php
+++ articles/xaruserapi/getmonthcount.php
@@ -18,8 +18,19 @@ function articles_userapi_getmonthcount($args)
     // the articles-specific columns too now
     $articlesdef = xarModAPIFunc('articles','user','leftjoin',$args);
 
-// TODO: check how to do this for non-mysql databases !
-    $query = 'SELECT LEFT(FROM_UNIXTIME(xar_pubdate),7) as mymonth, COUNT(*) FROM ' . $articlesdef['table'];
+    // Bug 1590 - Create custom query supported by each database.
+    $dbtype = xarCore_getSystemVar('DB.Type');
+    switch ($dbtype) {
+        case 'mysql':
+                $query = "SELECT LEFT(FROM_UNIXTIME(xar_pubdate),7) AS mymonth, COUNT(*) FROM " . $articlesdef['table'];
+            break;
+        case 'postgres':
+                $query = "SELECT TO_CHAR(ABSTIME(xar_pubdate),'YYYY-MM') AS mymonth, COUNT(*) FROM " . $articlesdef['table'];
+            break;
+        // TODO:  Add SQL queries for Oracle, etc.
+        default:
+            return;
+    }
     if (!empty($articlesdef['where'])) {
         $query .= ' WHERE ' . $articlesdef['where'];
     }
