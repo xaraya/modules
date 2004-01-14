50e976fb504c3675840d854a4202e2096b4aef95
rcave <rcave@rcave01.schwabfoundation.org>
2004-01-14 20:04:01 +0000

    xarldap/xaradmin/connectstatus.php: 	#  doh - Change to xarldap

--- xarldap/xaradmin/connectstatus.php
+++ xarldap/xaradmin/connectstatus.php
@@ -30,7 +30,7 @@ function xarldap_admin_connectstatus()
     // Confirm authorization key 
     if (!xarSecConfirmAuthKey()) {
         $msg = xarML('Invalid authorization key for creating new #(1) item',
-                    'IssueAreaPub');
+                    'xarldap');
         xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'NO_PERMISSION',
                        new SystemException($msg));
         return false;
