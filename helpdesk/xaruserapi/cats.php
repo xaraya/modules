bcd8bed4b12e3ba182dd581b298b7058dbd6a006
brian <brian@laptop.local>
2004-01-21 20:04:01 +0000

    helpdesk/xaruserapi/cats.php: 	#  just cleaned up some more code

--- helpdesk/xaruserapi/cats.php
+++ helpdesk/xaruserapi/cats.php
@@ -1,12 +1,17 @@
 <?php
+/**
+    Gets categories
+    @author Brian McGilligan
+*/
 function helpdesk_userapi_cats($args){
     extract($args);
-    if(!is_array($cids)) { $cids = array($cids); }
+
     $cats = xarModAPIFunc('categories', 'user', 'getchildren', 
                           array('cids' => $cids)
                          );
+    
     if(!$cats) { return; }   
-    //$l = array();
+    
     $l = "";                      
     if(!empty($cats) && is_array($cats)){
         foreach($cats as $cat){
