566f0877982a531405ca5e639799f450793c6597
jojodee <jojodee@hbwdev.net>
2004-01-21 20:04:01 +0000

    xarbb/xaruser/updatetopic.php: 	#  Finish anonymous posting - update the topic and forum view for anon posts. xarbb/xaruser/updatetopic.php: 	#  Removed some odd default values - assume they were a cut and paste. 	#  Get correct anonymous user id from config var

--- xarbb/xaruser/updatetopic.php
+++ xarbb/xaruser/updatetopic.php
@@ -3,7 +3,7 @@
  * File: $Id$
  * 
  * Update a forum topic
- * 
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2003 by the Xaraya Development Team.
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
@@ -14,30 +14,31 @@
 */
 function xarbb_user_updatetopic()
 {
-
 // We need to update the statistics about the forum and the topics here.
-// We do this by updating both tables at once and then giving the poster a chance to reply to the 
+// We do this by updating both tables at once and then giving the poster a chance to reply to the
 // topic or go back to the forum of which he came.
 
-    if (!xarVarFetch('tid','int:1:',$tid,10,XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('modify','int:1:',$modify, 0,XARVAR_NOT_REQUIRED)) return;
-    
+  if (!xarVarFetch('tid','int:1:',$tid,$tid,XARVAR_NOT_REQUIRED)) return;
+  if (!xarVarFetch('modify','int:1:',$modify, 0,XARVAR_NOT_REQUIRED)) return;
+
     // Start by updating the topic stats.
     $modid        = xarModGetIDFromName('xarbb');
+
     $comments = xarModAPIFunc('comments',
                               'user',
                               'get_multiple',
-                              array('modid'       => $modid,
-                                    'objectid'    => $tid));
+                               array('modid'       => $modid,
+                                     'objectid'    => $tid));
 
-
-     $isanon=$comments[0]['xar_postanon'];
+     $totalcomments=count($comments);
+     $isanon=$comments[$totalcomments-1]['xar_postanon'];
      $anonuid = xarConfigGetVar('Site.User.AnonymousUID');
-    // Start by updating the topic stats.
+
+   // Start by updating the topic stats.
     if ($isanon==1) {
-        $uid=$anonuid;
+        $poster=$anonuid;
     } else {
-        $uid = xarUserGetVar('uid');
+        $poster = xarUserGetVar('uid');
     }
     //Don't count up if the topic is being edited ? Need to add modify test here.
     if ($modify != 1){
@@ -45,7 +46,7 @@ function xarbb_user_updatetopic()
                        'user',
                        'updatetopicsview',
                        array('tid'      => $tid,
-                             'treplier' => $uid))) return;
+                             'treplier' => $poster))) return;
     }
     // The user API function is called
     $forum = xarModAPIFunc('xarbb',
@@ -59,10 +60,10 @@ function xarbb_user_updatetopic()
                            'updateforumview',
                            array('fid'      => $forum['fid'],
                                  'reply'    => 1,
-                                 'fposter'  => $uid))) return;
+                                 'fposter'  => $poster))) return;
     }
 
-    xarResponseRedirect(xarModURL('xarbb', 'user', 'viewtopic', array('tid' => $tid)));
+   xarResponseRedirect(xarModURL('xarbb', 'user', 'viewtopic', array('tid' => $tid)));
 
     return;
 }
