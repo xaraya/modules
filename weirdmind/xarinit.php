303ee90c8e7b943424f44b7433c3b014fc4c316d
rrandy <rrandy@smog.phatcom.net>
2004-02-03 20:04:02 +0000

    weirdmind/xarinit.php: 	#  fixed brackets

--- weirdmind/xarinit.php
+++ weirdmind/xarinit.php
@@ -1,13 +1,16 @@
 <?php
-function weirdmind_init() {
+function weirdmind_init() 
+{
     xarRegisterMask('AdminWeirdMind','All','weirdmind','All','All','ACCESS_ADMIN');
     return true;  
 }
-function weirdmind_upgrade($args = null) {
+function weirdmind_upgrade($args = null) 
+{
     return true;
 }
 
-function weirdmind_delete() {
+function weirdmind_delete() 
+{
     xarRemoveMasks('weirdmind');
     return true;
 }
