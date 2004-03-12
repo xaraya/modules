d2cd01c314ee200bfd6e534900bffe2621b84b39
Administrator <Administrator@paxtera.>
2004-03-12 20:04:03 +0000

    soapserver/lib/nusoap.php: 	#  BRACE?

--- soapserver/lib/nusoap.php
+++ soapserver/lib/nusoap.php
@@ -2189,7 +2189,8 @@
      *
      * @access private
      */
-     function webDescription(){
+     function webDescription()
+	 {
  		$b = '
  		<?xml version="1.0"?
 		 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><title>NuSOAP: '.$this->wsdl->serviceName.'</title>
