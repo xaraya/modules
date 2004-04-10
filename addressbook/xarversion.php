182657aa83bfd46896cdfb4ff5264f27c4b039b4
John.Cox <John.Cox@mcnabb.>
2004-04-10 20:04:04 +0000

    addressbook/xarversion.php: 	#  fix for bug 2675

--- addressbook/xarversion.php
+++ addressbook/xarversion.php
@@ -14,20 +14,21 @@
  * Based on pnAddressBook by Thomas Smiatek <thomas@smiatek.com>
  */
 
-$modversion['name'] = 'AddressBook';
-$modversion['id'] = '66417';
-$modversion['version'] = '1.2.5';
-$modversion['description'] = 'Xaraya Address Book';
-$modversion['credits'] = '';
-$modversion['help'] = '';
-$modversion['changelog'] = 'xardocs/changelog.txt';
-$modversion['license'] = 'xardocs/license.txt';
-$modversion['official'] = 1;
-$modversion['author'] = 'Garrett Hunter';
-$modversion['contact'] = 'Garrett Hunter <garret@blacktower.com>';
-$modversion['admin'] = 1;
-$modversion['user'] = 1;
+$modversion['name']         = 'AddressBook';
+$modversion['id']           = '66417';
+$modversion['version']      = '1.2.5';
+$modversion['description']  = 'Xaraya Address Book';
+$modversion['credits']      = '';
+$modversion['help']         = '';
+$modversion['changelog']    = 'xardocs/changelog.txt';
+$modversion['license']      = 'xardocs/license.txt';
+$modversion['official']     = 1;
+$modversion['author']       = 'Garrett Hunter';
+$modversion['contact']      = 'Garrett Hunter <garret@blacktower.com>';
+$modversion['admin']        = 1;
+$modversion['user']         = 1;
 $modversion['securityschema']   = array('AddressBook::' => '::');
-$modversion['class'] = 'Complete';
-$modversion['category'] = 'Content';
+$modversion['class']        = 'Complete';
+$modversion['category']     = 'Content';
+$modversion['dependency']   = array(775);
 ?>
