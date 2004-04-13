510819a6706d92697b829ad232be6fe82e1cc7e5
judgej <judgej@acad-ajp.>
2004-04-13 20:04:04 +0000

    mime/xaruserapi/get_mime_image.php: 	#  Bug 2706: provide parameters allowing suffixes to be specified.

--- mime/xaruserapi/get_mime_image.php
+++ mime/xaruserapi/get_mime_image.php
@@ -7,46 +7,59 @@
  *
  * @author  Carl P. Corliss
  * @access  public
- * @param   string mimeType    The mime type to find a correlating image for
+ * @param   string mimeType     The mime type to find a correlating image for
+ * @param   string fileSuffix   Image file suffix list (default: '.png')
+ * @param   string defaultBase  Default file base name (default: 'default')
  * @returns string
  */
 
 function mime_userapi_get_mime_image( $args )
 {
+    extract($args);
 
-    extract ( $args );
-
-     if (!isset($mimeType)) {
-        $msg = xarML('Missing parameter [#(1)] for API function [#(2)] in module [#(3)].',
-                     'mimeType', 'get_mime_image', 'mime');
+    if (!isset($mimeType)) {
+        // API location handled centrally.
+        $msg = xarML('Missing parameter [#(1)].', 'mimeType');
         xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
     }
 
+    // Defaults.
+    if (empty($fileSuffix)) {$fileSuffix = '.png';}
+    if (empty($defaultBase)) {$defaultBase = 'default';}
+
+    // Explode the list of suffixes.
+    // A list of suffixes to try can be given, e.g. '-8x8.gif|.png'
+    $fileSuffixes = explode('|', $fileSuffix);
+
     $mimeType = explode('/', $mimeType);
     if (count($mimeType) != 2) {
-        $imageFile = 'default.png';
+        $imageFile = $defaultBase . $fileSuffix;
     }
 
-    $imageFile = $mimeType[0] . '-' . $mimeType[1] . '.png';
-    $imageURI = xarTplGetImage($imageFile, 'mime');
+    // Try the complete mimetype-subtype image.
+    foreach($fileSuffixes as $fileSuffix) {
+        $imageFile = $mimeType[0] . '-' . $mimeType[1] . $fileSuffix;
+        if ($imageURI = xarTplGetImage($imageFile, 'mime')) {break;}
+    }
 
-    // try for the complete mimetype-subtype image
-    if ($imageURI != NULL) {
-        return $imageURI;
-    } else {
-        // otherwise, use the top level mimtype image
-        $imageFile = $mimeType[0] . '.png';
-        $imageURI = xarTplGetImage($imageFile, 'mime');
+    // Otherwise, try the top level mimetype image.
+    if ($imageURI == NULL) {
+        foreach($fileSuffixes as $fileSuffix) {
+            $imageFile = $mimeType[0] . $fileSuffix;
+            if ($imageURI = xarTplGetImage($imageFile, 'mime')) {break;}
+        }
 
-        if ($imageURI != NULL) {
-            return $imageURI;
-        } else {
-            return xarTplGetImage('default.png', 'mime');
+        if ($imageURI == NULL) {
+            foreach($fileSuffixes as $fileSuffix) {
+                $imageFile = $defaultBase . $fileSuffix;
+                if ($imageURI = xarTplGetImage($imageFile, 'mime')) {break;}
+            }
         }
     }
 
-    // Should NEVER get here.
-    return FALSE;
+    // Single point of return.
+    // We also have 'imageFile' set, which could be a useful (alternative) return value.
+    return $imageURI;
 }
 
 ?>
