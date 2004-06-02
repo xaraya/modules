a6a8f918cac7cfd0e94d9ccf2ca6fe869db9c859
marc <marc@marclaptop.>
2004-06-02 20:04:06 +0000

    htmlarea/xarversion.php: 	#  htmlarea: adjust version file

--- htmlarea/xarversion.php
+++ htmlarea/xarversion.php
@@ -1,7 +1,7 @@
 <?php
 $modversion['name'] = 'htmlarea';
 $modversion['id'] = '3009';
-$modversion['version'] = '1.0';
+$modversion['version'] = '1.0.0';
 $modversion['description'] = 'Incorporate the HTMLArea editor into Xaraya modules';
 $modversion['credits'] = 'xardocs/credits.txt';
 $modversion['help'] = 'xardocs/help.txt';
