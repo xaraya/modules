d25ddf9325597f03645b085ae0a924645f4b8fde
jojodee <jojodee@hbwdev.net>
2004-06-08 20:04:06 +0000

    release/xaruserapi/countitems.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/modules/release/xaruserapi/countitems.php release/xaruserapi/getallrids.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/modules/release/xaruserapi/getallrids.php

---
+++ release/xaruserapi/countitems.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * File: $Id:
+ * 
+ * Utility function counts number of items held by this module
+ * 
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Release
+ * @author jojodee
+ */
+/**
+ * utility function to count the number of items held by this module
+ *
+ * @author jojodee
+ * @returns integer
+ * @return number of items held by this module
+ * @raise DATABASE_ERROR
+ */
+function release_userapi_countitems($args)
+{
+	extract($args);
+
+    if (!isset($idtypes)) {
+        $idtypes = 1;
+    }
+    if ($idtypes == 3){
+        $whereclause= "WHERE xar_type = 'module'";
+    }elseif ($idtypes==2) {
+        $whereclause= "WHERE xar_type = 'theme'";
+    }else {
+        $whereclause= "WHERE xar_type = 'theme' or xar_type = 'module'";
+    }
+
+
+    $dbconn =& xarDBGetConn();
+    $xartable =& xarDBGetTables();
+    // It's good practice to name the table and column definitions you are
+    // getting - $table and $column don't cut it in more complex modules
+    $releasetable = $xartable['release_id'];
+     $query = "SELECT COUNT(1)
+             FROM $releasetable "
+             .$whereclause;
+
+    $result = &$dbconn->Execute($query);
+
+    // Check for an error with the database code, adodb has already raised
+    // the exception so we just return
+    if (!$result) return; 
+    // Obtain the number of items
+    list($numitems) = $result->fields; 
+    // All successful database queries produce a result set, and that result
+    // set should be closed when it has been finished with
+    $result->Close(); 
+    // Return the number of items
+    return $numitems;
+} 
+
+?>
---
+++ release/xaruserapi/getallrids.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * get all users
+ * @returns array
+ * @return array of users, or false on failure
+ */
+function release_userapi_getallrids($args)
+{
+    extract($args); 
+
+    // Optional arguments.
+    if (!isset($startnum)) {
+        $startnum = 1;
+    }
+    if (!isset($numitems)) {
+        $numitems = -1;
+    }
+    if (!isset($idtypes)) {
+        $idtypes = 1;
+    }
+    if ($idtypes == 3){
+        $whereclause= "WHERE xar_type = 'module'";
+    }elseif ($idtypes==2) {
+        $whereclause= "WHERE xar_type = 'theme'";
+    }else {
+        $whereclause= "WHERE xar_type = 'theme' or xar_type = 'module'";
+    }
+
+    $releaseinfo = array();
+
+    // Security Check
+    if(!xarSecurityCheck('OverviewRelease')) return;
+
+    // Get database setup
+    $dbconn =& xarDBGetConn();
+    $xartable =& xarDBGetTables();
+
+    $releasetable = $xartable['release_id'];
+
+    $query = "SELECT xar_rid,
+                     xar_uid,
+                     xar_name,
+                     xar_desc,
+                     xar_type,
+                     xar_certified,
+                     xar_approved
+            FROM $releasetable ".
+            $whereclause."
+            ORDER BY xar_rid";
+    if (!empty($certified)) {
+        $query .= " WHERE xar_certified = '" . xarVarPrepForStore($certified). "'";
+    }
+
+    $result = $dbconn->SelectLimit($query, $numitems, $startnum-1);
+    if (!$result) return;
+
+    // Put users into result array
+    for (; !$result->EOF; $result->MoveNext()) {
+        list($rid, $uid, $name, $desc, $type, $certified, $approved) = $result->fields;
+        if (xarSecurityCheck('OverviewRelease', 0)) {
+            $releaseinfo[] = array('rid'        => $rid,
+                                   'uid'        => $uid,
+                                   'name'       => $name,
+                                   'desc'       => $desc,
+                                   'type'       => $type,
+                                   'certified'  => $certified,
+                                   'approved'   => $approved);
+        }
+    }
+
+    $result->Close();
+
+    // Return the users
+    return $releaseinfo;
+}
+
+?>
