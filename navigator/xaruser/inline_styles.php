c0365e92fdc8666377348d615700fd6d797d80c3
ccorliss <ccorliss@schwabfoundation.org>
2004-06-23 20:04:06 +0000

    navigator/xarstyles/navigator-listmenu.css: 	#  navigator/xarstyles/navigator-listmenu.css 	#  navigator/xartemplates/admin-tag_generator.xd 	#  navigator/xartemplates/user-menutype_list.xd 	#  navigator/xaruser/inline_styles.php

--- navigator/xaruser/inline_styles.php
+++ navigator/xaruser/inline_styles.php
@@ -10,6 +10,9 @@ function navigator_user_inline_styles( $args )
 
     $styleList = unserialize(xarModGetVar('navigator', 'style.list.inline'));
 
+    // Make sure the list menu get's it's styles included as well...
+    xarTplAddStyleLink('navigator','navigator-listmenu');
+
     if (!isset($styleList) || !is_array($styleList) || !count($styleList)) {
         return '';
     } else {
