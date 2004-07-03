9cf254509800755417a8a0fa0418e9d4c2c38431
mikespub <mikespub@fully.qualified.hostname>
2004-07-03 20:04:07 +0000

    stats/xaruserapi/get_browser_data.php: 	#  bug 2827 fix Firebird stats

--- stats/xaruserapi/get_browser_data.php
+++ stats/xaruserapi/get_browser_data.php
@@ -2,58 +2,59 @@
 
 function stats_userapi_get_browser_data($args)
 {
-	extract($args);
-	unset($args);
-	$args = compact('top10');
-	
+    extract($args);
+    unset($args);
+    $args = compact('top10');
+    
     // API function to get the hits by browsers
     list($brdata, $brsum, $brmax) = xarModAPIFunc('stats',
-												  'user',
-												  'getbybrowser',
-												  $args);
-	$browsers = array();
+                                                  'user',
+                                                  'getbybrowser',
+                                                  $args);
+    $browsers = array();
 
-	if (empty($brdata)) {
+    if (empty($brdata)) {
         $msg = xarML('No browser data available');
         xarExceptionSet(XAR_USER_EXCEPTION, 'NO_BROWSER_DATA',
             new DefaultUserException(__FILE__ . '(' . __LINE__ . '): ' . $msg));
         return;
-    } 	
-	foreach($brdata as $browser){
-		switch ($browser['agent']) {
-			case 'Microsoft Internet Explorer': //TODO: is this really only on MAC??
+    }     
+    foreach($brdata as $browser){
+        $brname = '';
+        switch ($browser['agent']) {
+            case 'Microsoft Internet Explorer': //TODO: is this really only on MAC??
                 $brpic = 'ie5mac.png';
                 $brname = xarML('Microsoft Internet Explorer');
                 break;
-			case 'Msie':
-				$brpic = 'msie.png';
+            case 'Msie':
+                $brpic = 'msie.png';
                 $brname = xarML('Microsoft Internet Explorer');
                 break;
             case 'Mozilla':
-				$brpic  = 'mozilla.png';
+                $brpic  = 'mozilla.png';
                 $brname = xarML('Mozilla');
                 break;
-			case 'Opera':
-				$brpic = 'opera.png';
+            case 'Opera':
+                $brpic = 'opera.png';
                 $brname = xarML('Opera');
                 break;
-			case 'ns':         //TODO: is this used like this?
-			case 'Netscape':
-			case 'Netscape6':
-				$brpic = 'netscape7.png';
+            case 'ns':         //TODO: is this used like this?
+            case 'Netscape':
+            case 'Netscape6':
+                $brpic = 'netscape7.png';
                 $brname = xarML('Netscape');
                 break;
             case 'Safari':
-				$brpic = 'safari.png';
+                $brpic = 'safari.png';
                 $brname = xarML('Safari');
                 break;
             case 'Chimera':
             case 'Camino':
-				$brpic = 'camino.png';
+                $brpic = 'camino.png';
                 $brname = xarML('Camino');
-                break;				
+                break;                
             case 'Galeon':
-				$brpic = 'galeon.png';
+                $brpic = 'galeon.png';
                 $brname = xarML('Galeon');
                 break;
             case 'Phoenix':
@@ -61,30 +62,31 @@ function stats_userapi_get_browser_data($args)
                 $brname = 'Mozilla Firefox';
                 // fallthrough => no break;
             case 'Mozilla Firebird':
-				$brpic = 'px.png';
+            case 'Firebird':
+                $brpic = 'px.png';
                 if (empty($brname)) {
-                	$brname = xarML('Mozilla Firebird');
+                    $brname = xarML('Mozilla Firebird');
                 }
                 break;
-			case 'Konqueror':
-				$brpic = 'konqueror.png';
+            case 'Konqueror':
+                $brpic = 'konqueror.png';
                 $brname = xarML('Konqueror');
                 break;
             default:
-				$brname = xarML('Unknown');
-				$brpic  = 'question.gif';
-		}
+                $brname = xarML('Unknown');
+                $brpic  = 'question.gif';
+        }
         if(!$top10) $brname .= " $browser[agver]";
-		$browsers[] = array('name' => $brname,
-							'rel'  => sprintf('%01.2f',(100*$browser['hits']/$brsum)),
-							'abs'  => $browser['hits'],
-							'wid'  => round(($barlen*$browser['hits']/$brmax)),
-							'pic'  => $brpic);
-	}
-	unset($brdata, $brsum, $brmax, $browser, $brname, $brpic);
+        $browsers[] = array('name' => $brname,
+                            'rel'  => sprintf('%01.2f',(100*$browser['hits']/$brsum)),
+                            'abs'  => $browser['hits'],
+                            'wid'  => round(($barlen*$browser['hits']/$brmax)),
+                            'pic'  => $brpic);
+    }
+    unset($brdata, $brsum, $brmax, $browser, $brname, $brpic);
 
-	$data = compact('browsers');
-	return $data;
+    $data = compact('browsers');
+    return $data;
 }
 
 ?>
