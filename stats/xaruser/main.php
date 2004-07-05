d3aba2d1bee8794eb0738676f4d8d718a405f0f7
voll <voll@yahoo.com>
2004-07-05 20:04:07 +0000

    stats/xartemplates/user-main.xd: 	#  bug 2865 many files stats/xaruser/main.php: 	#  bug 2865 many files

--- stats/xaruser/main.php
+++ stats/xaruser/main.php
@@ -12,149 +12,147 @@
 function stats_user_main()
 {
     // Security check
-	if(!xarSecurityCheck('OverviewStats')) return;
+    if(!xarSecurityCheck('OverviewStats')) return;
 	
     // load the locale data
     $localeData =& xarMLSLoadLocaleData();
 
     // Initialize vars
     $picpath = 'modules/stats/xarimages';
-	$barlen  = 230;
+    $barlen  = 230;
    
     // Get the stats-module installation date a.k.a. start-of-stats-collecting
-    $startdate = xarLocaleFormatDate('%D', xarModGetVar('stats','startdate'));
+    $startdate = xarModGetVar('stats','startdate');
     
     // API function to get the best days figure
-	list($bestday, $day['mosthits']) = xarModAPIFunc('stats',
-													 'user',
-													 'gettopday',
-													 array('type' => 'best'));
-    $day['best'] = xarLocaleFormatDate('%D',
-                       gmmktime(0,0,0,$bestday['month'],$bestday['day'],$bestday['year']));
-	unset($bestday);
+    list($bestday, $day['mosthits']) = xarModAPIFunc('stats',
+                                                     'user',
+                                                     'gettopday',
+                                                     array('type' => 'best'));
+    $day['best'] = gmmktime(0,0,0,$bestday['month'],$bestday['day'],$bestday['year']);
+    unset($bestday);
 
     // API function to get the worst days figure
-	list($worstday, $day['leasthits']) = xarModAPIFunc('stats',
-													   'user',
-													   'gettopday',
-													   array('type' => 'worst'));
-    $day['worst'] = xarLocaleFormatDate('%D',
-                       gmmktime(0,0,0,$worstday['month'],$worstday['day'],$worstday['year']));
-	unset($worstday);
+    list($worstday, $day['leasthits']) = xarModAPIFunc('stats',
+                                                       'user',
+                                                       'gettopday',
+                                                       array('type' => 'worst'));
+    $day['worst'] = gmmktime(0,0,0,$worstday['month'],$worstday['day'],$worstday['year']);
+    unset($worstday);
 
     // API function to get the total hits
     $hits['total'] = xarModAPIFunc('stats',
-	                               'user',
+                                   'user',
      	                           'gettotalhits');
 
     // API function to get the hits of today
-	$today = explode('-', gmdate('Y-n-j'));
+    $today = explode('-', gmdate('Y-n-j'));
     $hits['today'] = xarModAPIFunc('stats',
-		                           'user',
-		                           'getday',
-		                           array('year'  => $today[0],
-		                                 'month' => $today[1],
-	      		                         'day'   => $today[2]));
+                                   'user',
+                                   'getday',
+                                   array('year'  => $today[0],
+                                         'month' => $today[1],
+                                         'day'   => $today[2]));
 
     // API function to get the hits of yesterday (respects DST jumps)
-	$yesterday = explode('-', gmdate(('Y-n-j'), gmmktime(0,0,0,$today[1],$today[2]-1,$today[0])));
+    $yesterday = explode('-', gmdate(('Y-n-j'), gmmktime(0,0,0,$today[1],$today[2]-1,$today[0])));
     $hits['yesterday'] = xarModAPIFunc('stats',
-		                               'user',
-		                               'getday',
-			                           array('year'  => $yesterday[0],
-											 'month' => $yesterday[1],
-											 'day'   => $yesterday[2]));
-	unset($today, $yesterday);
+                                       'user',
+                                       'getday',
+                                       array('year'  => $yesterday[0],
+                                             'month' => $yesterday[1],
+                                             'day'   => $yesterday[2]));
+    unset($today, $yesterday);
 
     // get the hits by browsers
-	$top10 = true;
-	$args = compact('top10', 'picpath', 'barlen');
-	extract(xarModAPIFunc('stats','user','get_browser_data',$args));
+    $top10 = true;
+    $args = compact('top10', 'picpath', 'barlen');
+    extract(xarModAPIFunc('stats','user','get_browser_data',$args));
 
     // get the hits by operating system
-	extract(xarModAPIFunc('stats','user','get_os_data', $args));
+    extract(xarModAPIFunc('stats','user','get_os_data', $args));
 
     // API function to get the hits of the week
     list($phdata, $phsum, $phmax) = xarModAPIFunc('stats',
 												  'user',
 												  'getperhour');
-	$perhour = array();
-	foreach($phdata as $key => $val) {
-		$phhour = sprintf('%02d', $val['hour']);
-		$perhour[$key] = array('name' => $phhour.':00 - '.$phhour.':59',
-							   'abs'  => $val['hits'],
-							   'rel'  => sprintf('%01.2f',(100*$val['hits']/$phsum)),
-							   'wid'  => round($barlen*$val['hits']/$phmax));
-	}
-	unset($phdata, $phsum, $phmax, $phhour);
+    $perhour = array();
+    foreach($phdata as $key => $val) {
+        $phhour = sprintf('%02d', $val['hour']);
+        $perhour[$key] = array('name' => $phhour.':00 - '.$phhour.':59',
+                               'abs'  => $val['hits'],
+                               'rel'  => sprintf('%01.2f',(100*$val['hits']/$phsum)),
+                               'wid'  => round($barlen*$val['hits']/$phmax));
+    }
+    unset($phdata, $phsum, $phmax, $phhour);
 
     $hour = xarModAPIFunc('stats',
-						  'user',
-						  'gettophour',
-						  $perhour);
+                          'user',
+                          'gettophour',
+                           $perhour);
     
-	// API function to get the hits of the week
+    // API function to get the hits of the week
     //TODO: add to the api an argument of what year/month/week the stats are. if empty, cumulate all
-	list($pwddata, $pwdsum, $pwdmax) = xarModAPIFunc('stats',
-													 'user',
-													 'getperweekday');
+    list($pwddata, $pwdsum, $pwdmax) = xarModAPIFunc('stats',
+                                                     'user',
+                                                     'getperweekday');
 	$perweekday = array();
 	foreach ($pwddata as $key => $val) {
         $localeIndex = '/dateSymbols/weekdays/'.++$val['weekday'].'/full';
         $perweekday[$key] = array('name' => $localeData[$localeIndex],        
-								  'abs'  => $val['hits'],
-								  'rel'  => sprintf('%01.2f',(100*$val['hits']/$pwdsum)),
-								  'wid'  => round($barlen*$val['hits']/$pwdmax));
+                                  'abs'  => $val['hits'],
+                                  'rel'  => sprintf('%01.2f',(100*$val['hits']/$pwdsum)),
+                                  'wid'  => round($barlen*$val['hits']/$pwdmax));
     }
-	unset($pwddata, $pwdsum, $pwdmax, $localeIndex);
+    unset($pwddata, $pwdsum, $pwdmax, $localeIndex);
 
-	$weekday = xarModAPIFunc('stats',
-							 'user',
-							 'gettopweekday',
-							 $perweekday);
+    $weekday = xarModAPIFunc('stats',
+                             'user',
+                             'gettopweekday',
+                             $perweekday);
 
     // get hits of the last seven days
-	list($l7data, $l7sum, $l7max) = xarModAPIFunc('stats',
-												  'user',
-												  'getlast7days');
+    list($l7data, $l7sum, $l7max) = xarModAPIFunc('stats',
+                                                  'user',
+                                                  'getlast7days');
     $last7days = array();
     foreach ($l7data as $key => $val) {
-//TODO: use dateformat-medium from locales file here
-        $formatted = xarLocaleFormatDate('%D',
-                         gmmktime(0,0,0,$val['month'],$val['day'],$val['year']));        
-        $last7days[$key] = array('formated' => $formatted,        
-								 'abs'      => $val['hits'],
-								 'rel'      => sprintf('%01.2f',(100*$val['hits']/$l7sum)),
-								 'wid'      => round($barlen*$val['hits']/$l7max));
+    //TODO: use dateformat-medium from locales file here
+        $unformatted = gmmktime(0,0,0,$val['month'],$val['day'],$val['year']);
+        $last7days[$key] = array('unformatted' => $unformatted,        
+                                 'abs'         => $val['hits'],
+                                 'rel'         => sprintf('%01.2f',(100*$val['hits']/$l7sum)),
+                                 'wid'         => round($barlen*$val['hits']/$l7max));
     }
-	unset($l7data, $l7sum, $l7max, $formatted);
+    unset($l7data, $l7sum, $l7max, $unformatted);
 
-	// get overall hits per month. same month of different years are added!
-	//TODO: Put the searched for year as input var to the api func. if empty, cumulate all years
-	list($pmdata, $pmsum, $pmmax) = xarModAPIFunc('stats',
-												  'user',
-												  'getpermonth');
+    // get overall hits per month. same month of different years are added!
+    //TODO: Put the searched for year as input var to the api func. if empty, cumulate all years
+    list($pmdata, $pmsum, $pmmax) = xarModAPIFunc('stats',
+                                                  'user',
+                                                  'getpermonth');
     $permonth = array();
     foreach ($pmdata as $key => $val) {
         $localeIndex = '/dateSymbols/months/'.$val['month'].'/full';
         $permonth[$key] = array('name' => $localeData[$localeIndex],        
-								'abs'  => $val['hits'],
-								'rel'  => sprintf('%01.2f',(100*$val['hits']/$pmsum)),
-								'wid'  => round($barlen*$val['hits']/$pmmax));
+                                'abs'  => $val['hits'],
+                                'rel'  => sprintf('%01.2f',(100*$val['hits']/$pmsum)),
+                                'wid'  => round($barlen*$val['hits']/$pmmax));
     }
-	unset($pmdata, $pmsum, $pmmax, $barlen, $localeIndex);
+    unset($pmdata, $pmsum, $pmmax, $barlen, $localeIndex);
 
-	// get misc stats
-	$misc = xarModAPIFunc('stats',
-						  'user',
-						  'getmisc');
+    // get misc stats
+    $misc = xarModAPIFunc('stats',
+                          'user',
+                          'getmisc');
 
-	// arrange return values
-    $data = compact('startdate', 'hits', 'day', 'browsers', 'os', 'perhour', 'perweekday',
-					'last7days', 'permonth', 'weekday', 'hour', 'misc', 'picpath');
+    // arrange return values
+    $data = compact('startdate', 'hits', 'day', 'browsers', 'os',
+                    'perhour', 'perweekday', 'last7days', 'permonth',
+                    'weekday', 'hour', 'misc', 'picpath');
 
-	// return data to BL template
-	return $data;
+    // return data to BL template
+    return $data;
 }
 
 ?>
