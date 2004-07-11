287494d38369cba9e16f88ca65be23bc0442bebd
mikespub <mikespub@fully.qualified.hostname>
2004-07-11 20:04:07 +0000

    xarcachemanager/config.caching.php.dist: 	#  support page caching for non-anonymous users xarcachemanager/xaradmin/pages.php: 	#  support page caching for non-anonymous users xarcachemanager/xartemplates/admin-pages.xd: 	#  support page caching for non-anonymous users

--- xarcachemanager/xaradmin/pages.php
+++ xarcachemanager/xaradmin/pages.php
@@ -23,12 +23,81 @@ function xarcachemanager_admin_pages($args)
         return $data;
     }
 
+    $cachingConfigFile = $varCacheDir . '/config.caching.php';
+
+    if (!file_exists($cachingConfigFile)) {
+        $msg=xarML('That is strange.  The #(1) file seems to be 
+                    missing.', $cachingConfigFile);
+        xarExceptionSet(XAR_SYSTEM_EXCEPTION,'MODULE_FILE_NOT_EXIST',
+                        new SystemException($msg));
+            
+        return false;
+    }
+
+    include $cachingConfigFile;
+
+    $keyslist = str_replace( '.', '', array_keys($cachingConfiguration));
+    $valueslist = array_values($cachingConfiguration);
+    $data['settings'] = array();
+    
+    $arraysize = sizeof($keyslist);
+    for ($i=0;$i<$arraysize;$i++) {
+        $data['settings'][$keyslist[$i]] = $valueslist[$i];
+    }
+
+    $filter = array('Class' => 2);
+    $data['themes'] = xarModAPIFunc('themes',
+                                    'admin',
+                                    'getlist', $filter);
+
+    $data['groups'] = xarModAPIFunc('roles','user','getallgroups');
+
     xarVarFetch('submit','str',$submit,'');
     if (!empty($submit)) {
         // Confirm authorisation code
         if (!xarSecConfirmAuthKey()) return;
 
-        // ... do stuff ...
+        xarVarFetch('groups','isset',$groups,array(),XARVAR_NOT_REQUIRED);
+
+        $grouplist = array();
+        foreach ($data['groups'] as $idx => $group) {
+            if (!empty($groups[$group['uid']])) {
+                $data['groups'][$idx]['checked'] = 1;
+                $grouplist[] = $group['uid'];
+            }
+        }
+    
+        if (!is_writable($cachingConfigFile)) {
+            $msg=xarML('The caching configuration file is not writable by the web server.  
+                       #(1) must be writable by the web server for 
+                       the output caching to be managed by xarCacheManager.', $cachingConfigFile);
+            xarExceptionSet(XAR_SYSTEM_EXCEPTION,'FUNCTION_FAILED',
+                            new SystemException($msg));
+            return false;
+        }
+    
+        $cachingConfig = join('', file($cachingConfigFile));
+   
+        $cachegroups = join(';', $grouplist); 
+        if (preg_match('/Page.CacheGroups/', $cachingConfig)) {
+            $cachingConfig = preg_replace('/\[\'Page.CacheGroups\'\]\s*=\s*(\'|\")(.*)\\1;/', "['Page.CacheGroups'] = '$cachegroups';", $cachingConfig);
+        } else {
+            // we put everything on 1 line to avoid DOS vs. Unix newline issue
+            $newstuff = '$cachingConfiguration[\'Page.CacheGroups\'] = \'' . $cachegroups . '\'; ?>';
+            $cachingConfig = preg_replace('/\?>/', $newstuff, $cachingConfig);
+        }
+    
+        $fp = fopen ($cachingConfigFile, 'wb');
+        fwrite ($fp, $cachingConfig);
+        fclose ($fp);
+
+    } elseif (!empty($data['settings']['PageCacheGroups'])) {
+        $grouplist = explode(';',$data['settings']['PageCacheGroups']);
+        foreach ($data['groups'] as $idx => $group) {
+            if (in_array($group['uid'],$grouplist)) {
+                $data['groups'][$idx]['checked'] = 1;
+            }
+        }
     }
 
     // Get some page caching configurations
