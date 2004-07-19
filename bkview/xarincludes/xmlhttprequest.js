afc97a5fa0cb540b47a9a1a5c0433ac083ddca95
marcel <marcel@hsdev.com>
2004-07-19 20:04:07 +0000

    bkview/xarstyles/bkweb.css: 	#  adjustment of the navtab style so it is less intrusive for the other styles bkview/xartemplates/admin-main.xd: 	#  using strike tag, just for fun bkview/xartemplates/includes/deltatree.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/includes/user-menu.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/includes/user-view.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-annotateview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-browseview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-csetview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-deltaview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-diffview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-display.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-historyview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-main.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-patchview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-relatedview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-statsview.xd: 	#  adding the xmlhttprequest support bkview/xartemplates/user-view.xd: 	#  adding the xmlhttprequest support bkview/xarincludes/xmlhttprequest.js: 	#  BitKeeper file /var/bk/xar/modules/dev/bkview/xarincludes/xmlhttprequest.js bkview/xartemplates/includes/user-trailer.xd: 	#  BitKeeper file /var/bk/xar/modules/dev/bkview/xartemplates/includes/user-trailer.xd

---
+++ bkview/xarincludes/xmlhttprequest.js
@@ -0,0 +1,63 @@
+// Global vars for the request object, what we should add to the url and which
+// id is in the tag, which subtree we should replace with the result of the
+// request.
+// Copyright 2004 Marcel van der Boom <marcel@hsdev.com>
+var req;
+var tagid = 'bkview-modbody-id';
+var postfix = "&pageName=module";
+
+function loadContent(url) {
+    if (window.XMLHttpRequest) {
+        document.body.style.cursor='wait';
+        req = new XMLHttpRequest();
+        req.onreadystatechange = processReqChange;
+        req.open("POST", url + postfix, true);
+        req.send(null);
+        return false;
+    } else if (window.ActiveXObject) {
+        document.body.style.cursor='wait';
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+        if (req) {
+            req.onreadystatechange = processReqChange;
+            req.open("POST", url + postfix, true);
+            req.send();
+        }
+        return false;
+    } else {
+        return true; // do the normal action
+    }   
+}
+
+// Handler called on state changes of the request object
+function processReqChange()
+{
+    if (req.readyState == 4) {
+        if (req.status == 200) {
+            // Do whatever we need with the content returned
+            tag = document.getElementById(tagid);
+            if(tag == null) {
+                // not found, fallback
+                document.location = req.url;
+                return true;
+            }
+            // Make sure we replace the right tag, and dont leave anything behind
+            myparent = tag.parentNode;
+            tag.id ='dummytogetridoftheoriginal';
+            tag.innerHTML = req.responseText;
+            newtag = document.getElementById(tagid);
+            if(newtag == null) {
+                alert('cant find the new tag [' + tagid + ']');
+                tag.innerHTML = req.responseText;
+                document.body.style.cursor='default';
+                return false;
+            } else {
+                copyofnew = newtag.cloneNode(true);
+                myparent.replaceChild(copyofnew,tag);
+                document.body.style.cursor='default';
+                return false; // cancel the normal action
+            }
+        } else {
+            alert("There was a problem retrieving the XML data:\n" +  req.statusText);
+        }
+    }
+}
