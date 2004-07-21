86a65388766444852348dcf03d1131908307f579
mikespub <mikespub@fully.qualified.hostname>
2004-07-21 20:04:07 +0000

    imagefeed/xarinit.php: 	#  bug 90 imagefeed imagefeed/xartemplates/user-main.xd: 	#  bug 90 imagefeed imagefeed/xaruser.php: 	#  bug 90 imagefeed

--- imagefeed/xarinit.php
+++ imagefeed/xarinit.php
@@ -16,6 +16,25 @@
  */
 function imagefeed_init()
 {
+/*  // no longer supported
+    xarModSetVar('imagefeed', 'url', 'http://coolpick.com/way/cool/syn/babe.html');
+    xarModSetVar('imagefeed', 'find', '7babpol.shtml"\n><img src="([^"]+)"');
+    xarModSetVar('imagefeed', 'prefix', '');
+    xarModSetVar('imagefeed', 'title', 'Babe of the Day');
+    xarModSetVar('imagefeed', 'descr', 'CoolPick.com');
+    xarModSetVar('imagefeed', 'link', 'http://coolpick.com/way/cool/syn/babe.html');
+    xarModSetVar('imagefeed', 'refresh', 24*60*60);
+*/
+
+    // sample configuration
+    xarModSetVar('imagefeed', 'url', 'http://mikespub.net/pictures/daily.php');
+    xarModSetVar('imagefeed', 'find', '\t\t<img SRC="([^"]+)"');
+    xarModSetVar('imagefeed', 'prefix', 'http://mikespub.net');
+    xarModSetVar('imagefeed', 'title', 'Daily Picture at Mike\'s Pub');
+    xarModSetVar('imagefeed', 'descr', '<a href="/pictures/daily.php\?id=\d+">(.*?)</a>');
+    xarModSetVar('imagefeed', 'link', '<a href="(/pictures/daily.php\?id=\d+)"');
+    xarModSetVar('imagefeed', 'refresh', 24*60*60);
+
     return true;
 }
 
--- imagefeed/xaruser.php
+++ imagefeed/xaruser.php
@@ -1,4 +1,5 @@
 <?php
+
 // File: $Id$
 // ----------------------------------------------------------------------
 // Xaraya eXtensible Management System
@@ -10,25 +11,74 @@
 // ----------------------------------------------------------------------
 
 
+
 /**
  * the main user function
  */
 function imagefeed_user_main()
 {
+    // basic data
+    $url     = xarModGetVar('imagefeed', 'url');
+    $find    = xarModGetVar('imagefeed', 'find');
+    $prefix  = xarModGetVar('imagefeed', 'prefix');
+    $title   = xarModGetVar('imagefeed', 'title');
+    $descr   = xarModGetVar('imagefeed', 'descr');
+    $link    = xarModGetVar('imagefeed', 'link');
+    $refresh = xarModGetVar('imagefeed', 'refresh');
 
-	// basic data
-    $url    = "http://coolpick.com/way/cool/syn/babe.html";
-    $needle = '7babpol.shtml"\n><img src="';
+    // get actual site source
+    $lines = xarModAPIFunc('base', 'user', 'getfile',
+                           array('url'       => $url,
+                                 'cached'    => true,
+                                 'cachedir'  => 'cache/rss',
+                                 'refresh'   => $refresh,
+                                 'extension' => '.html'));
+    if (empty($lines)) return;
 
-	// get actual site sources
-    $lines_array = file($url);
-    $lines = implode('', $lines_array);
+    // retrieve the image location
+    if (preg_match("°".$find."°i", $lines, $matches)) {
+        $image = $matches[1];
+        if (!empty($prefix) && !stristr($image,$prefix)) {
+            $image = $prefix . $image;
+        }
+    } else {
+        $image = '';
+    }
 
-	// retriev the image location
-    $match_flg = preg_match("°".$needle."([^\"]+)(\")°", $lines, $matches);
+    // retrieve the link if it's variable
+    if (strstr($link,'(')) {
+        if (preg_match("°".$link."°i", $lines, $matches)) {
+            $link = $matches[1];
+            if (!empty($prefix) && !stristr($link,$prefix)) {
+                $link = $prefix . $link;
+            }
+        } else {
+            $link = $url;
+        }
+    }
 
-    return array('piclocation' => $matches[1],
-    			 'link' => 'http://coolpick.com/way/cool/syn/babe.html',
-    			 'title' => 'CoolPick.com');
+    // retrieve the description if it's variable
+    if (strstr($descr,'(')) {
+        if (preg_match("°".$descr."°i", $lines, $matches)) {
+            $descr = $matches[1];
+        } else {
+            $descr = $title;
+        }
+    }
+
+    // prepare template variables
+    $data = array('piclocation' => $image,
+                  'title' => xarVarPrepForDisplay($title),
+                  'descr' => xarVarPrepHTMLDisplay($descr),
+                  'link' => $link);
+
+    // only 1 itemid here (for now) !
+    $data['module'] = 'imagefeed';
+    $data['itemtype'] = 0;
+    $data['itemid'] = 1;
+    $data['returnurl'] = xarModURL('imagefeed','user','main');
+    $data['hookoutput'] = xarModCallHooks('item', 'display', 1, $data);
+
+    return $data;
 }
 ?>
