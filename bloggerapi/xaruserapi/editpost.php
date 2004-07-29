6e4971ff5ecc9724f2949559ba864589ab224f79
marcel <marcel@hsdev.com>
2004-07-29 20:04:07 +0000

    bloggerapi/xaruserapi/editpost.php: 	#  aha

--- bloggerapi/xaruserapi/editpost.php
+++ bloggerapi/xaruserapi/editpost.php
@@ -53,7 +53,7 @@ function bloggerapi_userapi_editpost($msg)
         // FIXME: test for exceptions
         $pubType= xarModGetVar('bloggerapi','bloggerpubtype');
         $modId = xarModGetIDFromName('articles');
-        $cids = xarModAPIFunc('categories','user','getlinks',array('iids'=>$iids,'modid'=>$modId,'itemtype' => $pubType,'reverse'=>0));
+        $cids = array_keys(xarModAPIFunc('categories','user','getlinks',array('iids'=>$iids,'modid'=>$modId,'itemtype' => $pubType,'reverse'=>0)));
         xarLogVariable('Categories retrieved', $cids, XARLOG_LEVEL_WARNING);
         if (!xarModAPIFunc('articles','admin','update',array('aid'=>$article['aid'], 'title'=>$title,
                                                             'summary'=>$content, 'ptid' => $pubType, 'cids' => $cids,
