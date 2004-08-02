286ea2301a374eba1a6e5280fbc4da56ef4475a5
marcel <marcel@hsdev.com>
2004-08-02 20:04:08 +0000

    reports/xaradminapi/create_connection.php: 	#  typo

--- reports/xaradminapi/create_connection.php
+++ reports/xaradminapi/create_connection.php
@@ -15,7 +15,7 @@ function reports_adminapi_create_connection($args)
 
 	$conn_id = $dbconn->GenId();
 
-	$sql = "INSERT INTO $tab ($cols[id],$cols[name],$cols[?,description],$cols[server],$cols[type],$cols[database],$cols[user],$cols[password]) 
+	$sql = "INSERT INTO $tab ($cols[id],$cols[name],$cols[description],$cols[server],$cols[type],$cols[database],$cols[user],$cols[password]) 
 		    VALUES (?,?,?,?,?,?,?,?)";
     $bindvars = array($conn_id, $conn_name, $conn_desc, $conn_server, $conn_type, $conn_database, $conn_user, $conn_password);
 
