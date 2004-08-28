16b0a3b3b0164ba12ecca435d1fc2683c0de93d0
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    xmlrpcvalidatorapi/xaruserapi/arrayofstructs.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xaruserapi/counttheentities.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xaruserapi/easystruct.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xaruserapi/echostruct.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xaruserapi/manytypes.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xaruserapi/moderatesizearraycheck.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xaruserapi/nestedstruct.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xaruserapi/simplestructreturn.php: 	#  tabs to 4 spaces xmlrpcvalidatorapi/xartemplates/admin-main.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed xmlrpcvalidatorapi/xarversion.php: 	#  standardize displaynames

--- xmlrpcvalidatorapi/xarversion.php
+++ xmlrpcvalidatorapi/xarversion.php
@@ -3,6 +3,7 @@
 $modversion['name'] = 'XML-RPC validator API';
 $modversion['id'] = '746';
 $modversion['version'] = '1.0.0';
+$modversion['displayname']    = xarML('XML-RPCValidator');
 $modversion['description'] = 'Validation API for XML-RPC server';
 $modversion['official'] = 1;
 $modversion['author'] = 'Marcel van der Boom';
@@ -10,6 +11,6 @@ $modversion['contact'] = 'marcel@hsdev.com';
 $modversion['admin'] = 1;
 $modversion['user'] = 0;
 $modversion['class'] = 'admin';
-$modversion['category'] = 'Miscellaneous'; 
+$modversion['category'] = 'Miscellaneous';
 $modversion['dependency'] = array(743);
 ?>
