de4fb42b234e9fa0a0007330ef7e894705ce914b
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    userpoints/xaradmin/createrank.php: 	#  tabs to 4 spaces userpoints/xaradmin/modifyconfig.php: 	#  tabs to 4 spaces userpoints/xaradmin/modifyrank.php: 	#  tabs to 4 spaces userpoints/xaradmin/updaterank.php: 	#  tabs to 4 spaces userpoints/xaradminapi/addpoints.php: 	#  tabs to 4 spaces userpoints/xaradminapi/createhook.php: 	#  tabs to 4 spaces userpoints/xaradminapi/deleteall.php: 	#  tabs to 4 spaces userpoints/xaradminapi/removehook.php: 	#  tabs to 4 spaces userpoints/xarinit.php: 	#  tabs to 4 spaces userpoints/xaruser/display.php: 	#  tabs to 4 spaces userpoints/xaruserapi/get.php: 	#  tabs to 4 spaces userpoints/xaruserapi/getpointstypes.php: 	#  tabs to 4 spaces userpoints/xartemplates/admin-main.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed userpoints/xartemplates/admin-modifyconfig.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed userpoints/xartemplates/admin-modifyrank.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed userpoints/xartemplates/admin-newrank.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed userpoints/xarversion.php: 	#  standardize displaynames

--- userpoints/xarversion.php
+++ userpoints/xarversion.php
@@ -1,18 +1,19 @@
 <?php
 /**
  * File: $Id: s.xaradmin.php 1.28 03/02/08 17:38:40-05:00 John.Cox@mcnabb. $
- * 
+ *
  * Userpoints System
- * 
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2002 by the Xaraya Development Team.
  * @link http://www.xaraya.com
  * @subpackage userpoints module
- * @author Vassilis Stratigakis 
+ * @author Vassilis Stratigakis
  */
 $modversion['name']           = 'Userpoints';
 $modversion['id']             = '782';
 $modversion['version']        = '1.0.0';
+$modversion['displayname']    = xarML('Userpoints');
 $modversion['description']    = 'Earn Points For Participating';
 $modversion['credits']        = 'xardocs/credits.txt';
 $modversion['help']           = 'xardocs/help.txt';
