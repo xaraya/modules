2d368369b92110c6aae40b92d4e43c0ed1338dab
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    authinvision/xarblocks/register.php: 	#  tabs to 4 spaces authinvision/xarblocks/usercp.php: 	#  tabs to 4 spaces authinvision/xarinit.php: 	#  tabs to 4 spaces authinvision/xaruser.php: 	#  tabs to 4 spaces authinvision/xaruserapi.php: 	#  tabs to 4 spaces authinvision/xartemplates/admin-main.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed authinvision/xarversion.php: 	#  standardize displaynames

--- authinvision/xarversion.php
+++ authinvision/xarversion.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 $modversion['name'] = 'AuthInvision';
 $modversion['id'] = '950';
 $modversion['version'] = '1.0.0';
+$modversion['displayname'] = xarML('Authinvision');
 $modversion['description'] = 'Xaraya/Invision Board authentication module';
 $modversion['credits'] = 'xardocs/credits.txt';
 $modversion['help'] = 'xardocs/help.txt';
