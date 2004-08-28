e02a573c7f2311c99e7c7a26d6542cd19fd6f92e
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    referer/xartemplates/admin-main.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed referer/xartemplates/admin-modifyconfig.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed referer/xartemplates/admin-view.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed referer/xartemplates/includes/navtabs.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed referer/xarversion.php: 	#  standardize displaynames

--- referer/xarversion.php
+++ referer/xarversion.php
@@ -1,19 +1,20 @@
 <?php
 /**
  * File: $Id: s.xarinit.php 1.11 03/01/18 11:39:31-05:00 John.Cox@mcnabb. $
- * 
+ *
  * Xaraya Referers
- * 
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2002 by the Xaraya Development Team.
  * @license GPL <http://www.gnu.org/licenses/gpl.html>
  * @link http://www.xaraya.org
  * @subpackage Referer Module
- * @author John Cox et al. 
+ * @author John Cox et al.
  */
 $modversion['name']           = 'Referers';
 $modversion['id']             = '90';
 $modversion['version']        = '1.0.1';
+$modversion['displayname']    = xarML('Referers');
 $modversion['description']    = 'Referer Tracking.  Statistics extension';
 $modversion['credits']        = 'xardocs/credits.txt';
 $modversion['help']           = 'xardocs/help.txt';
