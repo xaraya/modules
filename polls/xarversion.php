c183d35feca8d2f9d6f4753d7bb9f868151791d4
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    polls/xaradmin/display.php: 	#  tabs to 4 spaces polls/xaradmin/list.php: 	#  tabs to 4 spaces polls/xaradminapi/resequence.php: 	#  tabs to 4 spaces polls/xarblocks/poll.php: 	#  tabs to 4 spaces polls/xaruser/search.php: 	#  tabs to 4 spaces polls/xaruserapi/get.php: 	#  tabs to 4 spaces polls/xaruserapi/getall.php: 	#  tabs to 4 spaces polls/xaruserapi/gethooked.php: 	#  tabs to 4 spaces polls/xaruserapi/vote.php: 	#  tabs to 4 spaces polls/xartemplates/admin-display.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/admin-modifyconfig.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/admin-modifyhook.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/admin-new.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/admin-newhook.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/blocks/poll.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/blocks/pollAdmin.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/blocks/polls-poll-modify.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/user-list.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/user-results.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xartemplates/user-resultshook.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed polls/xarversion.php: 	#  standardize displaynames

--- polls/xarversion.php
+++ polls/xarversion.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * File: $Id$
- * 
+ *
  * Polls version information
- * 
+ *
  * @copyright (C) 2003 by the Xaraya Development Team.
  * @license GPL <http://www.gnu.org/licenses/gpl.html>
  * @link http://www.xaraya.com
@@ -14,6 +14,7 @@
 $modversion['name'] = 'polls';
 $modversion['id'] = '23';
 $modversion['version'] = '1.3.0';
+$modversion['displayname']    = xarML('Polls');
 $modversion['description'] = 'Conduct polls';
 $modversion['credits'] = 'xardocs/credits.txt';
 $modversion['help'] = 'xardocs/help.txt';
