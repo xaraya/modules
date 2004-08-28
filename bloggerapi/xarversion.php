8e38e8c5d28f7f5f2c96a6cddf8c984267885052
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    bloggerapi/xaradminapi/getmenulinks.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/deletepost.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/editpost.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/getdmap.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/getpost.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/getrecentposts.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/getuserinfo.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/getusersblogs.php: 	#  tabs to 4 spaces bloggerapi/xaruserapi/newpost.php: 	#  tabs to 4 spaces bloggerapi/xartemplates/admin-main.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed bloggerapi/xartemplates/admin-modifyconfig.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed bloggerapi/xarversion.php: 	#  standardize displaynames

--- bloggerapi/xarversion.php
+++ bloggerapi/xarversion.php
@@ -3,6 +3,7 @@
 $modversion['name'] = 'Blogger API';
 $modversion['id'] = '745';
 $modversion['version'] = '1.0.0';
+$modversion['displayname']      = xarML('BloggerAPI');
 $modversion['description'] = 'Blogger API implementation for Xaraya';
 $modversion['official'] = 1;
 $modversion['author'] = 'Marcel van der Boom';
@@ -10,6 +11,6 @@ $modversion['contact'] = 'marcel@hsdev.com';
 $modversion['admin'] = 1;
 $modversion['user'] = 0;
 $modversion['class'] = 'admin';
-$modversion['category'] = 'Miscellaneous'; 
+$modversion['category'] = 'Miscellaneous';
 $modversion['dependency'] = array(743,744);
 ?>
