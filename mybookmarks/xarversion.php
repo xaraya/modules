efcb8f426ab37957575ae4599240bbb41544ee1f
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    mybookmarks/xartemplates/admin-config.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed mybookmarks/xartemplates/admin-new-mybookmarks.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed mybookmarks/xarversion.php: 	#  standardize displaynames

--- mybookmarks/xarversion.php
+++ mybookmarks/xarversion.php
@@ -14,6 +14,7 @@
 $modversion['name']           = 'mybookmarks';
 $modversion['id']             = '178';
 $modversion['version']        = '0.1.0';
+$modversion['displayname']    = xarML('MyBookmarks');
 $modversion['description']    = 'MyBookmarks share your favorite urls';
 $modversion['credits']        = 'xardocs/credits.txt';
 $modversion['help']           = 'xardocs/docs.txt';
