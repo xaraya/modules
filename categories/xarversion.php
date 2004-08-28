33747550108cac050d60f4ab620becbbe6fe89e5
marc <marc@marclaptop.>
2004-08-28 20:04:08 +0000

    categories/xarincludes/HTML_TreeMenu-1.1.5/docs/example.php: 	#  tabs to 4 spaces categories/xarincludes/HTML_TreeMenu-1.1.5/treemenu.php: 	#  tabs to 4 spaces categories/xaruserapi/navigation.php: 	#  tabs to 4 spaces categories/xartemplates/admin-config.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed categories/xartemplates/admin-modifyhook.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed categories/xartemplates/admin-newhook.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed categories/xartemplates/admin-privileges.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed categories/xartemplates/user-main.xd: 	#  tabs to 4 spaces 	#  Let's pass this thru QA before someone comes to a bug fix and 	#  get QAed categories/xarversion.php: 	#  standardize displaynames

--- categories/xarversion.php
+++ categories/xarversion.php
@@ -11,21 +11,22 @@
  * @subpackage categories module
  * @author Jim McDonald, Flávio Botelho <nuncanada@xaraya.com>, mikespub <postnuke@mikespub.net>
 */
-$modversion['name'] = 'categories';
-$modversion['version'] = '2.3.0';
-$modversion['id'] = '147';
-$modversion['description'] = 'Categorised data utility';
-$modversion['credits'] = 'xardocs/credits.txt';
-$modversion['help'] = 'xardocs/help.txt';
-$modversion['changelog'] = 'xardocs/changelog.txt';
-$modversion['license'] = 'xardocs/license.txt';
-$modversion['official'] = 1;
-$modversion['author'] = 'Jim McDonald';
-$modversion['contact'] = 'http://www.mcdee.net/';
-$modversion['admin'] = 1;
-$modversion['user'] = 0;
-$modversion['class'] = 'Utility';
-$modversion['category'] = 'Content';
-$modversion['securityschema'] = array('categories::category' => 'Category name::Category ID',
+    $modversion['name'] = 'categories';
+    $modversion['id'] = '147';
+    $modversion['version'] = '2.3.0';
+    $modversion['displayname']    = xarML('Categories');
+    $modversion['description'] = 'Categorised data utility';
+    $modversion['credits'] = 'xardocs/credits.txt';
+    $modversion['help'] = 'xardocs/help.txt';
+    $modversion['changelog'] = 'xardocs/changelog.txt';
+    $modversion['license'] = 'xardocs/license.txt';
+    $modversion['official'] = 1;
+    $modversion['author'] = 'Jim McDonald';
+    $modversion['contact'] = 'http://www.mcdee.net/';
+    $modversion['admin'] = 1;
+    $modversion['user'] = 0;
+    $modversion['class'] = 'Utility';
+    $modversion['category'] = 'Content';
+    $modversion['securityschema'] = array('categories::category' => 'Category name::Category ID',
                                       'categories::item' => 'Category ID:Module ID:Item ID');
 ?>
