9f34a1a3fef6426f502bc0cf2060792b1253248d
nuncanada <nuncanada@ng0.(none)>
2004-08-31 20:04:08 +0000

    cachesecurity/xarinit.php: 	#  There was a table missing, i was testing before thru upgrade so i didnt 	#  notice it was missing from the init. 	#  (Can't we make this kind management smarter?) 	# 	#  Fixes bug #3271

--- cachesecurity/xarinit.php
+++ cachesecurity/xarinit.php
@@ -23,206 +23,6 @@ function cachesecurity_init()
     xarDBLoadTableMaintenanceAPI();
     $sitePrefix = xarDBGetSiteTablePrefix();
 
-/*****
-*
-* CREATE TABLE xar_seccache_privileges (
-*  xar_realm varchar(32) NOT NULL default '',
-*  xar_module varchar(32) NOT NULL default '',
-*  xar_component varchar(32) NOT NULL default '',
-*  xar_instance1 varchar(32) NOT NULL default '',
-*  xar_instance2 varchar(32) NOT NULL default '',
-*  xar_instance3 varchar(32) NOT NULL default '',
-*  xar_instance4 varchar(32) NOT NULL default '',
-*  xar_instance5 varchar(32) NOT NULL default '',
-*  xar_instance6 varchar(32) NOT NULL default '',
-*  xar_level int(11) NOT NULL default '0',
-*  xar_priv_id int(11) NOT NULL default '0',
-*  PRIMARY KEY  (xar_priv_id),
-*  KEY i_xar_seccache_privlevel (xar_level),
-*  KEY i_xar_seccache_privmain (xar_realm,xar_module,xar_component,xar_instance1,xar_instance2,xar_instance3,xar_instance4,xar_instance5,xar_instance6,xar_level,xar_priv_id)
-*) TYPE=MyISAM;
-******/
-/*
-    $query = xarDBCreateTable($tables['security_cache_privileges'],
-             array('xar_priv_id'  => array('type'       => 'integer',
-                                      'null'        => false,
-                                      'default'     => '0',
-                                      'increment'   => false,
-                                      'primary_key' => true),
-                   'xar_realm' => array('type'      => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_module' => array('type'     => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_component' => array('type'  => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance1' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance2' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance3' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance4' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance5' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance6' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_level' => array('type'      => 'integer',
-                                      'null'        => false,
-                                      'default'     => '0'),
-                    ));
-
-   if (!$dbconn->Execute($query)) return;
-
-    $index = array('name'      => 'i_'.$sitePrefix.'_seccache_privileges_main',
-                   'fields'    => array('xar_realm',
-                                                  'xar_module',
-                                                  'xar_component',
-                                                  'xar_instance1',
-                                                  'xar_instance2',
-                                                  'xar_instance3',
-                                                  'xar_instance4',
-                                                  'xar_instance5',
-                                                  'xar_instance6',
-                                                  'xar_level'),
-                   'unique'    => FALSE);
-    $query = xarDBCreateIndex($tables['security_cache_privileges'],$index);
-    if (!$dbconn->Execute($query)) return;
-
-    $index = array('name'      => 'i_'.$sitePrefix.'_seccache_privileges_level',
-                   'fields'    => array('xar_level'),
-                   'unique'    => FALSE);
-    $query = xarDBCreateIndex($tables['security_cache_privileges'],$index);
-    if (!$dbconn->Execute($query)) return;
-
-    xarDB_importTables(array('security_cache_privileges' => $tables['security_cache_privileges']));
-*/
-/*****
-*
-* CREATE TABLE xar_seccache_masks (
-*  xar_realm varchar(32) NOT NULL default '',
-*  xar_module varchar(32) NOT NULL default '',
-*  xar_component varchar(32) NOT NULL default '',
-*  xar_instance1 varchar(32) NOT NULL default '',
-*  xar_instance2 varchar(32) NOT NULL default '',
-*  xar_instance3 varchar(32) NOT NULL default '',
-*  xar_instance4 varchar(32) NOT NULL default '',
-*  xar_instance5 varchar(32) NOT NULL default '',
-*  xar_instance6 varchar(32) NOT NULL default '',
-*  xar_level int(11) NOT NULL default '0',
-*  xar_mask_id int(11) NOT NULL default '0',
-*  xar_name varchar(32) NOT NULL default '',
-*  PRIMARY KEY  (xar_mask_id),
-*  KEY i_xar_security_masks_name (xar_name)
-*) TYPE=MyISAM;
-******/
-
-/*
-    $query = xarDBCreateTable($tables['security_cache_masks'],
-             array('xar_mask_id'  => array('type'       => 'integer',
-                                      'null'        => false,
-                                      'default'     => '0',
-                                      'increment'   => false,
-                                      'primary_key' => true),
-                   'xar_name'  => array('type'       => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),                   
-                   'xar_realm' => array('type'      => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_module' => array('type'     => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_component' => array('type'  => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance1' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance2' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance3' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance4' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance5' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_instance6' => array('type'   => 'varchar',
-                                      'size'        => 32,
-                                      'null'        => false,
-                                      'default'     => ''),
-                   'xar_level' => array('type'      => 'integer',
-                                      'null'        => false,
-                                      'default'     => '0'),
-                    ));
-
-   if (!$dbconn->Execute($query)) return;
-
-    $index = array('name'      => 'i_'.$sitePrefix.'_seccache_masks_main',
-                   'fields'    => array('xar_name',
-                                                  'xar_realm',
-                                                  'xar_module',
-                                                  'xar_component',
-                                                  'xar_instance1',
-                                                  'xar_instance2',
-                                                  'xar_instance3',
-                                                  'xar_instance4',
-                                                  'xar_instance5',
-                                                  'xar_instance6',
-                                                  'xar_level'),
-                   'unique'    => true);
-    $query = xarDBCreateIndex($tables['security_cache_masks'],$index);
-    if (!$dbconn->Execute($query)) return;
-
-    $index = array('name'      => 'i_'.$sitePrefix.'_seccache_masks_name',
-                              'fields'       => array('xar_name'),
-                              'unique'    => FALSE);
-    $query = xarDBCreateIndex($tables['security_cache_masks'],$index);
-    if (!$dbconn->Execute($query)) return;
-
-    xarDB_importTables(array('security_cache_masks' => $tables['security_cache_masks']));
-*/
-/*****
-*
-CREATE TABLE xar_seccache_privsgraph (
-  xar_priv_id int(11) NOT NULL default '0',
-  xar_priv_sibbling_id int(11) NOT NULL default '0',
-  UNIQUE KEY xar_seccache_sibbling_id_key (xar_priv_sibbling_id,xar_priv_id)
-) TYPE=MyISAM;
-*
-******/
-
     $query = xarDBCreateTable($tables['security_cache_privsgraph'],
              array('xar_priv_id'  => array('type'       => 'integer',
                                       'null'        => false,
@@ -282,32 +82,39 @@ CREATE TABLE xar_seccache_rolesgraph (
 
     xarDB_importTables(array('security_cache_rolesgraph' => $tables['security_cache_rolesgraph']));
 
+    // Set up database tables
+	$query = xarDBCreateTable($tables['security_cache_privsmasks'],
+    	array(
+    		'xar_priv_id'  => array(
+    			'type'       => 'integer',
+        		'null'        => false,
+                'default'     => '0',
+                'increment'   => false,
+                'primary_key' => false),
+			'xar_mask_id'  => array(
+				'type'       => 'integer',
+                'null'        => false,
+                'default'     => '0',
+                'increment'   => false,
+                'primary_key' => false),
+		));
 
-    //Creates indexes in the main privileges/masks tables
-    $index = array('name'      => 'i_'.$sitePrefix.'_seccache_privileges',
-                   'fields'    => array('xar_realm (20)',
-                                                  'xar_module (20)',
-                                                  'xar_component (20)',
-                                                  'xar_pid',
-                                                  'xar_instance',
-                                                  'xar_level'),
-                   'unique'    => false); //Table doesnt support unique subkeys
-    $query = xarDBCreateIndex($tables['privileges'],$index);
     if (!$dbconn->Execute($query)) return;
-    
 
-    //Creates indexes in the main privileges/masks tables
-    $index = array('name'      => 'i_'.$sitePrefix.'_seccache_masks',
-                   'fields'    => array('xar_name (20)',
-                                                  'xar_realm (20)',
-                                                  'xar_module (20)',
-                                                  'xar_component (20)',
-                                                  'xar_instance',
-                                                  'xar_level'),
-                   'unique'    => false); //This should be true, but we have
-                   //incongruent data in the database by the default install
-    $query = xarDBCreateIndex($tables['security_masks'],$index);
+	$index = array(
+    	'name'      => 'i_'.$sitePrefix.'_seccache_privsmasks_main',
+        'fields'    => array('xar_priv_id', 'xar_mask_id'),
+        'unique'    => true);
+    $query = xarDBCreateIndex($tables['security_cache_privsmasks'],$index);
     if (!$dbconn->Execute($query)) return;
+
+	xarDB_importTables(array('security_cache_privsmasks' => $tables['security_cache_privsmasks']));
+
+    //set up configvars
+    xarConfigSetVar('CacheSecurity.on', false);
+    xarConfigSetVar('CacheSecurity.rolesgraph', false);
+    xarConfigSetVar('CacheSecurity.privsgraph', false);
+    xarConfigSetVar('CacheSecurity.privsmasks', false);
 
     // set up permissions masks.
     xarRegisterMask('AdminCacheSecurity', 'All', 'cachesecurity', 'Item', 'All', 'ACCESS_ADMIN');
@@ -438,6 +245,13 @@ function cachesecurity_upgrade($oldversion)
  */
 function cachesecurity_delete()
 {
+    //set up configvars
+    //FIXME: We need someway to delete configuration variables!!!
+    xarConfigSetVar('CacheSecurity.on', false);
+    xarConfigSetVar('CacheSecurity.rolesgraph', false);
+    xarConfigSetVar('CacheSecurity.privsgraph', false);
+    xarConfigSetVar('CacheSecurity.privsmasks', false);
+
     // Drop the tables
     $dbconn =& xarDBGetConn();
     $tables =& xarDBGetTables();
@@ -453,6 +267,10 @@ function cachesecurity_delete()
     if (empty($query)) return; // throw back
     if (!$dbconn->Execute($query)) return;
 */
+    $query = xarDBDropTable($tables['security_cache_privsmasks']);
+    if (empty($query)) return; // throw back
+    if (!$dbconn->Execute($query)) return;
+
     $query = xarDBDropTable($tables['security_cache_rolesgraph']);
     if (empty($query)) return; // throw back
     if (!$dbconn->Execute($query)) return;
