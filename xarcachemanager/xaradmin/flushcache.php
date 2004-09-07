7f27d8975193159ee8e943c1de5f61d1d458d873
jsb <jsb@sting.in-beames.net>
2004-09-07 20:04:09 +0000

    xarcachemanager/xaradmin/flushcache.php: 	#  If we're passed a directory, flush its contents. xarcachemanager/xartemplates/admin-flushcache.xd: 	#  Display subdirectories for cache flushing

--- xarcachemanager/xaradmin/flushcache.php
+++ xarcachemanager/xaradmin/flushcache.php
@@ -57,7 +57,11 @@ function xarcachemanager_admin_flushcache($args)
         if ($flushkey == '-') {
             $data['notice'] = xarML("You must select a cache key to flush.  If there is no cache key to select the output cache is empty.");
         } else {
-            xarOutputFlushCached($flushkey);
+            if (!empty($flushkey) && !strpos('-', $flushkey)) {
+                xarOutputFlushCached('', "$outputCacheDir/$flushkey");
+            } else {
+                xarOutputFlushCached($flushkey);
+            }
             $data['notice'] = xarML("Cached #(1) files have been successfully flushed.", $flushkey);
         }
         
