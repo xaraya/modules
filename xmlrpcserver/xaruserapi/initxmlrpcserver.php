11ef7cf2b1581620b2fbf36a3bf44d12f7028aeb
marcel <marcel@hsdev.com>
2004-10-07 20:04:10 +0000

    xmlrpcserver/xarincludes/xmlrpcs.inc: 	#  $args convention xmlrpcserver/xaruser/rsd.php: 	#  add the metaweblogapi as the preferred editting protocol xmlrpcserver/xaruserapi/initxmlrpcserver.php: 	#  Go over the api's in a loop instead of dealing with them one by one in a block of code

--- xmlrpcserver/xaruserapi/initxmlrpcserver.php
+++ xmlrpcserver/xaruserapi/initxmlrpcserver.php
@@ -6,7 +6,7 @@
  * Initialization of XML-RPC server
  *
  * @package modules
- * @copyright (C) 2003 by the Xaraya Development Team.
+ * @copyright (C) 2004 by the Xaraya Development Team.
  * @link http://www.xaraya.com
  * 
  * @subpackage xmlrpcserver
@@ -30,34 +30,19 @@ function xmlrpcserver_userapi_initxmlrpcserver()
 
     $functions=array();
     
-    // System API should always be available
-    if (xarModIsAvailable('xmlrpcsystemapi')) {
-        xarLogMessage("xmlrpcserver: registering XML-RPC system methods");
-        $_xmlrpc_methods = xarModAPIFunc('xmlrpcsystemapi','user','getdmap');
-        $functions = array_merge($functions, $_xmlrpc_methods);
-    } else {
-        xarLogMessage("xmlrpcserver: XML-RPC system API not available, this is required");
-        return false;
+    // Loop over the potentially available api's
+    $apis = array('xmlrpcsystemapi', 'xmlrpcvalidatorapi', 'bloggerapi', 'metaweblogapi');
+    foreach($apis as $index => $api) {
+        if (xarModIsAvailable($api)) {
+            xarLogMessage("xmlrpcserver: registering XML-RPC methods for $api");
+            $_xmlrpc_methods = xarModAPIFunc($api, 'user', 'getdmap');
+            $functions = array_merge($functions, $_xmlrpc_methods);
+        } elseif($api == 'xmlrpcsystemapi') {
+            // System API should always be available
+            xarLogMessage("xmlrpcserver: XML-RPC system API not available, this is required");
+            return false;
+        }
     }
-
-    // Blogger API
-    if (xarModIsAvailable('bloggerapi')) {
-        xarLogMessage("xmlrpcserver: registering XML-RPC Blogger methods");
-        $_xmlrpc_methods = xarModAPIFunc('bloggerapi','user','getdmap');
-        $functions = array_merge($functions, $_xmlrpc_methods);
-    }
-    
-    // Validator1 API
-    if (xarModIsAvailable('xmlrpcvalidatorapi')) {
-        xarLogMessage('xmlrpcserver: registering XML-RPC validator methods');
-        $_xmlrpc_methods = xarModAPIFunc('xmlrpcvalidatorapi','user','getdmap');
-        $functions = array_merge($functions, $_xmlrpc_methods);
-    }
-
-    // MetaWebLog API
-    //include_once 'modules/xmlrpcserver/api/metaweblog.php';
-    // merge meta weblog functions
-    //$functions = array_merge($functions, $_xmlrpc_metaweblog_dmap);
     
     //create server instance
     $server = new xmlrpc_server($functions);
