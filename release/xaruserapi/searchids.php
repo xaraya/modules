9b3d95f3fd76690a1878ec523e86a273f07c3753
John.Cox <John.Cox@mcnabb.>
2004-10-16 20:04:10 +0000

    release/xartemplates/user-display.xd: 	#  fix for 3435 release/xaruserapi/searchids.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/export/modules/release/xaruserapi/searchids.php

---
+++ release/xaruserapi/searchids.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Searches all active comments based on a set criteria
+ *
+ * @author Carl P. Corliss (aka rabbitt)
+ * @access private
+ * @returns mixed description of return
+ */
+function release_userapi_searchids($args) 
+{
+    if (empty($args) || count($args) < 1) {
+        return;
+    }
+
+    $modid = '';
+
+    extract($args);
+    $dbconn =& xarDBGetConn();
+    $xartable =& xarDBGetTables();
+    $ctable = &$xartable['release_id'];
+    $where = '';
+    // initialize the commentlist array
+    $commentlist = array();
+    $sql = "SELECT  xar_rid,
+                    xar_uid,
+                    xar_displayname,
+                    xar_tstatus,
+                    xar_ttime
+              FROM  $ctable
+              WHERE  (";
+
+    $bindvars = array();
+
+    if (isset($rid)) {
+        $sql .= "xar_rid LIKE ?";
+        $bindvars[] = $rid;
+    }
+
+    if (isset($displname)) {
+        if (isset($rid)) {
+            $sql .= " OR ";
+        }
+        $sql .= "xar_tpost LIKE ?";
+        $bindvars[] = $displname;
+    }
+
+    if (isset($desc)) {
+        if (isset($rid) || isset($displayname)) {
+            $sql .= " OR ";
+        }
+        $sql .= " xar_tposter = ?";
+        $bindvars[] = $desc;
+    }
+
+    $sql .= ")  ORDER BY xar_ttime DESC";
+
+    $result =& $dbconn->Execute($sql, $bindvars);
+    if (!$result) return;
+
+    // if we have nothing to return
+    // we return nothing ;) duh? lol
+    if ($result->EOF) {
+        return array();
+    }
+
+    // zip through the list of results and
+    // add it to the array we will return
+    while (!$result->EOF) {
+        $row = $result->GetRowAssoc(false);
+        $row['xar_uid'] = xarUserGetVar('name',$row['xar_uid']);
+        $commentlist[] = $row;
+        $result->MoveNext();
+    }
+
+    $result->Close();
+    return $commentlist;
+}
+?>
