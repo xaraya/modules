53c8060ea22ea975848c9efaa44186fb85136d7b
John.Cox <John.Cox@mcnabb.>
2004-10-18 20:04:10 +0000

    xarbb/xartemplates/user-viewtopic.xd: 	#  remove xarsec call from template so it will cache. xarbb/xartemplates/user-quickreply.xd: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/export/html/modules/xarbb/xartemplates/user-quickreply.xd xarbb/xaruser/quickreply.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/export/html/modules/xarbb/xaruser/quickreply.php

---
+++ xarbb/xaruser/quickreply.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * File: $Id$
+ * 
+ * Quick Reply
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.org
+ *
+ * @subpackage  xarbb Module
+ * @author John Cox
+*/
+function xarbb_user_quickreply()
+{
+    if (!xarVarFetch('fid','int:1:',$data['fid'])) return;
+    if (!xarVarFetch('title', 'str', $data['ttitle'])) return;
+    if (!xarVarFetch('text', 'str', $data['text'])) return;
+    if (!xarVarFetch('tid','int:1:',$data['tid'])) return;
+
+
+    return $data;
+}
+?>
