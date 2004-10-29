8867ec7d071231087767d5a3b2ade364479f7436
marcel <marcel@hsdev.com>
2004-10-29 20:04:10 +0000

    paypalipn/xaradminapi/create.php: 	#  updates paypalipn/xardocs/license.txt: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/export/modules/paypalipn/xardocs/license.txt paypalipn/xaradminapi/create.php: 	#  xarExceptionSet -> xarErrorSet paypalipn/xaruserapi/get.php: 	#  xarExceptionSet -> xarErrorSet

--- paypalipn/xaruserapi/get.php
+++ paypalipn/xaruserapi/get.php
@@ -27,7 +27,7 @@ function paypalipn_userapi_get($args)
     // Argument check
     if (!isset($id) || !is_numeric($id)) {
         $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)', 'item ID', 'user', 'get', 'paypalipn');
-        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
+        xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
         return;
     } 
     // Get database setup
@@ -47,7 +47,7 @@ function paypalipn_userapi_get($args)
     if ($result->EOF) {
         $result->Close();
         $msg = xarML('This item does not exists');
-        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'ID_NOT_EXIST', new SystemException(__FILE__ . '(' . __LINE__ . '): ' . $msg));
+        xarErrorSet(XAR_SYSTEM_EXCEPTION, 'ID_NOT_EXIST', new SystemException(__FILE__ . '(' . __LINE__ . '): ' . $msg));
         return;
     } 
 
