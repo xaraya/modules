0648e76831527ba778657dd7a0577728c82c82c6
roger <roger@asphyxia.com>
2004-10-29 20:04:10 +0000

    workflow/xaradminapi/createhook.php: 	#  xarExceptionSet -> xarErrorSet

--- workflow/xaradminapi/createhook.php
+++ workflow/xaradminapi/createhook.php
@@ -16,7 +16,7 @@ function workflow_adminapi_createhook($args)
     if (!isset($objectid) || !is_numeric($objectid)) {
         $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
                     'object ID', 'admin', 'createhook', 'workflow');
-        xarExceptionSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
+        xarErrorSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
                        new SystemException($msg));
         return;
     }
@@ -37,7 +37,7 @@ function workflow_adminapi_createhook($args)
     if (empty($modid)) {
         $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
                     'module name', 'admin', 'createhook', 'workflow');
-        xarExceptionSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
+        xarErrorSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
                        new SystemException($msg));
         return;
     }
