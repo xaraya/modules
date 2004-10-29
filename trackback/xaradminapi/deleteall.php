004b523b4e989695d02784b08c954b1d078db0b8
John.Cox <John.Cox@mcnabb.>
2004-10-29 20:04:10 +0000

    trackback/xaradminapi/deleteall.php: 	#  xarvarprepforstore

--- trackback/xaradminapi/deleteall.php
+++ trackback/xaradminapi/deleteall.php
@@ -14,20 +14,16 @@ function trackback_adminapi_deleteall($args)
     // When called via hooks, we should get the real module name from objectid
     // here, because the current module is probably going to be 'modules' !!!
     if (!isset($objectid) || !is_string($objectid)) {
-        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
-                    'object ID (= module name)', 'admin', 'deleteall', 'Trackback');
-        xarExceptionSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
-                       new SystemException($msg));
-        return false;
+        $msg = xarML('Invalid Parameter Count');
+        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
+        return;
     }
 
     $modId = xarModGetIDFromName($objectid);
     if (empty($modId)) {
-        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
-                    'module ID', 'admin', 'deleteall', 'Trackback');
-        xarExceptionSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
-                       new SystemException($msg));
-        return false;
+        $msg = xarML('Invalid Parameter Count');
+        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
+        return;
     }
 
     // TODO: re-evaluate this for hook calls !!
@@ -43,8 +39,9 @@ function trackback_adminapi_deleteall($args)
     $trackBackTable = $tables['trackback'];
 
     $query = "DELETE FROM $trackBackTable
-            WHERE moduleid = '" . xarVarPrepForStore($modId) . "'";
-    $result =& $dbconn->Execute($query);
+            WHERE moduleid = ?";
+    $bindvars = array($modId);
+    $result =& $dbconn->Execute($query,$bindvars);
     if (!$result) return;
 
     // hmmm, I think we'll skip calling more hooks here... :-)
