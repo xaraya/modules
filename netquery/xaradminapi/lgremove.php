4cd6b3f312207c107986fd351aeac5e9f8d52e35
jojodee <jojodee@ahaadev.net>
2004-11-01 20:04:11 +0000

    netquery/xaradminapi/lgremove.php: 	#  QA, typo

--- netquery/xaradminapi/lgremove.php
+++ netquery/xaradminapi/lgremove.php
@@ -22,7 +22,7 @@ function netquery_adminapi_lgremove($args)
     $LGRouterTable = $xartable['netquery_lgrouter'];
     $query = "DELETE FROM $LGRouterTable
             WHERE router_id = ?";
-    $result =& $dbconn->Execute($query, (array)$router_id);
+    $result =& $dbconn->Execute($query, array((int)$router_id));
     if (!$result) return;
     return true;
 }
