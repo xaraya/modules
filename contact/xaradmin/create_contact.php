020cdbb37396a09b392392e763bf0548b613b996
jojodee <jojodee@ahaadev.net>
2004-11-01 20:04:11 +0000

    contact/xaradmin/create_contact.php: 	#  Bindvars, qa contact/xaradminapi/addcitycompany.php: 	#  Bindvars, qa contact/xaradminapi/create.php: 	#  Bindvars, qa contact/xaradminapi/create_city.php: 	#  Bindvars, qa contact/xaradminapi/create_contact.php: 	#  Bindvars, qa contact/xaradminapi/create_contact_attributes.php: 	#  Bindvars, qa contact/xaradminapi/create_contact_departments.php: 	#  Bindvars, qa contact/xaradminapi/create_department.php: 	#  Bindvars, qa contact/xaradminapi/create_location.php: 	#  Bindvars, qa contact/xaradminapi/create_titles.php: 	#  Bindvars, qa contact/xaradminapi/delete.php: 	#  Bindvars, qa contact/xaradminapi/update.php: 	#  Bindvars, qa contact/xaradminapi/update_city.php: 	#  Bindvars, qa contact/xaradminapi/update_contact.php: 	#  Bindvars, qa contact/xaradminapi/update_department.php: 	#  Bindvars, qa contact/xaradminapi/update_location.php: 	#  Bindvars, qa contact/xaradminapi/update_titles.php: 	#  Bindvars, qa contact/xaruserapi/get.php: 	#  Bindvars, qa contact/xaruserapi/getall.php: 	#  Bindvars, qa contact/xaradmin/create_contact.php: 	#  QA xarVarCleanFromInput deprecated

--- contact/xaradmin/create_contact.php
+++ contact/xaradmin/create_contact.php
@@ -7,121 +7,64 @@ function contact_admin_create_contact($args)
     // function should be obtained from xarVarCleanFromInput(), getting them
     // from other places such as the environment is not allowed, as that makes
     // assumptions that will not hold in future versions of Xaraya
-    list($id,
-         $department,
-         $title,
-         $firstname,
-         $lastname,
-         $address,
-         $address2,
-         $city,
-         $state,
-         $zip,
-         $country,
-         $mail,
-         $phone,
-         $fax,
-         $mobile,
-         $pager,
-         $typephone,
-         $typefax,
-         $typemobile,
-         $typepager,
-         $active,
-         $ICQ,
-         $AIM,
-         $YIM,
-         $MSNM,
-         $titleID,
-         $image,
-         $hide,
-         $udfirstname,
-         $udlastname,
-         $udaddress,
-         $udaddress2,
-         $udcity,
-         $udstate,
-         $udzip,
-         $udcountry,
-         $udmail,
-         $udphone,
-         $udfax,
-         $udmobile,
-         $udpager,
-         $udtypephone,
-         $udtypefax,
-         $udtypemobile,
-         $udtypepager,
-         $udactive,
-         $udICQ,
-         $udAIM,
-         $udYIM,
-         $udMSNM,
-         $udtitleID,
-         $udimage,
-         $udhide,
-         $addnewcontact,
-         $adddepartment,
-         $newtitles,
-         $newcity,
-         $lastimage) = xarVarCleanFromInput('id',
-                                         'department',
-                                         'title',
-                                         'firstname',
-                                         'lastname',
-                                         'address',
-                                         'address2',
-                                         'city',
-                                         'state',
-                                         'zip',
-                                         'country',
-                                         'mail',
-                                         'phone',
-                                         'fax',
-                                         'mobile',
-                                         'pager',
-                                         'typephone',
-                                         'typefax',
-                                         'typemobile',
-                                         'typepager',
-                                         'active',
-                                         'ICQ',
-                                         'AIM',
-                                         'YIM',
-                                         'MSNM',
-                                         'titleID',
-                                         'image',
-                                         'hide',
-                                         'addfirstname',
-                                         'addlastname',
-                                         'addaddress',
-                                         'addaddress2',
-                                         'addcity',
-                                         'addstate',
-                                         'addzip',
-                                         'addcountry',
-                                         'addmail',
-                                         'addphone',
-                                         'addfax',
-                                         'addmobile',
-                                         'addpager',
-                                         'addtypephone',
-                                         'addtypefax',
-                                         'addtypemobile',
-                                         'addtypepager',
-                                         'addactive',
-                                         'addICQ',
-                                         'addAIM',
-                                         'addYIM',
-                                         'addMSNM',
-                                         'addtitleID',
-                                         'addimage',
-                                         'addhide',
-                                         'addnewcontact',
-                                         'adddepartment',
-                                         'newtitles',
-                                         'newcity',
-                                         'lastimage');
+     if (!xarVarFetch('id', 'isset', $id, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('department', 'str:1:', $department, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('title', 'str:1:', $title, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('firstname', 'str:1:', $firstname, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('lastname', 'str:1:', $lastname, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('address', 'str:1:', $address, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('address2', 'str:1:', $address2, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('city', 'str:1:', $city, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('state', 'str:1:', $state, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('zip', 'str:1:', $zip, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('country', 'str:1:', $country, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('mail', 'str:1:', $mail, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('phone', 'str:1:', $phone, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('fax', 'str:1:', $fax, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('mobile', 'str:1:', $mobile, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('pager', 'str:1:', $pager, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('typephone', 'str:1:', $typephone, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('typefax', 'str:1:', $typefax, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('typemobile', 'str:1:', $typemobile, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('typepager', 'str:1:', $typepager, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('active', 'isset', $active, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('ICQ', 'str:1:', $ICQ, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('AIM', 'str:1:', $AIM, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('YIM', 'str:1:', $YIM, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('MSNM', 'str:1:', $MSNM, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('titleID', 'str:1:', $titleID, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('image', 'str:1:', $image, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('hide', 'str:1:', $hide, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udfirstname', 'str:1:', $addfirstname, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udlastname', 'str:1:', $addlastname, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udaddress', 'str:1:', $addaddress, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udaddress2', 'str:1:', $addaddress2, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udcity', 'str:1:', $addcity, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udstate', 'str:1:', $addstate, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udzip', 'str:1:', $addzip, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udcountry', 'str:1:', $addcountry, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udmail', 'str:1:', $addmail, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udphone', 'str:1:', $addphone, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udfax', 'str:1:', $addfax, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udmobile', 'str:1:', $addmobile, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udpager', 'str:1:', $addpager, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udtypephone', 'str:1:', $addtypephone, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udtypefax', 'str:1:', $addtypefax, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udtypemobile', 'str:1:', $addtypemobile, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udtypepager', 'str:1:', $addtypepager, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udactive', 'str:1:', $addactive, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udICQ', 'str:1:', $addICQ, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udAIM', 'str:1:', $addAIM, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udYIM', 'str:1:', $addYIM, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udMSNM', 'str:1:', $addMSNM, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udtitleID', 'str:1:', $addtitleID, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udimage', 'str:1:', $addimage, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('udhide', 'isset', $addhide, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('addnewcontact', 'isset', $addnewcontact, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('adddepartment', 'isset', $adddepartment, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('newtitles', 'isset', $newtitles, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('newcity', 'isset', $newcity, '', XARVAR_NOT_REQUIRED)) return;
+     if (!xarVarFetch('lastimage', 'isset', $lastimage, '', XARVAR_NOT_REQUIRED)) return;
 
     // Admin functions of this type can be called by other modules.  If this
     // happens then the calling module will be able to pass in arguments to
