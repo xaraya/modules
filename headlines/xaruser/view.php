79f2d447216b00edf0af0678f0a1d5bade3bc202
John.Cox <John.Cox@mcnabb.>
2004-11-02 20:04:11 +0000

    headlines/xaruser/view.php: 	#  rethinking some things in the headlines...

--- headlines/xaruser/view.php
+++ headlines/xaruser/view.php
@@ -4,13 +4,6 @@ function headlines_user_view()
     // Security Check
     if(!xarSecurityCheck('ReadHeadlines')) return;
     xarVarFetch('hid', 'id', $hid, XARVAR_PREP_FOR_DISPLAY);
-
-    // Require the xmlParser class
-    require_once('modules/base/xarclass/xmlParser.php');
-
-    // Require the feedParser class
-    require_once('modules/base/xarclass/feedParser.php');
-
     // The user API function is called
     $links = xarModAPIFunc('headlines',
                           'user',
@@ -22,83 +15,21 @@ function headlines_user_view()
     } else {
         $data['catid'] = '';
     }
-    $data['hid'] = $hid;
-
     // Check and see if a feed has been supplied to us.
     if(isset($links['url'])) {
         $feedfile = $links['url'];
     } else {
         $feedfile = "";
     }
-    // Sanitize the URL provided to us since
-    // some people can be very mean.
-    $feedfile = preg_replace("/\.\./","donthackthis",$feedfile);
-    $feedfile = preg_replace("/^\//","ummmmno",$feedfile);
+    // The user API function is called
+    $data = xarModAPIFunc('headlines',
+                          'user',
+                          'process',
+                          array('feedfile' => $feedfile));
 
-    // Get the feed file (from cache or from the remote site)
-    $feeddata = xarModAPIFunc('base', 'user', 'getfile',
-                              array('url' => $feedfile,
-                                    'cached' => true,
-                                    'cachedir' => 'cache/rss',
-                                    'refresh' => 3600,
-                                    'extension' => '.xml'));
-    if (!$feeddata) {
-        return; // throw back
-    }
+    xarTplSetPageTitle(xarVarPrepForDisplay($data['chantitle']));
 
-    // Create a need feedParser object
-    $p = new feedParser();
-
-    // Tell feedParser to parse the data
-    $info = $p->parseFeed($feeddata);
-
-    if (empty($info['warning'])){
-        foreach ($info as $content){
-             foreach ($content as $newline){
-                    if(is_array($newline)) {
-                        if (isset($newline['description'])){
-                            $description = $newline['description'];
-                        } else {
-                            $description = '';
-                        }
-                        if (isset($newline['title'])){
-                            $title = $newline['title'];
-                        } else {
-                            $title = '';
-                        }
-                        if (isset($newline['link'])){
-                            $link = $newline['link'];
-                        } else {
-                            $link = '';
-                        }
-
-                        $feedcontent[] = array('title' => $title, 'link' => $link, 'description' => $description);
-                }
-            }
-        }
-
-        if (!empty($links['title'])){
-            $data['chantitle'] = $links['title'];
-        } else {
-            $data['chantitle']  =   $info['channel']['title'];
-        }
-        if (!empty($links['desc'])){
-            $data['chandesc'] = $links['desc'];
-        } else {
-            $data['chandesc']   =   $info['channel']['description'];
-        }
-        $data['chanlink']   =   $info['channel']['link'];
-
-        xarTplSetPageTitle(xarVarPrepForDisplay($data['chantitle']));
-
-    } else {
-        $msg = xarML('There is a problem with a feed.');
-        xarErrorSet(XAR_USER_EXCEPTION, 'MISSING_DATA', new DefaultUserException($msg));
-        return;
-    }
-
-    $data['feedcontent'] = $feedcontent;
-
+    $data['hid'] = $hid;
     $data['module'] = 'headlines';
     $data['itemtype'] = 0;
     $data['itemid'] = $hid;
@@ -114,9 +45,7 @@ function headlines_user_view()
     } else {
         $data['hooks'] = $hooks;
     }
-
     $data['authid'] = xarSecGenAuthKey();
-
     return $data;
 }
 ?>
