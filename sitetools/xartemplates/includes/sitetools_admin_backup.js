a05ef12a920e618dbf8e4b1b6ead26c23fd2abd4
marcel <marcel@hsdev.com>
2004-11-04 20:04:11 +0000

    BitKeeper/triggers/post-incoming.notice: 	#  tabs2spaces sitetools/xartemplates/includes/sitetools_admin_backup.js: 	#  tabs2spaces

--- sitetools/xartemplates/includes/sitetools_admin_backup.js
+++ sitetools/xartemplates/includes/sitetools_admin_backup.js
@@ -2,5 +2,5 @@ function CheckAll(checkornot)
 {
 for (var i = 0; i < document.SelectedTablesForm.elements.length; i++) {
   document.SelectedTablesForm.elements[i].checked = checkornot; 
-}                                                                     	
+}                                                                         
 }
