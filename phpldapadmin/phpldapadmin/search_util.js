820571d690e4904bc77d17b64a220f9bdada9d38
marcel <marcel@hsdev.com>
2004-11-04 20:04:11 +0000

    BitKeeper/triggers/post-incoming.notice: 	#  tabs2spaces phpldapadmin/phpldapadmin/LICENSE: 	#  tabs2spaces phpldapadmin/phpldapadmin/config.php.example: 	#  tabs2spaces phpldapadmin/phpldapadmin/doc/CREDITS: 	#  tabs2spaces phpldapadmin/phpldapadmin/doc/ChangeLog: 	#  tabs2spaces phpldapadmin/phpldapadmin/doc/INSTALL-es.txt: 	#  tabs2spaces phpldapadmin/phpldapadmin/doc/INSTALL-fr.txt: 	#  tabs2spaces phpldapadmin/phpldapadmin/doc/ROADMAP: 	#  tabs2spaces phpldapadmin/phpldapadmin/entry_chooser.js: 	#  tabs2spaces phpldapadmin/phpldapadmin/lang/Makefile: 	#  tabs2spaces phpldapadmin/phpldapadmin/ldap_error_codes.txt: 	#  tabs2spaces phpldapadmin/phpldapadmin/search_util.js: 	#  tabs2spaces phpldapadmin/phpldapadmin/style.css: 	#  tabs2spaces phpldapadmin/xardocs/license.txt: 	#  tabs2spaces

--- phpldapadmin/phpldapadmin/search_util.js
+++ phpldapadmin/phpldapadmin/search_util.js
@@ -10,7 +10,7 @@ var servers = new Array();
 
 
 //---------------------------------------------------------------------
-//		Definition of the object server			      	
+//        Definition of the object server                      
 //---------------------------------------------------------------------
 
 //constructor of the server
@@ -20,57 +20,57 @@ var servers = new Array();
 
 function server(id,name,base_dn){
 
-	//the properties of the object
-	this.id =id;
-	this.name = name;
-	this.base_dn = base_dn;
+    //the properties of the object
+    this.id =id;
+    this.name = name;
+    this.base_dn = base_dn;
 
-	// the method of the server
-	this.getId=getId;
-	this.setId=setId;
-	this.getName = getName;
-	this.setName = setName;
-	this.setBaseDn = setBaseDn;
-	this.getBaseDn = getBaseDn;
+    // the method of the server
+    this.getId=getId;
+    this.setId=setId;
+    this.getName = getName;
+    this.setName = setName;
+    this.setBaseDn = setBaseDn;
+    this.getBaseDn = getBaseDn;
 }
 // set the id of the server
 function setId(id){
-	this.id = id;
-}	
-	
+    this.id = id;
+}    
+    
 //return the id of the server
 function getId(){
-	return this.id;
+    return this.id;
 }
 
 // set the name of the server
 function setName(name){
-	this.name = name;
-}	
+    this.name = name;
+}    
 
-// return the name of the server	
+// return the name of the server    
 function getName(){
-	return this.name;
+    return this.name;
 }
 
 // return the base dn of the server
 function getBaseDn(){
-	return this.base_dn;
-}	
+    return this.base_dn;
+}    
 
 // set the base dn of the server
 function setBaseDn(base_dn){
-	this.base_dn = base_dn;
+    this.base_dn = base_dn;
 }
 
 //-----------------------------------------------------------------------
-//			End of the definition of the server
+//            End of the definition of the server
 //-----------------------------------------------------------------------
 
 
 // add a server object to the array of server
 function addToServersList(obj_server){
-	servers[servers.length] = obj_server;
+    servers[servers.length] = obj_server;
 }
 
 
