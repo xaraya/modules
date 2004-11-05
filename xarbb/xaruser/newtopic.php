1304890c835e6d1757dbd6c6572eb27c7e651aa7
voll <voll@yahoo.com>
2004-11-05 20:04:11 +0000

    xarbb/xaruser/newtopic.php: 	#  adjust input with xarvarfetch

--- xarbb/xaruser/newtopic.php
+++ xarbb/xaruser/newtopic.php
@@ -18,7 +18,7 @@
 function xarbb_user_newtopic()
 {
     if (!xarVarFetch('phase', 'str:1:10', $phase, 'form', XARVAR_NOT_REQUIRED, XARVAR_PREP_FOR_DISPLAY)) return;
-    if (!xarVarFetch('ttitle', 'str:1:100', $ttitle, '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('ttitle', 'str:1:120', $ttitle, '', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('tpost', 'str', $tpost, '', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('tstatus', 'int', $tstatus, '', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('fid', 'id', $fid, NULL, XARVAR_DONT_SET)) return;
