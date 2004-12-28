e69481f0fcc7099b8bb312f96765a82d9e433880
mikespub <mikespub@fully.qualified.hostname>
2004-12-28 20:04:12 +0000

    newsgroups/xartemplates/user-main.xd: 	#  bug 2553 - handle error messages newsgroups/xaruser/main.php: 	#  bug 2553 - handle error messages

--- newsgroups/xaruser/main.php
+++ newsgroups/xaruser/main.php
@@ -13,21 +13,35 @@ function newsgroups_user_main()
     $port       = xarModGetVar('newsgroups', 'port');
     $wildmat    = xarModGetVar('newsgroups', 'wildmat');
 
+    $data['server'] = "news://$server";
     xarTplSetPageTitle(xarVarPrepForDisplay($server));
 
     $newsgroups = new Net_NNTP();
-    $newsgroups -> connect($server, $port);
+    $rs = $newsgroups -> connect($server, $port);
+    if (PEAR::isError($rs)) {
+        $data['error_message'] = $rs->message;
+        $newsgroups->quit();
+        return $data;
+    }
 
 // TODO: pre-load complete list of newsgroups and let admin select
 //       instead of retrieving the list each time here
 
     if (empty($wildmat) || !strstr($wildmat,',')) {
         $data['items'] = $newsgroups->getGroups(true, $wildmat);
+        if (PEAR::isError($data['items'])) {
+            $data['error_message'] = $data['items']->message;
+            $data['items'] = array();
+        }
     } else {
         $matches = explode(',',$wildmat);
         $data['items'] = array();
         foreach ($matches as $match) {
             $items = $newsgroups->getGroups(true, $match);
+            if (PEAR::isError($items)) {
+                $data['error_message'] = $items->message;
+                break;
+            }
             $data['items'] = array_merge($data['items'], $items);
         }
     }
@@ -36,7 +50,6 @@ function newsgroups_user_main()
     // Debug
     //var_dump($data['items']);
 
-    $data['server'] = "news://$server";
     // Return the template variables defined in this function
     return $data;
 }
