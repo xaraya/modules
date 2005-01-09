bb013ffb79d725ba02428daf4d661781508596db
mikespub <mikespub@fully.qualified.hostname>
2005-01-09 20:05:01 +0000

    autolinks/xaruserapi/transform.php: 	#  bug 3094 - invalid $invalid

--- autolinks/xaruserapi/transform.php
+++ autolinks/xaruserapi/transform.php
@@ -14,8 +14,7 @@ function autolinks_userapi_transform($args)
 
     // Argument check
     if (!isset($extrainfo)) {
-        $msg = xarML('Invalid Parameter Count',
-                    join(', ', $invalid), 'userapi', 'transform', 'Autolinks');
+        $msg = xarML('Invalid Parameter Count');
         xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
                        new SystemException($msg));
         return;
