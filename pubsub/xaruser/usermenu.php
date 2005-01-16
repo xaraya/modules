bcee001fac5569652c8e71bffc0b8eec7903c0d9
mikespub <mikespub@fully.qualified.hostname>
2005-01-16 20:05:01 +0000

    pubsub/xaruser/usermenu.php: 	#  bug 3492 - pubsub usermenu fixes

--- pubsub/xaruser/usermenu.php
+++ pubsub/xaruser/usermenu.php
@@ -3,18 +3,52 @@
 function pubsub_user_usermenu($args)
 {
     extract($args);
+    // old usermenu approach
     xarVarFetch('action','str:1:',$action,'menu',XARVAR_NOT_REQUIRED);
+
+    // set by user-account template in roles
+    xarVarFetch('phase','notempty', $phase, '', XARVAR_NOT_REQUIRED);
+    if (!empty($phase)) {
+        $action = $phase;
+    }
 
     switch($action) {
         case 'menu':
             return xarTplModule('pubsub','user','usermenu');
             break;
 
+        case 'form':
         case 'list':
             xarTplSetPageTitle(xarVarPrepForDisplay(xarML('Your Subscriptions')));
             $items = xarModAPIFunc('pubsub','user','getsubscriptions',
                                    array('userid' => xarUserGetVar('uid')));
             if (!isset($items)) return;
+            // get the itemtype descriptions if available
+            $todo = array();
+            foreach ($items as $id => $item) {
+                if (!empty($item['itemtype'])) {
+                    $todo[$item['modname']] = 1;
+                }
+            }
+            if (count($todo) > 0) {
+                $itemtypes = array();
+                foreach ($todo as $modname => $val) {
+                    // Get the list of all item types for this module (if any)
+                    $mytypes = xarModAPIFunc($modname,'user','getitemtypes',
+                                             // don't throw an exception if this function doesn't exist
+                                             array(), 0);
+                    if (!empty($mytypes)) {
+                        // save the label, title and url for each itemtype
+                        $itemtypes[$modname] = $mytypes;
+                    }
+                }
+                foreach ($items as $id => $item) {
+                    if (!empty($item['itemtype']) && !empty($itemtypes[$item['modname']]) && !empty($itemtypes[$item['modname']][$item['itemtype']])) {
+                        // set the itemtype to its corresponding label for display
+                        $items[$id]['itemtype'] = $itemtypes[$item['modname']][$item['itemtype']]['label'];
+                    }
+                }
+            }
             return xarTplModule('pubsub','user','usermenu',
                                 array('action' => 'list',
                                       'items' => $items));
