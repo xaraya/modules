0507811d46985433f31769eb0f412b029229bf4b
John.Cox <John.Cox@mcnabb.>
2005-03-20 20:05:03 +0000

    xaruserapi/image.php: 	#  bug 3844 xaruserapi/url.php: 	#  bug 3844 xartemplates/user-image.xd: 	#  BitKeeper file C:/cygwin/home/dev/www/xaraya/stable/html/modules/bbcode/xartemplates/user-image.xd

--- xaruserapi/url.php
+++ xaruserapi/url.php
@@ -6,19 +6,19 @@ function bbcode_userapi_url($args)
 
     // [url]xxxx://www.phpbb.com[/url] code..
     $patterns[0] = "#\[url\]([a-z]+?://){1}(.*?)\[/url\]#si";
-    $replacements[0] = xarTplModule('bbcode','user', 'underline', array('replace1' => '\\1', 'replace2' => '\\2'));
+    $replacements[0] = xarTplModule('bbcode','user', 'url', array('replace1' => '\\1', 'replace2' => '\\2'));
 
     // [url]www.phpbb.com[/url] code.. (no xxxx:// prefix).
     $patterns[1] = "#\[url\](.*?)\[/url\]#si";
-    $replacements[1] = xarTplModule('bbcode','user', 'underline', array('replace1' => '\\1'));
+    $replacements[1] = xarTplModule('bbcode','user', 'url', array('replace1' => '\\1'));
 
     // [url=xxxx://www.phpbb.com]phpBB[/url] code..
     $patterns[2] = "#\[url=([a-z]+?://){1}(.*?)\](.*?)\[/url\]#si";
-    $replacements[2] = xarTplModule('bbcode','user', 'underline', array('replace1' => '\\1', 'replace2' => '\\2', 'replace3' => '\\3'));
+    $replacements[2] = xarTplModule('bbcode','user', 'url', array('replace1' => '\\1', 'replace2' => '\\2', 'replace3' => '\\3'));
 
     // [url=www.phpbb.com]phpBB[/url] code.. (no xxxx:// prefix).
     $patterns[3] = "#\[url=(.*?)\](.*?)\[/url\]#si";
-    $replacements[3] = xarTplModule('bbcode','user', 'underline', array('replace1' => '\\1', 'replace2' => '\\2'));
+    $replacements[3] = xarTplModule('bbcode','user', 'url', array('replace1' => '\\1', 'replace2' => '\\2'));
 
     $message = preg_replace($patterns, $replacements, $message);
 
