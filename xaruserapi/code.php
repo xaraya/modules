5151e1e164c16513ee8baae4a4c23978898e1d6f
John.Cox <John.Cox@mcnabb.>
2005-03-20 20:05:03 +0000

    xaruserapi/code.php: 	#  bug 3960

--- xaruserapi/code.php
+++ xaruserapi/code.php
@@ -2,76 +2,131 @@
 function bbcode_userapi_code($args)
 {
     extract($args);
-    $tags_found = array();
-    //Create an associative array, including the character
-    //offset of each end/start tag in the message.
-    for($j = 0; ($j = strpos($message, '[', $j)); $j++) {
-        if(strcasecmp(substr($message, $j, 7), '[code]') == 0)
-            $tags_found[$j] = 's';
-        elseif(strcasecmp(substr($message, $j, 8), '[/code]') == 0)
-            $tags_found[$j] = 'e';
+    $is_html_disabled = false;
+    // First things first: If there aren't any "[code]" strings in the message, we don't
+    // need to process it at all.
+    if (!strpos(strtolower($message), "[code]"))
+    {
+        return $message;    
     }
-
-      /*
-      This will be faster, but no stripos() until PHP 5.0 :-( 
-      for($j = 0; ($j = stripos($message, '[code]', $j)); $j++) 
-        $tags_found[$j] = 's';
-      for($j = 0; ($j = stripos($message, '[/code]', $j)); $j++) 
-        $tags_found[$j] = 'e';
-      ksort($tags_found);
-      */
-  
-    //If no tags found, return.
-    if(empty($tags_found)) {
-        return $message;
-    }
-
-    $stack = array();
-    $is_well_formed = TRUE;
-    foreach($tags_found as $k => $v) {
-        //If we have a start tag, hold on to it in the stack
-        if($v == 's') {
-            array_push($stack, $k);
-        }
-        //If we have an end tag
-        else{
-        //If we have a pending start tag, we have a matach
-            if(!empty($stack)) {
-                array_pop($stack);
+    
+    // Second things second: we have to watch out for stuff like [1code] or [/code1] in the 
+    // input.. So escape them to [#1code] or [/code#1] for now:
+    $message = preg_replace("/\[([0-9]+?)code\]/si", "[#\\1code]", $message);
+    $message = preg_replace("/\[\/code([0-9]+?)\]/si", "[/code#\\1]", $message);
+    
+    $stack = Array();
+    $curr_pos = 1;
+    $max_nesting_depth = 0;
+    while ($curr_pos && ($curr_pos < strlen($message)))
+    {    
+        $curr_pos = strpos($message, "[", $curr_pos);
+    
+        // If not found, $curr_pos will be 0, and the loop will end.
+        if ($curr_pos)
+        {
+            // We found a [. It starts at $curr_pos.
+            // check if it's a starting or ending code tag.
+            $possible_start = substr($message, $curr_pos, 6);
+            $possible_end = substr($message, $curr_pos, 7);
+            if (strcasecmp("[code]", $possible_start) == 0)
+            {
+                // We have a starting code tag.
+                // Push its position on to the stack, and then keep going to the right.
+                array_push($stack, $curr_pos);
+                ++$curr_pos;
             }
-            //If we don't have a pending start tag, mark string
-            //as malformed and remove extranneous end tag from our list.
-            else {
-                $is_well_formed = FALSE;
-                unset($tags_found[$k]); //This is safe because 'foreach' operates on a copy
+            else if (strcasecmp("[/code]", $possible_end) == 0)
+            {
+                // We have an ending code tag.
+                // Check if we've already found a matching starting tag.
+                if (sizeof($stack) > 0)
+                {
+                    // There exists a starting tag. 
+                    $curr_nesting_depth = sizeof($stack);
+                    $max_nesting_depth = ($curr_nesting_depth > $max_nesting_depth) ? $curr_nesting_depth : $max_nesting_depth;
+                    
+                    // We need to do 2 replacements now.
+                    $start_index = array_pop($stack);
+
+                    // everything before the [code] tag.
+                    $before_start_tag = substr($message, 0, $start_index);
+
+                    // everything after the [code] tag, but before the [/code] tag.
+                    $between_tags = substr($message, $start_index + 6, $curr_pos - $start_index - 6);
+
+                    // everything after the [/code] tag.
+                    $after_end_tag = substr($message, $curr_pos + 7);
+
+                    $message = $before_start_tag . "[" . $curr_nesting_depth . "code]";
+                    $message .= $between_tags . "[/code" . $curr_nesting_depth . "]";
+                    $message .= $after_end_tag;
+                    
+                    // Now.. we've screwed up the indices by changing the length of the string. 
+                    // So, if there's anything in the stack, we want to resume searching just after it.
+                    // otherwise, we go back to the start.
+                    if (sizeof($stack) > 0)
+                    {
+                        $curr_pos = array_pop($stack);
+                        array_push($stack, $curr_pos);
+                        ++$curr_pos;
+                    }
+                    else
+                    {
+                        $curr_pos = 1;
+                    }
+                }
+                else
+                {
+                    // No matching start tag found. Increment pos, keep going.
+                    ++$curr_pos;    
+                }
+            }
+            else
+            {
+                // No starting tag or ending tag.. Increment pos, keep looping.,
+                ++$curr_pos;    
+            }
+        }
+    } // while
+    
+    if ($max_nesting_depth > 0)
+    {
+        for ($i = 1; $i <= $max_nesting_depth; ++$i)
+        {
+            $start_tag = escape_slashes(preg_quote("[" . $i . "code]"));
+            $end_tag = escape_slashes(preg_quote("[/code" . $i . "]"));
+            
+            $match_count = preg_match_all("/$start_tag(.*?)$end_tag/si", $message, $matches);
+    
+            for ($j = 0; $j < $match_count; $j++)
+            {
+                $before_replace = escape_slashes(preg_quote($matches[1][$j]));
+                $after_replace = $matches[1][$j];
+                
+                if (($i < 2) && !$is_html_disabled)
+                {
+                    // don't escape special chars when we're nested, 'cause it was already done
+                    // at the lower level..
+                    // also, don't escape them if HTML is disabled in this post. it'll already be done
+                    // by the posting routines.
+                    $after_replace = htmlspecialchars($after_replace);    
+                }
+                
+                $str_to_match = $start_tag . $before_replace . $end_tag;
+
+                if (phpversion() > "4.2.0"){
+                    highlight_string($after_replace, TRUE);
+                }
+                
+                $message = preg_replace("/$str_to_match/si", xarML('Code') . ": <div style='width: 90%; overflow: auto;'><pre> " . bbcode_br2nl($after_replace) . "</pre></div>", $message);
             }
         }
     }
-  
-    //Fast Path: is we know our string is well formed, then we can do a batch replace
-    if($is_well_formed && empty($stack)) {
-        //No str_ireplace until PHP 5.0 :-(
-        $message = preg_replace('/\[code\]/i', '<p>' . xarML('code') . ':</p><blockquote><div style="width: 90%; overflow: auto;">', $message);
-        $message = preg_replace('/\[\/code\]/i', '</div></blockquote>', $message);
-        return $message;
-    }
-
-    //Get rid of extra start tags, if any
-    foreach($stack as $v) {
-        unset($tags_found[$v]);
-    }
-
-    //Now rebuild the string using $tags_found
-    $new_offset = 0;
-    foreach($tags_found as $k => $v) {
-        if($v == 's') {
-            $message = & substr_replace($message, '<p>' . xarML('code') . ':</p><blockquote><div style="width: 90%; overflow: auto;">', $k + $new_offset, 7);
-            $new_offset += 11;
-        } else {
-            $message = & substr_replace($message, '</div></blockquote>', $k + $new_offset, 8);
-            $new_offset += 5;
-        }
-    }
+    
+    // Undo our escaping from "second things second" above..
+    $message = preg_replace("/\[#([0-9]+?)code\]/si", "[\\1code]", $message);
+    $message = preg_replace("/\[\/code#([0-9]+?)\]/si", "[/code\\1]", $message);
     return $message;
 }
 ?>
