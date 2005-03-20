b33e431ad557a0062fa327662d14aa2c5715cfae
John.Cox <John.Cox@mcnabb.>
2005-03-20 20:05:03 +0000

    xaruserapi/image.php: 	#  bug 4064 fix.  credit jaboonday <jaboonday@gmail.com>

--- xaruserapi/image.php
+++ xaruserapi/image.php
@@ -6,7 +6,7 @@ function bbcode_userapi_image($args)
     extract($args);
 
     // This is the part that you add for new bbcode.  Basically, search for the correct tag, then load the template for the tag, then replace what is found.
-    $message = preg_replace("#\[img\](http://)?(.*?)\[/img\]#si", xarTplModule('bbcode','user', 'image', array('replace' => '\\2')), $message);
+    $message = preg_replace("#\[img\](http://)?(.*?)\[/img\]#si", xarTplModule('bbcode','user', 'image', array('replace' => '\\1\\2')), $message);
 
     // Always have to return the message.
     return $message;
