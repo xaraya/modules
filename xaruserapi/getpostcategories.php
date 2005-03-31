0428739dc4a5fb20b9d994cac5785f2c1d5d6b66
marcel <marcel@hsdev.com>
2005-03-31 20:05:03 +0000

    xartemplates/moveabletype-getcategorylist.xd: 	#  - reuse this template for the getPostCategories method xaruserapi/getpostcategories.php: 	#  BitKeeper file /var/bk/xar/core/marcel/html/modules/moveabletype/xaruserapi/getpostcategories.php xaruserapi/setpostcategories.php: 	#  BitKeeper file /var/bk/xar/core/marcel/html/modules/moveabletype/xaruserapi/setpostcategories.php

---
+++ xaruserapi/getpostcategories.php
@@ -0,0 +1,59 @@
+<?php
+
+function moveabletype_userapi_getpostcategories($args)
+{
+    xarLogMessage('Moveabletype api: getpostcategories', XARLOG_LEVEL_WARNING);
+    extract($args);
+    
+    // get the params
+    $sn1=$msg->getParam(0);  $postid        = $sn1->scalarval(); // NOTE: NOT blogid
+    $sn2=$msg->getParam(1);  $username      = $sn2->scalarval();
+    $sn3=$msg->getParam(2);  $password      = $sn3->scalarval();
+
+    // Try to login 
+    $err='';
+    $elements = array();
+    if (!xarUserLogin($username,$password)) {
+        $err = xarML("Invalid username or password for (#(1)) while getting recent posts",$username);
+    } else {
+        $pubtype=xarModGetVar('bloggerapi','bloggerpubtype');
+        // Get the categories for the item.
+        if(xarModIsAvailable('categories')) {
+            $itemCats = xarModAPIFunc('categories','user','getitemcats',array('modid' => 151, 'itemtype' => $pubtype, 'itemid' => $postid));
+        } else {
+            $err = xarML("The categories module is not available, this is required");
+        }
+        $itemCatKeys = array_keys($itemCats);
+        // First get the root categories of the publication type (as in: all the blogs)
+        $rootCats = array();
+        $rootCats = xarModGetVar('articles','mastercids.'.$pubtype);
+        if (!empty($rootCats)) $rootCats = explode(';',$rootCats);
+
+        $blog = array_intersect($rootCats, $itemCatKeys);
+        $blog = $blog[0];
+        xarLogMessage("MT: BLOG CAT: " . $blog);
+        if(!empty($itemCats)) {
+            // Construct an array of structs to return
+            foreach($itemCats as $index => $category) {
+                if($index == $blog) continue;
+                $elements[$index]['isPrimary']  = 0;
+                $elements[$index]['categoryId'] = $category['cid'];
+                $elements[$index]['categoryName'] = $category['name'];
+            }
+        }
+    }
+    // Simple debugging can be done by assigning a value to $err at this point
+    if (!empty($err)) {
+        $out = xarModAPIFunc('xmlrpcserver','user','faultresponse',array('errorstring' => $err));
+    }    else {
+        // Return value of the method is an array
+        $data['categories'] = $elements;
+        $out = xarModAPIFunc('xmlrpcserver','user','createresponse',
+                             array('module'  => 'moveabletype',
+                                   'command' => 'getcategorylist',
+                                   'params'  => $data)
+                             );
+    }
+    return $out;
+}
+?>
---
+++ xaruserapi/setpostcategories.php
@@ -0,0 +1,59 @@
+<?php
+
+function moveabletype_userapi_setpostcategories($args)
+{
+    xarLogMessage('Moveabletype api: setpostcategories', XARLOG_LEVEL_WARNING);
+    extract($args);
+    
+    // get the params
+    $sn1=$msg->getParam(0);  $postid        = $sn1->scalarval(); // NOTE: NOT blogid
+    $sn2=$msg->getParam(1);  $username      = $sn2->scalarval();
+    $sn3=$msg->getParam(2);  $password      = $sn3->scalarval();
+    $sn4=$msg->getParam(3);  $categories    = $sn4->getVal();
+    
+    // Try to login 
+    $err='';
+    if (!xarUserLogin($username,$password)) {
+        $err = xarML("Invalid username or password for (#(1)) while getting recent posts",$username);
+    } else {
+        $itemType=xarModGetVar('bloggerapi','bloggerpubtype');
+        // We do DO want to include the main category here, although that is XAR implementation on NEW, 
+        // it is required because we do a clean_first in the category linking below
+        // First get the root categories of the publication type (as in: all the blogs)
+        $rootCats = array();
+        $rootCats = xarModGetVar('articles','mastercids.'.$itemType);
+        if (!empty($rootCats)) $rootCats = explode(';',$rootCats);
+
+        // Now get the categories for the specific post of the specific post
+        $itemCats = xarModAPIFunc('categories', 'user','getitemcats',array('modid'=> 151, 'itemtype' => $itemType, 'itemid'   => $postid));
+        $itemCats = array_keys($itemCats);
+        
+        // The cat we want is the one which is in both
+        $cids = array_intersect($rootCats, $itemCats);
+        
+        // Get the other designated categories from the xml request
+        if(!empty($categories)) {
+            // Match the names we got from the client to ids, we only have to consider the subcats of blogid
+            foreach($categories as $catStruct) {
+                $cids[] = $catStruct['categoryId']->scalarval();
+                $isPrimary = $catStruct['isPrimary']->scalarval(); // TODO: Where do we put this in Xaraya?
+            }
+        }
+        // $cids now contains all the cat ids to which we should link $aid, if any
+        $result = xarModAPIFunc('categories','admin','linkcat', array('cids' => $cids, 
+                                                                      'iids' => array($postid), 
+                                                                      'modid' => 151, // articles 
+                                                                      'itemtype' => $itemType,
+                                                                      'clean_first' => true));
+        if(!$result) $err = xarML('A problem occurred while setting the categories for this entry');
+    }
+
+    // Return the right response
+    if(empty($err)) {
+        $output = xarModAPIFunc('xmlrpcserver','user','successresponse');
+    } else {
+        $output = xarModAPIFunc('xmlrpcserver','user','faultresponse',array('errorstring' => $err));
+    }
+    return $output;
+}
+?>
