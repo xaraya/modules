7cd0531754a6d8f59ed1fea0cfd6adc6d1151362
jojodee <jojodee@ahaadev.net>
2005-04-17 20:05:04 +0000

    xarincludes/nqTimer.class.php: 	#  QA Tabs

--- xarincludes/nqTimer.class.php
+++ xarincludes/nqTimer.class.php
@@ -1,9 +1,9 @@
 <?php
 /*******************************************************************************
-	nqTimer: A simple script to time script execution
-	Copyright (C) 2001 Roger Raymond ~ epsilon7@users.sourceforge.net
+    nqTimer: A simple script to time script execution
+    Copyright (C) 2001 Roger Raymond ~ epsilon7@users.sourceforge.net
 
-	This library is free software; you can redistribute it and/or
+    This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) any later version.
