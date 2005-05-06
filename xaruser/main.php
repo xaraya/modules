d6c6aa7c760b1672bed9ce03b048646b897a22c3
mikespub <mikespub@fully.qualified.hostname>
2005-05-06 20:05:05 +0000

    xaruser/main.php: 	#  bug 4414 - default to download function

--- xaruser/main.php
+++ xaruser/main.php
@@ -1,30 +1,7 @@
 <?php
 
-/*FUNC*/function uploads_user_main()
+/*FUNC*/function uploads_user_main($args)
 {
-  //this is a default page that will show the status of the item requested.
-    
-    // Security check
-    if (!xarSecurityCheck('ViewUploads')) return;
-    
-    $ulid = xarVarCleanFromInput('ulid');
-    $ulname = xarVarCleanFromInput('ulname');
-    
-    $thumbwidth  = xarVarCleanFromInput('thumbwidth');
-    $thumbheight = xarVarCleanFromInput('thumbheight');
-    $thumb = xarVarCleanFromInput('thumb');
-    
-    //Download the file to the user
-    return xarModAPIFunc('uploads',
-                          'admin',
-                          'download',
-                          array('ulid'=>$ulid
-                                  , 'ulname'=>$ulname
-                                  , 'thumbwidth'=>$thumbwidth
-                                  , 'thumbheight'=>$thumbheight
-                                  , 'thumb'=>$thumb
-                                ));
-     
-    
+    return xarModFunc('uploads','user','download',$args);
 }
 ?>
