778abf65942315cabf76c6c869b57149bf4203e6
mikespub <mikespub@fully.qualified.hostname>
2005-05-07 20:05:05 +0000

    xaruserapi/countitems.php: 	#  bug 4430 - SQLite strikes again xaruserapi/getmodules.php: 	#  bug 4431 - SQLite goes heavy :)

--- xaruserapi/getmodules.php
+++ xaruserapi/getmodules.php
@@ -22,29 +22,100 @@ function categories_userapi_getmodules($args)
     $dbconn =& xarDBGetConn();
     $xartable =& xarDBGetTables();
     $categoriestable = $xartable['categories_linkage'];
-    $bindvars = array();
-    // Get items
-    $sql = "SELECT xar_modid, xar_itemtype, COUNT(*), COUNT(DISTINCT xar_iid), COUNT(DISTINCT xar_cid)
-            FROM $categoriestable";
-    if (!empty($cid)) {
-        $sql .= " WHERE xar_cid = ?";
-        $bindvars[] = $cid;
-    }
-    $sql .= " GROUP BY xar_modid, xar_itemtype";
 
-    $result = $dbconn->Execute($sql,$bindvars);
-    if (!$result) return;
+    if($dbconn->databaseType == 'sqlite') {
 
-    $modlist = array();
-    while (!$result->EOF) {
-        list($modid,$itemtype,$numlinks,$numitems,$numcats) = $result->fields;
-        if (!isset($modlist[$modid])) {
-            $modlist[$modid] = array();
+    // TODO: see if we can't do this some other way in SQLite
+
+        $bindvars = array();
+        // Get links
+        $sql = "SELECT xar_modid, xar_itemtype, COUNT(*)
+                FROM $categoriestable";
+        if (!empty($cid)) {
+            $sql .= " WHERE xar_cid = ?";
+            $bindvars[] = $cid;
         }
-        $modlist[$modid][$itemtype] = array('items' => $numitems, 'cats' => $numcats, 'links' => $numlinks);
-        $result->MoveNext();
+        $sql .= " GROUP BY xar_modid, xar_itemtype";
+
+        $result = $dbconn->Execute($sql,$bindvars);
+        if (!$result) return;
+
+        $modlist = array();
+        while (!$result->EOF) {
+            list($modid,$itemtype,$numlinks) = $result->fields;
+            if (!isset($modlist[$modid])) {
+                $modlist[$modid] = array();
+            }
+            $modlist[$modid][$itemtype] = array('items' => 0, 'cats' => 0, 'links' => $numlinks);
+            $result->MoveNext();
+        }
+        $result->close();
+
+        // Get items
+        $sql = "SELECT xar_modid, xar_itemtype, COUNT(*)
+                FROM (SELECT DISTINCT xar_iid, xar_modid, xar_itemtype
+                      FROM $categoriestable";
+        if (!empty($cid)) {
+            $sql .= " WHERE xar_cid = ?";
+            $bindvars[] = $cid;
+        }
+        $sql .= ") GROUP BY xar_modid, xar_itemtype";
+
+        $result = $dbconn->Execute($sql,$bindvars);
+        if (!$result) return;
+
+        while (!$result->EOF) {
+            list($modid,$itemtype,$numitems) = $result->fields;
+            $modlist[$modid][$itemtype]['items'] = $numitems;
+            $result->MoveNext();
+        }
+        $result->close();
+
+        // Get cats
+        $sql = "SELECT xar_modid, xar_itemtype, COUNT(*)
+                FROM (SELECT DISTINCT xar_cid, xar_modid, xar_itemtype
+                      FROM $categoriestable";
+        if (!empty($cid)) {
+            $sql .= " WHERE xar_cid = ?";
+            $bindvars[] = $cid;
+        }
+        $sql .= ") GROUP BY xar_modid, xar_itemtype";
+
+        $result = $dbconn->Execute($sql,$bindvars);
+        if (!$result) return;
+
+        while (!$result->EOF) {
+            list($modid,$itemtype,$numcats) = $result->fields;
+            $modlist[$modid][$itemtype]['cats'] = $numcats;
+            $result->MoveNext();
+        }
+        $result->close();
+
+    } else {
+        $bindvars = array();
+        // Get items
+        $sql = "SELECT xar_modid, xar_itemtype, COUNT(*), COUNT(DISTINCT xar_iid), COUNT(DISTINCT xar_cid)
+                FROM $categoriestable";
+        if (!empty($cid)) {
+            $sql .= " WHERE xar_cid = ?";
+            $bindvars[] = $cid;
+        }
+        $sql .= " GROUP BY xar_modid, xar_itemtype";
+
+        $result = $dbconn->Execute($sql,$bindvars);
+        if (!$result) return;
+
+        $modlist = array();
+        while (!$result->EOF) {
+            list($modid,$itemtype,$numlinks,$numitems,$numcats) = $result->fields;
+            if (!isset($modlist[$modid])) {
+                $modlist[$modid] = array();
+            }
+            $modlist[$modid][$itemtype] = array('items' => $numitems, 'cats' => $numcats, 'links' => $numlinks);
+            $result->MoveNext();
+        }
+        $result->close();
     }
-    $result->close();
 
     return $modlist;
 }
