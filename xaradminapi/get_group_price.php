0e64e460f3799dfbb11c6afecf497be04499d476
marc <marc@marclaptop.>
2005-05-14 20:05:05 +0000

    xaradmin/geo_zones.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xaradmin/geo_zones.php.bak xaradmin/languages.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xaradmin/languages.php.bak xaradmin/modules.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xaradmin/modules.php.bak xaradminapi/call_function.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xaradminapi/call_function.php.bak xaradminapi/get_group_price.php: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xaradminapi/get_group_price.php xarblocks/languages.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xarblocks/languages.php.bak xarblocks/loginbox.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xarblocks/loginbox.php.bak xarblocks/order_history.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xarblocks/order_history.php.bak xarblocks/product_notifications.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xarblocks/product_notifications.php.bak xarblocks/reviews.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xarblocks/reviews.php.bak xarblocks/tell_a_friend.php.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xarblocks/tell_a_friend.php.bak xarstyles/tabs.css: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xarstyles/tabs.css xartemplates/blocks/configmenu.xd.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xartemplates/blocks/configmenu.xd.bak xartemplates/includes/admin-navigation.xd.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xartemplates/includes/admin-navigation.xd.bak xartemplates/includes/adminheader.xd.bak: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xartemplates/includes/adminheader.xd.bak xartemplates/unused/admin-categories.xd: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xartemplates/unused/admin-categories.xd xartemplates/unused/admin-categories_screenno.xd: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xartemplates/unused/admin-categories_screenno.xd xartemplates/unused/admin-categories_screenwromg.xd: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xartemplates/unused/admin-categories_screenwromg.xd xartemplates/unused/user-startold.xd: 	#  BitKeeper file C:/Program Files/Apache Group/Apache2/htdocs/changesdue/html/modules/commerce/xartemplates/unused/user-startold.xd

---
+++ xaradminapi/get_group_price.php
@@ -0,0 +1,37 @@
+<?php
+// ----------------------------------------------------------------------
+// Copyright (C) 2004: Marc Lutolf (marcinmilan@xaraya.com)
+// Purpose of file:  Configuration functions for commerce
+// ----------------------------------------------------------------------
+//  based on:
+//  (c) 2003 XT-Commerce
+//  (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+//  (c) 2002-2003 osCommerce (oscommerce.sql,v 1.83); www.oscommerce.com
+//  (c) 2003  nextcommerce (nextcommerce.sql,v 1.76 2003/08/25); www.nextcommerce.org
+// ----------------------------------------------------------------------
+
+function commerce_adminapi_get_group_price($args) {
+    include_once 'modules/xen/xarclasses/xenquery.php';
+    xarModAPILoad('commerce');
+    $xartables = xarDBGetTables();
+    extract($args);
+
+    // well, first try to get group price from database
+    $q = new xenQuery('SELECT',"personal_offers_by_customers_status_" . $group_id,'personal_offer');
+    $q->eq('products_id',$product_id);
+    if(!$q->run()) return;
+    $group_price_data = $q->row();
+
+    // if we found a price, everything is ok if not, we will create new entry
+    if ($group_price_data['personal_offer'] == '') {
+        $q = new xenQuery('INSERT',"personal_offers_by_customers_status_" . $group_id,'personal_offer');
+        $q->addfield(price_id,'');
+        $q->addfield(products_id,$product_id);
+        $q->addfield(quantity,1);
+        $q->addfield(personal_offer,0.00);
+        if(!$q->run()) return;
+        $group_price_data = $q->row();
+    }
+    return $group_price_data['personal_offer'];
+}
+?>
