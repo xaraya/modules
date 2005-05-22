528280b533e1d15f09ba60a367ed227232fb7792
marcel <marcel@newton.xaraya.com>
2005-05-22 20:05:05 +0000

    BitKeeper/triggers/cia-bk2xml.sh: 	#  BitKeeper file /var/bk/xaraya/modules/carts/BitKeeper/triggers/cia-bk2xml.sh BitKeeper/triggers/post-incoming.notice: 	#  BitKeeper file /var/bk/xaraya/modules/carts/BitKeeper/triggers/post-incoming.notice BitKeeper/etc/logging_ok: 	#  Logging to logging@openlogging.org accepted

---
+++ BitKeeper/triggers/cia-bk2xml.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+#
+# CIA bot client script for BitKeeper repositories, delivering via email
+#   -- Chris Larson <kergoth@handhelds.org>
+#
+# Based on ciabot_svn.sh by Micah Dowty <micah@navi.cx>
+#
+# See http://cia.navi.cx
+# for more information on what the CIA bot is and how it works.
+#
+############# There are some parameters for this script that you can customize:
+
+# Project information
+project_name="Xaraya"
+return_address="marcel@xaraya.com"
+
+# System
+sendmail_command="/usr/sbin/sendmail -t"
+
+############# Below this line you shouldn't have to change anything
+
+# Script arguments
+REV="$1"
+
+# The email address CIA lives at
+cia_address="cia@cia.navi.cx"
+
+author=`echo $BK_USER | sed 's/\&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g'`
+# Use the virgin/scenario name within our hierarchy 
+VIRGIN=$(basename $(dirname ${BKD_ROOT}))
+REPO=$(basename ${BKD_ROOT})
+module="$VIRGIN/$REPO "
+#module=`basename $(dirname $BKD_ROOT) | sed 's/\&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g'`
+log=`bk changes -r"$REV" -d":C:" | sed 's/\&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g'`
+tag=`bk changes -r"$REV" -d":TAG:" | sed 's/\&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g'`
+for file in `bk changes -n -v -r"$REV" -d"\\\$unless(:GFILE:=ChangeSet){:GFILE:}" | sort -u | sed 's/\&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g'`; do
+    files="$files<file>$file</file>"
+done
+
+# Send an email with the final XML message
+(cat <<EOF
+From: $return_address
+To: $cia_address
+Content-type: text/xml
+Subject: DeliverXML
+
+<message>
+    <generator>
+        <name>BitKeeper CIA Bot client shell script</name>
+        <version>1.0</version>
+    </generator>
+    <source>
+        <project>$project_name</project>
+        <module>$module</module>
+        <branch>$tag</branch>
+    </source>
+    <body>
+        <commit>
+            <revision>$REV</revision>
+            <author>$author</author>
+            <files>$files</files>
+            <log>$log</log>
+        </commit>
+    </body>
+</message>
+EOF
+) | $sendmail_command
+
+### The End ###
