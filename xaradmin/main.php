547313760d6a5fc980a97026654e6cb0927aeb4c
alberto <alberto@ftb.>
2005-05-25 20:05:05 +0000

    xaradmin/main.php: 	#  scrubbing xartemplates/admin-main.xd: 	#  scrubbing

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -9,7 +9,8 @@ function comments_admin_main()
     }
     // we only really need to show the default view (overview in this case)
     if (xarModGetVar('adminpanels', 'overview') == 0){
-        xarResponseRedirect(xarModURL('comments', 'admin', 'view'));
+        return array();
+        //xarResponseRedirect(xarModURL('comments', 'admin', 'view'));
     } else {
         xarResponseRedirect(xarModURL('comments', 'admin', 'stats'));
     }
