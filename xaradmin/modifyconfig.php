057ac414a21d494a66353e2cac919b5cfee47ccd
marcel <marcel@hsdev.com>
2005-06-06 20:05:06 +0000

    xarinit.php: 	#  - making the description of the objects a bit more generic 	#  - adding ice_configuration xaradmin/modifyconfig.php: 	#  BitKeeper file /var/bk/xar/core/commerce.xaraya.com/html/modules/commerce/xaradmin/modifyconfig.php xardata/ice_configuration-data.xml: 	#  BitKeeper file /var/bk/xar/core/commerce.xaraya.com/html/modules/commerce/xardata/ice_configuration-data.xml xardata/ice_configuration-def.xml: 	#  BitKeeper file /var/bk/xar/core/commerce.xaraya.com/html/modules/commerce/xardata/ice_configuration-def.xml

---
+++ xaradmin/modifyconfig.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * Modify configuration settings for the commerce module
+ *
+ * @package modules
+ * @copyright (C) 2005 by The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ * 
+ * @subpackage commerce
+ * @link  link to information for the subpackage
+ * @author author name <marcel@hsdev.com>
+ */
+
+function commerce_admin_modifyconfig($args)
+{
+    return xarModFunc('commerce','admin','commoninfo_object', array('objectname' => 'ice_configuration'));
+}
