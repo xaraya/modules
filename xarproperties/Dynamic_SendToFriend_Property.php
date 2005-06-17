5be041567ccfa63b9fc4181073375c047d8d5003
jojodee <jojodee@ahaadev.net>
2005-06-17 20:05:06 +0000

    xarinit.php: 	#  added support for localized date format xartemplates/admin-main.xd: 	#  template scrubbing xartemplates/admin-modifyconfig.xd: 	#  template scrubbing xaruser/send.php: 	#  added support for localized date format xaruser/sendfriend.php: 	#  added support for localized date format xarversion.php: 	#  added support for localized date format xarproperties/Dynamic_SendToFriend_Property.php: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/recommend/xarproperties/Dynamic_SendToFriend_Property.php xartemplates/admin-showinput-sendtofriend.xd: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/recommend/xartemplates/admin-showinput-sendtofriend.xd xartemplates/user-showoutput-sendtofriend.xd: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/recommend/xartemplates/user-showoutput-sendtofriend.xd

---
+++ xarproperties/Dynamic_SendToFriend_Property.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Dynamic Send To Friend Property
+ *
+ * @package dynamicdata
+ * @subpackage properties
+ */
+
+/**
+ * Class to handle Send To Friend property
+ *
+ * @package dynamicdata
+ */
+include_once "modules/dynamicdata/class/properties.php";
+class Dynamic_SendToFriend_Property extends Dynamic_Property
+{
+    function validateValue($value = null)
+    {
+        if (!empty($value)) {
+            $this->value = 1;
+        } else {
+            $this->value = 0;
+        }
+        return true;
+    }
+
+    function showInput($args = array())
+    {
+        extract($args);
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (empty($name)) {
+            $name = 'dd_' . $this->id;
+        }
+        if (empty($id)) {
+            $id = $name;
+        }
+        
+        $data['name']     = $name;
+        $data['id']       = $id;
+        $data['value']    = isset($value) ? xarVarPrepForDisplay($value) : xarVarPrepForDisplay($this->value);
+        $data['tabindex'] = !empty($tabindex) ? ' tabindex="'.$tabindex.'"': '';
+        $data['invalid']  = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) :'';
+
+        $template='sendtofriend';
+        return xarTplModule('recommend', 'admin', 'showinput', $data , $template);
+
+        /*
+        return '<input type="checkbox"'.
+               ' name="' . $name . '"' .
+               ' value="1"' .
+               ' id="'. $id . '"' .
+               (!empty($tabindex) ? ' tabindex="'.$tabindex.'"' : '') .
+               (!empty($value) ? ' checked="checked"' : '') .
+               ' />' .
+               (!empty($this->invalid) ? ' <span class="xar-error">'.xarML('Invalid #(1)', $this->invalid) .'</span>' : '');
+       */
+    }
+
+    function showOutput($args = array())
+    {   //tidy up this, add a few checks
+        extract($args);
+        if(!xarVarFetch('aid',  'isset', $aid,   NULL, XARVAR_DONT_SET)) {return;}
+
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+
+        if (!empty($value) && isset($aid)){
+        $data['aid']=    $aid;
+        $data['value']=  $value;
+           /* move to template
+            $alttext = xarML('Send this article to a friend');
+            $sendimg = xarTplGetImage('sendtofriend.gif', 'recommend');
+               $out= '<a href="'.xarModURL('recommend','user','sendtofriend',array('aid'=>$aid)).'"><img src="'.$sendimg.'" style="border:0;" alt="'.$alttext.'" /></a>';
+            return $out;
+          */
+        $template='sendtofriend';
+        return xarTplModule('recommend', 'user', 'showoutput', $data ,$template);
+
+       } else {
+       //value is not set - we don't want to show the link
+           return '';
+        }
+    }
+
+    /**
+     * Get the base information for this property.
+     *
+     * @returns array
+     * @return base information for this property
+     **/
+     function getBasePropertyInfo()
+     {
+         $args = array();
+         $baseInfo = array(
+                              'id'         => 106,
+                              'name'       => 'sendtofriend',
+                              'label'      => 'Send To A Friend',
+                              'format'     => '106',
+                              'validation' => '',
+                            'source'     => '',
+                            'dependancies' => '',
+                            'requiresmodule' => 'recommend',
+                            'aliases'        => '',
+                            'args'           => serialize($args)
+                            // ...
+                           );
+        return $baseInfo;
+     }
+
+}
+
+?>
