3e9d386a735fb8ac909535c50c18b8b85cfe7499
mikespub <mikespub@fully.qualified.hostname>
2005-06-19 20:05:06 +0000

    xaradmin/menu.php: 	#  bug 4250 - default to new representatives

--- xaradmin/menu.php
+++ xaradmin/menu.php
@@ -9,7 +9,7 @@ function helpdesk_admin_menu()
     if (!xarSecurityCheck('readhelpdesk')) return;
 
     xarVarFetch('func',      'str', $data['page'],       'main', XARVAR_NOT_REQUIRED);
-    xarVarFetch('itemtype',  'str', $data['itemtype'],   1,      XARVAR_NOT_REQUIRED);
+    xarVarFetch('itemtype',  'str', $data['itemtype'],   10,     XARVAR_NOT_REQUIRED);
     xarVarFetch('selection', 'str', $data['selection'],  '',     XARVAR_NOT_REQUIRED);
 
     $data['userisloggedin'] = xarUserIsLoggedIn();
