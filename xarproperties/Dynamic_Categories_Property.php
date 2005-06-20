e56db51235860152e97530673b1f79774c9f0832
jojodee <jojodee@ahaadev.net>
2005-06-20 20:05:06 +0000

    xarproperties/Dynamic_Categories_Property.php: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/categories/xarproperties/Dynamic_Categories_Property.php xarproperties/index.html: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/categories/xarproperties/index.html xartemplates/properties/index.html: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/categories/xartemplates/properties/index.html

---
+++ xarproperties/Dynamic_Categories_Property.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * File: $Id$
+ *
+ * Dynamic Categories Property
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage dynamicdata properties
+ * @author mikespub <mikespub@xaraya.com>
+*/
+/**
+ * handle static text property
+ *
+ * @package dynamicdata
+ *
+ */
+include_once "modules/dynamicdata/class/properties.php";
+ 
+class Dynamic_Categories_Property extends Dynamic_Property
+{
+    /**
+     * Get the base information for this property.
+     *
+     * @returns array
+     * @return base information for this property
+     **/
+     function getBasePropertyInfo()
+     {
+         $args = array();
+         $baseInfo = array(
+                            'id'         => 100,
+                            'name'       => 'categories',
+                            'label'      => 'Categories',
+                            'format'     => '100',
+                            'validation' => '',
+                            'source'     => 'hook module',
+                            'dependancies' => '',
+                            'requiresmodule' => 'categories',
+                            'aliases' => '',
+                            'args'           => serialize($args),
+                            // ...
+                           );
+        return $baseInfo;
+     }
+}
+
+
+?>
