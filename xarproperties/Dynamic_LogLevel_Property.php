96fdddcebb9dff486927e8a41a0068ee29a6317a
jojodee <jojodee@ahaadev.net>
2005-06-20 20:05:06 +0000

    xarproperties/Dynamic_LogLevel_Property.php: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/logconfig/xarproperties/Dynamic_LogLevel_Property.php xarproperties/index.html: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/logconfig/xarproperties/index.html xartemplates/properties/index.html: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/logconfig/xartemplates/properties/index.html xartemplates/properties/showinput-loglevel.xd: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/logconfig/xartemplates/properties/showinput-loglevel.xd xartemplates/properties/showoutput-loglevel.xd: 	#  BitKeeper file C:/servroot/cygwin/home/jojodee/bk/xarmodules/logconfig/xartemplates/properties/showoutput-loglevel.xd

---
+++ xarproperties/Dynamic_LogLevel_Property.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * File: $Id$
+ *
+ * Dynamic Radio Button Property
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage dynamicdata properties
+ * @author nuncanada <nuncanada@xaraya.com>
+*/
+
+/**
+ * handle logging levels property
+ * This should be in the 'Log Config' module. Alternative sources for dd properties
+ * doesnt seem to be working yet?
+ *
+ * @package dynamicdata
+ */
+include_once "modules/dynamicdata/class/properties.php";
+class Dynamic_LogLevel_Property extends Dynamic_Property
+{
+    var $options = array ('Emergency', 'Alert', 'Critical', 'Error', 'Warning', 'Notice', 'Info', 'Debug');
+    var $value = array();
+    
+    function Dynamic_LogLevel_Property($args)
+    {
+        $this->Dynamic_Property($args);
+    }
+
+    function validateValue($value = null)
+    {
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+
+        foreach ($this->options as $option) {
+            if (($value[$option] != 'ON') && ($value[$option] != 'OFF'))
+            {
+                $this->invalid = xarML('selection');
+                $this->value = null;
+                return false;
+            }
+        }
+
+        //HACK: Hack to allow to store the array:
+        $this->value = serialize($value);
+
+        return true;
+    }
+
+//    function showInput($name = '', $value = null, $options = array(), $id = '', $tabindex = '')
+    function showInput($args = array())
+    {
+        extract($args);
+        $data = array();
+
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+
+        if (empty($name)) {
+            $name = 'dd_'.$this->id;
+        }
+
+        //HACK: Hack to allow to store the array:
+        if (!empty($value) && !is_array($value)) {$value = unserialize($value);}
+
+        $data['value']   = $value;
+        $data['name']    = $name;
+        $data['tabindex'] =!empty($tabindex) ? ' tabindex="'.$tabindex.'" ' : '';
+        $data['invalid']  =!empty($this->invalid) ? ' <span class="xar-error">'.xarML('Invalid #(1)', $this->invalid) .'</span>' : '';
+        $data['options'] = $this->options;
+
+        $template="";
+        return xarTplProperty('logconfig', 'loglevel', 'showinput', $data);
+    }
+
+    function showOutput($args = array())
+    {
+         extract($args);
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        //$out = '';
+        $data=array();
+
+        //HACK: Hack to allow to store the array:
+        if (!empty($value) && !is_array($value)) {$value = unserialize($value);}
+
+        $data['value']   = $value;
+        $data['options'] = $this->options;
+
+        $template="";
+        return xarTplProperty('logconfig', 'loglevel', 'showoutput', $data);
+        // return $out;
+    }
+
+    /**
+     * Get the base information for this property.
+     *
+     * @returns array
+     * @return base information for this property
+     **/
+     function getBasePropertyInfo()
+     {
+         $args = array();
+         $baseInfo = array(
+                              'id'         => 107,
+                              'name'       => 'loglevel',
+                              'label'      => 'Logging Level',
+                              'format'     => '107',
+                              'validation' => '',
+                              'source'         => '',
+                              'dependancies'   => '',
+//                              'requiresmodule' => 'logconfig',
+                              'requiresmodule' => '',
+                              'aliases'        => '',
+                              'args'           => serialize($args),
+                            // ...
+                           );
+        return $baseInfo;
+     }
+
+}
+
+?>
