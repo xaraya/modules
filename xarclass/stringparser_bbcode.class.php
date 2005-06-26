b2c97dd9f24e9f36ec8e940af787b6ec27e3bb23
mikespub <mikespub@fully.qualified.hostname>
2005-06-26 20:05:06 +0000

    xarclass/stringparser.class.php: 	#  bug 4699 - avoid double loading of classes xarclass/stringparser_bbcode.class.php: 	#  bug 4699 - avoid double loading of classes

--- xarclass/stringparser_bbcode.class.php
+++ xarclass/stringparser_bbcode.class.php
@@ -32,6 +32,8 @@
  *  the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  *  MA 02111-1307, USA.
  */
+
+if (!defined('BBCODE_CLOSETAG_FORBIDDEN')) {
  
 require_once dirname(__FILE__).'/stringparser.class.php';
 
@@ -1758,6 +1760,8 @@ class StringParser_BBCode_Node_Element extends StringParser_Node {
         }
         return $str;
     }
+}
+
 }
 
 ?>
