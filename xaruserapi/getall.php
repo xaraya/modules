49d6347bfea3703fd8e102470d90cb37988cba5a
marcel <marcel@hsdev.com>
2005-07-06 00:40:00 +0000

    resigned from bad signatur!!!

--- xaruserapi/getall.php
+++ xaruserapi/getall.php
@@ -52,8 +52,7 @@ function example_userapi_getall($args)
         $invalid[] = 'numitems';
     } 
     if (count($invalid) > 0) {
-        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
-            join(', ', $invalid), 'user', 'getall', 'Example');
+        $msg = xarML('Invalid #(1)', join(', ', $invalid));
         xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
             new SystemException($msg));
         return;
