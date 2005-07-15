797534015aa4718e4285d487af7289a004846b6d
jojodee <jojodee@xaraya.com>
2005-07-15 13:53:01 +0000

    add the search.php file

---
+++ xaruser/search.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ *
+ * Courses Module
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage courses
+ * @original author Jim McDonalds, dracos, mikespub et al.
+ */
+
+/*
+ * @author Michel V.
+ * Search for a course via content description
+ *
+ *
+ */
+function courses_user_search()
+{
+    if (!xarVarFetch('q', 'isset', $q, NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('bool', 'isset', $bool, NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('sort','isset', $sort, NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('name', 'int:0:1', $name, 1, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('shortdesc', 'int:0:1', $shortdesc, 1, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('longdesc', 'int:0:1', $longdesc, 1, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('number', 'int:0:1', $number, 0, XARVAR_DONT_SET)) return;
+
+    $data = array();
+    $data['name'] = $name;
+    $data['number'] = $number;
+    $data['shortdesc'] = $shortdesc;
+    $data['longdesc'] = $longdesc;
+    
+    if($q == ''){
+        return $data;
+    }
+    
+    // Search course information
+    $data['courses'] = xarModAPIFunc('courses',
+                          'user',
+                          'search',
+                           array('name' => $name,
+                                 'number' => $number,
+                                 'shortdesc' => $shortdesc,
+                                 'longdesc' => $longdesc,
+                                 'q' => $q));
+
+    if (empty($data['courses'])){
+        $data['status'] = xarML('No Course Found Matching Search');
+    }
+
+    return $data;
+
+}
+
+?>
