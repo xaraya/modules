bf360e703b8215c40cfc744f00dfb137aba0003f
scmadmin <scmadmin@xaraya.com>
2005-07-19 10:13:20 +0000

    weather/xarinit.php: 	#  remove cache directory setting - no longer needed 	#  initial instance code, not really working yet weather/xartemplates/user-cc.xd: 	#  tweaks to the current weather display template weather/xaruserapi/decode_shorturl.php: 	#  fixes to shurt url decoding - less to decode now

--- weather/xarinit.php
+++ weather/xarinit.php
@@ -4,7 +4,6 @@ function weather_init()
     xarModSetVar('weather','partner_id','');
     xarModSetVar('weather','license_key','');
     xarModSetVar('weather','default_location','');
-    xarModSetVar('weather','cache_dir','modules/weather/xarcache');
     xarModSetVar('weather','cc_cache_time',60*30); // 30 minutes - these should not be changed
     xarModSetVar('weather','ext_cache_time',60*60*2); // 2 hours - these should not be changed
     xarModSetVar('weather','units','s');
@@ -15,6 +14,26 @@ function weather_init()
             array('modName' => 'weather',
                 'blockType' => 'current'))) return;
     
+    
+    // let's define our instances
+    $xartable =& xarDBGetTables();
+    $query = "SELECT DISTINCT i.xar_title 
+              FROM $xartable[block_instances] i, $xartable[block_types] t 
+              WHERE t.xar_id = i.xar_type_id AND t.xar_module = 'weather'";
+    $instances = array(
+        array(
+            'header' => 'Weather Block Title:',
+            'query' => $query,
+            'limit' => 20
+            )
+        );
+    xarDefineInstance('weather', 'Block', $instances);
+    
+    // let's define our masks
+    xarRegisterMask('ReadWeatherBlock', 'All', 'weather', 'Block', 'All', 'ACCESS_OVERVIEW');
+    xarRegisterMask('ViewWeather', 'All', 'weather', 'Item', 'All:All:All', 'ACCESS_OVERVIEW');
+    xarRegisterMask('ReadWeather', 'All', 'weather', 'Item', 'All:All:All', 'ACCESS_READ');
+    xarRegisterMask('AdminWeather', 'All', 'wethear', 'Item', 'All:All:All', 'ACCESS_ADMIN');
     return true;
 }
 
