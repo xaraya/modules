5078a204379ef2048ba85e9a8e161448535334aa
jojodee <jojodee@xaraya.com>
2005-07-27 11:51:50 +0000

    bug 4822 hardcoded table prefix for articles table

--- xaruserapi/geteventinfo.php
+++ xaruserapi/geteventinfo.php
@@ -8,19 +8,22 @@
  *   iid:      id of hooked item (in hooking module, e.g. an article id)
  *   itemtype: type of hooked item
  *   modid:    id of hooking module
- *      event:    current event data
+ *   event:    current event data
  */
 function julian_userapi_geteventinfo($args)
 {
     extract($args);
 
-   // Load up database
-   $dbconn =& xarDBGetConn();
-    
+    // Load up database
+    $dbconn =& xarDBGetConn();
+    $xartable =& xarDBGetTables();
+    $sitePrefix = xarDBGetSiteTablePrefix();
+    // Name for articles database entities
+    $articlestable = $sitePrefix . '_articles';
     // Try to find the link for the current module, item type and item id.
-   $query = "SELECT `xar_title`,`xar_summary` FROM `xar_articles` WHERE `xar_aid`='$iid'";
-   $result =& $dbconn->Execute($query);
-   if (!empty($result)) {
+    $query = "SELECT `xar_title`,`xar_summary` FROM $articlestable WHERE `xar_aid`='$iid'";
+    $result =& $dbconn->Execute($query);
+    if (!empty($result)) {
         if (!$result->EOF) {
             $obj = $result->FetchObject(false);
             $event['summary'] = $obj->xar_title;
