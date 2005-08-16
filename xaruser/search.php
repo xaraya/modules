b6c5cbbaebfe6314efe3ccb3daec3d89c5a03eb7
jojodee <jojodee@xaraya.com>
2005-08-16 18:46:15 +0000

    Add a search facility to release module

---
+++ xaruser/search.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ *
+ * Release Module
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Release
+ * @original author Jim McDonalds, dracos, mikespub et al.
+ */
+
+/*
+ * @author Michel V.
+ * @author jojodee
+ * Search for a release
+ *
+ */
+function release_user_search()
+{
+    if (!xarVarFetch('q',         'isset',  $q,        NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('bool',      'isset',  $bool,     NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('sort',      'isset',  $sort,     NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('regname',   'str:0:', $regname,  '',   XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('displname', 'str:0:', $displname,'',   XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('desc',      'str:0:', $desc,     '',   XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('rid',       'id',     $rid,      NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('uid',       'id',     $uid,      NULL, XARVAR_NOT_REQUIRED)) return;
+    if(!xarVarFetch('author',     'isset',  $author,   NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('authorsearch','isset',  $authorsearch,   NULL, XARVAR_DONT_SET)) {return;}
+    $data       = array();
+    $search     = array();
+
+  if (!isset($q) || strlen(trim($q)) <= 0) {
+        if (isset($author) && strlen(trim($author)) > 0) {
+            $q = $author;
+            $search['author']=$author;
+            $data['authorsearch']=1;
+        }
+    }
+
+     if($q == ''){
+        return $data;
+    }
+    // Default parameters
+    if (!isset($startnum)) {
+        $startnum = 1;
+    }
+    if (!isset($numitems)) {
+        $numitems = 10;
+    }
+    if (isset($rid)) {
+        $search['rid'] = $q;
+        $data['rid']=1;
+    } else {
+        $data['rid']=0;
+    }
+    if (isset($regname)) {
+        $search['regname'] = $q;
+        $data['regname']=1;
+    } else {
+        $data['regname']=0;
+    }
+    if (isset($displname)) {
+        $data['displname']=1;
+        $search['displname'] = $q;
+    } else {
+        $data['displname']=0;
+    }
+    if (isset($desc)) {
+         $search['desc'] = $q;
+         $data['desc'] = 1;
+    } else {
+        $data['desc']=0;
+    }
+
+
+     if (isset($author)) {
+        // Check user id is real (can't use roles api here - throws a fit - need a utility function)
+        $dbconn =& xarDBGetConn();
+        $xartable =& xarDBGetTables();
+
+        // Get user information
+        $rolestable = $xartable['roles'];
+        $query = "SELECT xar_uid
+                  FROM $rolestable
+                  WHERE xar_uname = ? or xar_name = ?";
+        $result =& $dbconn->Execute($query,array($search['author'],$search['author']));
+        if (!$result) return;
+        // if we found the uid add it to the search list,
+        // otherwise we won't bother searching for it
+        if (!$result->EOF) {
+            $uids = $result->fields;
+            $search['uid'] = $uids[0];
+        }
+        $result->Close();
+    } else {
+        $search['author']='';
+    }
+    $search['q']=$q;
+    $seach['modid']= xarModGetIDFromName('release');
+    // Search course information
+    $data['release'] = xarModAPIFunc('release','user','search',$search);
+
+    if (empty($data['release'])){
+        $data['status'] = xarML('No Extension Found that matches your search');
+    }
+
+    return $data;
+
+}
+
+?>
---
+++ xaruserapi/search.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ * Release Module
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2006 by the Xaraya Development Team
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Release
+ * @author release Development team
+ */
+
+/**
+ * Searches all releases
+ *
+ * @author jojodee
+ * @access private
+ * @returns mixed description of return
+ */
+function release_userapi_search($args)
+{
+    if (empty($args) || count($args) < 1) {
+        return;
+    }
+    extract($args);
+
+     if($q == ''){
+        return;
+    }
+
+    $dbconn =& xarDBGetConn();
+    $xartable =& xarDBGetTables();
+    $releasetable = & $xartable['release_id'];
+    $where = '';
+    $releases = array();
+    $sql = "SELECT  xar_rid,
+                    xar_uid,
+                    xar_regname,
+                    xar_displname,
+                    xar_desc
+              FROM  $releasetable
+              WHERE  (";
+
+    $bindvars = array();
+
+    if (isset($rid)) {
+        $sql .= "xar_rid LIKE ?";
+        $bindvars[] = $rid;
+    }
+
+    if (isset($uid)) {
+        if (isset($rid)) {
+            $sql .= " OR ";
+        }
+        $sql .= "xar_uid LIKE ? ";
+        $bindvars[] = $uid;
+    }
+
+    if (isset($regname)) {
+        if (isset($rid) || isset($tid)) {
+            $sql .= " OR ";
+        }
+        $sql .= " xar_regname = ?";
+
+        $bindvars[] = $regname;
+    }
+   if (isset($displname)) {
+        if (isset($rid) || isset($tid) || isset($regname)) {
+            $sql .= " OR ";
+        }
+        $sql .= " xar_displname = ?";
+        $bindvars[] = $displname;
+    }
+    if (isset($desc)) {
+        if (isset($rid) || isset($tid) || isset($regname) || isset($displname)) {
+            $sql .= " OR ";
+        }
+        $sql .= " xar_desc = ?";
+        $bindvars[] = $desc;
+    }
+    $sql .= ")  ORDER BY xar_rid ASC";
+
+    $result =& $dbconn->Execute($sql, $bindvars);
+    if (!$result) return;
+    // no results to return .. then return them :p
+    if ($result->EOF) {
+        return array();
+    }
+    for (; !$result->EOF; $result->MoveNext()) {
+        list($rid, $uid, $regname, $displname, $desc, $author) = $result->fields;
+        if (xarSecurityCheck('ReadRelease', 0)) {
+            $releases[] = array('rid' => $rid,
+                                'uid' => $uid,
+                               'regname' => $regname,
+                               'displname' => $displname,
+							   'desc' => $desc,
+                               'author' => xarUserGetVar('name',$uid));
+        }
+    }
+    $result->Close();
+
+    // Return the releases
+    return $releases;
+
+}
+?>
