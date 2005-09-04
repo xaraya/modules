888fc6647ab5445a5389f6db6b1c7b3df153781f
marcel <marcel@hsdev.com>
2005-09-04 11:08:39 +0000

    Use the standard tab interface in the templates Dont clutter the admin menu with similar menu entries

--- xaradminapi/getmenulinks.php
+++ xaradminapi/getmenulinks.php
@@ -17,22 +17,7 @@ function workflow_adminapi_getmenulinks()
                                                   'admin',
                                                   'monitor_processes'),
                               'title' => xarML('Monitor the workflow processes'),
-                              'label' => xarML('Monitor Processes'));
-        $menulinks[] = Array('url'   => xarModURL('workflow',
-                                                  'admin',
-                                                  'monitor_activities'),
-                              'title' => xarML('Monitor the workflow activities'),
-                              'label' => xarML('Monitor Activities'));
-        $menulinks[] = Array('url'   => xarModURL('workflow',
-                                                  'admin',
-                                                  'monitor_instances'),
-                              'title' => xarML('Monitor the workflow instances'),
-                              'label' => xarML('Monitor Instances'));
-        $menulinks[] = Array('url'   => xarModURL('workflow',
-                                                  'admin',
-                                                  'monitor_workitems'),
-                              'title' => xarML('Monitor the workflow workitems'),
-                              'label' => xarML('Monitor Workitems'));
+                              'label' => xarML('Monitoring'));
         $menulinks[] = Array('url'   => xarModURL('workflow',
                                                   'admin',
                                                   'processes'),
