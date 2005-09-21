6965bfaf9c8ec29027971aab0aa8b6bc2bc74bf6
jojodee <jojodee@xaraya.com>
2005-09-21 20:49:48 +0000

    Remove keywords in file header

--- xaradminapi/getmenulinks.php
+++ xaradminapi/getmenulinks.php
@@ -1,5 +1,15 @@
 <?php
 /**
+ * Xaraya Referers
+ * 
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2002 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.org
+ * @subpackage Referer Module
+ * @author John Cox et al. 
+ */
+/**
  * utility function pass individual menu items to the admin panels
  * 
  * @author the Example module development team 
