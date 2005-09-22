94fc19e628781b8884603bd78182f2ecdf957ff9
jojodee <jojodee@xaraya.com>
2005-09-22 01:26:22 +0000

    More standardizing  of templates, note that something is broken in Example module atm, seems to be in admin template, haven't got there yet - but will see what the probs are when i get there.

--- xaruser/usermenu.php
+++ xaruser/usermenu.php
@@ -1,58 +1,64 @@
 <?php
 /**
- * Display the user menu hook
- *
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2003 by the Xaraya Development Team.
+ * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage example
- * @author Example module development team
+ * @subpackage Example Module
  */
 
 /**
  * Display the user menu hook
  * This is a standard function to provide a link in the "Your Account Page"
  *
+ * @author the Example module development team
  * @param  $phase is the which part of the loop you are on
  */
 function example_user_usermenu($args)
 {
     extract($args);
-    // Security check  - if the user has read access to the menu, show a
-    // link to display the details of the item
+    /* Security check  - if the user has read access to the menu, show a
+     * link to display the details of the item
+     */
     if (!xarSecurityCheck('ViewExample')) return;
-    // First, lets find out where we are in our logic.  If the phase
-    // variable is set, we will load the correct page in the loop.
+    
+    /* First, lets find out where we are in our logic.  If the phase
+     * variable is set, we will load the correct page in the loop.
+     */
     if (!xarVarFetch('phase', 'str:1:100', $phase, 'menu', XARVAR_NOT_REQUIRED)) return;
 
     switch (strtolower($phase)) {
         case 'menu':
-            // We need to define the icon that will go into the page.
+            /* We need to define the icon that will go into the page. */
             $icon = 'modules/example/xarimages/preferences.gif';
-            // Now lets send the data to the template which name we choose here.
+
+            /* Now lets send the data to the template which name we choose here. */
             $data = xarTplModule('example', 'user', 'usermenu_icon', array('iconbasic' => $icon));
 
             break;
 
         case 'form':
-            // Its good practice for the user menu to be personalized.  In order to do so, we
-            // need to get some information about the user.
+            /* Its good practice for the user menu to be personalized.  In order to do so, we
+             * need to get some information about the user.
+             */
             $name = xarUserGetVar('name');
             $uid = xarUserGetVar('uid');
-            // We also need to set the SecAuthKey, in order to stop hackers from setting user
-            // vars off site.
+            /* We also need to set the SecAuthKey, in order to stop hackers from setting user
+             * vars off site.
+             */
             $authid = xarSecGenAuthKey('example');
-            // Lets get the value that we want to override from the preferences. Notice that we are
-            // xarModUserGetMod and not xarModGetVar so we can grab the overridden value.  You do
-            // not have to use a user variable for every module var that the module posses, just
-            // the variables that you want to override.
+            /* Lets get the value that we want to override from the preferences. Notice that we are
+             * xarModUserGetMod and not xarModGetVar so we can grab the overridden value.  You do
+             * not have to use a user variable for every module var that the module posses, just
+             * the variables that you want to override.
+             */
             $value = xarModGetUserVar('example', 'itemsperpage', $uid);
-            // if (empty($value)){
-            // $value = xarModGetVar('example', 'itemsperpage');
-            // }
-            // Now lets send the data to the template which name we choose here.
+            /* if (empty($value)){
+             * $value = xarModGetVar('example', 'itemsperpage');
+             * }
+             * Now lets send the data to the template which name we choose here.
+             */
             $data = xarTplModule('example', 'user', 'usermenu_form', array('authid' => $authid,
                     'name' => $name,
                     'uid' => $uid,
@@ -60,24 +66,28 @@ function example_user_usermenu($args)
             break;
 
         case 'update':
-            // First we need to get the data back from the template in order to process it.
-            // The example module is not setting any user vars at this time, but an example
-            // might be the number of items to be displayed per page.
+            /* First we need to get the data back from the template in order to process it.
+             * The example module is not setting any user vars at this time, but an example
+             * might be the number of items to be displayed per page.
+             */
             if (!xarVarFetch('uid', 'int:1:', $uid)) return;
             if (!xarVarFetch('itemsperpage', 'str:1:100', $itemsperpage, '20', XARVAR_NOT_REQUIRED)) return;
             if (!xarVarFetch('name', 'str:1:100', $name, '', XARVAR_NOT_REQUIRED)) return;
-            // Confirm authorisation code.
+            
+            /* Confirm authorisation code. */
             if (!xarSecConfirmAuthKey()) return;
 
             xarModSetUserVar('example', 'itemsperpage', $itemsperpage, $uid);
-            // Redirect back to the account page.  We could also redirect back to our form page as
-            // well by adding the phase variable to the array.
+            /* Redirect back to the account page.  We could also redirect back to our form page as
+             * well by adding the phase variable to the array.
+             */
             xarResponseRedirect(xarModURL('roles', 'user', 'account'));
 
             break;
     }
-    // Finally, we need to send our variables to block layout for processing.  Since we are
-    // using the data var for processing above, we need to do the same with the return.
+    /* Finally, we need to send our variables to block layout for processing.  Since we are
+     * using the data var for processing above, we need to do the same with the return.
+     */
     return $data;
 }
 ?>
--- xaruserapi/encode_shorturl.php
+++ xaruserapi/encode_shorturl.php
@@ -1,14 +1,11 @@
 <?php
 /**
- * Support for short URLs (user functions)
- * 
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2003 by the Xaraya Development Team.
+ * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage example
- * @author Example module development team 
+ * @subpackage Example Module
  */
 /*
  * Support for short URLs (user functions)
@@ -60,82 +57,89 @@
  */
 function example_userapi_encode_shorturl($args)
 { 
-    // Get arguments from argument array
-    extract($args); 
-    // Check if we have something to work with
+    /* Get arguments from argument array */
+    extract($args);
+    /* Check if we have something to work with */
     if (!isset($func)) {
         return;
-    } 
-    // Note : make sure you don't pass the following variables as arguments in
-    // your module too - adapt here if necessary
-    // default path is empty -> no short URL
-    $path = ''; 
-    // if we want to add some common arguments as URL parameters below
-    $join = '?'; 
-    // we can't rely on xarModGetName() here -> you must specify the modname !
-    $module = 'example'; 
-    // specify some short URLs relevant to your module
+    }
+    /* Note : make sure you don't pass the following variables as arguments in
+     * your module too - adapt here if necessary
+     * default path is empty -> no short URL
+     */
+    $path = '';
+    /* if we want to add some common arguments as URL parameters below */
+    $join = '?';
+
+    /* we can't rely on xarModGetName() here -> you must specify the modname ! */
+    $module = 'example';
+    /* specify some short URLs relevant to your module *?
     if ($func == 'main') {
-        $path = '/' . $module . '/'; 
-        // Note : if your main function calls some other function by default,
-        // you should set the path to directly to that other function
+        $path = '/' . $module . '/';
+        /* Note : if your main function calls some other function by default,
+         * you should set the path to directly to that other function
+         */
     } elseif ($func == 'view') {
-        $path = '/' . $module . '/list.html'; 
-        // we'll add the optional $startnum parameter below, as a regular
-        // URL parameter
-        // you might have some additional parameter that you want to use to
-        // create different virtual paths here - for example a category name
-        // if (!empty($cid) && is_numeric($cid)) {
-        // // use a cache to avoid re-querying for each URL in the same cat
-        // static $catcache = array();
-        // if (xarModAPILoad('categories','user')) {
-        // if (isset($catcache[$cid])) {
-        // $cat = $catcache[$cid];
-        // } else {
-        // $cat = xarModAPIFunc('categories','user','getcatinfo',
-        // array('cid' => $cid));
-        // // put the category in cache
-        // $catcache[$cid] = $cat;
-        // }
-        // if (!empty($cat) && !empty($cat['name'])) {
-        // // use the category name as part of the path here
-        // $path = '/' . $module . '/' . rawurlencode($cat['name']);
-        // }
-        // }
-        // }
-        // if you have some additional parameters that you want to keep as
-        // regular URL parameters - example for an array :
-        // if (isset($other) && is_array($other) && count($other) > 0) {
-        // foreach ($other as $id => $val) {
-        // $path .= $join . 'other['.$id.']='.$val;
-        // // change the join character (once would be enough)
-        // $join = '&';
-        // }
-        // }
+        $path = '/' . $module . '/list.html';
+        /* we'll add the optional $startnum parameter below, as a regular
+         * URL parameter
+         * you might have some additional parameter that you want to use to
+         * create different virtual paths here - for example a category name
+         * if (!empty($cid) && is_numeric($cid)) {
+         * // use a cache to avoid re-querying for each URL in the same cat
+         * static $catcache = array();
+         * if (xarModAPILoad('categories','user')) {
+         * if (isset($catcache[$cid])) {
+         * $cat = $catcache[$cid];
+         * } else {
+         * $cat = xarModAPIFunc('categories','user','getcatinfo',
+         * array('cid' => $cid));
+         * // put the category in cache
+         * $catcache[$cid] = $cat;
+         * }
+         * if (!empty($cat) && !empty($cat['name'])) {
+         * // use the category name as part of the path here
+         * $path = '/' . $module . '/' . rawurlencode($cat['name']);
+         * }
+         * }
+         * }
+         * if you have some additional parameters that you want to keep as
+         * regular URL parameters - example for an array :
+         * if (isset($other) && is_array($other) && count($other) > 0) {
+         * foreach ($other as $id => $val) {
+         * $path .= $join . 'other['.$id.']='.$val;
+         * // change the join character (once would be enough)
+         * $join = '&';
+         * }
+         * }
+         */
     } elseif ($func == 'display') {
-        // check for required parameters
+        /* check for required parameters */
         if (isset($exid) && is_numeric($exid)) {
-            $path = '/' . $module . '/' . $exid . '.html'; 
-            // you might have some additional parameter that you want to use to
-            // create different virtual paths here - for example a category name
-            // See above for an example...
+            $path = '/' . $module . '/' . $exid . '.html';
+            /* you might have some additional parameter that you want to use to
+             * create different virtual paths here - for example a category name
+             * See above for an example...
+             */
         } else {
-            // we don't know how to handle that -> don't create a path here
-            // Note : this generally means that someone tried to create a
-            // link to your module, but used invalid parameters for xarModURL
-            // -> you might want to provide a default path to return to
-            // $path = '/' . $module . '/list.html';
-        } 
+            /* we don't know how to handle that -> don't create a path here
+             * Note : this generally means that someone tried to create a
+             * link to your module, but used invalid parameters for xarModURL
+             * -> you might want to provide a default path to return to
+             * $path = '/' . $module . '/list.html';
+             */
+        }
     } else {
-        // anything else that you haven't defined a short URL equivalent for
-        // -> don't create a path here
-    } 
-    // add some other module arguments as standard URL parameters
+        /* anything else that you haven't defined a short URL equivalent for
+         * -> don't create a path here
+         */
+    }
+    /* add some other module arguments as standard URL parameters */
     if (!empty($path)) {
         if (isset($startnum)) {
             $path .= $join . 'startnum=' . $startnum;
             $join = '&';
-        } 
+        }
         if (!empty($catid)) {
             $path .= $join . 'catid=' . $catid;
             $join = '&';
@@ -144,12 +148,12 @@ function example_userapi_encode_shorturl($args)
                 $catid = join('+', $cids);
             } else {
                 $catid = join('-', $cids);
-            } 
+            }
             $path .= $join . 'catid=' . $catid;
             $join = '&';
-        } 
-    } 
+        }
+    }
 
     return $path;
-} 
+}
 ?>
