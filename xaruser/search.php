1f2e4602650f1d307b2798f571777080c4d0b79f
jojodee <jojodee@xaraya.com>
2005-09-28 04:24:25 +0000

    And I suppose i should actually add the search files ;)

---
+++ xaruser/search.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * Example Module
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Release
+ */
+
+/*
+ * @author Jo Dalle Nogare
+ * Search for an example
+ *
+ */
+function example_user_search()
+{
+    /* Required search hook fields */
+    if (!xarVarFetch('q',         'isset',  $q,        NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('bool',      'isset',  $bool,     NULL, XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('sort',      'isset',  $sort,     NULL, XARVAR_DONT_SET)) return;
+
+    /* Standard Pager information */
+    if(!xarVarFetch('startnum', 'int:0', $startnum,  NULL, XARVAR_NOT_REQUIRED)) {return;}
+
+    /* example module fields for possible searching and identification */
+    if (!xarVarFetch('name',   'str:0:', $name,  '',   XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('number', 'str:0:', $number,'',   XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('exid',      'id',  $exid,   NULL, XARVAR_NOT_REQUIRED)) return;
+
+    /* example for search where an author is involved, not included in this example module */
+    /* if(!xarVarFetch('author',     'isset',  $author,   NULL, XARVAR_DONT_SET)) {return;}
+     */
+
+    $data       = array();
+    $search     = array();
+
+    if($q == ''){
+        return $data;
+    }
+    /* Set some defaults here for the search and search result display */
+    if (!isset($startnum)) {
+        $startnum = 1;
+    }
+    if (!isset($numitems)) {
+        $numitems = 10;
+    }
+
+    /* Your module may need searching by more 'complex' fields such as author
+     * and if so you might include some similar code as below
+     */
+     /*
+    if (!isset($q) || strlen(trim($q)) <= 0) {
+        if (isset($author) && strlen(trim($author)) > 0) {
+            $q = $author;
+            $search['author']=$author;
+            $data['authorsearch']=1;
+        }
+    } else {
+        $search['author']='';
+        $data['authorsearch']=1;
+    }
+    */
+
+
+    /* Setup the data for the search form for the checkboxes and
+     * and other information needed for display
+     */
+
+    if (isset($exid)) {
+        $search['exid'] = $q;
+        $data['exid']=1;
+    } else {
+        $data['exid']=0;
+        $exid=0;
+    }
+    if (isset($xarname)) {
+        $search['name'] = $q;
+        $data['name']=1;
+    } else {
+        $data['name']=0;
+        $regname='';
+    }
+    if (isset($xarnumber)) {
+        $search['number'] = $q;
+        $data['number']=1;
+    } else {
+        $data['number']=0;
+        $exid=0;
+    }
+
+    /* Example code that you might use to find the uid of the author we're looking for
+     * Adjust for your own module if required. Demonstrates use of roles api call
+     * rather than direct db function to protect against roles table changes for example
+     */
+
+    /*if (!empty($author)) {
+        // Load API
+        if (!xarModAPILoad('roles', 'user')) return;
+        $user = xarModAPIFunc('roles','user','get',
+                             array('name' => $author));
+        if (!empty($user['uid'])) {
+            $search['authorid'] = $user['uid'];
+        } else {
+            $search['authorid']= null;
+            $search['author'] = null;
+        }
+    } else {
+        $search['authorid'] = null;
+        $search['author'] = null;
+    }
+    */
+    $search['q']=$q;
+
+    /* Call search hook for example module to Search example information */
+    if (!empty($q) || !empty($search)) {
+        $data['example'] = xarModAPIFunc('example','user','search',$search);
+    }
+    /* Prepare the message to return to search template if no match found */
+    if (empty($data['example'])){
+        $data['status'] = xarML('No Example item found that matches your search');
+    }
+
+    return $data;
+}
+?>
---
+++ xaruserapi/search.php
@@ -0,0 +1,100 @@
+<?php
+/*
+ * Example Module
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2005 by the Xaraya Development Team
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Example
+ */
+
+/**
+ * Searches all example items
+ *
+ * @author jojodee
+ * @access private
+ * @returns mixed description of return
+ */
+function example_userapi_search($args)
+{
+    if (empty($args) || count($args) < 1) {
+        return;
+    }
+    extract($args);
+
+     if($q == ''){
+        return;
+    }
+
+    $dbconn =& xarDBGetConn();
+    $xartable =& xarDBGetTables();
+    $exampletable = & $xartable['example'];
+    $where = '';
+    $releases = array();
+    $sql = "SELECT  xar_exid,
+                    xar_name,
+                    xar_number
+              FROM  $exampletable
+              WHERE  (";
+
+    /* setup arrays for the sql where clause, and the bindvars array */
+    $bindvars = array();
+    
+    /* include search on exid - the item id - if required */
+    /*
+    if (isset($exid)) {
+        $sql .= "xar_exid = ?";
+        $bindvars[] = $exid;
+    }
+    */
+    
+    /* if you needed author search with uid
+     * you might do something like the following.
+     * Don't forget to add to the other checks following as well*/
+    /*if (isset($uid)) {
+        if (isset($exid)) {
+            $sql .= " OR ";
+        }
+        $sql .= "xar_uid LIKE ? ";
+        $bindvars[] = $uid;
+    }
+    */
+
+    if (isset($name)) {
+        $sql .= " xar_name LIKE ?";
+        $bindvars[] = $name;
+    }
+    if (isset($number)) {
+        if (isset($name)) {
+            $sql .= " OR ";
+        }
+        $sql .= " xar_number = ?";
+        $bindvars[] = $number;
+    }
+    /* we will sort by the item name here, ascending */
+    $sql .=")  ORDER BY xar_name ASC";
+
+    $result =& $dbconn->Execute($sql, $bindvars);
+    if (!$result) return;
+    /* no results to return .. then return none :p */
+    if ($result->EOF) {
+        return array();
+    }
+    for (; !$result->EOF; $result->MoveNext()) {
+        list($exid, $name, $number) = $result->fields;
+        /* don't forget to add your user id uid here if you're searching on author */
+        if (xarSecurityCheck('ReadExample', 0)) {
+            $example[] = array('exid'   => $exid,
+                                'name'   => $name,
+                                'number' => $number);
+        }
+    }
+    $result->Close();
+
+    /* Return any results */
+    return $example;
+
+}
+?>
