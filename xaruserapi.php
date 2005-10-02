6bc9c29d82128080b5d9b2aadd5c76ec2d631959
niceguyeddie <niceguyeddie@xaraya.com>
2005-10-02 03:17:53 +0000

    initial cut of nofollow

---
+++ xaruserapi.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * File: $Id$
+ * 
+ * Google Rel=NoFollow Transform
+ * 
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2002 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.org
+ *
+ * @subpackage NoFollow
+ * @author John Cox
+*/
+function nofollow_userapi_transform($args) {
+    extract($args);
+    if ((!isset($objectid)) ||
+        (!isset($extrainfo))) {
+        $msg = xarML('Invalid Parameter Count', join(', ',$invalid), 'admin', 'create', 'bbcode');
+        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
+        return $args;
+    }
+    if (is_array($extrainfo)) {
+        if (isset($extrainfo['transform']) && is_array($extrainfo['transform'])) {
+            foreach ($extrainfo['transform'] as $key) {
+                if (isset($extrainfo[$key])) {
+                    $extrainfo[$key] = nofollow_transform($extrainfo[$key]);
+                }
+            }
+            return $extrainfo;
+        }
+        foreach ($extrainfo as $text) {
+            $result[] = nofollow_transform($text);
+        }
+    } else {
+        $result = nofollow_transform($text);
+    }
+    return $result;
+}
+function nofollow_transform($text) {
+    $text = preg_replace('/<a([^>]+)>/i', '<a\\1 rel="nofollow">', $text);
+    return $text;
+}
+?>
---
+++ xarversion.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * File: $Id$
+ * 
+ * Google Rel=NoFollow Transform
+ * 
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2002 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.org
+ *
+ * @subpackage NoFollow
+ * @author John Cox
+*/
+$modversion['name']             = 'nofollow';
+$modversion['version']          = '1.0.0';
+$modversion['id']               = '267';
+$modversion['description']      = 'The nofollow module provides the nofollow tag on all links where transform is used.';
+$modversion['license']          = 'xardocs/license.txt';
+$modversion['official']         = 0;
+$modversion['author']           = 'John Cox';
+$modversion['contact']          = 'http://www.wyome.com';
+$modversion['admin']            = 0;
+$modversion['user']             = 0;
+$modversion['securityschema']   = array();
+$modversion['class']            = 'Utility';
+$modversion['category']         = 'Global';
+?>
