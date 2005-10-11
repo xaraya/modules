bb242eeed6ef574fe3223b0b7392e11806082344
michelv <michelv@xarayahosting.nl>
2005-10-11 14:58:10 +0000

    Moving the call for a hooked event to its own API
    	* xaruserapi/gethooked.php: new file
    	* xaruser/displayhook.php: ...

--- xaruser/displayhook.php
+++ xaruser/displayhook.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * File: $Id$
- *
  * Display hook
  *
- * @package julian
- * @copyright (C) 2005 by the Xaraya Development Team.
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -20,6 +18,7 @@
  *
  * @author  Julian Development Team, Jorn MichelV. <michelv@xarayahosting.nl>
  * @access  public 
+ * @param   id $objectid
  * @param   array $extrainfo
  * @return  array
  * @todo    MichelV. <#> Split into queries
@@ -56,74 +55,14 @@ function julian_user_displayhook($args)
     }
      
     // Get item type.
-     if (isset($extrainfo['itemtype']) && is_numeric($extrainfo['itemtype'])) {
+    if (isset($extrainfo['itemtype']) && is_numeric($extrainfo['itemtype'])) {
         $itemtype = $extrainfo['itemtype'];
     } else {
         $itemtype = 0;
     }
+    $item = xarModAPIFunc('julian', 'user', 'gethooked', array('modid' => $modid, 'itemtype' => $itemtype, 'objectid' => $objectid));
 
-    $bl_data = array();
-    
-   // Load up database
-   $dbconn =& xarDBGetConn();
-   $xartable = xarDBGetTables();
-   $event_linkage_table = $xartable['julian_events_linkage'];
-
-    // Try to find the link for the current module, item type and item id.
-   $query = "SELECT * FROM " .  $event_linkage_table . " WHERE (`hook_modid`=$modid AND `hook_itemtype`=$itemtype AND `hook_iid`=$objectid)";
-   $result =& $dbconn->Execute($query);
-   if (!empty($result)) {
-        if (!$result->EOF) {
-            $edit_obj = $result->FetchObject(false);
-            
-            // Start/end date (and time)
-            $event_startdate = strtotime($edit_obj->dtstart);
-            $event_enddate   = strtotime($edit_obj->recur_until);
-            
-            $bl_data['event_startdate'] = date("F j, Y",$event_startdate);
-            $bl_data['event_starttime'] = date("g:i A",$event_startdate);
-            $bl_data['event_enddate'] = strcmp($event_enddate,'')==0 ? '' : date("F j, Y",$event_enddate);
-            
-            // All day or not
-            $bl_data['event_allday'] = ($edit_obj->isallday==1);
-
-            // Event duration
-            if (strcmp($edit_obj->duration,'')!=0) {
-                list($bl_data['event_dur_hours'], $bl_data['event_dur_minutes']) = explode(':',$edit_obj->duration);
-            }
-
-            //Checking to see which repeating rule was used so the event_repeat can be set.
-            if ($edit_obj->rrule==3 && $edit_obj->recur_count && $edit_obj->recur_interval && $edit_obj->recur_freq)
-                $bl_data['event_repeat'] = 2;
-            else if ($edit_obj->rrule && $edit_obj->recur_freq) 
-               $bl_data['event_repeat'] = 1;
-            else
-                $bl_data['event_repeat'] = 0;
-
-            //Depending on which recurrence rule was used, set the appropriate form fields.
-            switch ($bl_data['event_repeat']) {
-                case 1:
-                    $bl_data['event_repeat_every_freq'] = $edit_obj->recur_freq;    // time unit (1=day, 2=week, 3=month, 4=year)
-                    $bl_data['event_repeat_every_type']  = $edit_obj->rrule;            // every n time units
-                    break;
-                case 2:
-                    $bl_data['event_repeat_on_day'] = $edit_obj->recur_count;        // day of the week (mon-sun)
-                    $bl_data['event_repeat_on_num'] = $edit_obj->recur_interval;    // instance within month (1=1st, 2=2nd, ..., 5=last)
-                    $bl_data['event_repeat_on_freq'] = $edit_obj->recur_freq;        // every n months
-                    break;
-            }
-            
-            $result->Close();
-        }
-        else {
-            return 'There is no event hooked to this item.';
-        }
-    }
-    else {
-        return 'There is no event hooked to this item.';
-    }
-
-    return xarTplModule('julian','user','displayhook',$bl_data);
+    return xarTplModule('julian','user','displayhook',$item);
 }
 
 ?>
---
+++ xaruserapi/gethooked.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Get a hooked item
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Julian Module
+ * @link http://xaraya.com/index.php/release/319.html
+ * @author Julian Module Development Team
+ */
+
+/**
+ * Get a hooked item
+ * 
+ * The data for this hook is stored inside Julian in a seperate table. 
+ * In the long run, Julian should hook to itself
+ * This all has only been tested with Articles
+ *
+ * @author MichelV michelv@xarayahosting.nl
+ * @author Jorn
+ * @param  $args ['objectid'] id of item to get
+ * @returns array
+ * @return item array, or false on failure
+ * @raise BAD_PARAM, DATABASE_ERROR, NO_PERMISSION
+ */
+function julian_userapi_gethooked($args)
+{
+    extract($args);
+    /* Argument check - make sure that all required arguments are present and
+     * in the right format, if not then set an appropriate error message
+     * and return
+     */
+    if (!isset($modid) || !is_numeric($modid)) {
+        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
+            'item ID', 'user', 'gethooked', 'Julian');
+        xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
+            new SystemException($msg));
+        return;
+    }
+    
+    if (!isset($objectid) || !is_numeric($objectid)) {
+        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
+            'item ID', 'user', 'gethooked', 'Julian');
+        xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
+            new SystemException($msg));
+        return;
+    }
+   // Load up database
+   $dbconn =& xarDBGetConn();
+   $xartable = xarDBGetTables();
+   $event_linkage_table = $xartable['julian_events_linkage'];
+
+    // Try to find the link for the current module, item type and item id.
+   $query = "SELECT * FROM " .  $event_linkage_table . " WHERE (`hook_modid`=$modid AND `hook_itemtype`=$itemtype AND `hook_iid`=$objectid)";
+   $result =& $dbconn->Execute($query);
+   if (!empty($result)) {
+        if (!$result->EOF) {
+            $edit_obj = $result->FetchObject(false);
+            
+            // Start/end date (and time)
+            $event_startdate = strtotime($edit_obj->dtstart);
+            $event_enddate   = strtotime($edit_obj->recur_until);
+            
+            $item['event_startdate'] = date("F j, Y",$event_startdate);
+            $item['event_starttime'] = date("g:i A",$event_startdate);
+            $item['event_enddate'] = strcmp($event_enddate,'')==0 ? '' : date("F j, Y",$event_enddate);
+            
+            // All day or not
+            $item['event_allday'] = ($edit_obj->isallday==1);
+
+            // Event duration
+            if (strcmp($edit_obj->duration,'')!=0) {
+                list($item['event_dur_hours'], $item['event_dur_minutes']) = explode(':',$edit_obj->duration);
+            }
+
+            //Checking to see which repeating rule was used so the event_repeat can be set.
+            if ($edit_obj->rrule==3 && $edit_obj->recur_count && $edit_obj->recur_interval && $edit_obj->recur_freq)
+                $item['event_repeat'] = 2;
+            else if ($edit_obj->rrule && $edit_obj->recur_freq) 
+               $item['event_repeat'] = 1;
+            else
+                $item['event_repeat'] = 0;
+
+            //Depending on which recurrence rule was used, set the appropriate form fields.
+            switch ($item['event_repeat']) {
+                case 1:
+                    $item['event_repeat_every_freq'] = $edit_obj->recur_freq;    // time unit (1=day, 2=week, 3=month, 4=year)
+                    $item['event_repeat_every_type']  = $edit_obj->rrule;        // every n time units
+                    break;
+                case 2:
+                    $item['event_repeat_on_day'] = $edit_obj->recur_count;       // day of the week (mon-sun)
+                    $item['event_repeat_on_num'] = $edit_obj->recur_interval;    // instance within month (1=1st, 2=2nd, ..., 5=last)
+                    $item['event_repeat_on_freq'] = $edit_obj->recur_freq;       // every n months
+                    break;
+            }
+            
+            $result->Close();
+        }
+        else {
+            return xarML('There is no event hooked to this item.');
+        }
+    }
+    else {
+        return xarML('There is no event hooked to this item.');
+    }    
+
+    if (!xarSecurityCheck('ViewJulian')) { // TODO
+        return;
+    }
+    // Return the item array 
+    return $item;
+}
+?>
