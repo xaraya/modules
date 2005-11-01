553cb0c8c2b503b3b80968835443e187a5f62de1
michelv <michelv@xarayahosting.nl>
2005-11-01 16:11:48 +0000

    Adding new files Moved all block stuff into their respective files.
    	* xarblocks/modify-calevent.php: new file
    	* xarblocks/modify-calmonth.php: new file
    	* xartemplates/blocks/modify-calmonth.xd: new file

---
+++ xarblocks/modify-calevent.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Modify block settings
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Julian Module
+ * @link http://xaraya.com/index.php/release/36.html
+ * @author Example Module Development Team
+ */
+
+/**
+ * Julian Event Block - Modify block settings
+ *
+ * @author Julian Module development team
+ * @author MichelV MichelV@xarayahosting.nl
+ *
+ * @access  public
+ * @param   $blockinfo
+ * @return  $blockinfo data array
+*/
+function julian_caleventblock_modify($blockinfo)
+{
+    // Break out options from our content field
+    if (!is_array($blockinfo['content'])) {
+        $vars = unserialize($blockinfo['content']);
+    } else {
+        $vars = $blockinfo['content'];
+    }
+
+    // Defaults
+    if (!isset($vars['EventBlockDays'])) {
+        $vars['EventBlockDays'] = 7;
+    }
+    /* Defaults */
+    if (empty($vars['CatAware'])) {
+        $vars['CatAware'] = false;
+    } 
+    $vars['blockid'] = $blockinfo['bid'];
+    return $vars;
+}
+
+/**
+ * Updates the Block settings
+ */
+function julian_caleventblock_update($blockinfo)
+{
+    $vars = array();
+    if (!xarVarFetch('EventBlockDays', 'int', $vars['EventBlockDays'], 0, XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('CatAware', 'checkbox', $vars['CatAware'], FALSE, XARVAR_NOT_REQUIRED)) return;
+
+    $blockinfo['content'] = $vars;
+
+    return $blockinfo;
+}
+?>
---
+++ xarblocks/modify-calmonth.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Modify block settings
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Julian Module
+ * @link http://xaraya.com/index.php/release/36.html
+ * @author Example Module Development Team
+ */
+
+/**
+ * Julian Month Block - Modify block settings
+ *
+ * @author Julian Module development team
+ * @author MichelV MichelV@xarayahosting.nl
+ */
+function julian_calmonthblock_modify($blockinfo)
+{ 
+    /* Get current content */
+    if (!is_array($blockinfo['content'])) {
+        $vars = @unserialize($blockinfo['content']);
+    } else {
+        $vars = $blockinfo['content'];
+    }
+
+    /* Defaults */
+    if (empty($vars['CatAware'])) {
+        $vars['CatAware'] = false;
+    } 
+    $vars['blockid'] = $blockinfo['bid'];
+    
+    /* Send content to template */
+    return $vars;
+} 
+
+/**
+ * Update block settings
+ */
+function julian_calmonthblock_update($blockinfo)
+{
+    $vars = array();
+    if (!xarVarFetch('CatAware', 'checkbox', $vars['CatAware'], FALSE, XARVAR_NOT_REQUIRED)) return;
+    $blockinfo['content'] = $vars;
+    $vars['blockid'] = $blockinfo['bid'];
+    return $blockinfo;
+} 
+?>
