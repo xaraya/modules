871f70cbe7023e4654cedcc3f786cc750f72738e
jojodee <jojodee@xaraya.com>
2005-12-02 18:59:21 +0000

    Update headers

--- xaruser/contactus.php
+++ xaruser/contactus.php
@@ -2,12 +2,14 @@
 /**
  * Contact us main function
  *
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage Sitecontact
+ * @subpackage SiteContact Module
+ * @link http://xaraya.com/index.php/release/890.html
+ * @author Jo Dalle Nogare <jojodee@xaraya.com>
  */
 
 /**
