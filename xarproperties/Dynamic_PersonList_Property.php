fcbacbc36740fed4cffbe02d050bcee03eeaf07b
michelv <michelv@xarayahosting.nl>
2005-12-02 21:12:24 +0000

    Work on dynamic property 	* showinput-personlist.xd: dropped

--- xarproperties/Dynamic_PersonList_Property.php
+++ xarproperties/Dynamic_PersonList_Property.php
@@ -1,31 +1,39 @@
 <?php
 /**
- * Dynamic State List Property
+ * Dynamic Person List Property
  *
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @copyright (C) 2002-2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage Base module
+ * @subpackage SIGMA Personnel module
  */
-/*
- * @author John Cox
-*/
+/**
+ * @author MichelV
+ */
 include_once "modules/base/xarproperties/Dynamic_Select_Property.php";
 
 /**
- * handle the userlist property
+ * handle the personlist property
+ * This is a list type of property, it allows you to select a person from this module
  *
  * @package dynamicdata
  *
  */
-class Dynamic_StateList_Property extends Dynamic_Select_Property
+class Dynamic_PersonList_Property extends Dynamic_Select_Property
 {
 
-    function Dynamic_StateList_Property($args)
+    function Dynamic_PersonList_Property($args)
     {
         $this->Dynamic_Select_Property($args);
+        // Initialise the select option list.
+        $this->options = array();
+
+        // Handle user options if supplied.
+        if (!empty($this->validation)) {
+            $this->parseValidation($this->validation);
+        }
     }
 
     function validateValue($value = null)
@@ -34,10 +42,10 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
             $value = $this->value;
         }
         if (!empty($value)) {
-            if (is_string($value)) {
+            if (is_string($value)) { // Place simple check here
                 $this->value = $value;
             } else {
-                $this->invalid = xarML('State Listing');
+                $this->invalid = xarML('Person Listing');
                 $this->value = null;
                 return false;
             }
@@ -46,6 +54,32 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
         }
         return true;
     }
+
+    /*
+    // TODO: validate the selected user against the specified group(s).
+    function validateValue($value = null)
+    {
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (!empty($value)) {
+            // check if this is a valid user id
+            $uname = xarUserGetVar('uname', $value);
+            if (isset($uname)) {
+                $this->value = $value;
+                return true;
+            } else {
+                xarErrorHandled();
+            }
+        } elseif (empty($value)) {
+            $this->value = $value;
+            return true;
+        }
+        $this->invalid = xarML('selection');
+        $this->value = null;
+        return false;
+    }
+    */
 
 //    function showInput($name = '', $value = null, $options = array(), $id = '', $tabindex = '')
     function showInput($args = array())
@@ -65,108 +99,18 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
         $data['name']  = $name;
         $data['id']    = $id;
 
-       /*
-        $out = '<select' .
-       ' name="' . $name . '"' .
-       ' id="'. $id . '"' .
-       (!empty($tabindex) ? ' tabindex="'.$tabindex.'" ' : '') .
-       '>';
-       */
-        $soptions = array();
-        $soptions[] = array('id' =>'Please select', 'name' =>'Please select' );
-        $soptions[] = array('id' =>'Alabama', 'name' =>'Alabama');
-        $soptions[] = array('id' =>'Alaska', 'name' =>'Alaska');
-        $soptions[] = array('id' =>'Arizona', 'name' =>'Arizona');
-        $soptions[] = array('id' =>'Arkansas', 'name' =>'Arkansas');
-        $soptions[] = array('id' =>'California', 'name' =>'California');
-        $soptions[] = array('id' =>'Colorado', 'name' =>'Colorado');
-        $soptions[] = array('id' =>'Connecticut', 'name' =>'Connecticut');
-        $soptions[] = array('id' =>'Delaware', 'name' =>'Delaware');
-        $soptions[] = array('id' =>'Florida', 'name' =>'Florida');
-        $soptions[] = array('id' =>'Georgia', 'name' =>'Georgia');
-        $soptions[] = array('id' =>'Hawaii', 'name' =>'Hawaii');
-        $soptions[] = array('id' =>'Idaho', 'name' =>'Idaho');
-        $soptions[] = array('id' =>'Illinois', 'name' =>'Illinois');
-        $soptions[] = array('id' =>'Indiana', 'name' =>'Indiana');
-        $soptions[] = array('id' =>'Iowa     ', 'name' =>'Iowa');
-        $soptions[] = array('id' =>'Kansas     ', 'name' =>'Kansas');
-        $soptions[] = array('id' =>'Kentucky     ', 'name' =>'Kentucky');
-        $soptions[] = array('id' =>'Louisiana     ', 'name' =>'Louisiana');
-        $soptions[] = array('id' =>'Maine     ', 'name' =>'Maine');
-        $soptions[] = array('id' =>'Maryland     ', 'name' =>'Maryland');
-        $soptions[] = array('id' =>'Massachusetts     ', 'name' =>'Massachusetts');
-        $soptions[] = array('id' =>'Michigan     ', 'name' =>'Michigan');
-        $soptions[] = array('id' =>'Minnesota     ', 'name' =>'Minnesota');
-        $soptions[] = array('id' =>'Mississippi     ', 'name' =>'Mississippi');
-        $soptions[] = array('id' =>'Missouri     ', 'name' =>'Missouri');
-        $soptions[] = array('id' =>'Montana     ', 'name' =>'Montana');
-        $soptions[] = array('id' =>'Nebraska     ', 'name' =>'Nebraska');
-        $soptions[] = array('id' =>'Nevada     ', 'name' =>'Nevada');
-        $soptions[] = array('id' =>'New Hampshire     ', 'name' =>'New Hampshire');
-        $soptions[] = array('id' =>'New Jersey     ', 'name' =>'New Jersey');
-        $soptions[] = array('id' =>'New Mexico     ', 'name' =>'New Mexico');
-        $soptions[] = array('id' =>'New York     ', 'name' =>'New York');
-        $soptions[] = array('id' =>'North Carolina     ', 'name' =>'North Carolina');
-        $soptions[] = array('id' =>'North Dakota     ', 'name' =>'North Dakota');
-        $soptions[] = array('id' =>'Ohio     ', 'name' =>'Ohio');
-        $soptions[] = array('id' =>'Oklahoma     ', 'name' =>'Oklahoma');
-        $soptions[] = array('id' =>'Oregon     ', 'name' =>'Oregon');
-        $soptions[] = array('id' =>'Pennsylvania     ', 'name' =>'Pennsylvania');
-        $soptions[] = array('id' =>'Rhode Island     ', 'name' =>'Rhode Island');
-        $soptions[] = array('id' =>'South Carolina     ', 'name' =>'South Carolina');
-        $soptions[] = array('id' =>'South Dakota     ', 'name' =>'South Dakota');
-        $soptions[] = array('id' =>'Tennessee     ', 'name' =>'Tennessee');
-        $soptions[] = array('id' =>'Texas     ', 'name' =>'Texas');
-        $soptions[] = array('id' =>'Utah     ', 'name' =>'Utah');
-        $soptions[] = array('id' =>'Vermont     ', 'name' =>'Vermont');
-        $soptions[] = array('id' =>'Virginia     ', 'name' =>'Virginia');
-        $soptions[] = array('id' =>'Washington     ', 'name' =>'Washington');
-        $soptions[] = array('id' =>'West Virginia     ', 'name' =>'West Virginia');
-        $soptions[] = array('id' =>'Wisconsin     ', 'name' =>'Wisconsin');
-        $soptions[] = array('id' =>'Wyoming     ', 'name' =>'Wyoming');
-        $soptions[] = array('id' =>'Alberta     ', 'name' =>'Alberta');
-        $soptions[] = array('id' =>'British Columbia     ', 'name' =>'British Columbia');
-        $soptions[] = array('id' =>'Manitoba     ', 'name' =>'Manitoba');
-        $soptions[] = array('id' =>'New Brunswick     ', 'name' =>'New Brunswick');
-        $soptions[] = array('id' =>'Newfoundland and Labrador', 'name' =>'Newfoundland and Labrador');
-        $soptions[] = array('id' =>'Northwest Territories     ', 'name' =>'Northwest Territories');
-        $soptions[] = array('id' =>'Nova Scotia     ', 'name' =>'Nova Scotia');
-        $soptions[] = array('id' =>'Nunavut     ', 'name' =>'Nunavut');
-        $soptions[] = array('id' =>'Ontario     ', 'name' =>'Ontario');
-        $soptions[] = array('id' =>'Prince Edward Island     ', 'name' =>'Prince Edward Island');
-        $soptions[] = array('id' =>'Quebec     ', 'name' =>'Quebec');
-        $soptions[] = array('id' =>'Saskatchewan     ', 'name' =>'Saskatchewan');
-        $soptions[] = array('id' =>'Yukon Territory     ', 'name' =>'Yukon Territory');
-        $soptions[] = array('id' =>'Australian Capital Territory     ', 'name' =>'Australian Capital Territory');
-        $soptions[] = array('id' =>'New South Wales     ', 'name' =>'New South Wales');
-        $soptions[] = array('id' =>'Northern Territory     ', 'name' =>'Northern Territory');
-        $soptions[] = array('id' =>'Queensland     ', 'name' =>'Queensland');
-        $soptions[] = array('id' =>'South Australia     ', 'name' =>'South Australia');
-        $soptions[] = array('id' =>'Tasmania     ', 'name' =>'Tasmania');
-        $soptions[] = array('id' =>'Victoria     ', 'name' =>'Victoria');
-        $soptions[] = array('id' =>'Western Australia', 'name' =>'Western Australia');
-        $soptions[] = array('id' =>'Other', 'name' =>'Other');
-        /*for($i=0; isset($soptions[$i]); $i++) {
-            $out .= '<option';
-            $out .= ' value="'.$soptions[$i]['name'].'"';
-            if ($value == $soptions[$i]['name']) {
-                $out .= ' selected="selected">'.$soptions[$i]['name'].'</option>';
-            } else {
-                $out .= '>'.$soptions[$i]['name'].'</option>';
-            }
-        }*/
+        $persons = array();
+        // TODO: Add get for personlist
+        $persons[] = array('id' =>0, 'name' =>'Please select' );
+        $persons = xarModAPIFunc('roles', 'user', 'getall', $select_options);
 
-        /*$out .= '</select>' .
-               (!empty($this->invalid) ? ' <span class="xar-error">'.xarML('Invalid #(1)', $this->invalid) .'</span>' : '');
-        */
-
-        $data['soptions'] = $soptions;
+        $data['persons'] = $persons;
         $data['invalid']  = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) : '';
         $data['tabindex'] =! empty($tabindex) ? $tabindex : 0;
 
 
         $template="";
-        return xarTplProperty('base', 'statelist', 'showinput', $data);
+        return xarTplProperty('sigmapersonnel', 'personlist', 'showinput', $data);
 
         //return $out;
     }
@@ -188,7 +132,7 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
              $data['id']=$id;
          }
          $template="";
-         return xarTplProperty('base', 'statelist', 'showoutput', $data);
+         return xarTplProperty('sigmapersonnel', 'personlist', 'showoutput', $data);
 
     }
 
@@ -202,23 +146,124 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
      {
          $args = array();
          $baseInfo = array(
-                              'id'         => 43,
-                              'name'       => 'statelisting',
-                              'label'      => 'State Dropdown',
-                              'format'     => '43',
-                              'validation' => '',
-                            'source'     => '',
-                            'dependancies' => '',
-                            'requiresmodule' => '',
+                            'id'             => 418,
+                            'name'           => 'SIGMApersonnellisting',
+                            'label'          => 'Person Dropdown',
+                            'format'         => '418',
+                            'validation'     => '',
+                            'source'         => '',
+                            'dependancies'   => '',
+                            'requiresmodule' => 'sigmapersonnel',
                             'aliases'        => '',
                             'args'           => serialize($args)
                             // ...
                            );
         return $baseInfo;
      }
+     // Do we need the below?
+    /**
+     * Show the current validation rule in a specific form for this property type
+     *
+     * @param $args['name'] name of the field (default is 'dd_NN' with NN the property id)
+     * @param $args['validation'] validation rule (default is the current validation)
+     * @param $args['id'] id of the field
+     * @param $args['tabindex'] tab index of the field
+     * @returns string
+     * @return string containing the HTML (or other) text to output in the BL template
+     */
+    function showValidation($args = array())
+    {
+        extract($args);
 
+        $data = array();
+        $data['name']       = !empty($name) ? $name : 'dd_'.$this->id;
+        $data['id']         = !empty($id)   ? $id   : 'dd_'.$this->id;
+        $data['tabindex']   = !empty($tabindex) ? $tabindex : 0;
+        $data['invalid']    = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) :'';
+        $data['size']       = !empty($size) ? $size : 50;
 
+        if (isset($validation)) {
+            $this->validation = $validation;
+        // CHECKME: reset grouplist et al. first if we call this from elsewhere ?
+            $this->parseValidation($validation);
+        }
 
+    // TODO: adapt if the template uses a multi-select for groups
+        $data['grouplist'] = join(',', $this->grouplist);
+        $data['userstate'] = $this->userstate;
+    // TODO: adapt if the template uses a multi-select for fields
+        $data['showlist']  = join(',', $this->showlist);
+        $data['orderlist'] = join(',', $this->orderlist);
+        $data['showglue']  = xarVarPrepForDisplay($this->showglue);
+        $data['other']     = '';
+
+        // allow template override by child classes
+        if (!isset($template)) {
+            $template = '';
+        }
+        return xarTplProperty('roles', 'userlist', 'validation', $data);
+    }
+
+    /**
+     * Update the current validation rule in a specific way for this property type
+     *
+     * @param $args['name'] name of the field (default is 'dd_NN' with NN the property id)
+     * @param $args['validation'] validation rule (default is the current validation)
+     * @param $args['id'] id of the field
+     * @returns bool
+     * @return bool true if the validation rule could be processed, false otherwise
+     */
+    function updateValidation($args = array())
+    {
+        extract($args);
+
+        // in case we need to process additional input fields based on the name
+        if (empty($name)) {
+            $name = 'dd_'.$this->id;
+        }
+        // do something with the validation and save it in $this->validation
+        if (isset($validation)) {
+            if (!is_array($validation)) {
+                $this->validation = $validation;
+
+            } elseif (!empty($validation['other'])) {
+                $this->validation = $validation['other'];
+
+            } else {
+                $options = array();
+                if (!empty($validation['grouplist'])) {
+                // TODO: adapt if the template uses a multi-select for groups
+                    $options[] = 'group:' . $validation['grouplist'];
+                }
+                if (!empty($validation['userstate']) && is_numeric($validation['userstate'])) {
+                    $options[] = 'state:' . $validation['userstate'];
+                }
+                if (!empty($validation['showlist'])) {
+                // TODO: adapt if the template uses a multi-select for fields
+                    $templist = explode(',', $validation['showlist']);
+                    // Remove invalid elements (fields that are not valid).
+                    $showfilter = create_function(
+                        '$a', 'return preg_match(\'/^[-]?(name|uname|email|uid|state|date_reg)$/\', $a);'
+                    );
+                    $templist = array_filter($templist, $showfilter);
+                    if (count($templist) > 0) {
+                        $options[] = 'show:' . join(',', $templist);
+                    }
+                }
+                if (!empty($validation['orderlist'])) {
+                // TODO: adapt if the template uses a multi-select for fields
+                    $options[] = 'order:' . $validation['orderlist'];
+                }
+                if (!empty($validation['showglue'])) {
+                    $validation['showglue'] = str_replace(';', '\;', $validation['showglue']);
+                    $options[] = 'showglue:' . $validation['showglue'];
+                }
+                $this->validation = join(';', $options);
+            }
+        }
+
+        // tell the calling function that everything is OK
+        return true;
+    }
 }
-
 ?>
