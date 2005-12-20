4cf8513c30914fc145d9c809612e8ffff2b430f6
brian <brian@envisionnet.net>
2005-12-20 16:16:32 +0000

    Upped version and added index.html files where missing.
    	* index.html: new file
    	* xaradmin/index.html: new file
    	* xaradminapi/index.html: new file
    	* xardata/index.html: new file
    	* xardocs/changelog: new file
    	* xardocs/index.html: new file
    	* xarimages/index.html: new file
    	* xarstyles/index.html: new file
    	* xartemplates/index.html: new file
    	* xaruser/index.html: new file
    	* xaruserapi/index.html: new file
    	* xarversion.php: ...

--- xarversion.php
+++ xarversion.php
@@ -25,8 +25,8 @@
 
 $modversion['id'] = '910';
 $modversion['name'] = 'helpdesk';
-$modversion['version'] = '0.7.1';
-$modversion['displayname']    = xarML('HelpDesk');
+$modversion['version'] = '0.7.2';
+$modversion['displayname'] = xarML('HelpDesk');
 $modversion['description'] = 'HelpDesk Module';
 $modversion['credits'] = 'docs/credits.txt';
 $modversion['help'] = 'docs/help.txt';
