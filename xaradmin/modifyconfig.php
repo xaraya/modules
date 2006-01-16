ec1b8ee601d18b2c18aa515fc51fb18d1d0319cd
jojodee <jojodee@xaraya.com>
2006-01-16 00:13:04 +0000

    Clean up and out old code

--- xaradmin/modifyconfig.php
+++ xaradmin/modifyconfig.php
@@ -20,21 +20,6 @@ function sitecontact_admin_modifyconfig()
 {
     if (!xarSecurityCheck('AdminSiteContact')) return;
 
- /*   if (!empty($scid)) {
-        $scformdata=array();
-        $scformdata=xarModAPIFunc('sitecontact','user','getcontacttypes',array('scid'=>(int)$scid));
-        $data['sctypename']= ucfirst($scformdata[0]['sctypename']).' '.xarML('Form Settings');
-        $data['customtext'] = $scformdata[0]['customtext'];
-        $data['customtitle'] = $scformdata[0]['customtitle'];
-        $data['optiontext'] = $scformdata[0]['optiontext'];
-        $data['usehtmlemail']= (int)$scformdata[0]['usehtmlemail'];
-        $data['allowcopy'] = (int)$scformdata[0]['allowcopy'];
-        $data['webconfirmtext'] = $scformdata[0]['webconfirmtext'];
-        $data['notetouser'] =  $scformdata[0]['notetouser'];
-        $data['scdefaultemail']= $scformdata[0]['scdefaultemail'];
-        $data['scdefaultname']= $scformdata[0]['scdefaultname'];
-        $data['scid']=$scid;
-    } else { // get global defaults */
        /* Specify some labels and values for display */
         $data['sctypename']= xarML('Global Configuration');
         $data['customtext']  = xarModGetVar('sitecontact', 'customtext');
@@ -79,22 +64,18 @@ function sitecontact_admin_modifyconfig()
            $scforms[]=$scform;
     }
     $data['scforms']=$scforms;
-/*
-    if (!isset($scid)) {//let's use all for hooks for now
-        $scid =null;
-    } else {
-*/
-       // call modifyconfig hooks with module only for general hooks
-       //  - not required for general config but leave for backward compatibility
+
+    // call modifyconfig hooks with module only for general hooks
+    //  - not required for general config but leave for backward compatibility
         $hooks = xarModCallHooks('module', 'modifyconfig', 'sitecontact',
-                             array('module'   => 'sitecontact'));
+                           array('module'   => 'sitecontact'));
 
         if (empty($hooks)) {
             $data['hooks'] = array('dynamicdata' => xarML('You can add Dynamic Data fields here by hooking Dynamic Data to Sitecontact'));
         } else {
             $data['hooks'] = $hooks;
         }
-//    }
+
     /* Do we need this here .. I don't think so */
     // Get the list of current hooks for item displays
     $hooklist = xarModGetHookList('sitecontact','item','display',0);
@@ -103,37 +84,9 @@ function sitecontact_admin_modifyconfig()
         $seenhook[$hook['module']] = 1;
     }
 
-    /* Create a link for each sitecontact form  */
-    //Don't need this
-/*    $scfilters = array();
-    $scitem = array();
-
-    // Link to default settings
-    $scitem['sctitle'] = xarML('Default');
-    $scitem['scid'] = 0;
-    $scitem['sclink'] = xarModURL('sitecontact','admin','modifyconfig');
-    $scfilters[] = $scitem;
-
-    $data['sctypelink']=xarModURL('sitecontact','admin','managesctypes');
-    // Links to settings per publication type
-    foreach ($scformdata as $id => $scform) {
-        $scitem['sclink'] = xarModURL('sitecontact','admin','modifyconfig',
-                                         array('scid' => $scform['scid']));
-
-        $scitem['sctitle'] = ucwords($scform['sctypename']);
-        $scitem['scid'] = $scform['scid'];
-        $scfilters[] = $scitem;
-    }
-    */
-  //  $data['scfilters'] = $scfilters;
     $data['authid'] = xarSecGenAuthKey();
-//    if (!isset($scid)  || $scid < 1) {
+
         $data['link'] = xarModURL('sitecontact','admin','updateconfig');
-  /*  } else {
-        $data['link'] = xarModURL('sitecontact','admin','managesctypes',
-                                 array('action' => 'update'));
-    }
-  */
     /* Return the template variables defined in this function */
     return $data;
 }
