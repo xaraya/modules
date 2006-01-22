467530db355db0661bd8881d80f60b424ede6595
michelv <michelv@xarayahosting.nl>
2006-01-22 12:16:20 +0000

    Bug 5358 Bug 5347 xaruser/jump.php: add check on trailing zeros Thanks to Zsolt

--- xaruser/jump.php
+++ xaruser/jump.php
@@ -1,16 +1,15 @@
 <?php
 /**
  * Jump to another date
- * 
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ *
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Julian
- * @author Julian module development team 
+ * @author Julian module development team
  */
- 
 /**
  *
  * Forwards from the jump form to the page it's suppose to go to based on the jump to date.
@@ -27,14 +26,21 @@
  */
 
 function julian_user_jump($args)
-{ 
-    //This takes a month,day,year, and location to jump to and forwards it on to the new location. 
-    extract($args); 
+{
+    //This takes a month,day,year, and location to jump to and forwards it on to the new location.
+    extract($args);
     unset($args);
     xarVarFetch('jump_to','str::',$jump_to);
     xarVarFetch('jump_month','int',$jump_month);
     xarVarFetch('jump_day','int',$jump_day);
     xarVarFetch('jump_year','int',$jump_year);
+    // Bug 5358, 5347
+    if ($jump_month < 10) {
+        $jump_month = '0'.$jump_month;
+    }
+    if ($jump_day < 10) {
+        $jump_day = '0'.$jump_day;
+    }
     xarResponseRedirect(xarModURL('julian', 'user', $jump_to, array('cal_date' => $jump_year . $jump_month . $jump_day)));
 }
 ?>
