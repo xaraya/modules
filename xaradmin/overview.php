76fec830bf8571f0208d7339ef000f2213200e1e
ftb <ftb@xaraya.com>
2006-02-01 17:43:46 +0000

    admin-main.xd cleaning and added overview in menu

---
+++ xaradmin/overview.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Overview displays standard Overview page
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage headlines module
+ * @author John Cox
+*/
+
+/**
+ * Overview displays standard Overview page
+ *
+ * Only used if you actually supply an overview link in your adminapi menulink function
+ * and used to call the template that provides display of the overview
+ *
+ * @returns array xarTplModule with $data containing template data
+ * @return array containing the menulinks for the overview item on the main manu
+ * @since 2 Oct 2005
+ */
+function headlines_admin_overview()
+{
+   /* Security Check */
+    if (!xarSecurityCheck('AdminHeadlines',0)) return;
+
+    $data=array();
+    
+    /* if there is a separate overview function return data to it
+     * else just call the main function that usually displays the overview 
+     */
+
+    return xarTplModule('headlines', 'admin', 'main', $data,'main');
+}
+
+?>
