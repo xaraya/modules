d552370c0075a0d35022d42f9512ccdcae750aab
michelv <michelv@xarayahosting.nl>
2006-02-04 10:33:51 +0000

    xarproperties/Dynamic_PersonList_Property.php: update header

--- xarproperties/Dynamic_PersonList_Property.php
+++ xarproperties/Dynamic_PersonList_Property.php
@@ -3,7 +3,7 @@
  * Dynamic Person List Property
  *
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2005-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
