9f79fb6f74a6adc2d429abe40c17a66f024b280f
michelv <michelv@xarayahosting.nl>
2006-02-10 11:27:31 +0000

    xaruserapi/decode_shorturl.php: add category name

--- xaruserapi/decode_shorturl.php
+++ xaruserapi/decode_shorturl.php
@@ -2,8 +2,8 @@
 /**
  * Extract function and arguments from short URLs for this module
  * 
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -83,30 +83,30 @@ function courses_userapi_decode_shorturl($params)
         // the first part might be something variable like a category name
         // In order to match that, you'll have to retrieve all relevant
         // categories for this module, and compare against them...
-        // $cid = xarModGetVar('courses','mastercids');
-        // if (xarModAPILoad('categories','user')) {
-        // $cats = xarModAPIFunc('categories',
-        // 'user',
-        // 'getcat',
-        // array('cid' => $cid,
-        // 'return_itself' => true,
-        // 'getchildren' => true));
-        // // lower-case for fanciful search engines/people
-        // $params[1] = strtolower($params[1]);
-        // $foundcid = 0;
-        // foreach ($cats as $cat) {
-        // if ($params[1] == strtolower($cat['name'])) {
-        // $foundcid = $cat['cid'];
-        // break;
-        // }
-        // }
-        // // check if we found a matching category
-        // if (!empty($foundcid)) {
-        // $args['cid'] = $foundcid;
-        // // TODO: now analyse $params[2] for index, list, \d+ etc.
-        // // and return array('whatever', $args);
-        // }
-        // }
+        $cid = xarModGetVar('courses','mastercids');
+        if (xarModAPILoad('categories','user')) {
+            $cats = xarModAPIFunc('categories',
+                                  'user',
+                                  'getcat',
+                                   array('cid' => $cid,
+                                   'return_itself' => true,
+                                   'getchildren' => true));
+            // lower-case for fanciful search engines/people
+            $params[1] = strtolower($params[1]);
+            $foundcid = 0;
+            foreach ($cats as $cat) {
+               if ($params[1] == strtolower($cat['name'])) {
+                   $foundcid = $cat['cid'];
+                   break;
+               }
+            }
+           // check if we found a matching category
+           if (!empty($foundcid)) {
+              $args['cid'] = $foundcid;
+           // TODO: now analyse $params[2] for index, list, \d+ etc.
+           // and return array('whatever', $args);
+           }
+        }
         // we have no idea what this virtual path could be, so we'll just
         // forget about trying to decode this thing
         // you *could* return the main function here if you want to
