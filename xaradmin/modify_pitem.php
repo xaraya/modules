ed30f9a953e56a2f4fb638106df617e36beea30f
michelv <michelv@xarayahosting.nl>
2006-02-11 13:51:10 +0000

    xaradmin/modify_pitem.php: update itemtype for courselevels

--- xaradmin/modify_pitem.php
+++ xaradmin/modify_pitem.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Modify a pitem
+ * Modify a plan_item
  *
  * @package modules
  * @copyright (C) 2002-2006 The Digital Development Foundation
@@ -12,13 +12,14 @@
  * @author ITSP Module Development Team
  */
 /**
- * Modify a pitem
+ * Modify a plan_item
  *
  * This is a standard function that is called whenever an administrator
  * wishes to modify a current module item
  *
  * @author ITSP Module Development Team
  * @param  $ 'pitemid' the id of the item to be modified
+ * @param all parts...
  */
 function itsp_admin_modify_pitem($args)
 {
@@ -26,17 +27,17 @@ function itsp_admin_modify_pitem($args)
 
     /* Get parameters from whatever input we need.
      */
-    if (!xarVarFetch('pitemid',     'id',     $pitemid,    $pitemid,      XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('objectid',    'id',     $objectid,   $objectid,    XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('invalid',     'array',  $invalid,    array(),      XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('pitemid',     'id',     $pitemid,    $pitemid,  XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('objectid',    'id',     $objectid,   $objectid, XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('invalid',     'array',  $invalid,    array(),   XARVAR_NOT_REQUIRED)) return;
 
-    if (!xarVarFetch('pitemname',   'str:1:', $pitemname,  '',    XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('pitemdesc',   'str:1:', $pitemdesc,  '',    XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('pitemrules',  'str:1:', $pitemrules, '',   XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('credits',     'int:1:', $credits,    '',     XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('mincredit',   'int:1:', $mincredit,  '',   XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('dateopen',    'int:1:', $dateopen,   '',    XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('dateclose',   'int:1:', $dateclose,  '',   XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('pitemname',   'str:1:', $pitemname,  '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('pitemdesc',   'str:1:', $pitemdesc,  '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('pitemrules',  'str:1:', $pitemrules, '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('credits',     'int:1:', $credits,    '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('mincredit',   'int:1:', $mincredit,  '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('dateopen',    'int:1:', $dateopen,   '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('dateclose',   'int:1:', $dateclose,  '', XARVAR_NOT_REQUIRED)) return;
 
     if (!xarVarFetch('rule_cat',    'int:1:',                      $rule_cat,    $rule_cat,    XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('rule_type',   'str:1:',                      $rule_type,   $rule_type,   XARVAR_NOT_REQUIRED)) return;
@@ -46,8 +47,7 @@ function itsp_admin_modify_pitem($args)
     if (!empty($objectid)) {
         $pitemid = $objectid;
     }
-    /* The user API function is called.
-     */
+    /* The user API function is called. */
     $item = xarModAPIFunc('itsp',
                           'user',
                           'get_planitem',
@@ -62,8 +62,7 @@ function itsp_admin_modify_pitem($args)
     }
 
     // get the levels in courses
-    $levels = xarModAPIFunc('courses', 'user', 'gets', array('itemtype' => 3));
-
+    $levels = xarModAPIFunc('courses', 'user', 'gets', array('itemtype' => 1003));
 
     /* Call hooks */
     $item['module'] = 'itsp';
