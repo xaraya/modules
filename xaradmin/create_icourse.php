06cf9a61a896a337d5b7c7358bd0c1e60e762c5c
michelv <michelv@xarayahosting.nl>
2006-02-21 11:25:57 +0000

    Add files for external course creation 	* xaradmin/create_icourse.php: new file 	* xaradminapi/create_icourse.php: new file

---
+++ xaradmin/create_icourse.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Create a new external course
+ *
+ * @package modules
+ * @copyright (C) 2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage ITSP Module
+ * @link http://xaraya.com/index.php/release/572.html
+ * @author ITSP Module Development Team
+ */
+/**
+ * Create a new external course, itsp course
+ *
+ * Standard function to create a new plan
+ * This is a standard function that is called with the results of the
+ * form supplied by xarModFunc('itsp','admin','new') to create a new item
+ *
+ * @author michelv <michelv@xarayahosting.nl>
+ * @since 21 Feb 2006
+ * @param  int credits
+ * @param  int mincredit
+ * @param string dateopen
+ * @todo michelv <1> implement correct return url
+ * @return bool true on succes with redirect URL
+ */
+function itsp_admin_create_icourse($args)
+{
+    extract($args);
+
+    // The general parameters
+    if (!xarVarFetch('itspid',   'id',    $itspid )) return;
+    if (!xarVarFetch('pitemid',  'id',    $pitemid)) return;
+    if (!xarVarFetch('objectid', 'id',    $objectid, '', XARVAR_NOT_REQUIRED)) return;
+    // The external course data
+    if (!xarVarFetch('icourseid',      'id',        $icourseid,      '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('icoursetitle',   'str:1:255', $icoursetitle,   '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('icourseloc',     'str:1:255', $icourseloc,     '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('icoursedesc',    'str::',     $icoursedesc,    '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('icoursecredits', 'int::',     $icoursecredits, '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('icourselevel',   'str:1:255', $icourselevel,   '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('icourseresult',  'str:1:255', $icourseresult,  '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('icoursedate',    'str::',     $icoursedate,    '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('dateappr',       'str::',     $dateappr,       '', XARVAR_NOT_REQUIRED)) return;
+    // Invalid check
+    if (!xarVarFetch('invalid', 'array',  $invalid, array(), XARVAR_NOT_REQUIRED)) return;
+
+    /* Argument check. See if this one has already been entered
+
+    $item = xarModAPIFunc('itsp',
+                          'user',
+                          'validateitem',
+                          array('name' => $name));
+     */
+    // Argument check
+    $invalid = array();
+    if (empty($icoursecredits) || !is_numeric($icoursecredits)) {
+        $invalid['icoursecredits'] = 1;
+        $icourse = '';
+    }
+    if (empty($icoursetitle) || !is_string($icoursetitle)) {
+        $invalid['icoursetitle'] = 1;
+        $icoursetitle = '';
+    }
+/*
+    if (!empty($name) && $item['name'] == $name) {
+        $invalid['duplicate'] = 1;
+    }
+
+    // check if we have any errors
+    if (count($invalid) > 0) {
+        return xarModFunc('itsp', 'admin', 'new',
+                          array('invalid' => $invalid,
+                                'planname' => $planname,
+                                'plandesc' => $plandesc,
+                                'planrules' => $planrules,
+                                'credits' => $credits,
+                                'mincredit' => $mincredit,
+                                'dateopen' => $dateopen,
+                                'dateclose' => $dateclose));
+    }
+*/
+     /* Confirm authorisation code. */
+    if (!xarSecConfirmAuthKey()) return;
+    /* The API function is called. */
+    $icourseid = xarModAPIFunc('itsp',
+                              'admin',
+                              'create_icourse',
+                              array('itspid' => $itspid,
+                                    'pitemid'=> $pitemid,
+                                    'icourseid'=> $icourseid,
+                                    'icoursetitle'=>$icoursetitle,
+                                    'icourseloc'=> $icourseloc,
+                                    'icoursedesc'=>  $icoursedesc,
+                                    'icoursecredits'=> $icoursecredits,
+                                    'icourselevel'=> $icourselevel,
+                                    'icourseresult'=> $icourseresult,
+                                    'icoursedate'=>  $icoursedate,
+                                    'dateappr'=>  $dateappr));
+    /* The return value of the function is checked here, and if the function
+     * suceeded then an appropriate message is posted.
+     */
+    if (!isset($icourseid) && xarCurrentErrorType() != XAR_NO_EXCEPTION) return; // throw back
+    xarSessionSetVar('statusmsg', xarML('The external course was successfully created!'));
+    /* This function generated no output, and so now it is complete we redirect
+     * the user to an appropriate page for them to carry on their work
+     */
+    xarResponseRedirect(xarModURL('itsp', 'admin', 'view'));
+    /* Return true, in this case */
+    return true;
+}
+?>
---
+++ xaradminapi/create_icourse.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Create a new itsp plan item
+ *
+ * @package modules
+ * @copyright (C) 2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage ITSP Module
+ * @link http://xaraya.com/index.php/release/572.html
+ * @author ITSP Module Development Team
+ */
+/**
+ * Create a new itsp course (external)
+ *
+ * This is a standard adminapi function to create a module item
+ *
+ * @author the ITSP module development team
+ * @param  $args ['name'] name of the item
+ * @param  $args ['number'] number of the item
+ * @since 21 feb 2006
+ * @return int itsp item ID on success, false on failure
+ * @throws BAD_PARAM, NO_PERMISSION, DATABASE_ERROR
+ */
+function itsp_adminapi_create_icourse($args)
+{
+    /*
+    'itspid' => $itspid,
+                                    'pitemid'=> $pitemid,
+                                    'icourseid'=> $icourseid,
+                                    'icoursetitle'=>$icoursetitle,
+                                    'icourseloc'=> $icourseloc,
+                                    'icoursedesc'=>  $icoursedesc,
+                                    'icoursecredits'=> $icoursecredits,
+                                    'icourselevel'=> $icourselevel,
+                                    'icourseresult'=> $icourseresult,
+                                    'icoursedate'=>  $icoursedate,
+                                    'dateappr'=>  $dateappr
+    */
+    extract($args);
+    /* Argument check */
+    $invalid = array();
+    if (!isset($icoursetitle) || !is_string($icoursetitle)) {
+        $invalid[] = 'icoursetitle';
+    }
+    if (!isset($itspid) || !is_numeric($itspid)) {
+        $invalid[] = 'itspid';
+    }
+    if (!isset($pitemid) || !is_numeric($pitemid)) {
+        $invalid[] = 'pitemid';
+    }
+    if (count($invalid) > 0) {
+        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
+            join(', ', $invalid), 'admin', 'create_icourse', 'ITSP');
+        xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
+            new SystemException($msg));
+        return;
+    }
+    /* Security check - important to do this as early on as possible to
+     * avoid potential security holes or just too much wasted processing
+     */
+    if (!xarSecurityCheck('EditITSP', 1, 'ITSP', "$itspid:All:All")) {//TODO: check
+        return;
+    }
+    $datemodi = time();
+    $modiby = xarUserGetVar('uid');
+    // Get database setup
+    $dbconn =& xarDBGetConn();
+    $xartable =& xarDBGetTables();
+    $icoursestable = $xartable['itsp_itsp_courses'];
+    /* Get next ID in table */
+    $nextId = $dbconn->GenId($planitemstable);
+    $query = "INSERT INTO $planitemstable (
+               xar_icourseid,
+               xar_pitemid,
+               xar_itspid,
+               xar_icoursetitle,
+               xar_icourseloc,
+               xar_icoursedesc,
+               xar_icoursecredits,
+               xar_icourselevel,
+               xar_icourseresult,
+               xar_icoursedate,
+               xar_dateappr,
+               xar_datemodi,
+               xar_modiby)
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    /* Create an array of values which correspond to the order of the
+     * Question marks in the statement above.
+     */
+    $bindvars = array($nextId,
+               $pitemid,
+               $itspid,
+               $icoursetitle,
+               $icourseloc,
+               $icoursedesc,
+               $icoursecredits,
+               $icourselevel,
+               $icourseresult,
+               $icoursedate,
+               $dateappr,
+               $datemodi,
+               $modiby);
+    $result = &$dbconn->Execute($query,$bindvars);
+
+    /* Check for an error with the database code, adodb has already raised
+     * the exception so we just return
+     */
+    if (!$result) return;
+
+    /* Get the ID of the item that we inserted. */
+    $pitemid = $dbconn->PO_Insert_ID($icoursestable, 'xar_icourseid');
+/*
+    // Let any hooks know that we have created a new item.
+    $item = $args;
+    $item['module'] = 'itsp';
+    $item['itemtype'] = 3;
+    $item['itemid'] = $pitemid;
+    xarModCallHooks('item', 'create', $pitemid, $item);
+
+    */
+    /* Return the id of the newly created item to the calling process */
+    return $icourseid;
+}
+?>
