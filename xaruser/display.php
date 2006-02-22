844b42b2c928f5bce17a61b9dad0f2a8a619d633
michelv <michelv@xarayahosting.nl>
2006-02-22 10:47:59 +0000

    Basic plan display

--- xaruser/display.php
+++ xaruser/display.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Display the ITSP for one user
+ * Display a plan
  *
  * @package modules
- * @copyright (C) 2005-2006 The Digital Development Foundation
+ * @copyright (C) 2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -12,71 +12,66 @@
  * @author ITSP Module Development Team
  */
 /**
- * Display the user's ITSP
+ * Display a plan
  *
  * This is a standard function to provide detailed informtion on a single item
  * available from the module.
  *
- * @author the ITSP module development team
+ * @author MichelV <michelv@xarayahosting.nl>
+ * @since 22 Feb 2006
  * @param  $args an array of arguments (if called by other modules)
  * @param  $args ['objectid'] a generic object id (if called by other modules)
- * @param  $args ['itspid'] the item id used for this itsp module
+ * @param  int $planid the plan id used for this itsp module
  */
 function itsp_user_display($args)
 {
     extract($args);
 
-    if (!xarVarFetch('itspid', 'id', $itspid)) return;
+    if (!xarVarFetch('planid', 'id', $planid)) return;
     if (!xarVarFetch('objectid', 'id', $objectid, $objectid, XARVAR_NOT_REQUIRED)) return;
 
     /* At this stage we check to see if we have been passed $objectid, the
      * generic item identifier.
      */
     if (!empty($objectid)) {
-        $itspid = $objectid;
+        $planid = $objectid;
     }
     /* Add the ITSP user menu */
     $data = xarModAPIFunc('itsp', 'user', 'menu');
-    /* The API function is called.  The arguments to the function are passed in
-     * as their own arguments array.
-     * Security check 1 - the get() function will fail if the user does not
-     * have at least READ access to this item (also see below).
+    /* get the plan
      */
     $item = xarModAPIFunc('itsp',
         'user',
-        'get',
-        array('itspid' => $itspid));
+        'get_plan',
+        array('planid' => $planid));
     if (!isset($item) && xarCurrentErrorType() != XAR_NO_EXCEPTION) return; /* throw back */
 
-    /* If your module deals with different types of items, you should specify the item type
-     * here, before calling any hooks
-     */
-      $item['itemtype'] = 2;
      /* Security check 2 - if your API function does *not* check for the
      * appropriate access rights, or if for some reason you require higher
      * access than READ for this function, you *must* check this here !
-     * if (!xarSecurityCheck('CommentITSP',0,'Item',"$item[name]:All:$item[exid]")) {
-     * return $data;
-     *}
-     */
+     * $planid:$pitemid:*/
+     if (!xarSecurityCheck('ReadITSPPlan',0,'Plan',"$planid:All:All")) {
+         return $data;
+     }
 
     /* Let any transformation hooks know that we want to transform some text.
      * You'll need to specify the item id, and an array containing the names of all
      * the pieces of text that you want to transform (e.g. for autolinks, wiki,
      * smilies, bbcode, ...).
      */
+    $item['itemtype'] = 1;
     $item['transform'] = array('name');
     $item = xarModCallHooks('item',
         'transform',
-        $itspid,
+        $planid,
         $item);
     // Fill in the details of the item.
-    $data['name_value'] = $item['name'];
-    $data['number_value'] = $item['number'];
+    $data['name_value'] = $item['planname'];
+    // $data['number_value'] = $item['number'];
+    $data['item'] = $item;
+    $data['planid'] = $planid;
 
-    $data['itspid'] = $itspid;
-
-    $data['is_bold'] = xarModGetVar('itsp', 'bold');
+  //  $data['is_bold'] = xarModGetVar('itsp', 'bold');
     /* Note : module variables can also be specified directly in the
      * blocklayout template by using &xar-mod-<modname>-<varname>;
      * Note that you could also pass on the $item variable, and specify
@@ -88,16 +83,16 @@ function itsp_user_display($args)
      * You should use this -instead of globals- if you want to make
      * information available elsewhere in the processing of this page request
      */
-    xarVarSetCached('Blocks.itsp', 'itspid', $itspid);
+    xarVarSetCached('Blocks.itsp', 'planid', $planid);
     /* Let any hooks know that we are displaying an item.
      */
     $item['returnurl'] = xarModURL('itsp',
         'user',
         'display',
-       array('itspid' => $itspid));
+       array('planid' => $planid));
     $hooks = xarModCallHooks('item',
         'display',
-        $itspid,
+        $planid,
         $item);
     if (empty($hooks)) {
         $data['hookoutput'] = array();
@@ -107,7 +102,7 @@ function itsp_user_display($args)
     /* Once again, we are changing the name of the title for better
      * Search engine capability.
      */
-    xarTplSetPageTitle(xarVarPrepForDisplay($item['name']));
+    xarTplSetPageTitle(xarVarPrepForDisplay($item['planname']));
     /* Return the template variables defined in this function */
     return $data;
 }
