aca8405f93ce77d6c2d654dac24151382a10fd59
michelv <michelv@xarayahosting.nl>
2006-02-22 10:37:25 +0000

    xaruser/itsp.php: Use gracefull check on existing ITSP

--- xaruser/itsp.php
+++ xaruser/itsp.php
@@ -40,21 +40,36 @@ function itsp_user_itsp($args)
     /* Add the ITSP user menu */
     $data = xarModAPIFunc('itsp', 'user', 'menu');
 
-    $uid = xarUserGetVar('uid');
-    /* The API function is called.  The arguments to the function are passed in
+
+    // We have a valid ITSP?
+    /* The API function is called. The arguments to the function are passed in
      * as their own arguments array.
      * Security check 1 - the get() function will fail if the user does not
      * have at least READ access to this item (also see below).
      */
-    $item = xarModAPIFunc('itsp',
-        'user',
-        'get',
-        array('itspid' => $itspid, 'userid' => $uid));
-    if (!isset($item) && xarCurrentErrorType() != XAR_NO_EXCEPTION) return; /* throw back */
 
-    /* If your module deals with different types of items, you should specify the item type
-     * here, before calling any hooks
-     */
+    if (empty($itspid)) {
+        $item = xarModAPIFunc('itsp',
+                          'user',
+                          'get_itspid',
+                          array('userid' => xarUserGetVar('uid')));
+    } else {
+
+        // The user API function is called to get the ITSP
+        $item = xarModAPIFunc('itsp',
+                              'user',
+                              'get_itspid',
+                              array('itspid' => $itspid));
+    }
+
+    // First see if there is an id to get.
+    if (empty($item)) {
+        $data['itspid'] = $itspid;
+        xarTplSetPageTitle(xarML('Individual Training and Supervision Plan'));
+
+        return $data;
+    }
+
      $item['itemtype'] = 2;
      /* Security check 2 - if your API function does *not* check for the
      * appropriate access rights, or if for some reason you require higher
@@ -78,17 +93,7 @@ function itsp_user_itsp($args)
     $data['itspid'] = $itspid;
 
     //$data['is_bold'] = xarModGetVar('itsp', 'bold');
-    /* Note : module variables can also be specified directly in the
-     * blocklayout template by using &xar-mod-<modname>-<varname>;
-     * Note that you could also pass on the $item variable, and specify
-     * the labels directly in the blocklayout template. But make sure you
-     * use the <xar:ml>, <xar:mlstring> or <xar:mlkey> tags then, so that
-     * labels can be translated for other languages...
-     * Save the currently displayed item ID in a temporary variable cache
-     * for any blocks that might be interested (e.g. the Others block)
-     * You should use this -instead of globals- if you want to make
-     * information available elsewhere in the processing of this page request
-     */
+
     xarVarSetCached('Blocks.itsp', 'itspid', $itspid);
     /* Let any hooks know that we are displaying an item.
      */
