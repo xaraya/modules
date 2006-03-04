4a6df46b792f6e32c219a6554f2a9979da5347ed
michelv <michelv@xarayahosting.nl>
2006-03-04 15:33:14 +0000

    Remove overview var. Add file 	* xaradmin/overview.php: new file

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -3,7 +3,7 @@
  * Hitcount
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -11,22 +11,15 @@
  * @link http://xaraya.com/index.php/release/177.html
  * @author Hitcount Module Development Team
  */
-
 /**
  * Add a standard screen upon entry to the module.
- * @returns output
- * @return output with Autolinks Menu information
+ * @return bool true on success of redirect
  */
 function hitcount_admin_main()
 {
     // Security Check
     if(!xarSecurityCheck('AdminHitcount')) return;
-    if (xarModGetVar('adminpanels', 'overview') == 0){
-        // Return the output
-        return array();
-    } else {
-        xarResponseRedirect(xarModURL('hitcount', 'admin', 'modifyconfig'));
-    }
+    xarResponseRedirect(xarModURL('hitcount', 'admin', 'modifyconfig'));
     // success
     return true;
 }
---
+++ xaradmin/overview.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Overview displays standard Overview page
+ *
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Hitcount Module
+ * @link http://xaraya.com/index.php/release/177.html
+ * @author Hitcount Module Development Team
+ */
+/**
+ * Overview function that displays the standard Overview page
+ *
+ * This function shows the overview template, currently admin-main.xd.
+ * The template contains overview and help texts
+ *
+ * @author the Hitcount module development team
+ * @return array xarTplModule with $data containing template data
+ * @since 4 March 2006
+ */
+function hitcount_admin_overview()
+{
+   /* Security Check */
+    if(!xarSecurityCheck('AdminHitcount',0)) return;
+
+    $data=array();
+
+    /* if there is a separate overview function return data to it
+     * else just call the main function that displays the overview
+     */
+
+    return xarTplModule('hitcount', 'admin', 'main', $data,'main');
+}
+
+?>
