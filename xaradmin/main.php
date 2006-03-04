673846ebdecd7b7e35069c0a50f6811019b5f8c7
michelv <michelv@xarayahosting.nl>
2006-03-04 10:52:57 +0000

    xaradmin/main.php: removing overview var

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -1,32 +1,24 @@
 <?php
 /**
+ * Julian Main administration function
+ *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2005-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Julian Module
+ * @link http://xaraya.com/index.php/release/319.html
+ * @author Julian Module Development Team
  */
 /**
  * the main administration function
+ * @return mixed
  */
 function julian_admin_main()
 {
-
-// Security Check
     if (!xarSecurityCheck('Editjulian')) return;
-
-    if (xarModGetVar('adminpanels', 'overview') == 0){
-        $welcome = '';
-
-        // Return the template variables defined in this function
-        return array('welcome' => $welcome);
-    } else {
-        xarResponseRedirect(xarModURL('julian', 'admin', 'modifyconfig'));
-    }
-    // success
-    return true;
+    return array();
 
 }
-
 ?>
