92830b7f4444a5521bd667fdacab357c41276ef7
michelv <michelv@xarayahosting.nl>
2006-03-05 13:29:51 +0000

    xaradmin/main.php: remove overview var

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -1,28 +1,23 @@
 <?php
 /**
- * File: $Id:
- * 
  * icecast main administration function
- * 
+ *
  * @copyright (C) 2004 by Johnny Robeson
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link 
+ * @link
  *
  * @subpackage icecast
- * @author Johnny Robeson 
+ * @author Johnny Robeson
+ */
+/**
+ * Show main template
  */
  function icecast_admin_main()
-{ 
-    if (!xarSecurityCheck('AdminIcecast')) return; 
-    
-    if (xarModGetVar('adminpanels', 'overview') == 0) {
-        $data = xarModAPIFunc('icecast', 'admin', 'menu'); 
-        $data['welcome'] = xarML('Welcome to the administration part of this icecast module...'); 
-        return $data;  
-    } else {
-        xarResponseRedirect(xarModURL('icecast', 'admin', 'view'));
-    }
-     
-    return true;
-} 
+{
+    if (!xarSecurityCheck('AdminIcecast')) return;
+
+    $data = xarModAPIFunc('icecast', 'admin', 'menu');
+    $data['welcome'] = xarML('Welcome to the administration part of this icecast module...');
+    return $data;
+}
 ?>
