5b18e99780217bf9cf2eb4f33a24e57e385f692f
michelv <michelv@xarayahosting.nl>
2006-03-06 16:19:00 +0000

    xaradmin/createcourse.php: clean-up

--- xaradmin/createcourse.php
+++ xaradmin/createcourse.php
@@ -3,7 +3,7 @@
  * Create a new course
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -11,13 +11,22 @@
  * @link http://xaraya.com/index.php/release/179.html
  * @author Courses module development team
  */
-
 /**
  * This is a standard function that is called with the results of the
  * form supplied by xarModFunc('courses','admin','newcourse') to create a new course
  *
- * @param  $name the name of the course to be created
- * @param  $number the number of the course to be created
+ * @param string $name the name of the course to be created
+ * @param string $number the number of the course to be created
+ * @param int $coursetype the number of the course to be created
+ * @param  $level the number of the course to be created
+ * @param string $shortdesc the number of the course to be created
+ * @param int $intendedcredits the number of the course to be created
+ * @param string $freq the number of the course to be created
+ * @param string $contact the number of the course to be created
+ * @param int $contactuid the uid of the coordinator of the course to be created
+ * @param int $hidecourse the number of the course to be created
+ * @return array
+ * @throws
  */
 function courses_admin_createcourse($args)
 {
@@ -26,7 +35,7 @@ function courses_admin_createcourse($args)
     // Get parameters from whatever input we need.
     if (!xarVarFetch('name',        'str:1:', $name, '', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('number',      'str:1:', $number, '',XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('coursetype',  'int:1:', $coursetype, 10, XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('coursetype',  'int:1:', $coursetype, 1, XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('level',       'isset', $level, '', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('shortdesc',   'str:1:', $shortdesc, '', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('intendedcredits', 'int:1:30', $intendedcredits, '', XARVAR_NOT_REQUIRED)) return;
@@ -62,17 +71,17 @@ function courses_admin_createcourse($args)
         xarSessionSetVar('statusmsg', xarML('Please check the data you have provided!'));
         // call the admin_newcourse function and return the template vars
         return xarModFunc('courses', 'admin', 'newcourse',
-                          array('name' => $name,
-                                'number' => $number,
-                                'coursetype' => $coursetype,
-                                'level' => $level,
-                                'shortdesc' => $shortdesc,
+                          array('name'          => $name,
+                                'number'        => $number,
+                                'coursetype'    => $coursetype,
+                                'level'         => $level,
+                                'shortdesc'     => $shortdesc,
                                 'intendedcredits' => $intendedcredits,
-                                'freq' => $freq,
-                                'contact' => $contact,
-                                'contactuid' => $contactuid,
-                                'hidecourse' => $hidecourse,
-                                'invalid' => $invalid));
+                                'freq'          => $freq,
+                                'contact'       => $contact,
+                                'contactuid'    => $contactuid,
+                                'hidecourse'    => $hidecourse,
+                                'invalid'       => $invalid));
     }
     // Confirm authorisation code.
     if (!xarSecConfirmAuthKey()) return;
