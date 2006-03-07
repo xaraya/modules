f8fe492cbf50816793f18ca397fd5a90e5a6a559
jojodee <jojodee@xaraya.com>
2006-03-07 17:05:26 +0000

    update for changes in overviews

---
+++ xaradmin/overview.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Overview for Translations
+ *
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Translations
+ * @link http://xaraya.com/index.php/release/77.html
+ */
+
+/**
+ * Overview displays standard Overview page
+ */
+function translations_admin_overview()
+{
+    $data=array();
+    //just return to main function that displays the overview
+    return xarTplModule('translations', 'admin', 'main', $data, 'main');
+}
+
+?>
