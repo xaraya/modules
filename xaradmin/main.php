3703086f753d048608657b577dc6a538475b63b5
jojodee <jojodee@xaraya.com>
2006-03-07 08:13:04 +0000

    Remove adminpanel overview modvar and overview from admin menu, update some headers while here, there is no overview text here

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -1,5 +1,17 @@
 <?php
-
+/**
+ * XProject Module main admin function
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage XProject Module
+ * @link http://xaraya.com/index.php/release/665.html
+ * @author XProject Module Development Team
+ */
+ 
 function xproject_admin_main()
 {
     xarResponseRedirect(xarModURL('xproject','admin','modifyconfig'));
--- xaradminapi/getmenulinks.php
+++ xaradminapi/getmenulinks.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * Utility function to pass admin menu links
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage XProject Module
+ * @link http://xaraya.com/index.php/release/665.html
+ * @author XProject Module Development Team
+ */
 /**
  * utility function pass individual menu items to the main menu
  *
@@ -9,15 +20,6 @@
  */
 function xproject_adminapi_getmenulinks()
 {
-
-    if (!xarSecurityCheck('ViewXProject', 0)) {
-
-        $menulinks[] = Array('url'   => xarModURL('xproject',
-                                                   'admin',
-                                                   'main'),
-                              'title' => xarML('The overview of this module and its functions'),
-                              'label' => xarML('Overview'));
-    }
 
     if (!xarSecurityCheck('AddXProject', 0)) {
 
