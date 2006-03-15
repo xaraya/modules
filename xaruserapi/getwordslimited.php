08ef84c8f6148deda77e1e2420a558b64b31c8d4
michelv <michelv@xarayahosting.nl>
2006-03-15 07:20:13 +0000

    xaruserapi/getwordslimited.php: update header with info

--- xaruserapi/getwordslimited.php
+++ xaruserapi/getwordslimited.php
@@ -3,23 +3,22 @@
  * Keywords Module
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Keywords Module
  * @link http://xaraya.com/index.php/release/187.html
  * @author mikespub
-*/
-
+ */
 /**
  * get entries for a module item
  *
- * @param $args['modid'] module id
- * @returns array
- * @return array of keywords
+ * This function gets the restricted keywords for one module
+ *
+ * @param int $args['moduleid'] module id
+ * @return array of keywords, sorted ASC
  * @throws BAD_PARAM, NO_PERMISSION, DATABASE_ERROR
-
  */
 function keywords_userapi_getwordslimited($args)
 {
