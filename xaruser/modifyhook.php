0608f997ee0776d72c153a3106e8365bcfd51b6b
michelv <michelv@xarayahosting.nl>
2006-03-15 09:08:22 +0000

    xardocs/changelog.txt: Add bug ids to todo xaruser/modifyhook.php: remove old code xaruser/week.php: header

--- xaruser/modifyhook.php
+++ xaruser/modifyhook.php
@@ -87,10 +87,7 @@ function julian_user_modifyhook($args)
     $data['event_repeat_on_num'] = 0;    // instance within month (1st, 2nd, ..., last=5)
     $data['event_repeat_on_freq'] = '';  // frequency (every x months)
 
-    // start date and time
- //  $data['event_year'] ='';
- //  $data['event_month'] ='';
- //  $data['event_day'] = '';
+
    $data['event_endyear'] ='';
    $data['event_endmonth'] ='';
    $data['event_endday'] = '';
@@ -107,145 +104,7 @@ function julian_user_modifyhook($args)
     if(empty($item)) {
         return "error";
     }
-    /*
-   // Load up database
-   $dbconn =& xarDBGetConn();
-   $xartable =& xarDBGetTables();
-   $event_linkage_table = $xartable['julian_events_linkage'];
 
-    // Try to find the link for the current module, item type and item id.
-   $query = "SELECT * FROM " .  $event_linkage_table . " WHERE ( hook_modid =$modid AND  hook_itemtype =$itemtype AND  hook_iid =$objectid)";
-   $result = $dbconn->Execute($query);
-   if (!empty($result)) {
-        if (!$result->EOF) {
-            $edit_obj = $result->FetchObject(false);
-            // Summary aka Title
-            $data['event_summary'] = $edit_obj->summary;
-            // Start/end date (and time)
-            $event_startdate = strtotime($edit_obj->dtstart);
-            $event_enddate   = strtotime($edit_obj->recur_until);
-
-            // All day or not
-            $data['event_allday'] = ($edit_obj->isallday==1);
-
-            // Event duration
-            if (strcmp($edit_obj->duration,'')!=0) {
-                list($data['event_dur_hours'], $data['event_dur_minutes']) = explode(':',$edit_obj->duration);
-            }
-
-            //Checking to see which repeating rule was used so the event_repeat can be set.
-            if ($edit_obj->rrule==3 && $edit_obj->recur_count && $edit_obj->recur_interval && $edit_obj->recur_freq)
-                $data['event_repeat'] = 2;
-            else if ($edit_obj->rrule && $edit_obj->recur_freq)
-                $data['event_repeat'] = 1;
-
-            //Depending on which recurrence rule was used, set the appropriate form fields.
-            switch ($data['event_repeat']) {
-                case 1:
-                    $data['event_repeat_every_freq'] = $edit_obj->recur_freq;    // time unit (1=day, 2=week, 3=month, 4=year)
-                    $data['event_repeat_every_type']  = $edit_obj->rrule;            // every n time units
-                    break;
-                case 2:
-                    $data['event_repeat_on_day'] = $edit_obj->recur_count;        // day of the week (mon-sun)
-                    $data['event_repeat_on_num'] = $edit_obj->recur_interval;    // instance within month (1=1st, 2=2nd, ..., 5=last)
-                    $data['event_repeat_on_freq'] = $edit_obj->recur_freq;        // every n months
-                    break;
-            }
-
-            $result->Close();
-        } else {
-            return xarTplModule('julian','user','edithook',$data);
-            // ERROR: no link to this object was found!!!
-        }
-    } else {
-        return xarTplModule('julian','user','edithook',$data);
-        // ERROR: no link to this object was found!!!
-    }
-
-
-    // start date
-   list($data['event_year'],   $data['event_month'],   $data['event_day'])    = explode("-",date("Y-m-d",$event_startdate));
-   list($data['event_endyear'],$data['event_endmonth'],$data['event_endday']) = explode("-",date("Y-m-d",$event_enddate));
-
-   //Building start hour options
-   $start_hour_options = '';
-   for($i = 1;$i <= 12; $i++)
-   {
-     $j = str_pad($i,2,"0",STR_PAD_LEFT);
-     $start_hour_options.='<option value="'.$i.'"';
-     if ($i == $hour) $start_hour_options.= " SELECTED";
-      $start_hour_options.='>'.$j.'</option>';
-   }
-   $data['start_hour_options'] = $start_hour_options;
-
-    // Building duration minute options
-    // Get the interval
-    $StartMinInterval = xarModGetVar('julian', 'StartMinInterval');
-    if ($StartMinInterval == 1) {
-        $sminend = 60;
-    } elseif ($StartMinInterval == 5) {
-        $sminend = 56;
-    } elseif ($StartMinInterval == 10) {
-        $sminend = 51;
-    } elseif ($StartMinInterval == 15) {
-        $sminend = 46;
-    }
-
-    $start_minute_options='';
-    for($i = 0;$i < $sminend; $i = $i + $StartMinInterval) {
-        $j = str_pad($i,2,"0",STR_PAD_LEFT);
-        $start_minute_options.='<option value="'.$j.'"';
-        $start_minute_options.='>'.$j.'</option>';
-    }
-    $data['start_minute_options'] = $start_minute_options;
-
-    // start AM/PM
-    $data['event_startampm'] = (strcmp(date("a",$event_startdate),"am")!=0);    // true=PM, false=AM
-
-   //Building duration hour options
-   $dur_hour_options = '';
-   for($i = 0;$i <= 24; $i++)
-   {
-     $j = str_pad($i,2,"0",STR_PAD_LEFT);
-     $dur_hour_options.='<option value="'.$i.'"';
-     if ($i == $data['event_dur_hours']) $dur_hour_options.= " selected";
-     $dur_hour_options.='>'.$j.'</option>';
-   }
-   $data['dur_hour_options'] = $dur_hour_options;
-
-    // Building duration minute options
-    // Get the interval
-    $DurMinInterval = xarModGetVar('julian', 'DurMinInterval');
-    if ($DurMinInterval == 1) {
-        $minend = 60;
-    } elseif ($DurMinInterval == 5) {
-        $minend = 56;
-    } elseif ($DurMinInterval == 10) {
-        $minend = 51;
-    } elseif ($DurMinInterval == 15) {
-        $minend = 46;
-    }
-
-    $dur_minute_options = '';
-    for($i = 0;$i < $minend; $i = $i + $DurMinInterval) {
-        $j = str_pad($i,2,"0",STR_PAD_LEFT);
-        $dur_minute_options.='<option value="'.$j.'"';
-        $dur_minute_options.='>'.$j.'</option>';
-    }
-    $data['dur_minute_options'] = $dur_minute_options;
-
- //   $data['summary'] = $summary;
-*/
-// Options for include template
-
-    /*
-    if($item['recur_until']) {
-       // End date and time
-       // determine the end date for a recurring event
-       // TODO: With the new get.php this should be rewritten
-       list($event_endyear,$event_endmonth,$event_endday) = explode("-",$item['recur_until']);
-    }
-*/
     // start date
    list($data['event_year'],  $data['event_month'],   $data['event_day'])    = explode("-",date("Y-m-d",$item['ts_start']));
    list($event_endyear,$event_endmonth,$event_endday) = explode("-",date("Y-m-d",$item['ts_end']));
@@ -255,13 +114,7 @@ function julian_user_modifyhook($args)
     $hour = date("h",strtotime($item['event_starttime'])); //12 hour format
     $ampm = !strcmp(date("a",strtotime($item['event_starttime'])),"am")?0:1;
     $minute = date("i",strtotime($item['event_starttime']));
-    /*
-    list($year,$month,$day) = explode("-",date("Y-m-d",strtotime($item['dtstart'])));
-    //set the start date parts in the data array
-    $data['todays_month'] = $month;
-    $data['todays_year'] = $year;
-    $data['todays_day'] = $day;
-*/
+
     // If there is not a duration, set dur_hours and dur_minutes.
     // list($item['event_dur_hours'], $item['event_dur_minutes']) = explode(':',$edit_obj->duration);
     // Default for both is empty string.
@@ -271,29 +124,6 @@ function julian_user_modifyhook($args)
         $dur_hours = $item['event_dur_hours'];
         $dur_minutes = $item['event_dur_minutes'];
     }
-/*
-            //Checking to see which repeating rule was used so the event_repeat can be set.
-            if ($edit_obj->rrule==3 && $edit_obj->recur_count && $edit_obj->recur_interval && $edit_obj->recur_freq) {
-                $item['event_repeat'] = 2;
-            } else if ($edit_obj->rrule && $edit_obj->recur_freq) {
-               $item['event_repeat'] = 1;
-            } else {
-                $item['event_repeat'] = 0;
-            }
-
-            //Depending on which recurrence rule was used, set the appropriate form fields.
-            switch ($item['event_repeat']) {
-                case 1:
-                    $item['event_repeat_every_freq'] = $edit_obj->recur_freq;  // time unit (1=day, 2=week, 3=month, 4=year)
-                    $item['event_repeat_every_type'] = $edit_obj->rrule;       // every n time units
-                    break;
-                case 2:
-                    $item['event_repeat_on_day'] = $edit_obj->recur_count;     // day of the week (mon-sun)
-                    $item['event_repeat_on_num'] = $edit_obj->recur_interval;  // instance within month (1=1st, 2=2nd, ..., 5=last)
-                    $item['event_repeat_on_freq'] = $edit_obj->recur_freq;     // every n months
-
-
-*/
     //Checking to see which repeating rule was used so the event_repeat can be set.
     $event_repeat = $item['event_repeat'];
 
@@ -459,7 +289,6 @@ function julian_user_modifyhook($args)
      $data['allday_checked'][1] = '';
      $data['timeddisabled'] = 'disabled';
    }
-
 
     return xarTplModule('julian','user','edithook',$data);
 }
