9f9add510844a5ffd55c8f540c7412b621167517
jojodee <jojodee@xaraya.com>
2006-03-24 10:45:15 +0000

    Headers and misc minor updates

--- xarinit.php
+++ xarinit.php
@@ -3,13 +3,13 @@
  * Initialise the registration module
  *
  * @package modules
- * @copyright (C) 2005-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Registration module
  * @link http://xaraya.com/index.php/release/30205.html
- * @author Jan Schrage, John Cox, Gregor Rothfuss
+ * @author Jo Dalle Nogare <jojodee@xaraya.com>
  */
 
 /**
