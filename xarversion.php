d3d262bc1d7c65d73c58e76f6e465bf1c4d8c437
jojodee <jojodee@xaraya.com>
2006-04-09 02:32:44 +0000

    Update headers and vars

--- xarversion.php
+++ xarversion.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * File: $Id$
- *
  * Invision Power Board Authentication
  *
  * @package authentication
- * @copyright (C) 2003 by the Xaraya Development Team.
+ * @copyright (C) 2003-2006 by the Xaraya Development Team.
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -25,7 +23,6 @@ $modversion['author'] = 'Brian McCloskey';
 $modversion['contact'] = 'brian@nexusden.com';
 $modversion['admin'] = 1;
 $modversion['user'] = 0;
-$modversion['securityschema'] = array('authinvision2::' => '::');
 $modversion['class'] = 'Authentication';
 $modversion['category'] = 'Global';
 ?>
