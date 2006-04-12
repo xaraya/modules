7f9e239d292405b36cc0e0849923fae28be3ec9f
jojodee <jojodee@xaraya.com>
2006-04-12 04:44:56 +0000

    Add some template consistency, menu

---
+++ xaradmin/overview.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Displays standard Overview page
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Smilies
+ * @link http://xaraya.com/index.php/release/153.html
+ */
+/**
+ * Overview function that displays the standard Overview page
+ *
+ */
+function smilies_admin_overview()
+{
+   /* Security Check */
+    if (!xarSecurityCheck('AdminSmilies',0)) return;
+
+    $data=array();
+
+    /* if there is a separate overview function return data to it
+     * else just call the main function that displays the overview
+     */
+
+    return xarTplModule('smilies', 'admin', 'main', $data,'main');
+}
+
+?>
