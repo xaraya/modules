6b6f7e0a32397adfdb5b239442f4649bf878b53d
jojodee <jojodee@xaraya.com>
2006-04-13 19:04:46 +0000

    Remove deprecated adminpanel modvar check

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -16,17 +16,6 @@ function messages_admin_main()
 
     if (!xarSecurityCheck( 'EditMessages')) return;
 
-    // Check if we should show the overview page
-    // The admin system looks for a var to be set to skip the introduction
-    // page altogether.  This allows you to add sparse documentation about the
-    // module, and allow the site admins to turn it on and off as they see fit.
-    if (xarModGetVar('adminpanels', 'overview') == 0) {
-
-        // Yes we should
-        $data = messages_admin_common( 'Overview' );
-        return $data;
-
-    }
 
     // No we shouldn't. So we redirect to the admin_view() function.
     xarResponseRedirect(
