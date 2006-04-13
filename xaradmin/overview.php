456087b79eb4036f65c358c315542ded6ef2fc93
jojodee <jojodee@xaraya.com>
2006-04-13 11:13:14 +0000

    Check overview and redirect, add common menu

---
+++ xaradmin/overview.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Displays standard Overview page
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Changelog Module
+ * @link http://xaraya.com/index.php/release/185.html
+ */
+/**
+ * Overview function that displays the standard Overview page
+ *
+ */
+function changelog_admin_overview()
+{
+   /* Security Check */
+    if (!xarSecurityCheck('AdminChangeLog',0)) return;
+
+    $data=array();
+
+    /* if there is a separate overview function return data to it
+     * else just call the main function that displays the overview
+     */
+
+    return xarTplModule('changelog', 'admin', 'main', $data,'main');
+}
+
+?>
