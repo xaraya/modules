8d23734fa2b13e10518848ef694318faf5f1412b
jojodee <jojodee@xaraya.com>
2006-04-13 18:48:59 +0000

    Remove adminpanel modvar check - deprecated

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -7,11 +7,8 @@ function scheduler_admin_main()
 { 
     if (!xarSecurityCheck('AdminScheduler')) return;
 
-    if (xarModGetVar('adminpanels', 'overview') == 0) {
-        return array();
-    } else {
         xarResponseRedirect(xarModURL('scheduler', 'admin', 'modifyconfig'));
-    }
+
     return true;
 } 
 
