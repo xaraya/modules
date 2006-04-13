001cc07f7027f8ad26c96beaef3ec503903a2d07
jojodee <jojodee@xaraya.com>
2006-04-13 19:00:34 +0000

    Remove deprecated adminpanel var, sec check

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -10,12 +10,7 @@ function newsgroups_admin_main()
 // Security Check
     if(!xarSecurityCheck('AdminNewsgroups')) return;
 
-    if (xarModGetVar('adminpanels', 'overview') == 0){
-        // Return the output
-        return array();
-    } else {
         xarResponseRedirect(xarModURL('newsgroups', 'admin', 'modifyconfig'));
-    }
     // success
     return true;
 }
