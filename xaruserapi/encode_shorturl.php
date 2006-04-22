af8d74a6ed5fa1f16326a344b7d6d26171c982cd
jojodee <jojodee@xaraya.com>
2006-04-22 21:29:18 +0000

    Update short url encoding

--- xaruserapi/encode_shorturl.php
+++ xaruserapi/encode_shorturl.php
@@ -15,30 +15,12 @@
  *
  * Supported URLs :
  *
- * /roles/
- * /roles/123
- * /roles/account
- * /roles/account/[module]
+ * /registration/privacy
+ * /registration/terms
  *
- * /roles/list
- * /roles/list/viewall
- * /roles/list/X
- * /roles/list/viewall/X
- *
- * /roles/login
- * /roles/logout
- * /roles/password
- * /roles/privacy
- * /roles/terms
- *
- * /roles/register
- * /roles/register/registration
- * /roles/register/checkage
- *
- * /roles/settings
- * /roles/settings/form (deprecated)
- *
- * @author Marc Lutolf <marcinmilan@xaraya.com>
+ * /registration/register
+ * /registration/checkage
+ * /registration
  * @author the roles module development team
  * @param $args the function and arguments passed to xarModURL
  * @returns string
@@ -48,118 +30,47 @@ function registration_userapi_encode_shorturl($args)
 {
     // Get arguments from argument array
     extract($args);
+    if (!isset($func)) {return;}
 
-    // Check if we have something to work with
-    if (!isset($func)) {
-        return;
-    }
-    unset($args['func']);
-
-    // Initialise the path.
     $path = array();
-
-    // we can't rely on xarModGetName() here -> you must specify the modname.
+    $get = $args;
     $module = 'registration';
 
-    switch($func) {
-        case 'main':
-            // Note : if your main function calls some other function by default,
-            // you should set the path to directly to that other function
-            break;
-        case 'view':
-            $path[] = 'list';
-            if (!empty($phase) && $phase == 'viewall') {
-                unset($args['phase']);
-                $path[] = 'viewall';
-            }
-            if (!empty($letter)) {
-                unset($args['letter']);
-                $path[] = $letter;
-            }
-            break;
+    $aliasisset = xarModGetVar($module, 'useModuleAlias');
+    $aliasname = xarModGetVar($module, 'aliasname');
 
-        case 'lostpassword':
-            $path[] = 'password';
-            break;
+    if (!empty($aliasisset) && !empty($aliasname)) {
+        $module_for_alias = xarModGetAlias($aliasname);
 
-        case 'showloginform':
-            $path[] = 'login';
-            break;
+        if ($module_for_alias == $module) {
+            $module = $aliasname;
+        }
+    }
 
-        case 'account':
-            $path[] = 'account';
-            if(!empty($moduleload)) {
-                // Note: this handles usermenu requests for hooked modules (including roles itself).
-                unset($args['moduleload']);
-                $path[] = $moduleload;
-            }
-            break;
+    $path[] = $module;
 
-        case 'terms':
-        case 'privacy':
-        case 'logout':
-            $path[] = $func;
-            break;
+    if ($func == 'main') {
+         $path[] = 'main';
+        // Consume the 'func' parameter only.
+        unset($get['func']);
+    } elseif ($func == 'terms' || $func == 'privacy') {
+        $path[] = $func;
+        unset($get['func']);
 
-        case 'usermenu':
-            $path[] = 'settings';
-            if (!empty($phase) && ($phase == 'formbasic' || $phase == 'form')) {
-                // Note : this URL format is no longer in use
-                unset($args['phase']);
-                $path[] = 'form';
-            }
-            break;
-
-        case 'register':
+    } elseif ($func == 'register') {
             $path[] = 'register';
             if (!empty($phase)) {
                 // Bug 4404: registerform and registration are aliases.
                 if ($phase == 'registerform' || $phase == 'registration' || $phase == 'checkage') {
                     unset($args['phase']);
-                    $path[] = ($phase == 'registerform' ? 'registration' : $phase);
+                    $path[] = $phase=='checkage'?'checkage':'';//($phase == 'registerform' ? 'registration' : $phase);
                 } else {
                     // unsupported phase - must be passed via forms
                 }
             }
-            break;
-
-        case 'display':
-            // check for required parameters
-            if (isset($uid) && is_numeric($uid)) {
-                unset($args['uid']);
-                $path[] = $uid;
-            }
-            break;
-
-        default:
-            break;
+    } else {
+        //hmmm
     }
-    
-
-    // If no short URL path was obtained above, then there is no encoding.
-    if (empty($path)) {
-        // Return without a short URL.
-        return;
-    }
-
-    // Modify some other module arguments as standard URL parameters.
-    // Turn a 'cids' array into a 'catid' string.
-    if (!empty($cids) && count($cids) > 0) {
-        unset($args['cids']);
-        if (!empty($andcids)) {
-            $args['catid'] = join('+', $cids);
-        } else {
-            $args['catid'] = join('-', $cids);
-        }
-    }
-
-    // Slip the module name or alias in at the start of the path.
-    array_unshift($path, $module);
-
-    return array(
-        'path' => $path,
-        'get' => $args
-    );
+    return array('path' => $path, 'get' => $get);
 }
-
 ?>
