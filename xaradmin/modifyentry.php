a93f24d775c1cf1e917bc9b70fbd6088b96f2d5e
jojodee <jojodee@xaraya.com>
2006-05-03 11:19:58 +0000

    General headers

--- xaradmin/modifyentry.php
+++ xaradmin/modifyentry.php
@@ -2,13 +2,14 @@
 /**
  * Modify an entry of the encyclopedia
  *
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2003 by the Xaraya Development Team.
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Encyclopedia Module
- * @author Marc Lutolf <marcinmilan@xaraya.com>
+ * @link http://xaraya.com/index.php/release/221.html
+ * @author Marc Lutolf
  */
 
 function encyclopedia_admin_modifyentry()
