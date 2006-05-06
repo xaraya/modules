e80c004bc1b813e85ebe61730cfb3d97cf2bc4fe
michelv <michelv@xarayahosting.nl>
2006-05-06 07:08:02 +0000

    Clean up header Lowercast name remove sec scheme

--- xarversion.php
+++ xarversion.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * File: $Id$
- *
  * Workflow initialization functions
  *
- * @copyright (C) 2003 by the Xaraya Development Team.
- * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @package modules
+ * @copyright (C) 2003-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
- * @subpackage workflow
- * @author mikespub
+ *
+ * @subpackage Workflow Module
+ * @link http://xaraya.com/index.php/release/188.html
+ * @author Workflow Module Development Team
  */
-$modversion['name']           = 'Workflow';
+$modversion['name']           = 'workflow';
 $modversion['id']             = '188';
 $modversion['version']        = '1.5.0';
 $modversion['displayname']    = xarML('Workflow');
@@ -24,7 +25,6 @@ $modversion['author']         = 'mikespub';
 $modversion['contact']        = 'http://www.xaraya.com/';
 $modversion['admin']          = 1;
 $modversion['user']           = 1;
-$modversion['securityschema'] = array('Workflow::Item' => 'To be defined...');
 $modversion['class']          = 'Utility';
 $modversion['category']       = 'Miscellaneous';
 ?>
