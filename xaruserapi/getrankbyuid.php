1e81cb7f10bb6463ce481370ff6dc5fc40925e97
markfrawley <markfrawley@gmail.com>
2006-05-18 17:49:10 +0000

    Add userapi getrankbyuid patch file

---
+++ xaruserapi/getrankbyuid.php
@@ -0,0 +1,61 @@
+<?php
+//Going to make some calls from the other functions
+require_once('getallranks.php');    
+require_once('get.php');    
+
+/**
+ * Get a rankname for a user
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Userpoints Module
+ * @link http://xaraya.com/index.php/release/782.html
+ * @author Mark Frawley
+ */
+
+
+/**
+ * get a rankname for a user
+ *
+ * @author Mark Frawley
+ * @param uid $uid uid of user to get rankname for
+ * @return $user_rank string, or empty string on failure
+ * @raise BAD_PARAM, DATABASE_ERROR, NO_PERMISSION
+ */
+function userpoints_userapi_getrankbyuid($args) {
+    
+    extract($args);
+    
+    if(isset($uid) && $uid!="") {
+        $user_score = userpoints_userapi_get( array('uid' => $uid) );
+    } 
+       
+	$aRanks = userpoints_userapi_getallranks($args);
+    $user_rank = "";
+    $user_rid=0;
+    $old_score = 0; //needed because we are doing a search
+
+    foreach($aRanks as $rankRow) {
+        $rid       = $rankRow['id'];
+        $rankname = $rankRow['rankname'];
+        $rankminscore = $rankRow['rankminscore'];
+            
+        if($user_score >= $rankminscore && $rankminscore >= $old_score) {
+
+            $user_rank = $rankname;
+            $user_rid = $rid; //rank id
+            $old_score = $rankminscore;
+        }
+    }
+
+    if (!xarSecurityCheck('ReadUserpointsRank', 1, 'Rank', "$user_rank:$user_rid")) {
+        return;
+    }
+    return($user_rank);
+      
+}
+
+?>
