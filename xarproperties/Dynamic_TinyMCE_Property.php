07f753f1ed723545720e9e394dae053dc6f71bc3
jojodee <jojodee@xaraya.com>
2006-05-21 15:58:24 +0000

    Add generic checkInput function to tinymce to see if this fixes immediate problem in bug 5697

--- xarproperties/Dynamic_TinyMCE_Property.php
+++ xarproperties/Dynamic_TinyMCE_Property.php
@@ -40,7 +40,18 @@ class Dynamic_TinyMCE_Property extends Dynamic_Property
         }
     }
 
-
+    function checkInput($name='', $value = null)
+    {
+        if (empty($name)) {
+            $name = 'dd_'.$this->id;
+        }
+        // store the fieldname for validations who need them (e.g. file uploads)
+        $this->fieldname = $name;
+        if (!isset($value)) {
+            if (!xarVarFetch($name, 'isset', $value,  NULL, XARVAR_DONT_SET)) {return;}
+        }
+        return $this->validateValue($value);
+    }
      function validateValue($value = null) 
      {
         if (!isset($value)) {
