ca672859483c3f5724788d8fd1d976d80020a793
jojodee <jojodee@xaraya.com>
2006-05-25 19:30:17 +0000

    Add simple line break transform as additional non-exclusive option to the smart line breaks

--- xaruserapi/transformoutput.php
+++ xaruserapi/transformoutput.php
@@ -76,9 +76,9 @@ function html_userapitransformoutput($text)
     */
 
     if (strlen(trim($text)) == 0) return '';
-
+    $dobreak = xarModGetVar('html', 'dobreak');
     $dotransform = xarModGetVar('html', 'dolinebreak');
-    $br=0;
+
     if ($dotransform == 1){
   	    $text = $text . "\n"; // just to make things a little easier, pad the end
 	    $text = preg_replace('|<br />\s*<br />|', "\n\n", $text);
@@ -94,11 +94,13 @@ function html_userapitransformoutput($text)
 	    $text = str_replace('</blockquote></p>', '</p></blockquote>', $text);
 	    $text = preg_replace('!<p>\s*(</?(?:table|thead|tfoot|caption|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|hr|pre|select|form|blockquote|address|math|p|h[1-6])[^>]*>)!', "$1", $text);
 	    $text = preg_replace('!(</?(?:table|thead|tfoot|caption|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|blockquote|address|math|p|h[1-6])[^>]*>)\s*</p>!', "$1", $text);
-	    if ($br) $text = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $text); // optionally make line breaks
+	    if ($dobreak==1) $text = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $text); // optionally  line breaks
         $text = preg_replace('!(</?(?:table|thead|tfoot|caption|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|blockquote|address|math|p|h[1-6])[^>]*>)\s*<br />!', "$1", $text);
 	    $text = preg_replace('!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)>)!', '$1', $text);
 	    $text = preg_replace('!(<pre.*?>)(.*?)</pre>!ise', " stripslashes('$1') .  stripslashes(clean_pre('$2'))  . '</pre>' ", $text);
-    } else {
+    } elseif ($dobreak==1) { // just do line breaks
+         $text = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $text);
+    }else {
         $text = $text;
     }
 
