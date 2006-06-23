92f8704b4f6eabfdec36f9c9b4b433ef5ad46fd6
brian <brian@envisionnet.net>
2006-06-23 01:34:11 +0000

    upping version number. 	* xarversion.php: ...

--- xarversion.php
+++ xarversion.php
@@ -9,7 +9,7 @@
  *
  * @subpackage Helpdesk Module
  * @link http://www.abraisontechnoloy.com/
- * @author Brian McGilligan <brianmcgilligan@gmail.com>
+ * @author Brian McGilligan <brian@mcgilligan.us>
  */
 // Is Based On:
 /********************************************************/
@@ -25,34 +25,21 @@
 
 $modversion['id']           = '910';
 $modversion['name']         = 'Help Desk';
-$modversion['version'] = '0.8.0';
+$modversion['version']      = '0.8.1';
 $modversion['displayname']  = xarML('Help Desk');
 $modversion['description']  = 'Helpdesk Module';
+$modversion['author']       = 'Brian McGilligan';
+$modversion['contact']      = 'brian@mcgilligan.us';
 $modversion['credits']      = 'docs/credits.txt';
 $modversion['help']         = 'docs/help.txt';
 $modversion['changelog']    = 'docs/changelog.txt';
 $modversion['license']      = 'docs/license.txt';
 $modversion['official']     = 0;
-$modversion['author']       = 'Brian McGilligan';
-$modversion['contact']      = 'brian@envisionnet.net';
 $modversion['user']         = 1;
 $modversion['admin']        = 1;
 $modversion['class']        = 'Complete';
 $modversion['category']     = 'Content';
-$modversion['displayname'] = xarML('Help Desk');
-$modversion['description'] = 'Help Desk Module';
-$modversion['credits'] = 'docs/credits.txt';
-$modversion['help'] = 'docs/help.txt';
-$modversion['changelog'] = 'docs/changelog.txt';
-$modversion['license'] = 'docs/license.txt';
-$modversion['official'] = 0;
-$modversion['author'] = 'Brian McGilligan';
-$modversion['contact'] = 'brian@mcgilligan.us';
-$modversion['user'] = 1;
-$modversion['admin'] = 1;
 $modversion['securityschema'] = array('helpdesk::' => '::');
-$modversion['class'] = 'Complete';
-$modversion['category'] = 'Content';
 
 /*
     270 - Security Module
