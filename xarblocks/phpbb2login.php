a7baf7f232b0f331b7afe11a9ce78946aa2bcecb
jojodee <jojodee@xaraya.com>
2006-07-11 13:04:52 +0000

    Check for roles links that need changing, update as necessary, bring templates into line with standard classes, adjust blocks - not sure if they were working, just updating classes and changed links for roles

--- xarblocks/phpbb2login.php
+++ xarblocks/phpbb2login.php
@@ -1,10 +1,16 @@
 <?php
 /**
+ * Login via a block
  *
- * Login via a block.
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
  *
- */
-
+ * @subpackage authldap
+ * @link http://xaraya.com/index.php/release/77102.html
+ * @author Alexander GQ Gerasiov <gq@gq.pp.ru>
+*/
 /**
  * initialise block
  */
