1a33ed8c22b2173626d4e0711430b14a4fe1790b
michelv <michelv@xarayahosting.nl>
2006-07-20 12:59:40 +0000

    Three more

--- xaradmin/modifyconfig.php
+++ xaradmin/modifyconfig.php
@@ -3,12 +3,13 @@
  * Modify site configuration
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage wizards
- * @author Marc Lutolf
+ * @link http://xaraya.com/index.php/release/3007.html
+ * @author Marc Lutolf <marcinmilan@xaraya.com>
  */
 /**
  * Modify site configuration
--- xarclass/xarModuleWizards.php
+++ xarclass/xarModuleWizards.php
@@ -3,13 +3,14 @@
  * Purpose of file:  Module Wizards API
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
+ * @subpackage wizards
+ * @link http://xaraya.com/index.php/release/3007.html
  * @author Marc Lutolf <marcinmilan@xaraya.com>
-*/
-
+ */
 /**
  * Get the available wizards
  * Just returns an array
