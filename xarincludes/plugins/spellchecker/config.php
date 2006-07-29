0bd4da5109221003d714ac1e08f05833e2df1da1
jojodee <jojodee@xaraya.com>
2006-07-29 00:46:05 +0000

    Update the spellchecker config file so it should work with googlespell out of the box

--- xarincludes/plugins/spellchecker/config.php
+++ xarincludes/plugins/spellchecker/config.php
@@ -1,24 +1,26 @@
 <?php
-	$spellCheckerConfig = array();
+    $spellCheckerConfig = array();
 
-	// Spellchecker class use
-	require_once("classes/TinyPspellShell.class.php"); // Command line pspell
-	 require_once("classes/TinyGoogleSpell.class.php"); // Google web service
-	 require_once("classes/TinyPspell.class.php"); // Internal PHP version
+    // Spellchecker class use - uncomment those classes that are wanted for use
+    //require_once("classes/TinyPspellShell.class.php"); // Command line pspell
+    require_once("classes/TinyGoogleSpell.class.php"); // Google web service
+    //require_once("classes/TinyPspell.class.php"); // Internal PHP version
 
-	// General settings
-	$spellCheckerConfig['enabled'] = false;
+    // General settings - this is important
+    $spellCheckerConfig['enabled'] = true;
 
-	// Default settings
-	$spellCheckerConfig['default.language'] = 'en';
-	$spellCheckerConfig['default.mode'] = PSPELL_FAST;
 
-	// Normaly not required to configure
-	$spellCheckerConfig['default.spelling'] = "";
-	$spellCheckerConfig['default.jargon'] = "";
-	$spellCheckerConfig['default.encoding'] = "";
+    // Only use the following if you are using the pspell classes else don't worry about them
+    // Default settings
+    $spellCheckerConfig['default.language'] = 'en';
+    $spellCheckerConfig['default.mode'] = PSPELL_FAST;
 
-	// Pspell shell specific settings
-	$spellCheckerConfig['tinypspellshell.aspell'] = '/usr/bin/aspell';
-	$spellCheckerConfig['tinypspellshell.tmp'] = '/tmp';
+    // Normaly not required to configure
+    $spellCheckerConfig['default.spelling'] = "";
+    $spellCheckerConfig['default.jargon'] = "";
+    $spellCheckerConfig['default.encoding'] = "";
+
+    // Pspell shell specific settings
+    $spellCheckerConfig['tinypspellshell.aspell'] = '/usr/bin/aspell';
+    $spellCheckerConfig['tinypspellshell.tmp'] = '/tmp';
 ?>
