11b7bddb928474124648c2af7f687a01b2361e77
mfl <mfl@netspan.ch>
2006-08-26 16:54:34 +0000

    some renaming and adding the utility module specific files

---
+++ xaradmin/modifyconfig-utility.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * Function purpose to be added
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2006 by to be added
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link to be added
+ * @subpackage Foo Module
+ * @author Marc Lutolf <mfl@netspan.ch>
+ *
+ * Purpose of file:  to be added
+ *
+ * @param to be added
+ * @return to be added
+ *
+ */
+
+// Use this version of the modifyconfig file when creating utility modules
+
+function foo_admin_modifyconfig()
+{
+    // Security Check
+    if (!xarSecurityCheck('AdminFoo')) return;
+    if (!xarVarFetch('phase', 'str:1:100', $phase, 'modify', XARVAR_NOT_REQUIRED, XARVAR_PREP_FOR_DISPLAY)) return;
+    if (!xarVarFetch('tab', 'str:1:100', $data['tab'], 'foo_general', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('tabmodule', 'str:1:100', $tabmodule, 'foo', XARVAR_NOT_REQUIRED)) return;
+    $hooks = xarModCallHooks('module', 'getconfig', 'foo');
+	if (!empty($hooks) && isset($hooks['tabs'])) {
+		foreach ($hooks['tabs'] as $key => $row) {
+			$configarea[$key]  = $row['configarea'];
+			$configtitle[$key] = $row['configtitle'];
+			$configcontent[$key] = $row['configcontent'];
+		}
+		array_multisort($configtitle, SORT_ASC, $hooks['tabs']);
+	} else {
+		$hooks['tabs'] = array();
+	}
+    switch (strtolower($phase)) {
+        case 'modify':
+        default:
+            switch ($data['tab']) {
+                case 'foo_general':
+                    break;
+                case 'tab2':
+                    break;
+                case 'tab3':
+                    break;
+                default:
+                    break;
+            }
+
+            break;
+
+        case 'update':
+            // Confirm authorisation code
+            if (!xarSecConfirmAuthKey()) return;
+			if (!xarVarFetch('itemsperpage', 'str:1:4:', $itemsperpage, '20', XARVAR_NOT_REQUIRED, XARVAR_PREP_FOR_DISPLAY)) return;
+			if (!xarVarFetch('shorturls', 'checkbox', $shorturls, false, XARVAR_NOT_REQUIRED)) return;
+			if (!xarVarFetch('bar', 'str:1', $bar, 'Bar', XARVAR_NOT_REQUIRED)) return;
+
+            if ($data['tab'] == 'foo_general') {
+				xarModVars::set('foo', 'itemsperpage', $itemsperpage);
+				xarModVars::set('foo', 'supportshorturls', $shorturls);
+				xarModVars::set('foo', 'bar', $bar);
+            }
+			$regid = xarModGetIDFromName($tabmodule);
+			xarModSetUserVar('foo', 'bar', $bar, $regid);
+
+            xarResponseRedirect(xarModURL('foo', 'admin', 'modifyconfig',array('tabmodule' => $tabmodule, 'tab' => $data['tab'])));
+            // Return
+            return true;
+            break;
+
+    }
+	$data['hooks'] = $hooks;
+	$data['tabmodule'] = $tabmodule;
+	$data['authid'] = xarSecGenAuthKey();
+    return $data;
+}
+?>
--- xardataapi/adminmenu.php
+++ xardataapi/adminmenu.php
@@ -1,32 +1,27 @@
 <?php
 /**
- * Mailer Module
  *
- * @package modules
- * @subpackage mailer module
- * @copyright (C) 2010 Netspan AG
- * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * Function purpose to be added
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2006 by to be added
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link to be added
+ * @subpackage Foo Module
  * @author Marc Lutolf <mfl@netspan.ch>
- */
-/**
- * Return the options for the admin menu
+ *
+ * Purpose of file:  to be added
+ *
+ * @param to be added
+ * @return to be added
  *
  */
 
 // TODO: turn this into an xml file
-    function mailer_dataapi_adminmenu()
-    {
-        if(!xarModVars::get('modules','disableoverview')) {
-            return array(
-                array('includes' => array('main','overview'), 'target' => 'main', 'label' => xarML('Mailer Overview')),
-                array('mask' => 'ManageMailer', 'includes' => 'view', 'target' => 'view', 'title' => xarML('Manage the master tables of this module'), 'label' => xarML('Master Tables')),
-                array('mask' => 'AdminMailer', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Modify the Mailer configuration'), 'label' => xarML('Modify Config')),
-            );
-        } else {
-            return array(
-                array('mask' => 'ManageMailer', 'includes' => 'view', 'target' => 'view', 'title' => xarML('Manage the master tables of this module'), 'label' => xarML('Master Tables')),
-                array('mask' => 'AdminMailer', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Modify the Mailer configuration'), 'label' => xarML('Modify Config')),
-            );
-        }
-    }
+	function foo_dataapi_adminmenu() {
+		return array(
+            array('includes' => array('main','overview'), 'target' => 'main', 'label' => xarML('Foo Overview')),
+            array('mask' => 'AdminAuthsystem', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Modify the Foo configuration'), 'label' => xarML('Modify Configuration')),
+		);
+	}
 ?>
