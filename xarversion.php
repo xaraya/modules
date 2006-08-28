f2875dd138b9ebc7a62397549692df8695bfef65
michelv <michelv@xarayahosting.nl>
2006-08-28 14:02:18 +0000

    Clean-up templates Remove inline formatting Add GPL to templates Fix some typos

--- xarversion.php
+++ xarversion.php
@@ -3,7 +3,7 @@
  * Xaraya HTML Module
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -11,23 +11,22 @@
  * @link http://xaraya.com/index.php/release/779.html
  * @author John Cox
  */
-
-$modversion['name'] = 'HTML';
-$modversion['id'] = '779';
-$modversion['version'] = '1.4.0';
-$modversion['displayname']    = xarML('HTML');
-$modversion['description'] = 'Configure the allowed HTML or your site';
-$modversion['credits'] = 'xardocs/credits.txt';
-$modversion['help'] = 'xardocs/help.txt';
-$modversion['changelog'] = 'xardocs/changelog.txt';
-$modversion['license'] = 'xardocs/license.txt';
-$modversion['coding'] = 'xardocs/coding.txt';
-$modversion['official'] = 1;
-$modversion['author'] = 'John Cox';
-$modversion['contact'] = 'admin@dinerminor.com';
-$modversion['admin'] = 1;
-$modversion['user'] = 0;
-$modversion['class'] = 'Admin';
-$modversion['category'] = 'Global';
+$modversion['name']         = 'HTML';
+$modversion['id']           = '779';
+$modversion['version']      = '1.4.0';
+$modversion['displayname']  = xarML('HTML');
+$modversion['description']  = 'Configure the allowed HTML or your site';
+$modversion['credits']      = 'xardocs/credits.txt';
+$modversion['help']         = 'xardocs/help.txt';
+$modversion['changelog']    = 'xardocs/changelog.txt';
+$modversion['license']      = 'xardocs/license.txt';
+$modversion['coding']       = 'xardocs/coding.txt';
+$modversion['official']     = 1;
+$modversion['author']       = 'John Cox';
+$modversion['contact']      = 'admin@dinerminor.com';
+$modversion['admin']        = 1;
+$modversion['user']         = 0;
+$modversion['class']        = 'Admin';
+$modversion['category']     = 'Global';
 //$modversion['dependency']  = array(778); BBCode Dependency in transformoutput removed
 ?>
