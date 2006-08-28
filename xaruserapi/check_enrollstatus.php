0b93b5b028ef78a43c997d2d51093aad8d7c2e59
michelv <michelv@xarayahosting.nl>
2006-08-28 09:52:53 +0000

    Add function to check for the status of enrollment for a course, based on courseid. 	* xaruserapi/check_enrollstatus.php: new file

--- xaruserapi/check_enrollstatus.php
+++ xaruserapi/check_enrollstatus.php
@@ -3,7 +3,7 @@
  * Get all courses for one student
  *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -12,23 +12,37 @@
  * @author Courses module development team
  */
 /**
- * Get all courses that a student is enrolled to.
+ * Get information about the status of a student for a certain course
  *
- * The student is the current user/role
+ * This function will check to see if a student is enrolled for a course, and
+   if so, what the status is, and return all relevant information
  *
  * @author MichelV <michelv@xarayahosting.nl>
- * @param numitems $ the number of items to retrieve (default -1 = all)
- * @param startnum $ start with this item number (default 1)
+ * @param int courseid The ID of the course the look for
+ * @param int userid The ID of the user that is a student
+ * @since 28 Aug 2006
  * @return array of items, or false on failure
  * @throws BAD_PARAM, DATABASE_ERROR, NO_PERMISSION
  */
-function courses_userapi_getall_enrolled($args)
+function courses_userapi_check_enrollstatus($args)
 {
     extract($args);
-    // Either pass the user id or get it from the current user
-    if (!isset($uid)) {
-        $uid = xarUserGetVar('uid');
+
+    if (!isset($userid) || !is_numeric($userid)) {
+        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
+            'user ID', 'user', 'check_enrollstatus', 'Courses');
+        xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
+            new SystemException($msg));
+        return;
     }
+    if (!isset($courseid) || !is_numeric($courseid)) {
+        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
+            'course ID', 'user', 'check_enrollstatus', 'Courses');
+        xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
+            new SystemException($msg));
+        return;
+    }
+
     $items = array();
     // Security check
     if (!xarSecurityCheck('ReadCourses')) return;
@@ -36,35 +50,32 @@ function courses_userapi_getall_enrolled($args)
     $dbconn =& xarDBGetConn();
     $xartable =& xarDBGetTables();
     $planningtable = $xartable['courses_planning'];
-    $coursestable = $xartable['courses'];
     $studentstable = $xartable['courses_students'];
-    // TODO: how to select by cat ids (automatically) when needed ???
-    $query = "SELECT $coursestable.xar_name,
-            $coursestable.xar_courseid,
+    // Build the query
+    $query = "SELECT
             $planningtable.xar_planningid,
             $planningtable.xar_startdate,
             $planningtable.xar_hideplanning,
+            $planningtable.xar_credits,
             $studentstable.xar_status,
             $studentstable.xar_regdate
-            FROM $coursestable
-            JOIN $planningtable
+            FROM $planningtable
             JOIN $studentstable
             ON ($planningtable.xar_planningid = $studentstable.xar_planningid)
-            WHERE ($studentstable.xar_userid = $uid
-            AND $coursestable.xar_courseid = $planningtable.xar_courseid)";
+            WHERE ($studentstable.xar_userid = $userid
+            AND $planningtable.xar_courseid = $courseid)";
      $result = &$dbconn->Execute($query);
     // Check for an error with the database code, adodb has already raised
     // the exception so we just return
     if (!$result) return;
     // Put items into result array.
     for (; !$result->EOF; $result->MoveNext()) {
-        list($name, $courseid, $planningid, $startdate, $hideplanning, $studstatus, $regdate) = $result->fields;
+        list($planningid, $startdate, $hideplanning, $credits, $studstatus, $regdate) = $result->fields;
         if (xarSecurityCheck('ReadCourses', 0, 'Course', "$courseid:$planningid:All")) {
-            $items[] = array('name'         => $name,
-                             'courseid'     => $courseid,
-                             'planningid'   => $planningid,
+            $items[] = array('planningid'   => $planningid,
                              'startdate'    => $startdate,
                              'hideplanning' => $hideplanning,
+                             'credits'      => $credits,
                              'studstatus'   => $studstatus,
                              'regdate'      => $regdate);
         }
