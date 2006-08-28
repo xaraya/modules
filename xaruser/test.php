fa23e55abd4c97933765b055ae37048b4b41a81b
mfl <mfl@netspan.ch>
2006-08-28 08:40:52 +0000

    add a test GUI file in the user type

--- xaruser/test.php
+++ xaruser/test.php
@@ -1,34 +1,6 @@
 <?php
-
-function calendar_user_test()
+function foo_user_test()
 {
-// some timing for now to see how fast|slow the parser is
-include_once('Benchmark/Timer.php');
-$t =& new Benchmark_Timer;
-$t->start();
-    $ical = xarMod::apiFunc('icalendar','user','factory','ical_parser');
-$t->setMarker('Class Instantiated');
-    xarVarFetch('file','str::',$file);
-$t->setMarker('File Var Fetched');
-    //$ical->setFile('code/modules/timezone/zoneinfo/America/Phoenix.ics');
-    $ical->setFile($file);
-$t->setMarker('File Set');
-    $ical->parse();
-$t->setMarker('Parsing Complete');
-
-$t->stop();
-
-    ob_start();
-        print_r($ical);
-        $ical_out = ob_get_contents();
-    ob_end_clean();
-
-    $data = array(
-        'ical'=>$ical_out,
-        'profile'=>$t->getOutput()
-    );
-
-    return $data;
+    return array();
 }
-
 ?>
