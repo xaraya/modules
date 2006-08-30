937a66647d3c54f91c784d215503821c23a8bc02
mfl <mfl@netspan.ch>
2006-08-30 09:34:15 +0000

    experimental class stuff. this is sure to change

---
+++ xarclasses/locations.php
@@ -0,0 +1,52 @@
+<?php
+	sys::import('modules.maps.xarclasses.collection');
+
+	class MapLocations extends BasicSet implements Locations
+	{
+	}
+
+	interface Locations
+	{
+	}
+
+	class BasicMapObject extends Object implements MapObject
+	{
+		public $id;
+		public $name;
+		public $description;
+		public $longitude;
+		public $latitude;
+
+		public function __construct($args=array())
+		{
+			$this->loaddata($args);
+		}
+		public function toArray()
+		{
+			return array(
+					'id' => $this->id,
+					'name' => $this->name,
+					'description' => $this->description,
+					'longitude' => $this->longitude,
+					'latitude' => $this->latitude,
+				);
+		}
+		public function add(array $args)
+		{
+			$this->loaddata($args);
+		}
+		private function loaddata(array $args)
+		{
+			if (isset($args['id'])) $this->id = $args['id'];
+			if (isset($args['name'])) $this->name = $args['name'];
+			if (isset($args['description'])) $this->description = $args['description'];
+			if (isset($args['longitude'])) $this->longitude = $args['longitude'];
+			if (isset($args['latitude'])) $this->latitude = $args['latitude'];
+		}
+	}
+
+	interface MapObject
+	{
+		public function add(array $args);
+	}
+?>
