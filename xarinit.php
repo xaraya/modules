c2acb0d868dcbbf4fd3e10c70e028aac169d3b77
brian <brian@envisionnet.net>
2006-08-31 00:50:17 +0000

    Remove some mask registers and adding little bit of help text.
    	* xarinit.php: ...
    	* xartemplates/admin-setup_security.xd: ...

--- xarinit.php
+++ xarinit.php
@@ -150,13 +150,13 @@ function helpdesk_init()
     // Module variable for testing
     xarModSetVar('helpdesk', 'debug message', '');
 
-    xarRegisterMask('viewhelpdesk',   'All','helpdesk','helpdesk','All', 'ACCESS_OVERVIEW');
-    xarRegisterMask('readhelpdesk',   'All','helpdesk','helpdesk','All', 'ACCESS_READ');
-    xarRegisterMask('submithelpdesk', 'All','helpdesk','helpdesk','All', 'ACCESS_COMMENT');
-    xarRegisterMask('edithelpdesk',   'All','helpdesk','helpdesk','All', 'ACCESS_EDIT');
-    xarRegisterMask('addhelpdesk',    'All','helpdesk','helpdesk','All', 'ACCESS_ADD');
-    xarRegisterMask('deletehelpdesk', 'All','helpdesk','helpdesk','All', 'ACCESS_DELETE');
-    xarRegisterMask('adminhelpdesk',  'All','helpdesk','helpdesk','All', 'ACCESS_ADMIN');
+//    xarRegisterMask('viewhelpdesk',   'All','helpdesk','helpdesk','All', 'ACCESS_OVERVIEW');
+//    xarRegisterMask('readhelpdesk',   'All','helpdesk','helpdesk','All', 'ACCESS_READ');
+//    xarRegisterMask('submithelpdesk', 'All','helpdesk','helpdesk','All', 'ACCESS_COMMENT');
+//    xarRegisterMask('edithelpdesk',   'All','helpdesk','helpdesk','All', 'ACCESS_EDIT');
+//    xarRegisterMask('addhelpdesk',    'All','helpdesk','helpdesk','All', 'ACCESS_ADD');
+//    xarRegisterMask('deletehelpdesk', 'All','helpdesk','helpdesk','All', 'ACCESS_DELETE');
+//    xarRegisterMask('adminhelpdesk',  'All','helpdesk','helpdesk','All', 'ACCESS_ADMIN');
 
     // let's hook cats in
     $cid = xarModAPIFunc('categories', 'admin', 'create',
