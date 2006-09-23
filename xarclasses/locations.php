a2c02951c30313c059a081387536902c1b815275
mfl <mfl@netspan.ch>
2006-09-23 20:27:17 +0000

    make the module work with the new foundation classes

--- xarclasses/locations.php
+++ xarclasses/locations.php
@@ -1,55 +1,55 @@
 <?php
-	sys::import('modules.maps.xarclasses.collection');
+    sys::import('structures.sets.collection');
 
-	class MapLocations extends BasicSet implements Locations
-	{
-	}
+    class MapLocations extends BasicSet implements Locations
+    {
+    }
 
-	interface Locations
-	{
-	}
+    interface Locations
+    {
+    }
 
-	class BasicMapObject extends Object implements MapObject
-	{
-		public $id;
-		public $name;
-		public $description;
-		public $windowtext;
-		public $longitude;
-		public $latitude;
+    class BasicMapObject extends Object implements MapObject
+    {
+        public $id;
+        public $name;
+        public $description;
+        public $windowtext;
+        public $longitude;
+        public $latitude;
 
-		public function __construct($args=array())
-		{
-			$this->loaddata($args);
-		}
-		public function toArray()
-		{
-			return array(
-					'id' => $this->id,
-					'name' => $this->name,
-					'description' => $this->description,
-					'windowtext' => $this->windowtext,
-					'longitude' => $this->longitude,
-					'latitude' => $this->latitude,
-				);
-		}
-		public function add(array $args)
-		{
-			$this->loaddata($args);
-		}
-		protected function loaddata(array $args)
-		{
-			if (isset($args['id'])) $this->id = $args['id'];
-			if (isset($args['name'])) $this->name = $args['name'];
-			if (isset($args['description'])) $this->description = $args['description'];
-			if (isset($args['windowtext'])) $this->windowtext = $args['windowtext'];
-			if (isset($args['longitude'])) $this->longitude = $args['longitude'];
-			if (isset($args['latitude'])) $this->latitude = $args['latitude'];
-		}
-	}
+        public function __construct($args=array())
+        {
+            $this->loaddata($args);
+        }
+        public function toArray()
+        {
+            return array(
+                    'id' => $this->id,
+                    'name' => $this->name,
+                    'description' => $this->description,
+                    'windowtext' => $this->windowtext,
+                    'longitude' => $this->longitude,
+                    'latitude' => $this->latitude,
+                );
+        }
+        public function add(array $args)
+        {
+            $this->loaddata($args);
+        }
+        protected function loaddata(array $args)
+        {
+            if (isset($args['id'])) $this->id = $args['id'];
+            if (isset($args['name'])) $this->name = $args['name'];
+            if (isset($args['description'])) $this->description = $args['description'];
+            if (isset($args['windowtext'])) $this->windowtext = $args['windowtext'];
+            if (isset($args['longitude'])) $this->longitude = $args['longitude'];
+            if (isset($args['latitude'])) $this->latitude = $args['latitude'];
+        }
+    }
 
-	interface MapObject
-	{
-		public function add(array $args);
-	}
+    interface MapObject
+    {
+        public function add(array $args);
+    }
 ?>
