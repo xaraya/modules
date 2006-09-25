d5dd861fb77c50ab0749805c8af1e0ac1600c0b8
johnny <johnny@localmomentum.net>
2006-09-25 23:29:32 +0000

    adding blank getmenulinks 	* xaradminapi: new dir 	* xaradminapi/getmenulinks.php: new file

---
+++ xaradminapi/getmenulinks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Get Menu Links
+ *
+ * @package modules
+ * @subpackage dojo
+ * @copyright The Digital Development Foundation, 2006
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @author Johnny Robeson <johnny@localmomentum.net>
+ **/
+ 
+function dojo_adminapi_getmenulinks($arg)
+{
+    return array();
+}
+?>
