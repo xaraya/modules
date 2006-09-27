b720ef940dcc26840f4efae4decf4cffe29b782e
marcel <marcel@hsdev.com>
2006-09-27 07:50:54 +0000

    Applying the patch from Sam on exposing MoveableType in the RSD 	* xaruser/main.php: use RSD function, instead of repeating the same code 	* xaruser/rsd.php: add MoveableType to the api's if available, clean up the function too
    This fixes bug 5942

--- xaruser/main.php
+++ xaruser/main.php
@@ -7,62 +7,6 @@
  */
 function xmlrpcserver_user_main($args)
 {
-    $data = array();
-    
-    // We need to set the pagename template here to the module
-    // xml template
-    if(!xarTplSetPageTemplateName('module')) {
-        // FIXME: What if module.xt does not exist?
-    }
-    
-    // Determine the Xaraya version
-    $data['xar_version'] = XARCORE_VERSION_NUM;
-    
-    // Determin which APIs we have and what stuff they need for the discovery
-    $data['xmlrpc_apis'] = array();
-    
-    // BloggerAPI
-    if(xarModIsAvailable('bloggerapi')) {
-        $data['xmlrpc_apis']['Blogger'] = array();
-        $data['xmlrpc_apis']['Blogger']['preferred'] = 'true'; // NOTE: string!
-        $data['xmlrpc_apis']['Blogger']['link'] = xarServerGetBaseURL() .'ws.php?type=xmlrpc';
-        // The blogId attribute is the blog to which the user can post, in xaraya this
-        // means one or more root categories which are linked to the pubtype. 
-        // The discovery only supports one category, so let's return the first one
-        // encountered.
-        $data['xmlrpc_apis']['Blogger']['others'] = '';
-        $pubtype = xarModGetVar('bloggerapi','bloggerpubtype');
-        if($pubtype && xarModIsAvailable('categories') && xarModIsAvailable('articles')) {
-            // Get the root categories for this publication type
-            $rootcats = xarModAPIFunc('articles','user','getrootcats',array('ptid'=>$pubtype));
-            if (!empty($rootcats)) {
-                xarLogVariable('rootcats', $rootcats);
-                $data['xmlrpc_apis']['Blogger']['others'] = 'blogID = "'.$rootcats[0]['catid'].'" ';
-            }
-        }
-    
-        // MetaWebLogAPI depends on BloggerAPI so it should be inside the if
-        if(xarModIsAvailable('metaweblogapi')) {
-            $data['xmlrpc_apis']['MetaWeblog'] = array();
-            $data['xmlrpc_apis']['MetaWeblog']['preferred'] = 'true'; // NOTE: string!
-            $data['xmlrpc_apis']['Blogger']['preferred'] = 'false';
-            $data['xmlrpc_apis']['MetaWeblog']['link'] = xarServerGetBaseURL() .'ws.php?type=xmlrpc';
-            // The blogId attribute is the blog to which the user can post, in xaraya this
-            // means one or more root categories which are linked to the pubtype. 
-            // The discovery only supports one category, so let's return the first one
-            // encountered.
-            $data['xmlrpc_apis']['MetaWeblog']['others'] = '';
-            if($pubtype && xarModIsAvailable('categories') && xarModIsAvailable('articles')) {
-                // Get the root categories for this publication type
-                $rootcats = xarModAPIFunc('articles','user','getrootcats',array('ptid'=>$pubtype));
-                if (!empty($rootcats)) {
-                    xarLogVariable('rootcats', $rootcats);
-                    $data['xmlrpc_apis']['MetaWeblog']['others'] = 'blogID = "'.$rootcats[0]['catid'].'" ';
-                }
-            }
-        }
-    }
-    return $data;
+    return xarModFunc('xmlrpcserver','user','rsd');
 }
-
 ?>
--- xaruser/rsd.php
+++ xaruser/rsd.php
@@ -1,10 +1,13 @@
 <?php
-
 /**
- * RSD xml constructor based on the available API's
- *
- * 
- */
+ * Construct an RSD xml snippet to allow client to discover what we have here
+ *  
+ * @package modules
+ * @subpackage xmlrpcserver
+ * @copyright The Digital Development Foundation, 2006
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @author Marcel van der Boom <mrb@hsdev.com>
+**/
 function xmlrpcserver_user_rsd($args)
 {
     $data = array();
@@ -18,50 +21,56 @@ function xmlrpcserver_user_rsd($args)
     // Determine the Xaraya version
     $data['xar_version'] = XARCORE_VERSION_NUM;
     
-    // Determin which APIs we have and what stuff they need for the discovery
+    // Determine which APIs we have and what stuff they need for the discovery
     $data['xmlrpc_apis'] = array();
     
     // BloggerAPI
-    if(xarModIsAvailable('bloggerapi')) {
-        $data['xmlrpc_apis']['Blogger'] = array();
-        $data['xmlrpc_apis']['Blogger']['preferred'] = 'true'; // NOTE: string!
-        $data['xmlrpc_apis']['Blogger']['link'] = xarServerGetBaseURL() .'ws.php?type=xmlrpc';
+    if(xarModIsAvailable('bloggerapi')) 
+    {
+        // Get some values we need throughout
+        $entrypoint = xarServerGetBaseUrl() . 'ws.php?type=xmlrpc';
+        $pubtype    = xarModGetVar('bloggerapi','bloggerpubtype');
+        if($pubtype && xarModIsAvailable('categories') && xarModIsAvailable('articles'))
+        {
+            // Get the root categories for this publication type
+            xarLogVariable('rootcats', $rootcats);
+            $rootcats = xarModAPIFunc('articles','user','getrootcats',array('ptid'=>$pubtype));
+        }
         // The blogId attribute is the blog to which the user can post, in xaraya this
-        // means one or more root categories which are linked to the pubtype. 
+        // means one or more root categories which are linked to the pubtype.
         // The discovery only supports one category, so let's return the first one
         // encountered.
-        $data['xmlrpc_apis']['Blogger']['others'] = '';
-        $pubtype = xarModGetVar('bloggerapi','bloggerpubtype');
-        if($pubtype && xarModIsAvailable('categories') && xarModIsAvailable('articles')) {
-            // Get the root categories for this publication type
-            $rootcats = xarModAPIFunc('articles','user','getrootcats',array('ptid'=>$pubtype));
-            if (!empty($rootcats)) {
-                xarLogVariable('rootcats', $rootcats);
-                $data['xmlrpc_apis']['Blogger']['others'] = 'blogID = "'.$rootcats[0]['catid'].'" ';
-            }
-        }
+        $data['xmlrpc_apis']['Blogger'] = array(
+            'preferred' => 'true',  // NOTE: string!
+            'link'      => $entrypoint,
+            'others'    => !empty($rootcats) ? 'blogId = "'.$rootcats[0]['catid'].'" ' : ''
+        );
     
         // MetaWebLogAPI depends on BloggerAPI so it should be inside the if
-        if(xarModIsAvailable('metaweblogapi')) {
-            $data['xmlrpc_apis']['MetaWeblog'] = array();
-            $data['xmlrpc_apis']['MetaWeblog']['preferred'] = 'true'; // NOTE: string!
+        if(xarModIsAvailable('metaweblogapi')) 
+        {
+            $data['xmlrpc_apis']['MetaWeblog'] = array(
+                'preferred'     => 'true',  // NOTE: string!
+                'link'          => $entrypoint,
+                'others'        => !empty($rootcats) ? 'blogId = "'.$rootcats[0]['catid'].'" ' : ''
+            );
+            // Metaweblog is now preferred over blogger
             $data['xmlrpc_apis']['Blogger']['preferred'] = 'false';
-            $data['xmlrpc_apis']['MetaWeblog']['link'] = xarServerGetBaseURL() .'ws.php?type=xmlrpc';
-            // The blogId attribute is the blog to which the user can post, in xaraya this
-            // means one or more root categories which are linked to the pubtype. 
-            // The discovery only supports one category, so let's return the first one
-            // encountered.
-            $data['xmlrpc_apis']['MetaWeblog']['others'] = '';
-            if($pubtype && xarModIsAvailable('categories') && xarModIsAvailable('articles')) {
-                // Get the root categories for this publication type
-                $rootcats = xarModAPIFunc('articles','user','getrootcats',array('ptid'=>$pubtype));
-                if (!empty($rootcats)) {
-                    xarLogVariable('rootcats', $rootcats);
-                    $data['xmlrpc_apis']['MetaWeblog']['others'] = 'blogID = "'.$rootcats[0]['catid'].'" ';
-                }
-            }
-        }
-    }
+
+            // MoveableType depends on MetaWeblogAPI so it should be inside the if
+            if(xarModIsAvailable('moveabletype')) 
+            {
+                $data['xmlrpc_apis']['MoveableType'] = array(
+                    'preferred'     =>  'true', // NOTE: string!
+                    'link'          =>  $entrypoint,
+                    'others'        =>  !empty($rootcats) ? 'blogId = "'.$rootcats[0]['catid'].'" ' : ''
+                );
+                // Make sure MoveableType ends up as the preferred one
+                $data['xmlrpc_apis']['MetaWeblog']['preferred'] = 'false'; // NOTE: string!
+                $data['xmlrpc_apis']['Blogger']['preferred']    = 'false'; // NOTE: string!
+            } // MoveableType
+        } // MetaWebLogApi
+    }// BloggerApi
     return $data;
 }
 
