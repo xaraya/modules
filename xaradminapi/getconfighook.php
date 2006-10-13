85a1cc3b72ee4e576fc4c59786af7a9e08c13d08
mfl <mfl@netspan.ch>
2006-10-13 13:03:09 +0000

    add config hook

--- xaradminapi/getconfighook.php
+++ xaradminapi/getconfighook.php
@@ -1,26 +1,14 @@
 <?php
-/**
- * Publications Module
- *
- * @package modules
- * @subpackage publications module
- * @category Third Party Xaraya Module
- * @version 2.0.0
- * @copyright (C) 2011 Netspan AG
- * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @author Marc Lutolf <mfl@netspan.ch>
- */
-function publications_adminapi_getconfighook($args)
+function foo_adminapi_getconfighook($args)
 {
     extract($args);
     if (!isset($extrainfo['tabs'])) $extrainfo['tabs'] = array();
-    $module = 'publications';
+    $module = 'foo';
     $tabinfo = array(
             'module'  => $module,
             'configarea'  => 'general',
-            'configtitle'  => xarML('Publications'),
-            'configcontent' => xarModFunc($module,'admin','modifyconfig_general'
-            )
+            'configtitle'  => xarML('Foo'),
+            'configcontent' => ''
     );
     $extrainfo['tabs'][] = $tabinfo;
     return $extrainfo;
