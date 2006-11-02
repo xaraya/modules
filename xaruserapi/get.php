8471bb77b65c541ed504743236b15b4c035e4d4a
jlinowes <jlinowes@xaraya.com>
2006-11-02 17:12:43 +0000

    added support for 'fields', 'extra' and 'ptid' args. See Bug 5925

--- xaruserapi/get.php
+++ xaruserapi/get.php
@@ -25,7 +25,6 @@
  * @param int $args['status'] status of article to get, and optional
  * @param string $args['language'] language of article to get
  * @param bool $args['withcids'] (optional) if we want the cids too (default false)
-// TODO
  * @param array $args['fields'] array with all the fields to return per article
  *                        Default list is : 'aid','title','summary','authorid',
  *                        'pubdate','pubtypeid','notes','status','body'
@@ -34,6 +33,7 @@
  *                       to the default list). So you can EITHER specify *all* the
  *                       fields you want with 'fields', OR take all the default
  *                       ones and add some optional fields with 'extra'
+ * @param id $args['ptid'] same as 'pubtypeid'
  * @return array article array, or false on failure
  */
 function articles_userapi_get($args)
@@ -49,6 +49,42 @@ function articles_userapi_get($args)
         xarErrorSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
                        new SystemException($msg));
         return false;
+    }
+
+    // allow ptid instead of pubtypeid, like getall and other api's (if both specified, ptid wins)
+    if (!empty($ptid))
+        $pubtypeid = $ptid;
+
+    // bypass this function, call getall instead?
+    if (isset($fields) || isset($extra)) {
+        if (!empty($aid))
+            $args['aids'] = array($aid);
+        if (!empty($pubtypeid))
+            $args['ptid'] = $pubtypeid;
+        $wheres = array();
+        if (!empty($title))
+            $wheres[] = "title eq '$title'";
+        if (!empty($summary))
+            $wheres[] = "summary eq '$summary'";
+        if (!empty($body))
+            $wheres[] = "body eq '$body'";
+        if (!empty($notes))
+            $wheres[] = "notes eq '$notes'";
+        if (!empty($withcids))
+            $fields[] = "cids";
+        foreach ($wheres as $w) {
+            if (isset($where))
+                $where .= " || $w";
+            else
+                $where = $w;
+        }
+        if (isset($where))
+            $args['where'] = $where;
+        $arts = xarModApiFunc('articles','user','getall', $args );
+        if (!empty($arts))
+            return current($arts);
+        else
+            return false;
     }
 
 // TODO: put all this in dynamic data and retrieve everything via there (including hooked stuff)
