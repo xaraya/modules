16742d84551cffed9e447bd605892700951d1da9
michelv <michelv@xarayahosting.nl>
2006-11-27 11:54:34 +0000

    Typo: cropping

--- xarversion.php
+++ xarversion.php
@@ -11,22 +11,22 @@
  * @link http://xaraya.com/index.php/release/152.html
  * @author Images Module Development Team
  */
-$modversion['name'] = 'Images';
-$modversion['id'] = '152';
-$modversion['version'] = '1.1.0';
-$modversion['displayname']    = xarML('Images');
-$modversion['description'] = 'Handles basic image manipulation - resizing/croping/scaling/rotating';
-$modversion['credits'] = 'docs/credits.txt';
-$modversion['help'] = 'docs/help.txt';
-$modversion['changelog'] = 'docs/changelog.txt';
-$modversion['license'] = 'docs/license.txt';
-$modversion['official'] = 1;
-$modversion['author'] = 'Carl P. Corliss (carl.corliss@xaraya.com)';
-$modversion['contact'] = 'http://www.xaraya.com/';
-$modversion['admin'] = 1;
-$modversion['class'] = 'Utility';
-$modversion['category'] = 'Global';
+$modversion['name']         = 'Images';
+$modversion['id']           = '152';
+$modversion['version']      = '1.1.0';
+$modversion['displayname']  = xarML('Images');
+$modversion['description']  = 'Handles basic image manipulation - resizing/cropping/scaling/rotating';
+$modversion['credits']      = 'docs/credits.txt';
+$modversion['help']         = 'docs/help.txt';
+$modversion['changelog']    = 'docs/changelog.txt';
+$modversion['license']      = 'docs/license.txt';
+$modversion['official']     = 1;
+$modversion['author']       = 'Carl P. Corliss (carl.corliss@xaraya.com)';
+$modversion['contact']      = 'http://www.xaraya.com/';
+$modversion['admin']        = 1;
+$modversion['class']        = 'Utility';
+$modversion['category']     = 'Global';
 // this module requires the gd extension (for now)
-$modversion['extensions'] = array('gd');
+$modversion['extensions']   = array('gd');
 
 ?>
