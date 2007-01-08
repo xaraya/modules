5498e337fe43c8ed32cc8c58c9138b498414aa34
michelv <michelv@xarayahosting.nl>
2007-01-08 11:19:28 +0000

    Update copyright years to 2007

--- xaruserapi/createhook.php
+++ xaruserapi/createhook.php
@@ -3,7 +3,7 @@
  * Hook for create Julian
  *
  * @package modules
- * @copyright (C) 2005-2006 The Digital Development Foundation
+ * @copyright (C) 2005-2007 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
