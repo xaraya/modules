2614395f50f5d6eefd7021c84fd28f875107438d
michelv <michelv@xarayahosting.nl>
2007-01-08 11:14:53 +0000

    Update copyright years to 2007

--- xaruserapi/check_enrolled.php
+++ xaruserapi/check_enrolled.php
@@ -3,7 +3,7 @@
  * Check to see if user is already attached to a planned course
  *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2007 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
