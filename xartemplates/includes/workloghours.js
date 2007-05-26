955d7e120d14b1b3fe2f28f1a4869bfe9c3369fd
marty.vance <marty.vance@gmail.com>
2007-05-26 02:24:53 +0000

    forgot to make add recursive: more ui cleanups, merged in chad\'s local changes manually

---
+++ xartemplates/includes/workloghours.js
@@ -0,0 +1,8 @@
+function update_hours_remaining() {
+    var prev_hours = getElementById('hours').value;
+    var prev_hours_remaining = getElementById('hours_remaining').value;
+
+    prev_hours = 
+    getElementById('hours_remaining').value = getElementById('hours_remaining').value - getElementById('hours').value
+    
+}
