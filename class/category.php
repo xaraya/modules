dc350ca8432f0c300787e057e66fa6da0376549e
mfl <mfl@netspan.ch>
2007-07-06 19:21:32 +0000

    fix for category class: when not importing categories go directly to the parent classes createItem method

--- class/category.php
+++ class/category.php
@@ -12,25 +12,27 @@
             // there may not be an entry point passed
             $entry = isset($entry) ? $entry : array();
 
-            // replace parentid imported with the local ones
-            $parentindex = $args['parent_id'];
-            if (in_array($parentindex,array_keys($this->parentindices))) {
-                $args['parent_id'] = $this->parentindices[$parentindex];
-            } else {
-                // there could be more than 1 entry point, therefore the array
-                if (count($entry > 0)) {
-                    $this->parentindices[$parentindex] = array_shift($entry);
-                    $args['parent_id'] = $this->parentindices[$parentindex];
-                } else {
-                    $args['parent_id'] = 0;
-                }
-            }
+            if (isset($args['parent_id'])) {
+            	// If this is an import replace parentid imported with the local ones
+				$parentindex = $args['parent_id'];
+				if (in_array($parentindex,array_keys($this->parentindices))) {
+					$args['parent_id'] = $this->parentindices[$parentindex];
+				} else {
+					// there could be more than 1 entry point, therefore the array
+					if (count($entry > 0)) {
+						$this->parentindices[$parentindex] = array_shift($entry);
+						$args['parent_id'] = $this->parentindices[$parentindex];
+					} else {
+						$args['parent_id'] = 0;
+					}
+				}
+			}
 
             // we have all the values, do it
             $id = parent::createItem($args);
 
             // add this category to the list of known parents
-            $this->parentindices[$args['id']] = $id;
+            if (isset($args['parent_id'])) $this->parentindices[$args['id']] = $id;
 
             // do the Celko dance and update all the left/right values
             return xarModAPIFunc('categories','admin','updatecelkolinks',array('cid' => $id));
