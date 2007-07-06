76b9b31db287f6677d6bd17f6875dfb50b92fe2a
mfl <mfl@netspan.ch>
2007-07-06 11:28:39 +0000

    allow basecategories per itemtype

--- xarproperties/categorypicker.php
+++ xarproperties/categorypicker.php
@@ -40,8 +40,9 @@ class CategoryPickerProperty extends DataProperty
         $currentbaseids = array();
         if (!xarVarFetch($name . '_categories_basecatcid', 'array', $basecid, array(), XARVAR_DONT_REUSE)) return;
         if (!xarVarFetch($name . '_categories_basecatname', 'array', $basename, array(), XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch($name . '_categories_basecatitemtype', 'array', $baseitemtype, array(), XARVAR_DONT_REUSE)) return;
         if (!xarVarFetch($name . '_categorypicker_localmodule', 'str', $localmodule, xarModGetName(), XARVAR_NOT_REQUIRED)) return;
-        if (!xarVarFetch($name . '_categorypicker_localitemtype', 'str', $localitemtype, 0, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch($name . '_categorypicker_localitemtype', 'int', $localitemtype, 0, XARVAR_NOT_REQUIRED)) return;
         $info = xarMod::getBaseInfo($localmodule);
 
         xarMod::loadDbInfo('categories');
@@ -49,18 +50,20 @@ class CategoryPickerProperty extends DataProperty
         for($i=0;$i<$numberofbasecats;$i++) {
             $thiscid = isset($basecid[$i]) && is_numeric($basecid[$i]) ? $basecid[$i] : 0;
             $thisname = (isset($basename[$i]) && !empty($basename[$i])) ? $basename[$i] : xarML('Base Category #(1)',$i+1);
+            $thisitemtype = isset($baseitemtype[$i]) ? $baseitemtype[$i] : 0;
             $thisbasecat = xarModAPIFunc('categories','user','getcatbase',array('name' => $thisname, 'module' => $name));
             if (!empty($thisbasecat)) {
                 $currentbaseids[] = $thisbasecat['id'];
                 $q = new xarQuery('UPDATE', $xartable['categories_basecategories']);
                 $q->eq('module_id',xarMod::getID($localmodule));
+                $q->eq('itemtype',$thisitemtype);
                 $q->eq('name',$thisname);
                 $q->addfield('category_id',$thiscid);
                 if (!$q->run()) return;
             } else {
                 $q = new xarQuery('INSERT', $xartable['categories_basecategories']);
                 $q->addfield('module_id',xarMod::getID($localmodule));
-                $q->addfield('itemtype',$localitemtype);
+                $q->addfield('itemtype',$thisitemtype);
                 $q->addfield('name',$thisname);
                 $q->addfield('category_id',$thiscid);
                 if (!$q->run()) return;
@@ -69,7 +72,7 @@ class CategoryPickerProperty extends DataProperty
         }
         $q = new xarQuery('DELETE', $xartable['categories_basecategories']);
         $q->eq('module_id',xarMod::getID($localmodule));
-        if (!empty($localitemtype)) $q->eq('itemtype',$localitemtype);
+        if (!empty($localitemtype)) $q->in('itemtype',$localitemtype);
         if (!empty($currentbaseids)) $q->notin('id',$currentbaseids);
         if (!$q->run()) return;
         return true;
@@ -88,25 +91,11 @@ class CategoryPickerProperty extends DataProperty
         } else {
             $data['categories_localitemtype'] = $data['itemtype'];
         }
-/*        $modid = xarModGetIDFromName($data['categories_localmodule']);
-        if (empty($modid)) {
-            $msg = xarML('Invalid #(1) for #(2) method #(3)() in module #(4)', 'module name', 'property', 'showinput', 'categories');
-            throw new BadParameterException(null,$msg);
-        }
-*/
+
         if (!isset($data['basecids'])) {
             $basecats = xarModAPIFunc('categories','user','getallcatbases',array('module' => $data['categories_localmodule'], 'itemtype' => $data['categories_localitemtype']));
         }
         if (!isset($data['categories_numberofbasecats'])) $data['categories_numberofbasecats'] = count($basecats);
-/*        $categories_basecatcid = array();
-        $categories_basecatname = array();
-        foreach ($basecats as $basecat) {
-            $categories_basecatcid[] = $basecat['cid'];
-            $categories_basecatname[] = $basecat['name'];
-        }
-        $data['categories_basecatcid'] = $categories_basecatcid;
-        $data['categories_basecatname'] = $categories_basecatname;
-        */
         $seencid = array();
         $items = array();
         for ($i = 0; $i < $data['categories_numberofbasecats']; $i++) {
@@ -118,13 +107,6 @@ class CategoryPickerProperty extends DataProperty
             // preserve order of root categories if possible - do not use this for multi-select !
             if (isset($cleancids[$i])) $seencid = array($cleancids[$i] => 1);
             // TODO: improve memory usage
-            // limit to some reasonable depth for now
-            /*
-            $item['select'] = xarModAPIFunc('categories', 'visual', 'makeselect',
-                                           array('values' => &$seencid,
-                                                 'name_prefix' => 'config_',
-                                                 'maximum_depth' => 4,
-                                                 'show_edit' => true));*/
             $items[] = $item;
         }
         unset($item);
@@ -156,12 +138,7 @@ class CategoryPickerProperty extends DataProperty
         if (empty($data['itemtype'])) {
             $data['itemtype'] = 0;
         }
-/*        $modid = xarModGetIDFromName($data['localmodule']);
-        if (empty($modid)) {
-            $msg = xarML('Invalid #(1) for #(2) method #(3)() in module #(4)', 'module name', 'property', 'showinput', 'categories');
-            throw new BadParameterException(null,$msg);
-        }
-*/
+
         $basecidlist = unserialize(xarModVars::get($data['localmodule'],'basecids',$data['itemtype']));
         if (!isset($data['basecids'])) $data['basecids'] = $basecidlist;
         if (!isset($data['numberofbasecats'])) $data['numberofbasecats'] = count($data['basecids']);
@@ -180,13 +157,6 @@ class CategoryPickerProperty extends DataProperty
             // preserve order of root categories if possible - do not use this for multi-select !
             if (isset($cleancids[$i])) $seencid = array($cleancids[$i] => 1);
             // TODO: improve memory usage
-            // limit to some reasonable depth for now
-            /*
-            $item['select'] = xarModAPIFunc('categories', 'visual', 'makeselect',
-                                           array('values' => &$seencid,
-                                                 'name_prefix' => 'config_',
-                                                 'maximum_depth' => 4,
-                                                 'show_edit' => true));*/
             $items[] = $item;
         }
         unset($item);
@@ -203,36 +173,6 @@ class CategoryPickerProperty extends DataProperty
 
         return parent::showOutput($data);
     }
-/*    public function checkInput($name = '', $value = null)
-    {
-        $name = empty($name) ? 'dd_'.$this->id : $name;
-        // store the fieldname for validations who need them (e.g. file uploads)
-        $this->fieldname = $name;
-        if (!isset($value)) {
-            list($isvalid, $years) = $this->fetchValue($name . '_year');
-            list($isvalid, $months) = $this->fetchValue($name . '_month');
-            list($isvalid, $days) = $this->fetchValue($name . '_day');
-//                if (!xarVarFetch($name . '_year', 'isset', $years,  NULL, XARVAR_DONT_SET)) {return;}
-//                if (!xarVarFetch($name . '_month', 'isset', $months,  NULL, XARVAR_DONT_SET)) {return;}
-//                if (!xarVarFetch($name . '_day', 'isset', $days,  NULL, XARVAR_DONT_SET)) {return;}
-        }
-        if (!isset($years) ||!isset($months) ||!isset($days)) return false;
-        $value = mktime(0,0,0,$months,$days,$years);
-    if (!xarVarFetch('numberofbasecats', 'int', $data['numberofbasecats'], 0, XARVAR_NOT_REQUIRED)) return;
-    $count = $data['numberofbasecats'];
-    $basecids = array();
-    if (!xarVarFetch('basecid', 'array', $basecid, '', XARVAR_DONT_REUSE)) return;
-    for($i=0;$i<$count;$i++) $basecids[] = isset($basecid[$i]) ? $basecid[$i] : 0;
-    if (!empty($ptid)) {
-        xarModSetUserVar('articles','basecids',serialize($basecids),$ptid);
-    } else {
-        xarModSetVar('articles','basecids',serialize($basecids));
-    }
-
-
-        return $this->validateValue($value);
-    }
-    */
 }
 
 ?>
