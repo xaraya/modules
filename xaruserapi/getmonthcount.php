0ecd7afe2d176968315c1f001fec0609f6e42b94
judgej <judgej@xaraya.com>
2007-08-20 17:23:28 +0000

    monthcounts API: support limiting counts by categories

--- xaruserapi/getmonthcount.php
+++ xaruserapi/getmonthcount.php
@@ -26,36 +26,87 @@ function articles_userapi_getmonthcount($args)
     // Get the field names and LEFT JOIN ... ON ... parts from articles
     // By passing on the $args, we can let leftjoin() create the WHERE for
     // the articles-specific columns too now
-    $articlesdef = xarModAPIFunc('articles','user','leftjoin',$args);
+    $articlesdef = xarModAPIFunc('articles', 'user', 'leftjoin', $args);
 
     // Bug 1590 - Create custom query supported by each database.
     $dbtype = xarDBGetType();
+
+    // If categories have been passed in, then join them in.
+    if (!empty($args['cids'])) {
+        // Get the LEFT JOIN ... ON ...  and WHERE (!) parts from categories
+        // This function supports itemtype arrays, so pass in ptids.
+        $categoriesdef = xarModAPIFunc(
+            'categories', 'user', 'leftjoin',
+            array(
+                'cids' => $args['cids'],
+                'andcids' => true, //$andcids,
+                'itemtype' => (isset($args['ptid']) ? $args['ptid'] : null),
+                'modid' => xarModGetIDFromName('articles'),
+            )
+        );
+
+        if (empty($categoriesdef)) return;
+    }
+
+    // Select distinct if we are limited by category.
+    $distinct = !empty($args['cids']) ? 'DISTINCT ' : '';
+
+    // TODO:  Add SQL queries for Oracle, etc.
     switch ($dbtype) {
         case 'mysql':
-            $query = "SELECT LEFT(FROM_UNIXTIME(xar_pubdate),7) AS mymonth, COUNT(*) FROM " . $articlesdef['table'];
+            $select = 'LEFT(FROM_UNIXTIME(xar_pubdate),7) AS mymonth, COUNT(' . $distinct . 'xar_aid)';
+            $from = $articlesdef['table'];
             break;
         case 'postgres':
-            $query = "SELECT TO_CHAR(ABSTIME(xar_pubdate),'YYYY-MM') AS mymonth, COUNT(*) FROM " . $articlesdef['table'];
+            $select = 'TO_CHAR(ABSTIME(xar_pubdate),\'YYYY-MM\') AS mymonth, COUNT(' . $distinct . 'xar_aid)';
+            $from = $articlesdef['table'];
             break;
         case 'mssql':
-            $query = "SELECT LEFT(CONVERT(VARCHAR,DATEADD(ss,xar_pubdate,'1/1/1970'),120),7) as mymonth, COUNT(*) FROM " . $articlesdef['table'];
+            $select = 'LEFT(CONVERT(VARCHAR,DATEADD(ss,xar_pubdate,\'1/1/1970\'),120),7) as mymonth, COUNT(' . $distinct . 'xar_aid)';
+            $from = $articlesdef['table'];
             break;
-        // TODO:  Add SQL queries for Oracle, etc.
         default:
             return;
     }
+
+    $where = array();
+
     if (!empty($articlesdef['where'])) {
-        $query .= ' WHERE ' . $articlesdef['where'];
+        $where[] = $articlesdef['where'];
     }
+
     switch ($dbtype) {
         case 'mssql':
-            $query .= " GROUP BY LEFT(CONVERT(VARCHAR,DATEADD(ss,xar_pubdate,'1/1/1970'),120),7)";
+            $groupby = 'LEFT(CONVERT(VARCHAR,DATEADD(ss,xar_pubdate,\'1/1/1970\'),120),7)';
             break;
         default:
-            $query .= ' GROUP BY mymonth';
+            $groupby = 'mymonth';
             break;
     }
+
+    if (!empty($args['cids'])) {
+        // add this for SQL compliance when there are multiple JOINs
+        // bug 4429: sqlite doesnt like the parentheses
+        if ($dbconn->databaseType != 'sqlite') $from = '(' . $from . ')';
+
+        // Add the LEFT JOIN ... ON ... parts from categories
+        $from .= ' INNER JOIN ' . $categoriesdef['table'];
+        $from .= ' ON ' . $categoriesdef['field'] . ' = ' . $articlesdef['aid'];
+
+        if (!empty($categoriesdef['more']) && $dbconn->databaseType != 'sqlite') {
+            $from = '(' . $from . ') ' . $categoriesdef['more'];
+        }
+
+        $where[] = $categoriesdef['where'];
+    }
+
+    // Put the query together.
+    $query = 'SELECT ' . $select . ' FROM ' . $from;
+    if (!empty($where)) $query .= ' WHERE ' . implode(' AND ', $where);
+    if (!empty($groupby)) $query .= ' GROUP BY ' . $groupby;
+
     $result =& $dbconn->Execute($query);
+
     if (!$result) return;
 
     $months = array();
