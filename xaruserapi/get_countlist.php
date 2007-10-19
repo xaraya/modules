9d9d264d14e9ee598ea2dceb4934d2c6e73605c3
andream <andream@xaraya.com>
2007-10-19 10:40:39 +0000

    allowing list of objectids to be optional

--- xaruserapi/get_countlist.php
+++ xaruserapi/get_countlist.php
@@ -18,7 +18,7 @@
  * @access  public
  * @param   integer   $modid        the id of the module that these nodes belong to
  * @param   integer   $itemtype     the item type that these nodes belong to
- * @param   array     $objectids    the list of ids of the items that these nodes belong to
+ * @param   array     $objectids    (optional) the list of ids of the items that these nodes belong to
  * @param   integer   $startdate    (optional) comments posted at startdate or later
  * @returns array     the number of comments for the particular modid/objectids pairs,
  *                    or raise an exception and return false.
@@ -38,15 +38,6 @@ function comments_userapi_get_countlist($args)
         $exception |= true;
     }
 
-
-    if ( !isset($objectids) || !is_array($objectids) ) {
-        $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
-                     'objectids', 'userapi', 'get_countlist', 'comments');
-        xarErrorSet(XAR_USER_EXCEPTION, 'BAD_PARAM',
-                        new SystemException($msg));
-        $exception |= true;
-    }
-
     if ($exception) {
         return false;
     }
@@ -58,12 +49,15 @@ function comments_userapi_get_countlist($args)
     $sql = "SELECT  $ctable[objectid], COUNT($ctable[cid]) as numitems
               FROM  $xartable[comments]
              WHERE  $ctable[modid]=$modid
-               AND  $ctable[objectid] IN ('" . join("', '",$objectids) . "')
                AND  $ctable[status]="._COM_STATUS_ON;
 
     if (isset($itemtype) && is_numeric($itemtype)) {
         $sql .= " AND $ctable[itemtype]=$itemtype";
     }
+
+    if ( isset($objectids) && is_array($objectids) ) {
+        $sql .= " AND  $ctable[objectid] IN ('" . join("', '",$objectids) . "')";
+	}
 
     if (!empty($startdate) && is_numeric($startdate)) {
         $sql .= " AND $ctable[cdate]>=$startdate";
