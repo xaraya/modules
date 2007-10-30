8bdaf412c250c9298aecb3e5b8b2dd99785753f9
michelv <michelv@xarayahosting.nl>
2007-10-30 11:30:00 +0000

    * xartemplates/admin-main.xd: update for general layout 	* xarversion.php: qa

--- xarversion.php
+++ xarversion.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * File: $Id$
- *
  * contains the module information
  *
  * @package modules
@@ -11,23 +9,22 @@
  * @subpackage trackback
  * @author John Cox
 */
-$modversion['name'] = 'TrackBack';
-$modversion['id'] = '183';
-$modversion['version'] = '1.0.0';
-$modversion['displayname']    = xarML('Trackback');
-$modversion['description'] = 'Allows for the sending and recieving of trackbacks';
-$modversion['credits'] = 'xardocs/credits.txt';
-$modversion['help'] = '';
-$modversion['changelog'] = '';
-$modversion['license'] = '';
-$modversion['official'] = 0;
-$modversion['author'] = 'John Cox';
-$modversion['contact'] = 'niceguyeddie@xaraya.com';
-$modversion['admin'] = 1;
-$modversion['user'] = 0;
-$modversion['securityschema'] = array('Trackback::' => 'Module Id : Module Page ID : Comment ID');
-$modversion['class'] = 'Utility';
-$modversion['category'] = 'Content';
+$modversion['name']         = 'TrackBack';
+$modversion['id']           = '183';
+$modversion['version']      = '1.0.0';
+$modversion['displayname']  = xarML('Trackback');
+$modversion['description']  = 'Allows for the sending and recieving of trackbacks';
+$modversion['credits']      = 'xardocs/credits.txt';
+$modversion['help']         = '';
+$modversion['changelog']    = '';
+$modversion['license']      = '';
+$modversion['official']     = 0;
+$modversion['author']       = 'John Cox';
+$modversion['contact']      = 'niceguyeddie@xaraya.com';
+$modversion['admin']        = 1;
+$modversion['user']         = 0;
+$modversion['class']        = 'Utility';
+$modversion['category']     = 'Content';
 // dependancies??
-$modversion['requires'] = array(14);
+$modversion['requires']     = array(14);
 ?>
