0bd6605e55c49a7c1d097dad17d209c415820850
icedlava <icedlava@2skies.com>
2007-11-27 05:19:13 +0000

    Update obfuemail

--- xaruserapi/obfuemail.php
+++ xaruserapi/obfuemail.php
@@ -16,6 +16,8 @@
  * Obfuscation for email - another method
  *
  * @author Jo Dalle Nogare
+ * Takes an email address, optional text  for the link text
+ * If no text supplied then the email address is used for the link text and partial obfuscated for the display
  * @param $email email address to be encoded
  * @$param $text optional text string to be displayed in email link
  * @$param $image optional boolean to display email image, false by default
@@ -29,21 +31,25 @@ function sitecontact_userapi_obfuemail($args)
 extract($args);
 
     if (!isset($email) || empty($email)) {return;}
-    if (!isset($text) || empty($text)) {$text = $email;}
-    if (!isset($usejs)) $usejs = FALSE;
-    if ($usejs) {
-       $maildata['link']=createMailto($email);
-    }else {
-        $maildata = array();
-        $encoded = bin2hex($email);
-        $encoded = chunk_split($encoded, 2, '%');
-        $encoded = '%' . substr($encoded, 0, strlen($encoded) - 1);
-        $maildata['encoded']=$encoded;
-    }
-    if (!isset($text) || empty($text)) {
-        $maildata['text']=xarML('email us');
-    }else{
+
+    $newemail = $email;
+    $maildata = array();
+    $encoded = bin2hex($newemail);
+    $encoded = chunk_split($encoded, 2, '%');
+    $encoded = '%' . substr($encoded, 0, strlen($encoded) - 1);
+    $maildata['encoded']=$encoded;
+
+    if (isset($text) && !empty($text)) {
         $maildata['text']=$text;
+    }else{
+        $newaddress = '';
+        for($intCounter = 0; $intCounter < strlen($email); $intCounter++){
+            $newaddress .= "&#" . ord(substr($email,$intCounter,1)) . ";";
+        }
+        $newtext=explode("&#64;", $newaddress);
+        $at = xarML(' at ');
+        $dot = xarML(' dot ');
+        $maildata['text'] = $newtext[0].$at. str_replace("&#46;",$dot,$newtext[1]);
     }
     if (isset($image) && TRUE==$image) {
        $img = xarTplGetImage('email.gif','sitecontact');
@@ -51,7 +57,6 @@ extract($args);
     }else {
         $maildata['link']= "<a href=\"mailto:{$maildata['encoded']}\">" .$imgsrc.$maildata['text'] . "</a>";
     }
-
 
     return $maildata;
 }
