19f8d18d710a69eb2686620abe8549592c5c0dd9
icedlava <icedlava@2skies.com>
2007-11-27 08:59:27 +0000

    Update headers

--- xaruserapi/obfuemail.php
+++ xaruserapi/obfuemail.php
@@ -3,7 +3,7 @@
  * Obfuscation function
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) 2002-2007 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -18,13 +18,13 @@
  * @author Jo Dalle Nogare
  * Takes an email address, optional text  for the link text
  * If no text supplied then the email address is used for the link text and partial obfuscated for the display
- * @param $email email address to be encoded
- * @$param $text optional text string to be displayed in email link
- * @$param $image optional boolean to display email image, false by default
+ * @$param text $email email address to be encoded
+ * @$param text $text optional text string to be displayed in email link
+ * @$param boolean $image optional flag to display email image, false by default
  * @return array $maildata with values of
- *    $$maildata['encoded'] the encoded email
- *    $$maildata['text'] the text displayed, defaults to 'email us' if null passed in
- *    $$maildata['link'] full link with displayed text if required
+ *    $maildata['encoded'] the encoded email
+ *    $maildata['text'] the text displayed, defaults to slight obfuscated replaced email address
+ *    $maildata['link'] full link with displayed text if required
  */
 function sitecontact_userapi_obfuemail($args)
 {
@@ -46,10 +46,10 @@ extract($args);
         for($intCounter = 0; $intCounter < strlen($email); $intCounter++){
             $newaddress .= "&#" . ord(substr($email,$intCounter,1)) . ";";
         }
-        $newtext=explode("&#64;", $newaddress);
+        $newtext=explode("&#064;", $newaddress);
         $at = xarML(' at ');
         $dot = xarML(' dot ');
-        $maildata['text'] = $newtext[0].$at. str_replace("&#46;",$dot,$newtext[1]);
+        $maildata['text'] = $newtext[0].$at. str_replace("&#046;",$dot,$newtext[1]);
     }
     if (isset($image) && TRUE==$image) {
        $img = xarTplGetImage('email.gif','sitecontact');
