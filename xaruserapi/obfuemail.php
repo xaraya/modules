df22bf334117922169b063b8e7176a02713db150
jojodee <jojodee@xaraya.com>
2007-12-01 12:58:54 +0000

    Update the obfuscation function inline with the 1x latest

--- xaruserapi/obfuemail.php
+++ xaruserapi/obfuemail.php
@@ -3,7 +3,7 @@
  * Obfuscation function
  *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2007The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -16,31 +16,48 @@
  * Obfuscation for email - another method
  *
  * @author Jo Dalle Nogare
- * @param $email email address to be encoded
- * @$param $text optional text string to be displayed in email link
+ * Takes an email address, optional text  for the link text
+ * If no text supplied then the email address is used for the link text and partial obfuscated for the display
+ * @$param text $email email address to be encoded
+ * @$param text $text optional text string to be displayed in email link
+ * @$param boolean $image optional flag to display email image, false by default
  * @return array $maildata with values of
- *    $$maildata['encoded'] the encoded email
- *    $$maildata['text'] the text displayed, defaults to 'email us' if null passed in
- *    $$maildata['link'] full link with displayed text if required
+ *    $maildata['encoded'] the encoded email
+ *    $maildata['text'] the text displayed, defaults to slight obfuscated replaced email address
+ *    $maildata['link'] full link with displayed text if required
  */
 function sitecontact_userapi_obfuemail($args)
 {
-    extract($args);
+ extract($args);
 
     if (!isset($email) || empty($email)) {return;}
 
-    /* Initialise the array that will hold the returned data*/
+    $newemail = $email;
     $maildata = array();
-    $encoded = bin2hex($email);
+    $encoded = bin2hex($newemail);
     $encoded = chunk_split($encoded, 2, '%');
     $encoded = '%' . substr($encoded, 0, strlen($encoded) - 1);
     $maildata['encoded']=$encoded;
-    if (!isset($text) || empty($text)) {
-        $maildata['text']=xarML('email us');
-    }else{
+
+    if (isset($text) && !empty($text)) {
         $maildata['text']=$text;
+    }else{
+        $newaddress = '';
+        for($intCounter = 0; $intCounter < strlen($email); $intCounter++){
+            $newaddress .= "&#" . ord(substr($email,$intCounter,1)) . ";";
+        }
+        $newtext=explode("&#64;", $newaddress);
+        $at = xarML(' at ');
+        $dot = xarML(' dot ');
+        $maildata['text'] = $newtext[0].$at. str_replace("&#46;",$dot,$newtext[1]);
     }
-    $maildata['link']= "<a href=\"mailto:" . $maildata['encoded'] . "\">" . $maildata['text'] . "</a>";
+    if (isset($image) && TRUE==$image) {
+       $img = xarTplGetImage('email.gif','sitecontact');
+       $maildata['link']= "<a href=\"mailto:{$maildata['encoded']}\"><img src=\"{$img}\" alt=\"Email\" />&#160;".$maildata['text'] . "</a>";
+    }else {
+        $maildata['link']= "<a href=\"mailto:{$maildata['encoded']}\">" .$maildata['text'] . "</a>";
+    }
+
     return $maildata;
 }
 ?>
