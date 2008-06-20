3dcf7ae371cf5c4607ddc4ed3dc6ef7e4592306b
mfl <mfl@netspan.ch>
2008-06-20 14:57:14 +0000

    more files added

---
+++ xaruser/modify.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Modify an item of the event object
+ *
+ */
+    function calendar_user_modify()
+    {
+        if (!xarSecurityCheck('EditCalendar')) return;
+
+        if (!xarVarFetch('itemid',  'int',  $data['itemid'], 0, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('page',  'str:1',  $data['page'], 'week', XARVAR_NOT_REQUIRED)) return;
+        $data['object'] = DataobjectMaster::getObject(array('name' => 'calendar_event'));
+        $data['object']->getItem(array('itemid' => $data['itemid']));        
+        $data['tplmodule'] = 'calendar';
+        $data['authid'] = xarSecGenAuthKey();
+        return $data;
+    }
+?>
