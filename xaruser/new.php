e1f987438ff2364b0203deb1051fe6505db6acf7
mfl <mfl@netspan.ch>
2008-06-20 14:24:24 +0000

    add new filesd

---
+++ xaruser/new.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Create a new item of the event object
+ *
+ */
+    function calendar_user_new()
+    {
+        if (!xarSecurityCheck('AddCalendar')) return;
+
+        if (!xarVarFetch('page',  'str:1',  $data['page'], 'week', XARVAR_NOT_REQUIRED)) return;
+        $data['object'] = DataobjectMaster::getObject(array('name' => 'calendar_event'));
+        $data['tplmodule'] = 'payments';
+        $data['authid'] = xarSecGenAuthKey('dynamicdata');
+        return $data;
+    }
+?>
