f60419f290f0170fa84ee082e32063e422f8f37a
mfl <mfl@netspan.ch>
2008-06-20 18:10:40 +0000

    work on modify and display functionality

--- xaruser/display.php
+++ xaruser/display.php
@@ -1,8 +1,19 @@
 <?php
+/**
+ * Display an item of the event object
+ *
+ */
 
-function calendar_user_display()
-{
+    function calendar_user_display()
+    {
+        if (!xarSecurityCheck('ReadCalendar')) return;
 
-}
-
+        if (!xarVarFetch('itemid',  'int',  $data['itemid'], 0, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('page',  'str:1',  $data['page'], 'week', XARVAR_NOT_REQUIRED)) return;
+        $data['object'] = DataobjectMaster::getObject(array('name' => 'calendar_event'));
+        $data['object']->getItem(array('itemid' => $data['itemid']));        
+        $data['tplmodule'] = 'calendar';
+        $data['authid'] = xarSecGenAuthKey();
+        return $data;
+    }
 ?>
