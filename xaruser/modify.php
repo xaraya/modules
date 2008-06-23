6e4fde8a1fa90329acf239ae221b2f34a00151e6
mfl <mfl@netspan.ch>
2008-06-23 05:48:31 +0000

    add page var to ddcontext

--- xaruser/modify.php
+++ xaruser/modify.php
@@ -9,7 +9,7 @@
 
         if (!xarVarFetch('itemid',  'int',  $data['itemid'], 0, XARVAR_NOT_REQUIRED)) return;
         if (!xarVarFetch('page',  'str:1',  $data['page'], 'week', XARVAR_NOT_REQUIRED)) return;
-        xarSession::setVar('ddcontext.calendar', array('page' => $page,
+        xarSession::setVar('ddcontext.calendar', array('page' => $data['page'],
                                                         ));
         $data['object'] = DataobjectMaster::getObject(array('name' => 'calendar_event'));
         $data['object']->getItem(array('itemid' => $data['itemid']));        
--- xaruser/new.php
+++ xaruser/new.php
@@ -8,7 +8,7 @@
         if (!xarSecurityCheck('AddCalendar')) return;
 
         if (!xarVarFetch('page',  'str:1',  $data['page'], 'week', XARVAR_NOT_REQUIRED)) return;
-        xarSession::setVar('ddcontext.calendar', array('page' => $page,
+        xarSession::setVar('ddcontext.calendar', array('page' => $data['page'],
                                                         ));
         $data['object'] = DataobjectMaster::getObject(array('name' => 'calendar_event'));
         $data['tplmodule'] = 'calendar';
