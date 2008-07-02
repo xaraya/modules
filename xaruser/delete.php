a8283272a25402e91ef6ad6e5e8f1a7f8f94f8ef
mfl <mfl@netspan.ch>
2008-07-02 10:54:03 +0000

    add delete files

---
+++ xaruser/delete.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Delete an item
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Dynamic Data module
+ * @link http://xaraya.com/index.php/release/182.html
+ * @author mikespub <mikespub@xaraya.com>
+ */
+/**
+ * delete item
+ * @param 'itemid' the id of the item to be deleted
+ * @param 'confirm' confirm that this item can be deleted
+ */
+
+sys::import('modules.dynamicdata.class.objects.master');
+
+function calendar_user_delete($args)
+{
+   extract($args);
+
+    if(!xarVarFetch('objectid',   'isset', $objectid,   NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('name',       'isset', $name,       NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('itemid',     'id',    $itemid                          )) {return;}
+    if(!xarVarFetch('confirm',    'isset', $confirm,    NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('noconfirm',  'isset', $noconfirm,  NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('join',       'isset', $join,       NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('table',      'isset', $table,      NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('tplmodule',  'isset', $tplmodule,  NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('template',   'isset', $template,   NULL, XARVAR_DONT_SET)) {return;}
+    if(!xarVarFetch('return_url', 'isset', $return_url, NULL, XARVAR_DONT_SET)) {return;}
+
+    $myobject = & DataObjectMaster::getObject(array('objectid' => $objectid,
+                                         'name'       => $name,
+                                         'join'       => $join,
+                                         'table'      => $table,
+                                         'itemid'     => $itemid,
+                                         'tplmodule'  => $tplmodule,
+                                         'template'   => $template,
+                                         'extend'     => false));  //Note: this means we only delete this extension, not the parent
+    if (empty($myobject)) return;
+    $data = $myobject->toArray();
+
+    // Security check
+    if(!xarSecurityCheck('DeleteDynamicDataItem',1,'Item',$data['moduleid'].":".$data['itemtype'].":".$data['itemid'])) return;
+
+    // recover any session var information and remove it from the var
+    $data = array_merge($data,xarModAPIFunc('dynamicdata','user','getcontext',array('module' => $tplmodule)));
+    //xarSession::setVar('ddcontext.' . $tplmodule, array('tplmodule' => $tplmodule));
+    extract($data);
+
+
+    $myobject->getItem();
+
+    if (empty($confirm)) {
+        // TODO: is this needed?
+        $data = array_merge($data,xarModAPIFunc('dynamicdata','admin','menu'));
+        $data['object'] = & $myobject;
+        if ($data['objectid'] == 1) {
+            $mylist = & DataObjectMaster::getObjectList(array('objectid' => $data['itemid'], 'extend' => false));
+            if (count($mylist->properties) > 0) {
+                $data['related'] = xarML('Warning : there are #(1) properties and #(2) items associated with this object !', count($mylist->properties), $mylist->countItems());
+            }
+        }
+        $data['authid'] = xarSecGenAuthKey();
+
+        if (file_exists('modules/' . $data['tplmodule'] . '/xartemplates/user-delete.xd') ||
+            file_exists('modules/' . $data['tplmodule'] . '/xartemplates/admin-delete-' . $data['template'] . '.xd')) {
+            return xarTplModule($data['tplmodule'],'user','delete',$data,$data['template']);
+        } else {
+            return xarTplModule('dynamicdata','admin','delete',$data,$data['template']);
+        }
+    }
+
+    return true;
+}
+
+?>
