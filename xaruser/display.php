8c2ef1aad94c36f81234a7ee98552a7ffe292c75
mfl <mfl@netspan.ch>
2008-08-11 19:33:53 +0000

    merge debris

--- xaruser/display.php
+++ xaruser/display.php
@@ -27,14 +27,14 @@ function messages_user_display( )
         $read_messages = array();
     }
 
-	//Psspl:Added the code for configuring the user-menu
-	$data['allow_newpm'] = xarModAPIFunc('messages' , 'user' , 'isset_grouplist');
+    //Psspl:Added the code for configuring the user-menu
+    $data['allow_newpm'] = xarModAPIFunc('messages' , 'user' , 'isset_grouplist');
     
-	//psspl:Added the code for resolving issue of modifing draft messages.
-	if ($folder != 'drafts') {
-   		$messages = xarModAPIFunc('messages', 'user', 'getall', array('folder' => $folder));
+    //psspl:Added the code for resolving issue of modifing draft messages.
+    if ($folder != 'drafts') {
+        $messages = xarModAPIFunc('messages', 'user', 'getall', array('folder' => $folder));
     } else {
-    	$messages = xarModAPIFunc('messages', 'user', 'checkdraft');	
+        $messages = xarModAPIFunc('messages', 'user', 'checkdraft');    
     }
     
     if (is_array($messages)) {
@@ -44,15 +44,15 @@ function messages_user_display( )
         $data['messages']                = $messages;
         //Psspl:Added the code for attaching foder type to the link.
        foreach($messages as  $key=>$message){
-        	
-        	if(isset($data['messages'][$key]['view_link']))
-        		$data['messages'][$key]['view_link']       .= "&folder=$folder"; 
-        	
-        	if(isset($data['messages'][$key]['modify_link']))
-        		$data['messages'][$key]['modify_link']     .= "&folder=$folder"; 
-        	
-        	if(isset($data['messages'][$key]['delete_link']))
-        		$data['messages'][$key]['delete_link'] 	   .= "&folder=$folder"; 
+            
+            if(isset($data['messages'][$key]['view_link']))
+                $data['messages'][$key]['view_link']       .= "&folder=$folder"; 
+            
+            if(isset($data['messages'][$key]['modify_link']))
+                $data['messages'][$key]['modify_link']     .= "&folder=$folder"; 
+            
+            if(isset($data['messages'][$key]['delete_link']))
+                $data['messages'][$key]['delete_link']     .= "&folder=$folder"; 
         }
         
         //Psspl:Added the code for read unread messages.
@@ -60,9 +60,9 @@ function messages_user_display( )
         $unread = 0;
         foreach($messages_inbox as $k => $message)
         {    if($message['status_alt'] == 'unread')
-        	{
-        		$unread++;
-        	}
+            {
+                $unread++;
+            }
         } 
         $data['header_attachment_image'] = xarTplGetImage('attachment.png');
         $data['header_status_image']     = xarTplGetImage('check_read.gif');
@@ -70,7 +70,7 @@ function messages_user_display( )
         $data['sent']                    = xarModAPIFunc('messages','user','count_sent');
         $data['total']                   = xarModAPIFunc('messages','user','count_total');
         $data['drafts']                  = xarModAPIFunc('messages','user','count_drafts');
-		$data['unread']  				 = $unread;
+        $data['unread']                  = $unread;
     } else {
         $list = array();
     }
--- xaruser/send.php
+++ xaruser/send.php
@@ -25,14 +25,14 @@ function messages_user_send()
     xarVarFetch('confirm', 'checkbox', $confirm, false, XARVAR_NOT_REQUIRED);
     xarVarFetch('draft',   'checkbox', $draft,   false, XARVAR_NOT_REQUIRED);
     xarVarFetch('postanon',   'checkbox', $postanon,   false, XARVAR_NOT_REQUIRED);
-	xarVarFetch('action',   'str', $action,   false, XARVAR_NOT_REQUIRED);
-	xarVarFetch('postanon_to',   'int', $postanon_to,   false, XARVAR_NOT_REQUIRED);	
+    xarVarFetch('action',   'str', $action,   false, XARVAR_NOT_REQUIRED);
+    xarVarFetch('postanon_to',   'int', $postanon_to,   false, XARVAR_NOT_REQUIRED);    
     if ($preview === true) {
         if($action == 'reply' or $action == 'prev_reply'){
-    		//$action = 'reply';
-    	} else {
-    		$action = 'preview';
-    	}       
+            //$action = 'reply';
+        } else {
+            $action = 'preview';
+        }       
     } elseif ($confirm === true) {
         $action = 'submit';
     } else {
@@ -45,15 +45,15 @@ function messages_user_send()
     $data['postanon']       = $postanon;   
     
     //Psspl:Added the code for configuring the user-menu
-	$data['allow_newpm']    = xarModAPIFunc('messages' , 'user' , 'isset_grouplist');
+    $data['allow_newpm']    = xarModAPIFunc('messages' , 'user' , 'isset_grouplist');
         
     if($action != 'submit') {
         $data['users'] = xarModAPIFunc('messages','user','get_users');
-		//Psspl:Added the code for checking user list
-		if(empty($data['users'])) {
-			$msg = xarML('There are no active users for sending messages');
-        	//throw new Exception($msg);		
-		}
+        //Psspl:Added the code for checking user list
+        if(empty($data['users'])) {
+            $msg = xarML('There are no active users for sending messages');
+            //throw new Exception($msg);        
+        }
         // djb - moving the numbers to the user-menu, adding these vars 
         $data['unread']                  = xarModAPIFunc('messages','user','count_unread');
         $data['sent']                    = xarModAPIFunc('messages','user','count_sent');
@@ -64,20 +64,20 @@ function messages_user_send()
 
     switch($action) {
         case "submit":
-			xarVarFetch('subject',   'str:1', $subject,   null, XARVAR_NOT_REQUIRED);
-			//minimum length is 7 character it sets &#160; for processing
-			xarVarFetch('body',   'str:7', $body,   null, XARVAR_NOT_REQUIRED); 
-			xarVarFetch('recipient',   'int:1', $recipient,   null, XARVAR_NOT_REQUIRED);
-			if($subject == null) {
-				$data['no_subject'] = 1;
-			}
-			if($body == null) {
-				$data['no_body'] = 1;
-			}
-			if($recipient == null){
-				$data['no_recipient'] = 1;
-			}
-			if(isset($data['no_subject']) || isset($data['no_body']) || isset($data['no_recipient'])){
+            xarVarFetch('subject',   'str:1', $subject,   null, XARVAR_NOT_REQUIRED);
+            //minimum length is 7 character it sets &#160; for processing
+            xarVarFetch('body',   'str:7', $body,   null, XARVAR_NOT_REQUIRED); 
+            xarVarFetch('recipient',   'int:1', $recipient,   null, XARVAR_NOT_REQUIRED);
+            if($subject == null) {
+                $data['no_subject'] = 1;
+            }
+            if($body == null) {
+                $data['no_body'] = 1;
+            }
+            if($recipient == null){
+                $data['no_recipient'] = 1;
+            }
+            if(isset($data['no_subject']) || isset($data['no_body']) || isset($data['no_recipient'])){
 
                 xarTplSetPageTitle( xarML('Post Message') );
 
@@ -104,46 +104,46 @@ function messages_user_send()
                 $data['recipient']                  = $recipient;
                 $data['subject']                    = $subject;
                 $data['body']                       = $body;
-				//==================================previview===============
-				//Psspl:check for sent messages reply
-				xarVarFetch('id',   'int:1', $id,   null, XARVAR_NOT_REQUIRED);
-				if(isset($id) and $id != null){
-					
-			    xarTplSetPageTitle( xarML('Messages :: Reply') );
+                //==================================previview===============
+                //Psspl:check for sent messages reply
+                xarVarFetch('id',   'int:1', $id,   null, XARVAR_NOT_REQUIRED);
+                if(isset($id) and $id != null){
+                    
+                xarTplSetPageTitle( xarML('Messages :: Reply') );
 
-					$replymessages = xarModAPIFunc('messages', 'user', 'get', array('id' => $id));
-			
+                    $replymessages = xarModAPIFunc('messages', 'user', 'get', array('id' => $id));
             
             
-            		if (!count($replymessages) || !is_array($replymessages)) {
-                		$data['error'] = xarML('Message ID nonexistant!');
-                	return $data;
-            	}
-
-            	if ($replymessages[0]['recipient_id'] != xarSession::getVar('role_id') &&
-                	$replymessages[0]['sender_id'] != xarSession::getVar('role_id')) {
-                    	$data['error'] = xarML("You are NOT authorized to view someone else's mail!");
-                    	return $data;
-            	}
             
-            	$data['id'] 			= $id;
-            	$data['post_url']       = xarModURL('messages', 'user', 'send');
-            	$data['input_title']    = xarML('Reply to a Message');
-            	$data['recipient']      = $replymessages[0]['sender_id'];
-            	$data['replymessage']   = $replymessages[0];
-			
-				// Get $recipient information
-        		$recipient_info         = xarRoles::get($data['recipient']);
-        		if (!$recipient_info) return;
-	       		$data['recipient_name'] = $recipient_info->getName();
+                    if (!count($replymessages) || !is_array($replymessages)) {
+                        $data['error'] = xarML('Message ID nonexistant!');
+                    return $data;
+                }
 
-            	$data['message']['postanon']  		= $postanon;
-            	$data['message']['postanon_to']  	= $replymessages[0]['postanon'];
-            	$data['postanon_to']  		        = $replymessages[0]['postanon'];
-            	$data['postanon']      				= $postanon;     
-				$data['action']                     = 'prev_reply';
-				
-			}
+                if ($replymessages[0]['recipient_id'] != xarSession::getVar('role_id') &&
+                    $replymessages[0]['sender_id'] != xarSession::getVar('role_id')) {
+                        $data['error'] = xarML("You are NOT authorized to view someone else's mail!");
+                        return $data;
+                }
+            
+                $data['id']             = $id;
+                $data['post_url']       = xarModURL('messages', 'user', 'send');
+                $data['input_title']    = xarML('Reply to a Message');
+                $data['recipient']      = $replymessages[0]['sender_id'];
+                $data['replymessage']   = $replymessages[0];
+            
+                // Get $recipient information
+                $recipient_info         = xarRoles::get($data['recipient']);
+                if (!$recipient_info) return;
+                $data['recipient_name'] = $recipient_info->getName();
+
+                $data['message']['postanon']        = $postanon;
+                $data['message']['postanon_to']     = $replymessages[0]['postanon'];
+                $data['postanon_to']                = $replymessages[0]['postanon'];
+                $data['postanon']                   = $postanon;     
+                $data['action']                     = 'prev_reply';
+                
+            }
                 return $data;
             }
 
@@ -169,14 +169,14 @@ function messages_user_send()
             return true;
             break;
 
-		case "prev_reply" : 
-			
-			xarVarFetch('id',   'int:1', $id,   null, XARVAR_NOT_REQUIRED); 
+        case "prev_reply" : 
             
-			xarTplSetPageTitle( xarML('Messages :: Reply') );
+            xarVarFetch('id',   'int:1', $id,   null, XARVAR_NOT_REQUIRED); 
+            
+            xarTplSetPageTitle( xarML('Messages :: Reply') );
 
             $replymessages = xarModAPIFunc('messages', 'user', 'get', array('id' => $id));
-			
+            
             
             
             if (!count($replymessages) || !is_array($replymessages)) {
@@ -190,37 +190,37 @@ function messages_user_send()
                     return $data;
             }
             
-            $data['id'] 			= $id;
+            $data['id']             = $id;
             $data['post_url']       = xarModURL('messages', 'user', 'send');
             $data['input_title']    = xarML('Reply to a Message');
             $data['recipient']      = $replymessages[0]['sender_id'];
             $data['replymessage']   = $replymessages[0];
-			
-			// Get $recipient information
-        	$recipient_info         = xarRoles::get($data['recipient']);
-        	if (!$recipient_info) return;
-	       	$data['recipient_name'] = $recipient_info->getName();
+            
+            // Get $recipient information
+            $recipient_info         = xarRoles::get($data['recipient']);
+            if (!$recipient_info) return;
+            $data['recipient_name'] = $recipient_info->getName();
 
             xarVarFetch('subject',   'str:1', $subject,   null, XARVAR_NOT_REQUIRED);
-			//minimum length is 7 character it sets &#160; for processing
-			xarVarFetch('body',   'str:7', $body,   null, XARVAR_NOT_REQUIRED); 
-			xarVarFetch('recipient',   'int:1', $recipient,   null, XARVAR_NOT_REQUIRED);
-			
-			if($subject == null) {
-				$data['no_subject'] = 1;
-			}
-			if($body == null) {
-				$data['no_body'] = 1;
-			}
-			if($recipient == null){
-				 $data['no_recipient'] = 1;
-			}
+            //minimum length is 7 character it sets &#160; for processing
+            xarVarFetch('body',   'str:7', $body,   null, XARVAR_NOT_REQUIRED); 
+            xarVarFetch('recipient',   'int:1', $recipient,   null, XARVAR_NOT_REQUIRED);
+            
+            if($subject == null) {
+                $data['no_subject'] = 1;
+            }
+            if($body == null) {
+                $data['no_body'] = 1;
+            }
+            if($recipient == null){
+                 $data['no_recipient'] = 1;
+            }
 
-			//field for postanom of previous 
-            $data['message']['postanon']  		= $postanon;
-            $data['message']['postanon_to']  	= $replymessages[0]['postanon'];
-            $data['postanon_to']  		        = $replymessages[0]['postanon'];
-            $data['postanon']      				= $postanon;     
+            //field for postanom of previous 
+            $data['message']['postanon']        = $postanon;
+            $data['message']['postanon_to']     = $replymessages[0]['postanon'];
+            $data['postanon_to']                = $replymessages[0]['postanon'];
+            $data['postanon']                   = $postanon;     
             
             $data['message']['sender']          = xarUserGetVar('name');
             $data['message']['senderid']        = xarSession::getVar('role_id');
@@ -230,14 +230,14 @@ function messages_user_send()
             $data['message']['date']            = xarLocaleFormatDate('%A, %B %d @ %H:%M:%S', time());
             $data['message']['raw_date']        = time();
             $data['message']['body']            = $body;
-			$data['message']['user_link']       = xarModURL('roles','user','display',
+            $data['message']['user_link']       = xarModURL('roles','user','display',
                                                   array('id' => xarUserGetVar('id')));
             
             $data['recipient']                  = $recipient;
             $data['subject']                    = $subject;
             $data['body']                       = $body;
-			     	
-            break;				
+                    
+            break;              
         case "reply":
             if (!xarVarFetch('id', 'int:1', $id)) return;
             xarTplSetPageTitle( xarML('Messages :: Reply') );
@@ -254,21 +254,21 @@ function messages_user_send()
                     $data['error'] = xarML("You are NOT authorized to view someone else's mail!");
                     return $data;
             }
-            $data['id'] 			= $id;
+            $data['id']             = $id;
             $data['post_url']       = xarModURL('messages', 'user', 'send');
             $data['input_title']    = xarML('Reply to a Message');
             $data['recipient']      = $messages[0]['sender_id'];
             $data['message']        = $messages[0];
-			$data['replymessage']   = $data['message'];
-		    $data['postanon_to']    = $messages[0]['postanon'];
-			$data['postanon']       = 1;
-			
-			$data['body']     = xarModAPIFunc('messages', 'user', 'reply_message_text', array('message' => $data['message']));
-			$subject  = xarModAPIFunc('messages', 'user', 'reply_message_subject', array('message' => $data['message']));
+            $data['replymessage']   = $data['message'];
+            $data['postanon_to']    = $messages[0]['postanon'];
+            $data['postanon']       = 1;
+            
+            $data['body']     = xarModAPIFunc('messages', 'user', 'reply_message_text', array('message' => $data['message']));
+            $subject  = xarModAPIFunc('messages', 'user', 'reply_message_subject', array('message' => $data['message']));
             //Psspl:Added the code for recipient name for anonymous messages
             $data['postanon']      = $messages[0]['postanon'];
-			TracePrint($subject,"Replysubject");
-			$data['subject']	= $subject;
+            TracePrint($subject,"Replysubject");
+            $data['subject']    = $subject;
             // Get $recipient information
             $recipient_info = xarRoles::get($data['recipient']);
             if (!$recipient_info) return;
@@ -278,19 +278,19 @@ function messages_user_send()
             xarVarFetch('subject',   'str:1', $subject,   null, XARVAR_NOT_REQUIRED);
            //minimum length is 7 character it sets &#160; for processing
             /*             
-			xarVarFetch('body',   'str:7', $body,   null, XARVAR_NOT_REQUIRED); 
-			xarVarFetch('recipient',   'int:1', $recipient,   null, XARVAR_NOT_REQUIRED);
-			
-			if($subject == null) {
+            xarVarFetch('body',   'str:7', $body,   null, XARVAR_NOT_REQUIRED); 
+            xarVarFetch('recipient',   'int:1', $recipient,   null, XARVAR_NOT_REQUIRED);
             */
-				$data['no_subject'] = 1;
-			}
-			if($body == null) {
-				$data['no_body'] = 1;
-			}
-			if($recipient == null){
-				 $data['no_recipient'] = 1;
-			}
+            
+            if($subject == null) {
+                $data['no_subject'] = 1;
+            }
+            if($body == null) {
+                $data['no_body'] = 1;
+            }
+            if($recipient == null){
+                 $data['no_recipient'] = 1;
+            }
             //Psspl:Comment the code for resolving preview message issue
             // added call to transform text srg 09/22/03
             /*list($body) = xarModCallHooks('item',
