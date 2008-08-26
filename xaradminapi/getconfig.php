92d4f6dd4029919e3b6e407e52adfcb06c52e2dc
mfl <mfl@netspan.ch>
2008-08-26 17:24:19 +0000

    change modvar to moditemvar

--- xaradminapi/getconfig.php
+++ xaradminapi/getconfig.php
@@ -8,23 +8,23 @@ include_once("./modules/commonutil.php");
 function messages_adminapi_getconfig( $args )
 {
     extract( $args );
-     	
-	$selectedGroup = unserialize(xarModVars::get('messages', "allowedSendMessages[$group]"));
-   	
-	TracePrint($selectedGroup, "selected");
-   	
-   	if (!empty($selectedGroup)) {  	 	 
-   		
-   		$selectedGroupStr = implode(",", $selectedGroup);//convert array into comma seprated string.
-   		
-   		TracePrint($selectedGroupStr, "str selected");
-   		
-   		return $selectedGroupStr;
-   	}
-   	else {
-   	  	$msg = xarML('Invalid select group info in #(1) function #(2)in module #(3)',
-	                     'adminapi', 'getconfig', 'messages');
-	    //throw new Exception($msg);
-	    return null;
-   	}
+        
+    $selectedGroup = unserialize(xarModItemVars::get('messages',"allowedSendMessages",$group));
+    
+    TracePrint($selectedGroup, "selected");
+    
+    if (!empty($selectedGroup)) {        
+        
+        $selectedGroupStr = implode(",", $selectedGroup);//convert array into comma seprated string.
+        
+        TracePrint($selectedGroupStr, "str selected");
+        
+        return $selectedGroupStr;
+    }
+    else {
+        $msg = xarML('Invalid select group info in #(1) function #(2)in module #(3)',
+                         'adminapi', 'getconfig', 'messages');
+        //throw new Exception($msg);
+        return null;
+    }
 }
--- xaruserapi/get_users.php
+++ xaruserapi/get_users.php
@@ -10,79 +10,79 @@ function messages_userapi_get_users( $args )
 {
 
     extract($args);
-	
-	   	$users = xarModAPIFunc('roles',	'user',
-								    	'getall',
-								    	array('state'   => 3,
-								    	'include_anonymous' => false,
-								    	'include_myself' => false));
-    	$userid = xarUserGetVar('id');
+    
+        $users = xarModAPIFunc('roles', 'user',
+                                        'getall',
+                                        array('state'   => 3,
+                                        'include_anonymous' => false,
+                                        'include_myself' => false));
+        $userid = xarUserGetVar('id');
 
-    	sys::import('modules.roles.class.xarQuery');
+        sys::import('modules.roles.class.xarQuery');
 
-    	$xartable = xarDB::getTables();
-    	$q = new xarQuery('SELECT');
-    	$q->addtable($xartable['roles'], 'r');
+        $xartable = xarDB::getTables();
+        $q = new xarQuery('SELECT');
+        $q->addtable($xartable['roles'], 'r');
 
-    	$q->eq('id', $userid);
+        $q->eq('id', $userid);
 
-    	$q->addtable($xartable['rolemembers'],'rm');
-    	$q->join('r.id', 'rm.role_id');
+        $q->addtable($xartable['rolemembers'],'rm');
+        $q->join('r.id', 'rm.role_id');
 
-    	if(!$q->run()) return;
-    	$CurrentUser =  $q->output();
-		
-    	$id=$CurrentUser[0]['parent_id'];
-    	$groupID=$CurrentUser[0]['parent_id'];
-    	
-    	$allowedSendMessages = unserialize(xarModVars::get('messages',"allowedSendMessages[$groupID]"));
-    	TracePrint($allowedSendMessages,"allowed Send Messages");
-    	
-    	$state = 3;//set state for active users
+        if(!$q->run()) return;
+        $CurrentUser =  $q->output();
+        
+        $id=$CurrentUser[0]['parent_id'];
+        $groupID=$CurrentUser[0]['parent_id'];
+        
+        $allowedSendMessages = unserialize(xarModItemVars::get('messages',"allowedSendMessages",$groupID));
+        TracePrint($allowedSendMessages,"allowed Send Messages");
+        
+        $state = 3;//set state for active users
 
 
-    	$xartable = xarDB::getTables();
-    	$q = new xarQuery('SELECT');
-    	$q->addtable($xartable['roles'], 'r');
-    	
-    	/*Psspl:get the selected user only
-    	if ($allowedSendMessages['childusersimploded']!=0) {
-	    	//get the selected group/user information with current userid.
-	    	$allowedUsers = explode(",",$allowedSendMessages['childusersimploded']);
-			  	
-			foreach ($allowedUsers as $key => $value){
-				$user_c[]=$q->peq('id' , $value);
-			}
-			
-			$q->qor($user_c); //use OR
-    	}
-		*/
-    	
-		$q->eq('state', $state);
-    	$q->ne('email', '');
-    	$q->ne('name' , 'Myself');
-    	$q->ne('itemtype' , 3);//check for user
-		 	
-    	$q->addtable($xartable['rolemembers'],'rm');
-    	$q->join('r.id', 'rm.role_id');
-    	/*Psspl:get the selected group only 
-    	*/
-    	//Check condition for selecting all group.
-    	if ($allowedSendMessages[0] != 0 or $allowedSendMessages != null ) {
-	    	//$allowedGroups = explode(",",$allowedSendMessages);
-	    	foreach ($allowedSendMessages as $key => $value) {
-	    		$group_c[] = $q->peq('rm.parent_id' , $value);//select the users only from selected group. 
-	    	}
-	    	
-	    	$q->qor($group_c); //set or condition
-    	}
-    	
-    	//function for echo the query.
-    	//$q->qecho();
-    	  	
-    	if(!$q->run()) return;
-	
-   		$users = $q->output();
+        $xartable = xarDB::getTables();
+        $q = new xarQuery('SELECT');
+        $q->addtable($xartable['roles'], 'r');
+        
+        /*Psspl:get the selected user only
+        if ($allowedSendMessages['childusersimploded']!=0) {
+            //get the selected group/user information with current userid.
+            $allowedUsers = explode(",",$allowedSendMessages['childusersimploded']);
+                
+            foreach ($allowedUsers as $key => $value){
+                $user_c[]=$q->peq('id' , $value);
+            }
+            
+            $q->qor($user_c); //use OR
+        }
+        */
+        
+        $q->eq('state', $state);
+        $q->ne('email', '');
+        $q->ne('name' , 'Myself');
+        $q->ne('itemtype' , 3);//check for user
+            
+        $q->addtable($xartable['rolemembers'],'rm');
+        $q->join('r.id', 'rm.role_id');
+        /*Psspl:get the selected group only 
+        */
+        //Check condition for selecting all group.
+        if ($allowedSendMessages[0] != 0 or $allowedSendMessages != null ) {
+            //$allowedGroups = explode(",",$allowedSendMessages);
+            foreach ($allowedSendMessages as $key => $value) {
+                $group_c[] = $q->peq('rm.parent_id' , $value);//select the users only from selected group. 
+            }
+            
+            $q->qor($group_c); //set or condition
+        }
+        
+        //function for echo the query.
+        //$q->qecho();
+            
+        if(!$q->run()) return;
+    
+        $users = $q->output();
 
     return $users;
 }
