524f018bd799a3705aab822207b584623609e401
mfl <mfl@netspan.ch>
2008-08-31 21:52:39 +0000

    improve display template

--- xaruser/display.php
+++ xaruser/display.php
@@ -5,30 +5,16 @@ function xarayatesting_user_display($args)
 {
     extract($args);
 
-    if(!xarVarFetch('objectid', 'isset', $objectid,  NULL, XARVAR_DONT_SET)) {return;}
     if(!xarVarFetch('name',     'isset', $name,      NULL, XARVAR_DONT_SET)) {return;}
-    if(!xarVarFetch('modid',    'isset', $moduleid,  NULL, XARVAR_DONT_SET)) {return;}
-    if(!xarVarFetch('itemtype', 'isset', $itemtype,  NULL, XARVAR_DONT_SET)) {return;}
     if(!xarVarFetch('itemid',   'isset', $itemid,    NULL, XARVAR_DONT_SET)) {return;}
-    if(!xarVarFetch('join',     'isset', $join,      NULL, XARVAR_DONT_SET)) {return;}
-    if(!xarVarFetch('table',    'isset', $table,     NULL, XARVAR_DONT_SET)) {return;}
-    if(!xarVarFetch('template', 'isset', $template,  NULL, XARVAR_DONT_SET)) {return;}
     if(!xarVarFetch('tplmodule','isset', $tplmodule, NULL, XARVAR_DONT_SET)) {return;}
 
-    $myobject = & DataObjectMaster::getObject(array(
-                                         'name' => $name,
-                                         'moduleid' => $moduleid,
-                                         'itemtype' => $itemtype,
-                                         'join'     => $join,
-                                         'table'    => $table,
-                                         'itemid'   => $itemid,
+    $object = DataObjectMaster::getObject(array('name' => $name,
                                          'tplmodule' => $tplmodule));
-    if (!isset($myobject)) return;
-    $myobject->getItem();
+    if (!isset($object)) return;
+    $object->getItem(array('itemid'   => $itemid));
 
     $data['object'] =& $myobject;
-    $data['items'] = array();
-
     return $data;
 }
 
