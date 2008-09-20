f70fbf9f3d4da4a0fbb5c65ea349ac5d5d6c2ae1
mfl <mfl@netspan.ch>
2008-09-20 14:37:49 +0000

    improve getlinkage to return names

--- xaruserapi/getlinkage.php
+++ xaruserapi/getlinkage.php
@@ -11,39 +11,48 @@ function categories_userapi_getlinkage($args)
         $module = xarModGetName();
     }
 
-	$modid = xarMod::getID($module);
+    $modid = xarMod::getID($module);
 
     $tables = xarDB::getTables();
-    $q = new xarQuery('SELECT',$tables['categories_linkage']);
+    $q = new xarQuery('SELECT');
+    $q->addtable($tables['categories_linkage'],'cl');
+    $q->addtable($tables['categories'],'c');
+    $q->join('c.id','cl.category_id');
     $q->eq('module_id',$modid);
     if (!empty($itemtype)) {
-	    if (is_array($itemtype)) {
-			$q->in('itemtype',$itemtype);
-		} else {
-			$q->eq('itemtype',$itemtype);
-		}
+        if (is_array($itemtype)) {
+            $q->in('itemtype',$itemtype);
+        } else {
+            $q->eq('itemtype',$itemtype);
+        }
     }
     if (!empty($itemid)) {
-	    if (is_array($itemid)) {
-			$q->in('item_id',$itemid);
-		} else {
-			$q->eq('item_id',$itemid);
-		}
+        if (is_array($itemid)) {
+            $q->in('item_id',$itemid);
+        } else {
+            $q->eq('item_id',$itemid);
+        }
     }
     if (!empty($basecid)) {
-	    if (is_array($basecid)) {
-			$q->in('basecategory',$basecid);
-		} else {
-			$q->eq('basecategory',$basecid);
-		}
+        if (is_array($basecid)) {
+            $q->in('basecategory',$basecid);
+        } else {
+            $q->eq('basecategory',$basecid);
+        }
     }
     if (!empty($categoryid)) {
-	    if (is_array($categoryid)) {
-			$q->in('category_id',$categoryid);
-		} else {
-			$q->eq('category_id',$categoryid);
-		}
+        if (is_array($categoryid)) {
+            $q->in('category_id',$categoryid);
+        } else {
+            $q->eq('category_id',$categoryid);
+        }
     }
+    $q->addfield('c.id AS id');
+    $q->addfield('c.name AS name');
+    $q->addfield('cl.basecategory AS basecategory_id');
+    $q->addfield('c.module_id AS module_id');
+    $q->addfield('c.itemid AS itemid');
+    $q->addfield('c.itemtype AS itemtype');
 //    $q->qecho();
     if (!$q->run()) return array();
     return $q->output();
