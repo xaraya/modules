0282bc7e5e853e9dbc353c7833881b9155a85051
crispcreations <crispcreations@googlemail.com>
2008-09-27 08:33:00 +0000

    Pushing updates to BBCode/n/nFix for Bug 5461/nFix for Bug 5081/nFix nesting issues with tags inside size tag/nFixed issue with plain urls appearing on same line even with linebreaks/

--- xaruserapi.php
+++ xaruserapi.php
@@ -6,7 +6,7 @@
  * http://www.pncommunity.de
  *
  * @package modules
- * @copyright (C) 2002-2008 The Digital Development Foundation
+ * @copyright (C) 2002-2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -55,18 +55,26 @@ function bbcode_transform($text)
     // BBClick functionality
     // matches an "xxxx://yyyy" URL at the start of a line, or after a space. 
     // xxxx can only be alpha characters. 
-    // yyyy is anything up to the first space, newline, comma, double quote or < 
-    $text = preg_replace("#(^|[\n ])([\w]+?://[^ \"\n\r\t<]*)#is", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $text); 
+    // yyyy is anything up to the first space, newline, comma, double quote [ or < 
+    // transform to BBCode, instead of HTML, let the BBCode template deal with display
+    // Bug, this doesn't catch plain urls inside other BBCode tags.
+    // Added regex to not match string beginning with [url] or [url=] allows all other tags to work
+    $text = preg_replace("#(^|[\n ]|[^[url.]])([\w]+?://[^ \"\n\r\t<[]*)#is", "\\1[url]\\2[/url]", $text); 
+    //$text = preg_replace("#(^|[\n ])([\w]+?://[^ \"\n\r\t<]*)#is", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $text); 
 
     // matches a "www|ftp.xxxx.yyyy[/zzzz]" kinda lazy URL thing
     // Must contain at least 2 dots. xxxx contains either alphanum, or "-"
     // zzzz is optional.. will contain everything up to the first space, newline, 
-    // comma, double quote or <. 
-    $text = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $text); 
+    // comma, double quote [ or <. 
+    // transform to BBCode tag, instead of HTML, let the BBCode template deal with display
+    $text = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<[]*)#is", "\\1[url=http://\\2]\\2[/url]", $text);  
+    //$text = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $text); 
 
     // matches an email@domain type address at the start of a line, or after a space.
     // Note: Only the followed chars are valid; alphanums, "-", "_" and or ".".
-    $text = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $text);
+    // transform to BBCode, instead of HTML, let the BBCode template deal with display
+    $text = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1[email]\\2@\\3[/email]", $text);
+    //$text = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $text);
 
     include_once 'modules/bbcode/xarclass/stringparser_bbcode.class.php';
     $bbcode = new StringParser_BBCode();
@@ -119,10 +127,13 @@ function bbcode_transform($text)
                       'inline', array ('listitem', 'block', 'inline', 'link'), array());
 
     $bbcode->addCode ('code', 'usecontent?', 'do_bbcode_code', array ('usecontent_param' => 'default'), 'link', array ('block', 'inline'), array ('link'));
+    // don't let paragraphs split the code
+    $bbcode->setCodeFlag ('code', 'paragraph_type', BBCODE_PARAGRAPH_BLOCK_ELEMENT);
     //bug 5217
     //$bbcode->addCode ('quote', 'usecontent?', 'do_bbcode_quote', array ('usecontent_param' => 'default'), 'link', array ('block', 'inline'), array ('link'));
     $bbcode->addCode ('quote', 'callback_replace', 'do_bbcode_quote', array ('usecontent_param' => 'default'), 'block', array ('block', 'inline','link'), array ());
-
+    // don't let paragraphs split the quotes
+    $bbcode->setCodeFlag ('quote', 'paragraph_type', BBCODE_PARAGRAPH_BLOCK_ELEMENT);
     $bbcode->addCode ('color', 'usecontent?', 'do_bbcode_color', array ('usecontent_param' => 'default'),
                       'inline', array ('listitem', 'block', 'inline', 'link'), array ('link'));
     //$bbcode->addCode ('img', 'usecontent', 'do_bbcode_img', array (),
@@ -130,9 +141,12 @@ function bbcode_transform($text)
                        'image', array ('listitem', 'block', 'inline', 'link'), array ());
     $bbcode->addCode ('bild', 'usecontent', 'do_bbcode_img', array (),
                       'image', array ('listitem', 'block', 'inline', 'link'), array ());
+    // Bug, nested tags not transformed inside size tag
+    $bbcode->addCode ('size', 'callback_replace', 'do_bbcode_size', array ('usecontent_param' => 'default'), 'inline', array ('listitem', 'block','inline', 'link'), array ());
+    /*
     $bbcode->addCode ('size', 'usecontent', 'do_bbcode_size', array (),
                       'inline', array ('listitem', 'block', 'inline', 'link'), array ());
-
+    */
     $bbcode->addCode ('arabic', 'callback_replace', 'do_bbcode_rtl', array (),
                       'inline', array ('listitem', 'block', 'inline', 'link'), array());
     $bbcode->addCode ('hebrew', 'callback_replace', 'do_bbcode_rtl', array (),
@@ -184,11 +198,12 @@ function do_bbcode_img ($action, $attributes, $content, $params, $node_object)
     if ($action == 'validate') {
         return true;
     }
+    // Why isn't this returned to a template like all the others?
     //return '<img src="'.htmlspecialchars($content).'" alt="">';  bug 5217
     if (!isset ($attributes['default'])) {
-            return '<img src="'.htmlspecialchars($content).'" alt="">';
+    		return '<img src="'.htmlspecialchars($content).'" alt="">';
     } else {
-            return '<img src="'.htmlspecialchars($attributes['default']).'" alt="">';
+    		return '<img src="'.htmlspecialchars($attributes['default']).'" alt="">';
     }
 }
 // Bug 4826
@@ -232,7 +247,6 @@ function do_bbcode_wiki ($action, $attributes, $content, $params, &$node_object)
         return xarTplModule('bbcode','user', 'wiki', array('iso' => $iso, 'replace' => $content ));
     }
 }
-
 function do_bbcode_yahoo ($action, $attributes, $content, $params, &$node_object) 
 {
     return xarTplModule('bbcode','user', 'yahoo', array('replace' => $content));
@@ -284,9 +298,9 @@ function do_bbcode_url ($action, $attributes, $content, $params, &$node_object)
     } else {
         // the code was specified as follows: [url]http://.../[/url]
         if (!isset ($attributes['default'])) {
-            return xarTplModule('bbcode','user', 'url', array('url' => htmlspecialchars($content)));
+            return xarTplModule('bbcode','user', 'url', array('url' => $content));
         } else {
-            return xarTplModule('bbcode','user', 'url', array('url' => htmlspecialchars($attributes['default']), 'name' => $content ));
+            return xarTplModule('bbcode','user', 'url', array('url' => $attributes['default'], 'name' => $content ));
         }
     }
 }
@@ -320,6 +334,16 @@ function do_bbcode_quote ($action, $attributes, $content, $params, &$node_object
 
 function do_bbcode_code ($action, $attributes, $content, $params, &$node_object) 
 {
+    // flag tags to be ignored by other modules handling linebreaks (ie HTML)
+    if (xarModGetVar('bbcode', 'dolinebreak') == 1) {
+      static $nolinebreaks = array();
+      if (!isset ($attributes['default'])) {
+          $nolinebreaks['code'] = 1;
+      } else {
+          $nolinebreaks['textarea'] = 1;
+      }
+      xarVarSetCached('Hooks.bbcode', 'nolinebreaks', join(';', array_keys($nolinebreaks)));
+    }
 
     if (!isset ($attributes['default'])) {
         return xarTplModule('bbcode','user', 'code', array('replace' => $content));
