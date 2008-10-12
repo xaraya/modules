ad55b9beb008859ce18f22a26cdbd2b18798dc5e
marty.vance <marty.vance@gmail.com>
2008-10-12 04:54:31 +0000

    update author/recipient status during display

--- xaruser/display.php
+++ xaruser/display.php
@@ -28,12 +28,15 @@ function messages_user_display( $args )
     $data['object'] = DataObjectMaster::getObject(array('name' => $object));
     $data['object']->getItem(array('itemid' => $id));
 
+    $current_user = xarSession::getVar('role_id');
+
     // Check that the current user is either sender or receiver
-    if (($data['object']->properties['to']->value != xarSession::getVar('role_id')) &&
-        ($data['object']->properties['from']->value != xarSession::getVar('role_id'))) {
+    if (($data['object']->properties['to']->value != $current_user) &&
+        ($data['object']->properties['from']->value != $current_user)) {
         return xarTplModule('messages','user','message_errors',array('layout' => 'bad_id'));
     }
 
+/*
     $read_messages = xarModUserVars::get('messages','read_messages');
 
 
@@ -42,16 +45,16 @@ function messages_user_display( $args )
     } else {
         $read_messages = array();
     }
-
+*/
 
     /*
      * if it's not already an array, then this must be
      * the first time we've looked at it
      * so let's make it an array :)
      */
-    if (!is_array($read_messages)) {
-        $read_messages = array();
-    }
+//    if (!is_array($read_messages)) {
+//        $read_messages = array();
+//    }
 
 //    $data['message'] = $messages[0];
     $data['action']  = 'display';
@@ -65,12 +68,19 @@ function messages_user_display( $args )
      $data['object']->properties['body']->setValue($body);
 
     /*
-     * Add this message id to the list of 'seen' messages
-     * if it's not already in there :)
+     * Mark this message as read
+     * Handle author and recipient for 'mark unread' (future)
      */
-    if (!in_array($id, $read_messages)) {
-        array_push($read_messages, $id);
-        xarModUserVars::set('messages','read_messages',serialize($read_messages));
+    if ($current_user == $data['object']->properties['from']->value) {
+        // don't update drafts
+        if($data['object']->properties['author_status']->value != MESSAGES_STATUS_DRAFT) {
+            $data['object']->properties['author_status']->setValue(MESSAGES_STATUS_READ);
+            $data['object']->updateItem();
+        }
+    } 
+    if ($current_user == $data['object']->properties['to']->value) {  
+        $data['object']->properties['recipient_status']->setValue(MESSAGES_STATUS_READ);
+        $data['object']->updateItem();
     }
 
     return $data;
