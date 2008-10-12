cfdca8c54dddf7d6a99884cf76775208d91f5d98
marty.vance <marty.vance@gmail.com>
2008-10-12 07:07:20 +0000

    migrate unread highlight in tab from inline style to css file; user-delete is functional again

--- xaruser/delete.php
+++ xaruser/delete.php
@@ -33,11 +33,22 @@ function messages_user_delete()
     $data['object']->getItem(array('itemid' => $id));
 
 
-    // Check that the current user is either sender or receiver
-    if (($data['object']->properties['to']->value != xarSession::getVar('role_id')) &&
-        ($data['object']->properties['from']->value != xarSession::getVar('role_id'))) {
-        return xarTplModule('messages','user','message_errors',array('layout' => 'bad_id'));
+    // Check the folder, and that the current user is either sender or receiver
+    switch ($folder) {
+        case 'inbox':
+            if ($data['object']->properties['to']->value != xarSession::getVar('role_id')) {
+                return xarTplModule('messages','user','message_errors',array('layout' => 'bad_id'));
+            }
+            break;
+        case 'drafts':
+        case 'sent':
+            if ($data['object']->properties['from']->value != xarSession::getVar('role_id')) {
+                return xarTplModule('messages','user','message_errors',array('layout' => 'bad_id'));
+            }
+            break;
     }
+
+    $data['folder'] = $folder;
 
 /*
     $messages = xarModAPIFunc('messages', 'user', 'get', array('id' => $id));
@@ -59,55 +70,26 @@ function messages_user_delete()
         case "confirmed":
 
             /*
-             * First, make sure we remove the message id from
-             * the read messages list, if this message has been seen.
-             */
-            $read_messages = xarModUserVars::get('messages','read_messages');
-            if (!empty($read_messages)) {
-                $read_messages = unserialize($read_messages);
-            } else {
-                $read_messages = array();
-            }
-
-
-            if ( ($key = array_search($id, $read_messages)) !== FALSE) {
-                unset($read_messages[$key]);
-                xarModUserVars::set('messages', 'read_messages', serialize($read_messages));
-            }
-
-            /*
              * Then go ahead and delete the message :)
              */
-            xarModAPIFunc('messages',
-                          'user',
-                          'delete',
-                           array('object' => $data['object'] ,
-                                 'folder' => $folder));
-            xarResponseRedirect(xarModURL('messages','user','view',array('folder' =>    xarSession::getVar('messages_currentfolder'))));
+
+            if ($folder == 'inbox') {
+                $data['object']->properties['recipient_delete']->setValue(MESSAGES_DELETED);
+            } elseif ($folder == 'sent') {
+                $data['object']->properties['author_delete']->setValue(MESSAGES_DELETED);
+            } else {
+                $data['object']->properties['recipient_delete']->setValue(MESSAGES_DELETED);
+                $data['object']->properties['author_delete']->setValue(MESSAGES_DELETED);
+            }
+
+            $data['object']->updateItem();
+
+            xarResponseRedirect(xarModURL('messages','user','view',array('folder' => $folder)));
             break;
 
         case "check":
-            $data['id']         = $id;
-        /*
-            $data['folder']     = $folder;
-            $data['message']    = $messages[0];
-            $data['action']     = $action;
-            $data['post_url']   = xarModURL('messages','user','delete');
-         */  
-            /*Psspl:Added the code for read messages.
-              * Add this message id to the list of 'seen' messages
-              * if it's not already in there :)
-            */
-            $read_messages = xarModUserVars::get('messages','read_messages');
-            if (!empty($read_messages)) {
-                $read_messages = unserialize($read_messages);
-            } else {
-                $read_messages = array();
-            }
-             if (!in_array($id, $read_messages)) {
-                array_push($read_messages, $id);
-                xarModUserVars::set('messages','read_messages',serialize($read_messages));
-            }
+            // nothing to do here, just return the object
+            $data['id'] = $data['object']->properties['id']->getValue();
             break;
     }
     return $data;
