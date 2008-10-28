6804674aa2e8d1a2ef639415ea169d31db4fe820
judgej <judgej@xaraya.com>
2008-10-28 17:43:51 +0000

    Removed some project-related URLs that got into a test script

--- xaruser/main.php
+++ xaruser/main.php
@@ -2,18 +2,15 @@
 
 function simplepie_user_main()
 {
-   $url = 'http://www.iema.net/jobs/view?theme=rss';
-   $url = 'http://www.newsfeedmaker.com/feed_rss.php?query=climate+change';
-   $url = 'http://www.iema.net/news/iemanews?cat=311&theme=rss';
-   $url = 'http://www.iema.net/envjobs/view?showform=simple&qtext=%27&catid=_297&theme=rss';
-   $url = 'http://www.iema.net/index.php/articles/weblinks?theme=rss';
+/*
+   $url = 'http://www.xaraya.com/index.php/news?theme=rss';
 
    $feed = xarModAPIfunc('simplepie', 'user', 'process', array('url' => $url));
 
    echo "<pre>";
    var_dump($feed);
    echo "</pre>";
-/*
+
    echo "<hr />";
 
    $pie = xarModAPIfunc('simplepie', 'user', 'newfeed');
@@ -26,7 +23,7 @@ function simplepie_user_main()
    var_dump($pie);
    echo "</pre>";
 */
-   echo "done";
+   return '';
 }
 
 ?>
