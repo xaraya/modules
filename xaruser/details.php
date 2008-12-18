644644dd37d3c5d2767865439f0ade64489ca23e
mfl <mfl@netspan.ch>
2008-12-18 11:08:54 +0000

    Dracos fix to extended forecast

--- xaruser/details.php
+++ xaruser/details.php
@@ -2,7 +2,9 @@
 function weather_user_details()
 {
     xarVarFetch('xwday','int:0:9',$xwday,0);
+    xarVarFetch('location','str',$location, xarModVars::get('weather','default_location'));
     $w = xarModAPIFunc('weather','user','factory');
+    $w->setLocation($location);
     return array(
         'day'=>$xwday,
         'wData'=>$w->ccData(),
