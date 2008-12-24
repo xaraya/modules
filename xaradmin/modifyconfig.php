6c5c9086213b7bb8e3e4a487f5956dda923964cc
mfl <mfl@netspan.ch>
2008-12-24 14:13:44 +0000

    remove DDF from this module

--- xaradmin/modifyconfig.php
+++ xaradmin/modifyconfig.php
@@ -3,7 +3,7 @@
  * Purpose of file
  *
  * @package modules
- * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @copyright (C) copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
