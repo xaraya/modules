2517877027afe60155fc17cc63fcf78a0bca8a91
mfl <mfl@netspan.ch>
2008-12-24 13:43:10 +0000

    remove DDF from this module

--- xaruserapi/get_count.php
+++ xaruserapi/get_count.php
@@ -3,7 +3,7 @@
  * Messages module
  *
  * @package modules
- * @copyright (C) 2002-2007 The Digital Development Foundation
+ * @copyright (C) 2002-2007 The copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
