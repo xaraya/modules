32b809b087900d874a00285c519ffeadb244d907
mfl <mfl@netspan.ch>
2008-12-24 17:58:27 +0000

    remove DDF from this module

--- xaradmin/waitingcontent.php
+++ xaradmin/waitingcontent.php
@@ -3,7 +3,7 @@
  * Waiting content hook
  *
  * @package modules
- * @copyright (C) 2008 The Digital Development Foundation
+ * @copyright (C) copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
