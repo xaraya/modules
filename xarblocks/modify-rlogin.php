bd9fe06e35d3fb7c10b37ea7bcf86079a80a2a06
mfl <mfl@netspan.ch>
2008-12-24 18:01:43 +0000

    remove DDF from this module

--- xarblocks/modify-rlogin.php
+++ xarblocks/modify-rlogin.php
@@ -3,7 +3,7 @@
  * Modify Function to the Blocks Admin
  *
  * @package modules
- * @copyright (C) 2005-2006 The Digital Development Foundation
+ * @copyright (C) copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
