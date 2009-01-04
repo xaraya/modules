7819d74d455842d8bef3495b8d68cc5878c083cb
icedlava <icedlava@2skies.com>
2009-01-04 13:22:41 +0000

    Add updated documentation and credit files

--- xarversion.php
+++ xarversion.php
@@ -3,26 +3,26 @@
  * Initialization functions
  *
  * @package modules
- * @copyright (C) 2002-2007 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage Images Module
- * @link http://xaraya.com/index.php/release/152.html
+ * @subpackage Xarigami Images Module
+ * @copyright (C) 2009 2skies.com
+ * @link http://xarigami.com/project/images
  * @author Images Module Development Team
  */
 $modversion['name']         = 'Images';
 $modversion['id']           = '152';
-$modversion['version']      = '1.1.0';
+$modversion['version']      = '1.1.1';
 $modversion['displayname']  = xarML('Images');
-$modversion['description']  = 'Handles basic image manipulation - resizing/cropping/scaling/rotating';
+$modversion['description']  = 'Handles image manipulation with resizing/cropping/scaling/rotating and various filters';
 $modversion['credits']      = 'docs/credits.txt';
 $modversion['help']         = 'docs/help.txt';
 $modversion['changelog']    = 'docs/changelog.txt';
 $modversion['license']      = 'docs/license.txt';
 $modversion['official']     = 1;
-$modversion['author']       = 'Carl P. Corliss (carl.corliss@xaraya.com)';
-$modversion['contact']      = 'http://www.xaraya.com/';
+$modversion['author']       = 'Carl P. Corliss (carl.corliss@xaraya.com), mikespub, jojodee';
+$modversion['contact']      = 'http:/xarigami.com/';
 $modversion['admin']        = 1;
 $modversion['class']        = 'Utility';
 $modversion['category']     = 'Global';
