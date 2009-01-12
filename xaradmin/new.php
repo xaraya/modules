3eaf73934ea7995dbfabb58476dc4428b32037d1
mfl <mfl@netspan.ch>
2009-01-12 13:00:25 +0000

    add generic new function

--- xaradmin/new.php
+++ xaradmin/new.php
@@ -7,20 +7,31 @@
     {
         if (!xarSecurityCheck('AddFoo')) return;
 
-        if (!xarVarFetch('name',       'str',    $name,   'foo_foo', XARVAR_NOT_REQUIRED)) return;
-        if (!xarVarFetch('confirm',    'isset',  $confirm, null, XARVAR_DONT_SET)) return;
+        if (!xarVarFetch('name',       'str',    $name,            'foo_foo', XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false, XARVAR_DONT_SET)) return;
 
         $data['object'] = DataObjectMaster::getObject(array('name' => $name));
         $data['tplmodule'] = 'foo';
-        $data['authid'] = xarSecGenAuthKey('dynamicdata');
+        $data['authid'] = xarSecGenAuthKey('foo');
 
-        if (isset($confirm)) {
+        if ($data['confirm']) {
+        
+            // Check for a valid confirmation key
+            if(!xarSecConfirmAuthKey()) return;
+            
+            // Get the data from the form
             $isvalid = $data['object']->checkInput();
+            
             if (!$isvalid) {
+                // Bad data: redisplay the form with error messages
                 return xarTplModule('foo','user','new', $data);        
             } else {
+                // Good data: create the item
                 $item = $data['object']->createItem();
+                
+                // Jump to the next page
                 xarResponseRedirect(xarModURL('foo','admin','view'));
+                return true;
             }
         }
         return $data;
