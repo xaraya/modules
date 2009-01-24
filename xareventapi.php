182549464ffbbbd10c92c3f839f7d05a638e8b56
hb <hb@xaraya.com>
2009-01-24 09:57:24 +0000

    Exclude Admins from Output Buffering Warning message on admin Overview on buffering error messages (Bug 6311)

--- xareventapi.php
+++ xareventapi.php
@@ -2,7 +2,7 @@
 /**
  * Opentracker event API functions
  *
- * @copyright (C) 2003 by the Xaraya Development Team.
+ * @copyright (C) 2003 - 2009 by the Xaraya Development Team.
  * @license GPL <http://www.gnu.org/licenses/gpl.html>
  * @link http://www.xaraya.com
  * @subpackage opentracker
@@ -12,7 +12,8 @@
 require_once(dirname(__FILE__) . '/xarOpenTracker.php');
 
 function _opentracker_exit_urls($buffer)
-{
+{ 
+  //TODO no code in regex, create replacement before 
   return preg_replace(
     "#<a href=(\"|')(http(s)*?://)([^\"']+)(\"|')#ime",
     '"<a href=\"".((substr_count(strtolower(\'\\2\\4\'), strtolower(xarServerGetBaseURL())) == 0) ? xarModUrl("opentracker", "user", "exit", array("proto"=>base64_encode(\'\\2\'), "url" => base64_encode(\'\\4\') ) ) : "\\2\\4" )."\""',
@@ -22,6 +23,12 @@ function _opentracker_exit_urls($buffer)
 
 function opentracker_eventapi_OnServerRequest()
 {
+  // don't buffer output and count hits for admins 
+  $countadmin = xarModGetVar('opentracker','countadmin');
+  if (empty($countadmin) && xarSecurityCheck('AdminOpentracker', 0)) {
+    return true;
+  }
+
   // track outgoing URLs
   $trackoutgoing = xarModGetVar('opentracker','trackoutgoing');
   if (!empty($trackoutgoing)) {
@@ -31,11 +38,6 @@ function opentracker_eventapi_OnServerRequest()
 
   header('P3P: CP="NOI NID ADMa OUR IND UNI COM NAV"');
 
-  // don't count admin hits
-  $countadmin = xarModGetVar('opentracker','countadmin');
-  if (empty($countadmin) && xarSecurityCheck('AdminOpentracker', 0)) {
-    return true;
-  }
   xarOpenTracker::log(
     array(
       'document' => $GLOBALS['xarTpl_pageTitle'],
