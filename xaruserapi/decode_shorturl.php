24bbfc62adb34504ed63fe66158f8f3a7be0d79f
crispcreations <crispcreations@googlemail.com>
2009-01-30 04:54:10 +0000

    Update to shorturl functions, minor template change to user-tweet.xd

--- xaruserapi/decode_shorturl.php
+++ xaruserapi/decode_shorturl.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * Extract function and arguments from short URLs
+ * Twitter Module 
  *
  * @package modules
  * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage Example Module
- * @link http://xaraya.com/index.php/release/36.html
- * @author Example Module Development Team
+ * @subpackage Twitter Module
+ * @link http://xaraya.com/index.php/release/991.html
+ * @author Chris Powis (crisp@crispcreations.co.uk)
  */
 /**
  * Extract function and arguments from short URLs for this module, and pass
  * them back to xarGetRequestInfo()
  *
- * @author the Example module development team
+ * @author Chris Powis (crisp@crispcreations.co.uk)
  * @param  array $params array containing the different elements of the virtual path
  * @return array containing func the function to be called and args the query
  *          string arguments, or empty if it failed
  */
-function example_userapi_decode_shorturl($params)
+function twitter_userapi_decode_shorturl($params)
 {
     /* Initialise the argument list we will return */
     $args = array();
-    $module = 'example';
+    $module = 'twitter';
     /* Check and see if we have a module alias */
-    $aliasisset = xarModGetVar('example', 'useModuleAlias');
-    $aliasname = xarModGetVar('example','aliasname');
+    $aliasisset = xarModGetVar($module, 'useModuleAlias');
+    $aliasname = xarModGetVar($module,'aliasname');
     if (($aliasisset) && isset($aliasname)) {
         $usealias   = true;
     } else{
@@ -40,61 +40,21 @@ function example_userapi_decode_shorturl($params)
      * as possible in trying to decode them...
      */
     if ($params[0] != $module) { /* it's possibly some type of alias */
-        $aliasname = xarModGetVar('example','aliasname');
+        $aliasname = xarModGetVar($module,'aliasname');
     }
     if (empty($params[1])) {
         /*( nothing specified -> we'll go to the main function */
         return array('main', $args);
     } elseif (preg_match('/^index/i', $params[1])) {
-        /* some search engine/someone tried using index.html (or similar)
-         * -> we'll go to the main function
-         */
         return array('main', $args);
-    } elseif (preg_match('/^list/i', $params[1])) {
-        /* something that starts with 'list' is probably for the view function
-         * Note : make sure your encoding/decoding is consistent ! :-)
-         */
-        return array('view', $args);
-    } elseif (preg_match('/^(\d+)/', $params[1], $matches)) {
-        /* something that starts with a number must be for the display function
-         * Note : make sure your encoding/decoding is consistent ! :-)
-         */
-        $exid = $matches[1];
-        $args['exid'] = $exid;
-        return array('display', $args);
+    } elseif (preg_match('/^tweet/i', $params[1])) {
+        return array('tweet', $args);
+    } elseif (preg_match('/^(\w+)/', $params[1], $matches)) {
+        $timeline = $matches[1];
+        $args['timeline'] = $timeline;
+        return array('main', $args);
     } else {
-        /* the first part might be something variable like a category name
-         * In order to match that, you'll have to retrieve all relevant
-         * categories for this module, and compare against them...
-         * $cid = xarModGetVar('example','mastercids');
-         * if (xarModAPILoad('categories','user')) {
-         * $cats = xarModAPIFunc('categories',
-         * 'user',
-         * 'getcat',
-         * array('cid' => $cid,
-         * 'return_itself' => true,
-         * 'getchildren' => true));
-         * // lower-case for fanciful search engines/people
-         * $params[1] = strtolower($params[1]);
-         * $foundcid = 0;
-         * foreach ($cats as $cat) {
-         * if ($params[1] == strtolower($cat['name'])) {
-         * $foundcid = $cat['cid'];
-         * break;
-         * }
-         * }
-         * // check if we found a matching category
-         * if (!empty($foundcid)) {
-         * $args['cid'] = $foundcid;
-         * // TODO: now analyse $params[2] for index, list, \d+ etc.
-         * // and return array('whatever', $args);
-         * }
-         * }
-         * we have no idea what this virtual path could be, so we'll just
-         * forget about trying to decode this thing
-         * you *could* return the main function here if you want to
-         * return array('main', $args);
-         */
+
     }
     /* default : return nothing -> no short URL decoded */
 }
--- xaruserapi/encode_shorturl.php
+++ xaruserapi/encode_shorturl.php
@@ -1,92 +1,38 @@
 <?php
 /**
- * Encode module parameters for Short URL support
+ * Twitter Module 
  *
  * @package modules
  * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage Example Module
- * @link http://xaraya.com/index.php/release/36.html
- * @author Example Module Development Team
+ * @subpackage Twitter Module
+ * @link http://xaraya.com/index.php/release/991.html
+ * @author Chris Powis (crisp@crispcreations.co.uk)
  */
 /**
  * return the path for a short URL to xarModURL for this module
  *
  * Support for short URLs (user functions)
  *
- * The following two functions encode module parameters into some
- * virtual path that will be added to index.php, and decode a virtual
- * path back to the original module parameters.
- *
- * The result is that people (and search engines) can use URLs like:
- *
- * - http://mysite.com/index.php/example/ (main function)
- * - http://mysite.com/index.php/example/list (view function)
- * - http://mysite.com/index.php/example/123 (display function)
- *
- * in addition to the 'normal' Xaraya URLs that look like:
- *
- * - http://mysite.com/index.php?module=example&func=display&exid=123
- *
- * You can also combine the two, e.g. for less frequently-used parameters:
- *
- * - http://mysite.com/index.php/example/list?startnum=21
- *
- *
- * Module developers who wish to support this feature are strongly
- * recommended to create virtual paths that are 'semantically meaningful',
- * so that people navigating in your module can understand at a glance what
- * the short URLs mean, and how they could e.g. display item 234 simply
- * by changing the .../123 into .../234
- *
- * For older modules with many different optional parameters and functions,
- * this generally implies re-thinking which parameters could easily be set
- * to some default to cover the most frequently-used cases, and rethinking
- * how each function could be represented inside some "virtual directory
- * structure". E.g. .../archive/2002/05/, .../forums/12/345, ../recent
- * or .../<categoryname>/123
- *
- * The same kind of encoding/decoding can be done for admin functions as well,
- * except that by default, the URLs will start with index.php/admin/example.
- * The encode/decode functions for admin functions are in xaradminapi.php.
- *
- * @author the Example module development team
+ * @author Chris Powis (crisp@crispcreations.co.uk)
  * @param  $args the function and arguments passed to xarModURL
  * @return string Path to be added to index.php for a short URL, or empty if failed
  */
-function example_userapi_encode_shorturl($args)
+function twitter_userapi_encode_shorturl($args)
 {
-    // Get arguments from argument array.
     extract($args);
 
-    // Check if we have something to work with.
-    // Returning without a value at any point will result in a 'long' URL
-    // being generated, i.e. a URL consisting entirely of GET paramaters.
     if (!isset($func)) {return;}
-
-    // The components of the path.
-    // On return, we can pass back two arrays: the 'path' part of the URL
-    // and the 'GET' part of the URL.
-    // In generating the path-part, we will consume the args passed in.
-    // We may even generate further get paramaters.
     $path = array();
     $get = $args;
 
     // This module name.
-    $module = 'example';
+    $module = 'twitter';
 
-    // Check if we have a module alias set.
-    // These two variables are set in the module config screen.
-    // They allow the module to take on a new alias.
     $aliasisset = xarModGetVar($module, 'useModuleAlias');
     $aliasname = xarModGetVar($module, 'aliasname');
-
-    // It should be noted that most module aliases are not set in this way.
-    // A module can have many aliases, and these can be linked to individual
-    // datasets within the module, and so are set dynamically here, according
-    // to the args passed in, rather than via module variables (as it is here).
 
     if (!empty($aliasisset) && !empty($aliasname)) {
         // Check this alias really is a module alias, by mapping
@@ -100,41 +46,18 @@ function example_userapi_encode_shorturl($args)
         }
     }
 
-    // The first part of the URL must be either the module name or one of its alias.
-    // Store the module or alias in the first part of the path.
     $path[] = $module;
-
-    // Specify some short URLs relevant to your module.
-    // If you have a module alias make provision for it.
-    // The following code should be changed to suit and
-    // demonstrates overtly how alias name is added instead
-    // of module.
 
     if ($func == 'main') {
         // Consume the 'func' parameter only.
         unset($get['func']);
-    } elseif ($func == 'view') {
-        $path[] = 'list';
-        unset($get['func']);
-    } elseif ($func == 'display') {
-        // check for required parameters
-        if (isset($exid) && is_numeric($exid)) {
-            unset($get['func']);
-
-            // Add the exid to the path, then consume it.
-            $path[] = $exid;
-            unset($get['exid']);
-
-            // You might have some additional parameter that you want to use to
-            // create different virtual paths here - for example a category name
-            // See above for an example...
-        } else {
-            // we don't know how to handle that -> don't create a path here
-            // Note : this generally means that someone tried to create a
-            // link to your module, but used invalid parameters for xarModURL
-            // -> you might want to provide a default path to return to
-            // $path = array($module, 'list')
+        if (isset($timeline) && !empty($timeline)) {
+          $path[] = $timeline;
+          unset($get['timeline']);
         }
+    } elseif ($func == 'tweet') {
+        $path[] = 'tweet';
+        unset($get['func']);
     } else {
         // anything else that you haven't defined a short URL equivalent for
         //  -> don't create a path here
