06cb01a59b10496d4acd90582d5b55bdcb721316
mfl <mfl@netspan.ch>
2009-02-12 13:26:01 +0000

    remove the link to xar.com and drop an unused file

--- xaradmin/view.php
+++ xaradmin/view.php
@@ -5,7 +5,7 @@
  * @package modules
  * @copyright (C) copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link http://www.xaraya.com
+ * 
  *
  * @subpackage Publications Module
  
