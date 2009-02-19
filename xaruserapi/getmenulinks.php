e6564471c929ae12c9d28f9b94554e079ea16c43
hb <hb@xaraya.com>
2009-02-19 22:42:46 +0000

    Bug 6326 user-menu.xd for xTasks

--- xaruserapi/getmenulinks.php
+++ xaruserapi/getmenulinks.php
@@ -3,6 +3,13 @@
 /**
  * utility function pass individual menu items to the main menu
  *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2002-2009 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ *
+ * @subpackage xtasks module
+ * @author Chad Kraeft <stego@xaraya.com>
+ *
  * @author the xTasks module development team
  * @returns array
  * @return array containing the menulinks for the main menu items.
@@ -11,29 +18,25 @@ function xtasks_userapi_getmenulinks()
 {
     $menulinks = array();
     
-    $menulinks[] = Array('url'   => xarModURL('xtasks',
-                                               'user',
-                                               'settings'),
-                          'title' => xarML('Change your task display preferences'),
-                          'label' => xarML('Settings'));
-
     if (xarSecurityCheck('ViewXTask', 0)) {
         $menulinks[] = Array('url'   => xarModURL('xtasks',
                                                    'user',
                                                    'main'),
+                              'active'=> array('main'),
                               'title' => xarML('The overview of this module and its functions'),
                               'label' => xarML('Overview'));
     }
 
     if (xarSecurityCheck('AddXTask', 0)) {
         $menulinks[] = Array('url'   => xarModURL('xtasks',
-                                                   'user',
+                                                   'admin',
                                                    'new'),
-                              'title' => xarML('Create a new project'),
+                              'title' => xarML('Create a new task'),
                               'label' => xarML('New Task'));
     }
 
     if (xarSecurityCheck('ReadXTask', 0)) {
+/*
         $menulinks[] = Array('url'   => xarModURL('xtasks',
                                                    'admin',
                                                    'view'),
@@ -51,11 +54,25 @@ function xtasks_userapi_getmenulinks()
                               'label' => xarML('Archive'));
 
         $menulinks[] = Array('url'   => xarModURL('xtasks',
-                                                   'user',
+                                                   'admin',
                                                    'search'),
                               'title' => xarML('Query project entries'),
                               'label' => xarML('Search Tasks'));
+*/
+        $menulinks[] = Array('url'    => xarModURL('xtasks',
+                                                   'user',
+                                                   'taskarchive'),
+                              'active'=> array('taskarchive'),
+                              'title' => xarML('Task Archive'),
+                              'label' => xarML('Archive'));
     }
+
+    $menulinks[] = Array('url'   => xarModURL('xtasks',
+                                               'user',
+                                               'settings'),
+                          'active'=> array('settings'),
+                          'title' => xarML('Change your task display preferences'),
+                          'label' => xarML('Settings'));
 
     if (empty($menulinks)){
         $menulinks = '';
