d2260d8c720387fbc99cbf74947f8bc7b868badb
hb <hb@xaraya.com>
2009-03-11 16:33:07 +0000

    MLS in xarversion.php

--- xarversion.php
+++ xarversion.php
@@ -3,7 +3,7 @@
  * Mime Module
  *
  * @package modules
- * @copyright (C) 2002-2008 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -16,9 +16,6 @@ $modversion['version']        = '1.2.0';
 $modversion['displayname']    = 'Mime';
 $modversion['description']    = 'Hook based module that returns the content-type of a given file.';
 $modversion['credits']        = 'xardocs/credits.txt';
-$modversion['help']           = 'xardocs/help.txt';
-$modversion['changelog']      = 'xardocs/changelog.txt';
-$modversion['license']        = 'xardocs/license.txt';
 $modversion['official']       = 1;
 $modversion['author']         = 'Carl P. Corliss <carl.corliss@xaraya.com>';
 $modversion['contact']        = 'http://www.xaraya.com/';
@@ -27,8 +24,8 @@ $modversion['user']           = 0;
 $modversion['class']          = 'Utility';
 $modversion['category']       = 'Content';
 
-if (false) { 
-    $d = xarML('Mime');
-    $d = xarML('Hook based module that returns the content-type of a given file.');
+if (false) {
+    xarML('Mime');
+    xarML('Hook based module that returns the content-type of a given file.');
 }
 ?>
