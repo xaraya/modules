1fd252dae5090f84fb3e84b9c4a59e17e25c91ef
mfl <mfl@netspan.ch>
2009-05-22 05:15:15 +0000

    add the ck and fck versions of this editor

--- xarproperties/editor.php
+++ xarproperties/editor.php
@@ -24,12 +24,30 @@ class EditorProperty extends TextAreaProperty
     public $desc       = 'Editor';
     public $reqmodules = array('ckeditor');
     
+    public $editor     = null;
+    public $version    = 'fckeditor';
+    
     function __construct(ObjectDescriptor $descriptor)
     {
         parent::__construct($descriptor);
         $this->tplmodule = 'ckeditor';
         $this->template  = 'editor';
-        $this->filepath  = 'modules/ckeditor/xarproperties';
+        $this->filepath  = 'modules/ckeditor/xarproperties';        
+    }
+
+    public function showInput(Array $data = array())
+    {
+        if ($this->version == 'fckeditor') {
+            sys::import('modules.ckeditor.xartemplates.includes.fckeditor.fckeditor');
+            $editorpath = 'modules/ckeditor/xartemplates/includes/fckeditor/';
+            if(!isset($data['name'])) $data['name'] = 'dd_'.$this->id;
+            $this->editor = new FCKeditor($data['name']) ;
+            $this->editor->BasePath = $editorpath;
+            $this->editor->Value = $this->value;
+            $data['editor'] = $this->editor;
+        }
+        $data['version'] = $this->version;
+        return parent::showInput($data);
     }
 }
 
