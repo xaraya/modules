d24c2e733f4ca7ad7f242ec281972f4bdd610874
mfl <mfl@netspan.ch>
2009-05-24 13:00:48 +0000

    latest updates

--- xartables.php
+++ xartables.php
@@ -11,11 +11,6 @@
         $xartable = array();
 
         $xartable['foo_tags']          = xarDB::getPrefix() . '_foo_tags';
-        $xartable['foo_posts']         = xarDB::getPrefix() . '_foo_posts';
-        $xartable['foo_users']         = xarDB::getPrefix() . '_foo_users';
-        $xartable['foo_tags_posts']    = xarDB::getPrefix() . '_foo_tags_posts';
-        $xartable['foo_subscriptions'] = xarDB::getPrefix() . '_foo_subscriptions';
-        $xartable['foo_visits']        = xarDB::getPrefix() . '_foo_visits';
 
         // Return the table information
         return $xartable;
