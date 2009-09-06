1376fb4b351f5bc37be8dd19d7ae240761c986bd
xaraya <xaraya@mikespub.net>
2009-09-06 16:12:35 +0000

    update copyright dates for diffs

--- xarblocks/featureditems.php
+++ xarblocks/featureditems.php
@@ -3,7 +3,7 @@
  * Featured items
  *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
