1e2b024b455ef5fcaf496a4a4e2296f663b88638
crispcreations <crispcreations@googlemail.com>
2009-10-15 18:51:59 +0000

    Convert userpanel block to class

--- xarblocks/userpanel.php
+++ xarblocks/userpanel.php
@@ -12,159 +12,118 @@
 /**
  * @author crisp <crisp@crispcreations.co.uk>
  */
-/**
- * Block init
- */
-function crispbb_userpanelblock_init()
+sys::import('xaraya.structures.containers.blocks.basicblock');
+
+class UserPanelBlock extends BasicBlock implements iBlock
 {
-    return array(
-        /* block defaults */
-        'showusername' => true,
-        'showaccount' => true,
-        'showtimenow' => true,
-        'showlastvisit' => true,
-        'showthisvisit' => true,
-        'showtotalvisit' => true,
-        'showwaiting' => true,
-        'showlogout' => true,
-        /* set some defaults for block caching */
-        'nocache' => 1, // cache by default, 0 = yes, 1 = no
-        'pageshared' => 0, // share across pages? 0 = no, 1 = yes
-        'usershared' => 0, // share across group members? 0 no, 1 = yes
-        'cacheexpire' => 1 // default refresh
-    );
-}
-/**
- * Block info array
- */
-function crispbb_userpanelblock_info()
-{
-    return array(
-        'text_type' => 'userpanel', // block name
-        'text_type_long' => 'userpanel', // block display name
-        'module' => 'crispbb', // module block belongs to
-        // your block may have no config options in which case you can omit 'func_update'
-        'func_update' => 'crispbb_userpanelblock_insert', // block update function
-        'allow_multiple' => true, // specify if there can be multiple instances of this block
-        'form_content' => false, // specify if this block has a form
-        'form_refresh' => false, // specify form refresh
-        'show_preview' => true // show preview
-    );
-}
+    public $no_cache            = 1;
+
+    public $name                = 'UserpanelBlock';
+    public $module              = 'crispbb';
+    public $text_type           = 'crispBB Userpanel';
+    public $text_type_long      = 'Displays user information for current logged in user';
+    public $show_preview        = true;
+
+    public $showusername        = true;
+    public $showaccount         = true;
+    public $showtimenow         = true;
+    public $showlastvisit       = true;
+    public $showthisvisit       = true;
+    public $showtotalvisit      = true;
+    public $showwaiting         = false;
+    public $showlogout          = true;
+
 /**
  * Display func.
- * Displays the block based on settings stored in $blockinfo
- * Outputs to userpanel.xd template
- * @param $blockinfo array containing title,content
-**/
-function crispbb_userpanelblock_display($blockinfo)
-{
-    sys::import('modules.crispbb.class.tracker');
+ * @param $data array containing title,content
+ */
+    function display(Array $data=array())
+    {
+        $data = parent::display($data);
+        if (empty($data)) return;
+        if (!xarUserIsLoggedIn()) return;
 
-    if (!xarSecurityCheck('ReadCrispBBBlock', 0, 'Block', $blockinfo['name'])) {return;}
+        $vars = isset($data['content']) ? $data['content'] : array();
+        // Defaults
+        if (!isset($vars['showusername']))   $vars['showusername']   = $this->showusername;
+        if (!isset($vars['showaccount']))    $vars['showaccount']    = $this->showaccount;
+        if (!isset($vars['showtimenow']))    $vars['showtimenow']    = $this->showtimenow;
+        if (!isset($vars['showlastvisit']))  $vars['showlastvisit']  = $this->showlastvisit;
+        if (!isset($vars['showthisvisit']))  $vars['showthisvisit']  = $this->showthisvisit;
+        if (!isset($vars['showtotalvisit'])) $vars['showtotalvisit'] = $this->showtotalvisit;
+        if (!isset($vars['showwaiting']))    $vars['showwaiting']    = $this->showwaiting;
+        if (!isset($vars['showlogout']))     $vars['showlogout']     = $this->showlogout;
 
-    if (!xarUserIsLoggedIn()) return;
+        $now = time();
+        sys::import('modules.crispbb.class.tracker');
+        if (xarVarIsCached('Blocks.crispbb', 'tracker')) {
+            $tracker = xarVarGetCached('Blocks.crispbb', 'tracker_object');
+        } else {
+            $tracker = unserialize(xarModUserVars::get('crispbb', 'tracker_object'));
+        }
+        $userpanel = $tracker->getUserPanelInfo();
+        $vars['id'] = $userpanel['id'];
+        $vars['name'] = $userpanel['name'];
+        if ($vars['showlastvisit']) {
+        $vars['lastvisit'] = $userpanel['lastvisit'];
+        }
+        if ($vars['showthisvisit']) {
+        $vars['visitstart'] = $userpanel['visitstart'];
+        }
+        if ($vars['showtotalvisit']) {
+        $vars['totalvisit'] = $userpanel['timeonline'];
+        }
+        if ($vars['showtimenow']) {
+            $vars['timenow'] = $now;
+        }
+        if ($vars['showwaiting']) {
+            $vars['showwaiting'] = xarMod::guiFunc('crispbb', 'admin', 'waitingcontent');
+        }
 
-    $defaults = crispbb_userpanelblock_init();
-
-    if (!is_array($blockinfo['content'])) {
-        $vars = @unserialize($blockinfo['content']);
-    } else {
-        $vars = $blockinfo['content'];
+        $data['content'] = $vars;
+        return $data;
     }
 
-    $blockinfo['content'] = '';
 
-    $data = array();
-    $data['showusername'] = isset($vars['showusername']) ? $vars['showusername'] : $defaults['showusername'];
-    $data['showaccount'] = isset($vars['showaccount']) ? $vars['showaccount'] : $defaults['showaccount'];
-    $data['showtimenow'] = isset($vars['showtimenow']) ? $vars['showtimenow'] : $defaults['showtimenow'];
-    $data['showlastvisit'] = isset($vars['showlastvisit']) ? $vars['showlastvisit'] : $defaults['showlastvisit'];
-    $data['showthisvisit'] = isset($vars['showthisvisit']) ? $vars['showthisvisit'] : $defaults['showthisvisit'];
-    $data['showtotalvisit'] = isset($vars['showtotalvisit']) ? $vars['showtotalvisit'] : $defaults['showtotalvisit'];
-    $data['showwaiting'] = isset($vars['showwaiting']) ? $vars['showwaiting'] : $defaults['showwaiting'];
-    $data['showlogout'] = isset($vars['showlogout']) ? $vars['showlogout'] : $defaults['showlogout'];
-
-    $now = time();
-    if (xarVarIsCached('Blocks.crispbb', 'tracker')) {
-        $tracker = xarVarGetCached('Blocks.crispbb', 'tracker_object');
-    } else {
-        $tracker = unserialize(xarModUserVars::get('crispbb', 'tracker_object'));
-    }
-    $userpanel = $tracker->getUserPanelInfo();
-    $data['id'] = $userpanel['id'];
-    $data['name'] = $userpanel['name'];
-    if ($data['showlastvisit']) {
-    $data['lastvisit'] = $userpanel['lastvisit'];
-    }
-    if ($data['showthisvisit']) {
-    $data['visitstart'] = $userpanel['visitstart'];
-    }
-    if ($data['showtotalvisit']) {
-    $data['totalvisit'] = $userpanel['timeonline'];
-    }
-    if ($data['showtimenow']) {
-        $data['timenow'] = $now;
-    }
-    if ($data['showwaiting']) {
-        $data['showwaiting'] = xarMod::guiFunc('crispbb', 'admin', 'waitingcontent');
-    }
-    $blockinfo['content'] = $data;
-
-    return $blockinfo;
-}
 /**
  * Modify Function to the Blocks Admin
- * This displays the blocks current settings in Modify Block Instance function
- * Outputs to userpanel-modify.xd template
- * @param $blockinfo array containing title,content
+ * @param $data array containing title,content
  */
-function crispbb_userpanelblock_modify($blockinfo)
-{
+    public function modify(Array $data=array())
+    {
+        $data = parent::modify($data);
 
-    $defaults = crispbb_userpanelblock_init();
+        // Defaults
+        if (!isset($data['showusername']))   $data['showusername']   = $this->showusername;
+        if (!isset($data['showaccount']))    $data['showaccount']    = $this->showaccount;
+        if (!isset($data['showtimenow']))    $data['showtimenow']    = $this->showtimenow;
+        if (!isset($data['showlastvisit']))  $data['showlastvisit']  = $this->showlastvisit;
+        if (!isset($data['showthisvisit']))  $data['showthisvisit']  = $this->showthisvisit;
+        if (!isset($data['showtotalvisit'])) $data['showtotalvisit'] = $this->showtotalvisit;
+        if (!isset($data['showwaiting']))    $data['showwaiting']    = $this->showwaiting;
+        if (!isset($data['showlogout']))     $data['showlogout']     = $this->showlogout;
 
-    if (!is_array($blockinfo['content'])) {
-        $vars = @unserialize($blockinfo['content']);
-    } else {
-        $vars = $blockinfo['content'];
+        return $data;
     }
 
-    $data = array();
-    $data['showusername'] = isset($vars['showusername']) ? $vars['showusername'] : $defaults['showusername'];
-    $data['showaccount'] = isset($vars['showaccount']) ? $vars['showaccount'] : $defaults['showaccount'];
-    $data['showtimenow'] = isset($vars['showtimenow']) ? $vars['showtimenow'] : $defaults['showtimenow'];
-    $data['showlastvisit'] = isset($vars['showlastvisit']) ? $vars['showlastvisit'] : $defaults['showlastvisit'];
-    $data['showthisvisit'] = isset($vars['showthisvisit']) ? $vars['showthisvisit'] : $defaults['showthisvisit'];
-    $data['showtotalvisit'] = isset($vars['showtotalvisit']) ? $vars['showtotalvisit'] : $defaults['showtotalvisit'];
-    $data['showwaiting'] = isset($vars['showwaiting']) ? $vars['showwaiting'] : $defaults['showwaiting'];
-    $data['showlogout'] = isset($vars['showlogout']) ? $vars['showlogout'] : $defaults['showlogout'];
-    $data['blockid'] = $blockinfo['bid'];
-     // Just return the template variables.
-    return $data;
-}
 /**
  * Updates the Block config from the Blocks Admin
- * This function accepts input from the form in userpanel-modify.xd
- * @param $blockinfo array containing title,content
+ * @param $data array containing title,content
  */
-function crispbb_userpanelblock_insert($blockinfo)
-{
-
-    $defaults = crispbb_userpanelblock_init();
-
-    $vars = array();
-    if (!xarVarFetch('showusername', 'checkbox', $vars['showusername'], false, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('showaccount', 'checkbox', $vars['showaccount'], false, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('showtimenow', 'checkbox', $vars['showtimenow'], false, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('showlastvisit', 'checkbox', $vars['showlastvisit'], false, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('showthisvisit', 'checkbox', $vars['showthisvisit'], false, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('showtotalvisit', 'checkbox', $vars['showtotalvisit'], false, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('showwaiting', 'checkbox', $vars['showwaiting'], false, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('showlogout', 'checkbox', $vars['showlogout'], false, XARVAR_NOT_REQUIRED)) return;
-
-    $blockinfo['content'] = $vars;
-    return $blockinfo;
+    public function update(Array $data=array())
+    {
+        $data = parent::update($data);
+        $vars = array();
+        if (!xarVarFetch('showusername', 'checkbox', $vars['showusername'], false, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('showaccount', 'checkbox', $vars['showaccount'], false, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('showtimenow', 'checkbox', $vars['showtimenow'], false, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('showlastvisit', 'checkbox', $vars['showlastvisit'], false, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('showthisvisit', 'checkbox', $vars['showthisvisit'], false, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('showtotalvisit', 'checkbox', $vars['showtotalvisit'], false, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('showwaiting', 'checkbox', $vars['showwaiting'], false, XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('showlogout', 'checkbox', $vars['showlogout'], false, XARVAR_NOT_REQUIRED)) return;
+        $data['content'] = $vars;
+        return $data;
+    }
 }
-
 ?>
