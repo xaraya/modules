5375cc18ccaadae0db661e049a6ac094bfe94fe6
crispcreations <crispcreations@googlemail.com>
2009-11-04 23:42:09 +0000

    Update pager functions; add pager example to user view function and add pagers to Approach 3 and 4

--- xaruser/view.php
+++ xaruser/view.php
@@ -36,7 +36,7 @@ function dyn_example_user_view()
     // Get user setting for 'items_per_page'
     $data['items_per_page'] = xarModUserVars::get('dyn_example','items_per_page');
 
-/* APPROACH #1 and #3 */
+/* APPROACH #1 #3 and #4 */
 
     // Load the DD master object class. This line will likely disappear in future versions
     sys::import('modules.dynamicdata.class.objects.master');
@@ -49,11 +49,12 @@ function dyn_example_user_view()
                      'startnum'  => $data['startnum'],
                      'status'    => DataPropertyMaster::DD_DISPLAYSTATE_ACTIVE
                     );
-    
-    // Count the items first if you want a full pager - otherwise you'll get simple previous/next links
-    $mylist->countItems($filters);
 
-    // Get the items 
+    // Count the items first if you want a full pager - otherwise you'll get simple previous/next links
+    /* APPROACH #3 and #4 Important you call this if you want to use the base-pager template tag */
+    $total = $mylist->countItems($filters);
+
+    // Get the items
     $items = $mylist->getItems($filters);
 
     // Pass along the whole object list to the template.  Only needed in Approach #1
@@ -91,7 +92,68 @@ function dyn_example_user_view()
             }
         }
     }
-    // TODO: add a pager here (needed for this approach)
+    // pager
+/* APPROACH #3 and #4: use the base-pager tag to build your pager */
+    /**
+     * Total number of items for the pager ($total), this must be set (see APPROACH #1)
+     * In many cases, this is the only parameter the pager tag will need
+     * you can safely omit any parameters in which you are using the pager defaults
+    **/
+    $data['total'] = $total;
+    /**
+     * itemsperpage for the pager, in this case we just use $data['items_per_page']
+     * since this module uses the items_per_page moduservar, we can omit this
+     * the pager tag will automatically use the items_per_page moduservar setting
+     * for the current module
+    **/
+    // $data['itemsperpage'] = $data['items_per_page'];
+    /**
+     * startnum for the pager, in this case we just use $data['startnum']
+     * since this module uses the 'startnum' param, we can omit this
+     * the pager tag will automatically look for a startnum parameter (eg in the url)
+    **/
+    // $data['startnum'] = $data['startnum'];
+    /**
+     * urltemplate, the pager accepts a template to use for page links
+     * this means you can specify eg, to use page= instead of startnum= (not recommended, btw)
+     * in your urls, or pass in extra params or remove others.
+     * normally this isn't necessary, and the current url with a different startnum for
+     * each page will suffice. When that's the case, we can omit this,
+     * the pager tag will automatically use the current url as it's template
+    **/
+    $data['urltemplate'] = xarServer::getCurrentURL(array('startnum' => '%%'));
+    /**
+     * urlitemmatch, in the example above we're using %% to indicate the value
+     * the pager should change when building links to pages, if you use something
+     * else, you'll need to let the pager tag know, in this case we're using the default
+     * the pager tag uses '%%' as the default value for urlitemmatch
+    **/
+    // $data['urlitemmatch'] = '%%';
+    /**
+     * blockitems, by default the pager displays pages in 'blocks' of 10
+     * use this if you want a different number of pages per block
+     * note, you could just set this in the template
+    **/
+    // $data['blockitems'] = 10;
+    /**
+     * tplmodule, use this if you want to use a pager template eg from dyn_example
+     * name your templates pager-[templatename].xt and place them
+     * in dyn_example/xartemplates/, then add tplmodule="$tplmodule" to the tag
+     * the pager tag uses 'base' as the default value for tplmodule
+     * note, you could just set this in the template
+    **/
+    // $data['tplmodule'] = 'dyn_example'; // dyn_example/xartemplates/
+    /**
+     * template, the base module comes with several layouts by way of templates
+     * pager-[default|multipage|multipagenext|multipageprev|openended].xt
+     * the pager tag uses 'default' as the default value for template
+     * note, you could just set this in the template
+    **/
+    // $date['template'] = 'mypager'; // $data['tplmodule']/xartemplates/pager-mypager.xt
+    /**
+     * see dyn_example/xartemplates/user-view.xt APPROACH #3 and #4
+     * for examples of using these values in the base-pager tag
+    **/
 /* end APPROACH #4 : getting only the raw item values via API */
 
 	if (isset($tab)) {
