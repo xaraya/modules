513589186100d84e725ef868bcb90df6de0bc98f
xaraya <xaraya@mikespub.net>
2009-11-04 21:04:20 +0000

    switch to version 2.x.x for jamaica

--- xarversion.php
+++ xarversion.php
@@ -13,7 +13,7 @@
  */
 $modversion['name']           = 'Change Log';
 $modversion['id']             = '185';
-$modversion['version']        = '1.1.1';
+$modversion['version']        = '2.0.0';
 $modversion['displayname']    = xarML('ChangeLog');
 $modversion['description']    = 'Keep track of changes to module items';
 $modversion['credits']        = '';
