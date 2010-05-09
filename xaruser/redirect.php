948131a4e3ce89040567b11789f9dfc94edfbc37
mfl <mfl@netspan.ch>
2010-05-09 16:55:57 +0000

    changes as we go along

--- xaruser/redirect.php
+++ xaruser/redirect.php
@@ -23,24 +23,24 @@ function publications_user_redirect($args)
     extract($args);
 
     if (!isset($id) || !is_numeric($id) || $id < 1) {
-        return xarML('Invalid article ID');
+        return xarML('Invalid publication ID');
     }
 
     // Load API
     if (!xarModAPILoad('publications', 'user')) return;
 
-    // Get article
-    $article = xarModAPIFunc('publications',
+    // Get publication
+    $publication = xarModAPIFunc('publications',
                             'user',
                             'get',
                             array('id' => $id));
 
-    if (!is_array($article)) {
-        $msg = xarML('Failed to retrieve article in #(3)_#(1)_#(2).php', 'user', 'get', 'publications');
+    if (!is_array($publication)) {
+        $msg = xarML('Failed to retrieve publication in #(3)_#(1)_#(2).php', 'user', 'get', 'publications');
         throw new DataNotFoundException(null, $msg);
     }
 
-    $ptid = $article['pubtype_id'];
+    $ptid = $publication['pubtype_id'];
 
     // Get publication types
     $pubtypes = xarModAPIFunc('publications','user','get_pubtypes');
@@ -51,7 +51,7 @@ function publications_user_redirect($args)
         if (empty($value['label'])) {
             continue;
         }
-        if ($value['format'] == 'url' && !empty($article[$field]) && $article[$field] != 'http://') {
+        if ($value['format'] == 'url' && !empty($publication[$field]) && $publication[$field] != 'http://') {
 // TODO: add some verifications here !
             $hooks = xarModCallHooks('item', 'display', $id,
                                      array('module'    => 'publications',
@@ -59,10 +59,10 @@ function publications_user_redirect($args)
                                           ),
                                      'publications'
                                     );
-            xarResponse::redirect($article[$field]);
+            xarResponse::redirect($publication[$field]);
             return true;
-        } elseif ($value['format'] == 'urltitle' && !empty($article[$field]) && substr($article[$field],0,2) == 'a:') {
-            $array = unserialize($article[$field]);
+        } elseif ($value['format'] == 'urltitle' && !empty($publication[$field]) && substr($publication[$field],0,2) == 'a:') {
+            $array = unserialize($publication[$field]);
             if (!empty($array['link']) && $array['link'] != 'http://') {
                 $hooks = xarModCallHooks('item', 'display', $id,
                                          array('module'    => 'publications',
