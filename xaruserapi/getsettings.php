a6750dd8e0120dc4a0bad3b804472e7701bcb741
mfl <mfl@netspan.ch>
2010-05-10 07:35:48 +0000

    changes as we go along

--- xaruserapi/getsettings.php
+++ xaruserapi/getsettings.php
@@ -29,9 +29,42 @@ function publications_userapi_getsettings($data)
         
     $pubtypeobject = DataObjectMaster::getObject(array('name' => 'publications_types'));
     $pubtypeobject->getItem(array('itemid' => $data['ptid']));
-    xarCore::setCached('publications', 'context' . $data['ptid'], $pubtypeobject->properties['configuration']->value);
-    $settings = $pubtypeobject->properties['configuration']->getValue();
+    $pubtypesettings = $pubtypeobject->properties['configuration']->getValue();
+    $globalsettings = publications_userapi_getglobalsettings();
+    $settings = $pubtypesettings + $globalsettings;
+    xarCore::setCached('publications', 'context' . $data['ptid'], serialize($settings));
     return $settings;
 }
 
+function publications_userapi_getglobalsettings()
+{
+    $settings = array(
+                'number_of_columns' => 1,
+                'items_per_page' => 20,
+                'defaultview' => "Sections",
+                'showcategories' => false,
+                'showcatcount' => false,
+                'showprevnext' => true,
+                'showcomments' => false,
+                'showhitcounts' => false,
+                'showratings' => false,
+                'showarchives' => false,
+                'showmap' => true,
+                'showpublinks' => false,
+                'showpubcount' => true,
+                'dotransform' => true,
+                'titletransform' => true,
+                'prevnextart' => true,
+                'usealias' => false,
+                'page_template' => "",
+                'defaultstate' => 2,
+                'defaultsort' => "name",
+                'showkeywords' => false,
+                'showsubmit' => false,
+                'admin_items_per_page' => 20,
+                'defaultprocessstate' => 0,
+                'allowtranslations' => true,
+                    );
+    return $settings;
+}
 ?>
