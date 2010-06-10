63655e5572224751f5fc055be2704a6a50a31045
mfl <mfl@netspan.ch>
2010-06-10 19:23:53 +0000

    more publications tag

--- xaruserapi/field-output.php
+++ xaruserapi/field-output.php
@@ -9,5 +9,16 @@
  * @param $args array containing the definition of the field (object, itemid, property, value, ...)
  * @return string containing the HTML (or other) text to output in the BL template
  */
+function publications_userapi_fieldoutput($args)
+{
+//    extract($args);
+//    if (!isset($object) || !isset($itemid) || !isset($field)) return '';
+//    sys::import('modules.dynamicdata.class.objects.master');
+//    $object = DataObjectMaster::getObject(array('name' => $object));
+//    $itemid = xarMod::apiFunc('publications','user','gettranslationid',array('id' => $itemid));
+//    $object->getItem(array('itemid' => $itemid));
+//    $field = $object->properties[$field]->getValue();
+//    return $field;
+}
 
 ?>
