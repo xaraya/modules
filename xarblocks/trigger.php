7631fa6044d7f99568453537ac378be99ec7b07c
mfl <mfl@netspan.ch>
2010-06-27 17:23:55 +0000

    rework the trigger block

--- xarblocks/trigger.php
+++ xarblocks/trigger.php
@@ -15,105 +15,64 @@
  * initialise block
  * @return bool true on success
  */
-function scheduler_triggerblock_init()
+// Inherit properties and methods from BasicBlock class
+sys::import('xaraya.structures.containers.blocks.basicblock');
+
+class TriggerBlock extends BasicBlock implements iBlock
 {
-    return true;
-}
+    public $name                = 'TriggerBlock';
+    public $module              = 'scheduler';
+    public $text_type           = 'trigger';
+    public $text_type_long      = 'Trigger for the scheduler (using an external trigger is better)';
+    public $xarversion          = '2.2.0';
+
+    public $form_content        = false;
+    public $form_refresh        = false;
+
+    public $showstatus          = false;
 
 /**
- * get information on block
- * @return array
+ * Display func.
+ * @param $data array containing title,content
  */
-function scheduler_triggerblock_info()
-{
-    // Values
-    return array('text_type' => 'trigger',
-        'module' => 'scheduler',
-        'text_type_long' => 'Trigger for the scheduler (using an external trigger is better)',
-        'allow_multiple' => false,
-        'form_content' => false,
-        'form_refresh' => false,
-        'show_preview' => false);
-}
+    function display(Array $data=array())
+    {
+        $data = parent::display($data);
+        if (empty($data)) return;
 
-/**
- * display block
- */
-function scheduler_triggerblock_display($blockinfo)
-{
-    // Get current content
-    if (!empty($blockinfo['content'])) {
-        $vars = @unserialize($blockinfo['content']);
-    } else {
-        $vars = array();
-    }
-    // Defaults
-    if (empty($vars['showstatus']) || !xarSecurityCheck('AdminScheduler',0)) {
-        $vars['showstatus'] = 0;
-    }
+        if (empty($data['showstatus'])) $data['showstatus'] = $this->showstatus;
 
-// TODO: this won't work on NFS-mounted or FAT (Win98) file systems, and ISAPI may do weird things too !
-//       So we need to find some better way to see if we're really the only ones playing here...
+    // TODO: this won't work on NFS-mounted or FAT (Win98) file systems, and ISAPI may do weird things too !
+    //       So we need to find some better way to see if we're really the only ones playing here...
 
-    // let's see if we're the only ones trying to run jobs at this moment
-    $GLOBALS['xarScheduler_LockFileHandle'] = fopen(xarCoreGetVarDirPath().'/cache/templates/scheduler.lock','w+');
-    if (empty($GLOBALS['xarScheduler_LockFileHandle']) || !flock($GLOBALS['xarScheduler_LockFileHandle'], LOCK_EX | LOCK_NB)) {
-        fclose($GLOBALS['xarScheduler_LockFileHandle']);
+        // let's see if we're the only ones trying to run jobs at this moment
+        $GLOBALS['xarScheduler_LockFileHandle'] = fopen(xarCoreGetVarDirPath().'/cache/templates/scheduler.lock','w+');
+        if (empty($GLOBALS['xarScheduler_LockFileHandle']) || !flock($GLOBALS['xarScheduler_LockFileHandle'], LOCK_EX | LOCK_NB)) {
+            fclose($GLOBALS['xarScheduler_LockFileHandle']);
+            if (empty($data['showstatus'])) {
+                return;
+            } else {
+                $data['content'] = xarML('Some other process is running jobs right now');
+                return $data;
+            }
+        }
+
+        // For some reason, PHP thinks it's in the Apache root during shutdown functions,
+        // so we save the current base dir here - otherwise xarModAPIFunc() will fail
+        $GLOBALS['xarScheduler_BaseDir'] = realpath('.');
+
+        // register the shutdown function that will execute the jobs after this script finishes
+        register_shutdown_function('scheduler_triggerblock_runjobs');
+
         if (empty($vars['showstatus'])) {
             return;
         } else {
-            $blockinfo['content'] = xarML('Some other process is running jobs right now');
-            return $blockinfo;
+            $data['content'] = xarML('Running Jobs');
+            return $data;
         }
     }
-
-    // For some reason, PHP thinks it's in the Apache root during shutdown functions,
-    // so we save the current base dir here - otherwise xarModAPIFunc() will fail
-    $GLOBALS['xarScheduler_BaseDir'] = realpath('.');
-
-    // register the shutdown function that will execute the jobs after this script finishes
-    register_shutdown_function('scheduler_triggerblock_runjobs');
-
-    if (empty($vars['showstatus'])) {
-        return;
-    } else {
-        $blockinfo['content'] = xarML('Running Jobs');
-        return $blockinfo;
-    }
 }
 
-/**
- * modify block settings
- */
-function scheduler_triggerblock_modify($blockinfo)
-{
-    // Get current content
-    $vars = @unserialize($blockinfo['content']);
-
-    // Defaults
-    if (empty($vars['showstatus'])) {
-        $vars['showstatus'] = 0;
-    }
-    $vars['blockid'] = $blockinfo['bid'];
-
-    // Return output
-    return xarTplBlock('scheduler','triggerAdmin',$vars);
-}
-
-/**
- * update block settings
- */
-function scheduler_triggerblock_update($blockinfo)
-{
-    $vars = array();
-    if(!xarVarFetch('showstatus',  'isset', $vars['showstatus'],  NULL, XARVAR_DONT_SET)) {return;}
-    if (empty($vars['showstatus'])) {
-        $vars['showstatus'] = 0;
-    }
-
-    $blockinfo['content'] = serialize($vars);
-    return $blockinfo;
-}
 
 /**
  * run scheduler jobs when the script is finished
