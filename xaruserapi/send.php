bf01550da8fe811721c9a97c0fd61fd8ef1a8f02
mfl <mfl@netspan.ch>
2010-06-30 16:07:49 +0000

    replace entitities in subject

--- xaruserapi/send.php
+++ xaruserapi/send.php
@@ -185,14 +185,19 @@
                     $tplString  = '<xar:template xmlns:xar="http://xaraya.com/2004/blocklayout">';
                     $tplString .= $subject;
                     $tplString .= '</xar:template>';
+
+                    // Make entities BL compatible
+                    $transformarray = xarMod::apiFunc('mailer','admin','transform_entities');
+                    $tplString  = str_replace(array_keys($transformarray), $transformarray, $tplString);
+                    
                     $subject = $blCompiler->compilestring($tplString);
                     $subject = xarTplString($subject,$data);
+
                     $tplString  = '<xar:template xmlns:xar="http://xaraya.com/2004/blocklayout">';
                     $tplString .= $message;
                     $tplString .= '</xar:template>';
                     
                     // Make entities BL compatible
-                    $transformarray = xarMod::apiFunc('mailer','admin','transform_entities');
                     $tplString  = str_replace(array_keys($transformarray), $transformarray, $tplString);
                     
                     $message = $blCompiler->compilestring($tplString);
