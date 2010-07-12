24db1000748adc89ef5d479c8c315045b3863498
ryan <ryan@webcommunicate.net>
2010-07-12 14:51:22 +0000

    add files; drop files

---
+++ xartemplates/includes/ckeditor/plugins/pgrfilemanager/currpageurl.php
@@ -0,0 +1,13 @@
+ <?php 
+ 
+ function curPageURL() {
+        $pageURL = (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on'))?'https':'http'; 
+        $pageURL .= '://' . $_SERVER['SERVER_NAME'];
+        if (isset($_SERVER['SERVER_PORT']) && ($_SERVER['SERVER_PORT'] != '80')) {
+            $pageURL .= ':' . $_SERVER['SERVER_PORT'];
+        } 
+        $pageURL .= $_SERVER['REQUEST_URI'];
+        return $pageURL;
+    }
+
+    ?>
