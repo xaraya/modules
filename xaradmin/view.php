10f2a0448d575153b20ebad7a033d2ca706ce97a
ryan <ryan@webcommunicate.net>
2010-10-25 22:17:15 +0000

    better use of the filters module here

--- xaradmin/view.php
+++ xaradmin/view.php
@@ -24,6 +24,8 @@ function comments_admin_view()
         return;
     }
 
+	if(!xarVarFetch('startnum', 'int', $startnum, 1, XARVAR_NOT_REQUIRED)) {return;}
+
 	$sort = xarMod::apiFunc('comments','admin','sort', array(
 		//how to sort if the URL or config say otherwise...
 		'sortfield_fallback' => 'date', 
@@ -34,10 +36,11 @@ function comments_admin_view()
 	$object = DataObjectMaster::getObject(array('name' => 'comments'));
 	$config = $object->configuration; 
 	$adminfields = $config['adminfields'];
+	$numitems = xarModVars::get('comments','items_per_page');
 
 	$filters = array();
 
-	// Total number of comments for the pager
+	// Total number of comments for use in the pager
 	$total = DataObjectMaster::getObjectList(array(
 							'name' => 'comments',
 							'numitems' => NULL,
@@ -46,61 +49,19 @@ function comments_admin_view()
 	$data['total'] = $total->countItems();
 
 	$filters_min_items = xarModVars::get('comments','filters_min_item_count');
-
+	
 	$data['makefilters'] = array();
-
 	$data['showfilters'] = false;
 
 	if(xarModIsAvailable('filters') && xarModVars::get('comments','enable_filters') && $data['total'] >= $filters_min_items) {
-		
 		$data['showfilters'] = true;
-
-		if(!xarVarFetch('filter', 'str', $filter, NULL, XARVAR_NOT_REQUIRED)) {return;}
-		if(!xarVarFetch('filterfield', 'str', $filterfield, NULL, XARVAR_NOT_REQUIRED)) {return;}
-
-		$data['filter'] = $filter;
-		$data['filterfield'] = $filterfield;
-		
-		if (!empty($config['filterfields'])) { 
-			$filterfields = $config['filterfields'];
-			$filterfields = explode(';',$filterfields);
-			foreach ($filterfields as $f) {
-				if (strstr($f,',')) {
-					$f = explode(',',$f);
-					$k = $f[0];
-					$v = $f[1];
-					$fields[$k] = $v;
-				} elseif (!empty($f)) {
-					$k = $f;
-					$v = ucwords($f);
-					$fields[$k] = $v;
-				}	
-			}
-			$filterfields = $fields;
-		} else {
-			$filterfields = array();
-		}
-
-		foreach ($filterfields as $thisfield=>$startval) {
-
-			$data['makefilters'][$thisfield] = $startval;
-
-			// If we're filtering on this field
-			if(isset($filter) && $filterfield == $thisfield) { 
-				// If the submitted value is different than the starting value...
-				if ($data['filter'] != $startval) {
-					$filters['where'] = $filterfield . ' LIKE "%' . $data['filter'] . '%"';
-		
-					$results = DataObjectMaster::getObjectList(array(
-												'name' => 'comments',
-												));
-					$items = $results->getItems($filters);
-					$data['results'] = count($items);	
-				}	
-			}
-
-		}
-
+		$filterfields = $config['filterfields'];
+		$get_results = xarMod::apiFunc('filters','user','dd_get_results', array(
+							'filterfields' => $filterfields,
+							'object' => 'comments'
+							)); 
+		$data = array_merge($data, $get_results);
+		if (isset($data['filters'])) $filters = $data['filters'];
 	} 
 
 	if(isset($filters['where'])) {
@@ -114,6 +75,8 @@ function comments_admin_view()
 	$list = DataObjectMaster::getObjectList(array(
 							'name' => 'comments',
 							'sort' => $sort,
+							'startnum' => $startnum,
+							'numitems' => $numitems,
 							'fieldlist' => $adminfields
 		));
 
