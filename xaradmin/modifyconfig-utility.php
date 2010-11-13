d6fe9ce3391872266b636316754f0a39937d00c5
mfl <mfl@netspan.ch>
2010-11-13 14:04:15 +0000

    add standard headers

--- xaradmin/modifyconfig-utility.php
+++ xaradmin/modifyconfig-utility.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * Foo Module
+ *
+ * @package modules
+ * @subpackage foo module
+ * @copyright (C) 2010 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+/**
  * Main configuration page for the foo object
  *
  */
