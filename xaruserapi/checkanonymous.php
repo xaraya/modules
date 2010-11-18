7ea7722b54e8e7c4dfae6aae2ea21eb6d857d3b2
ryan <ryan@webcommunicate.net>
2010-11-18 23:27:19 +0000

    add a file

---
+++ xaruserapi/checkanonymous.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Messages Module
+ *
+ * @package modules
+ * @copyright (C) copyright-placeholder
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Messages Module
+ * @link http://xaraya.com/index.php/release/6.html
+ * @author XarayaGeek
+ */ 
+/**
+ * Check to see if a message was anonymous
+ * @param int	$id the message id
+ * @return boolean  
+ */
+
+function messages_userapi_checkanonymous($args)
+{
+    extract($args);
+
+	if ($id == 0) { 
+		return false;
+	}
+
+	sys::import('modules.dynamicdata.class.objects.master');
+    
+	$object = DataObjectMaster::getObject(array('name' => 'messages_messages'));
+	$object->getItem(array('itemid' => $id));
+	$postanon = $object->properties['postanon']->value;
+
+	return $postanon;
+	 
+}
+?>
