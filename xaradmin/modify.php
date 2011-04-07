9d6b6ebb455ad04caf4f3e60fcfce8a65c1fd42c
mfl <mfl@netspan.ch>
2011-04-07 08:39:49 +0000

    mior fix

--- xaradmin/modify.php
+++ xaradmin/modify.php
@@ -28,7 +28,7 @@ function publications_admin_modify($args)
 
     // Get parameters
     if (!xarVarFetch('itemid',     'isset', $id, NULL, XARVAR_DONT_SET)) {return;}
-    if (!xarVarFetch('ptid',       'isset', $ptid, NULL, XARVAR_DONT_SET)) {return;}
+    if (!xarVarFetch('ptid',       'isset', $data['ptid'], NULL, XARVAR_DONT_SET)) {return;}
     if (!xarVarFetch('returnurl',  'str:1', $data['returnurl'], 'view', XARVAR_NOT_REQUIRED)) {return;}
     if (!xarVarFetch('name',       'str:1', $name, 'publications_types', XARVAR_NOT_REQUIRED)) {return;}
     if (!xarVarFetch('tab',        'str:1', $data['tab'], '', XARVAR_NOT_REQUIRED)) {return;}
