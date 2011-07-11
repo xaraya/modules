743bb810d9e5853875921a961f98b4b80b6d4710
mfl <mfl@netspan.ch>
2011-07-11 13:43:44 +0000

    enhance user display function

---
+++ xaruserapi/getitempubtype.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Publications Module
+ *
+ * @package modules
+ * @subpackage publications module
+ * @version 2.0.0
+ * @copyright (C) 2010 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+/**
+ * Given an itemid, get the publication type
+ * CHECKME: use get in place of this function?
+ */
+function publications_userapi_getitempubtype($args)
+{
+    if (empty($args['itemid']))
+        throw new MissingParameterException('itemid');
+        
+    sys::import('xaraya.structures.query');
+    $xartables = xarDB::getTables();
+    $q = new Query('SELECT',$xartables['publications']);
+    $q->addfield('pubtype_id');
+    $q->eq('id',$args['itemid']);
+    if (!$q->run()) return;
+    $result = $q->row();
+    return $result['pubtype_id'];
+}
+
+?>
