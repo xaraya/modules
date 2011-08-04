4956cf4b8ede55edee2d1e9f8feada6a8716e9ff
mfl <mfl@netspan.ch>
2011-08-04 21:04:21 +0000

    updates to user side

--- xaruser/main.php
+++ xaruser/main.php
@@ -23,12 +23,10 @@ function weather_user_main()
 
     /* Default view */
     $w = xarModAPIFunc('weather','user','factory');
+//    var_dump($w->forecastData());exit;
     return array(
         'wData'=>$w->ccData(),
         'eData'=>$w->forecastData()
         );
-
-    /* success so return true */
-    return true;
 }
 ?>
