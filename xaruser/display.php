bd4775f44b7e29dcc1429414cbe290f6679f0a42
mfl <mfl@netspan.ch>
2011-08-15 07:34:27 +0000

    redo some headers

--- xaruser/display.php
+++ xaruser/display.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * Publications module
+ * Publications Module
  *
  * @package modules
- * @copyright (C) copyright-placeholder
+ * @subpackage publications module
+ * @category Third Party Xaraya Module
+ * @version 2.0.0
+ * @copyright (C) 2011 Netspan AG
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link http://www.xaraya.com
- *
- * @subpackage Publications Module
- * 
  * @author Marc Lutolf <mfl@netspan.ch>
  */
 /**
