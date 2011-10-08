6fbe7375f07cf7f80d6e81918e878014ad2e802c
mfl <mfl@netspan.ch>
2011-10-08 09:40:46 +0000

    standardize the file headers

--- xarversion.php
+++ xarversion.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Shouter Module
+ *
+ * @package modules
+ * @subpackage shouter module
+ * @category Third Party Xaraya Module
+ * @version 2.0.0
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.zwiggybo.com
+ * @link http://xaraya.com/index.php/release/236.html
+ * @author Neil Whittaker
+ */
     $modversion['name']           = 'shouter';
     $modversion['id']             = '236';
     $modversion['version']        = '2.0.0';
