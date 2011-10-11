60f8b5f8cbfd4737e859a8aaa4db99a61e90ccdb
crispcreations <crispcreations@googlemail.com>
2011-10-11 14:24:25 +0000

    Blocks: refactor for 2.3

--- xarblocks/weather.php
+++ xarblocks/weather.php
@@ -3,45 +3,51 @@
 
     class Weather_WeatherBlock extends BasicBlock
     {
-        public $no_cache            = 1;
+    // File Information, supplied by developer, never changes during a versions lifetime, required
+    protected $type             = 'weather';
+    protected $module           = 'weather'; // module block type belongs to, if any
+    protected $text_type        = 'Current Weather';  // Block type display name
+    protected $text_type_long   = 'Show current weather conditions'; // Block type description
+    // Additional info, supplied by developer, optional
+    protected $type_category    = 'block'; // options [(block)|group]
+    protected $author           = '';
+    protected $contact          = '';
+    protected $credits          = '';
+    protected $license          = '';
 
-        public $name                = 'WeatherBlock';
-        public $module              = 'weather';
-        public $text_type           = 'Current';
-        public $text_type_long      = 'Current Conditions';
-        public $allow_multiple      = true;
-        public $show_preview        = true;
+    // blocks subsystem flags
+    protected $show_preview = true;  // let the subsystem know if it's ok to show a preview
+    protected $show_help    = false; // let the subsystem know if this block type has a help() method
 
-        function display(Array $data=array())
+        function display()
         {
-            $data = parent::display($data);
-            if(!xarSecurityCheck('ReadWeatherBlock',1,'Block',"All:" . $data['name'] . ":All",'All')) return;
-            
+            $data = $this->getContent();
+
             // Get variables from content block
             $w = xarModAPIFunc('weather','user','factory');
             $blocks = unserialize(xarModUserVars::get('math','locations'));
             try {
-                $data['content']['blockstoshow'] = xarModUserVars::get('math','locationnumber');
+                $data['blockstoshow'] = xarModUserVars::get('math','locationnumber');
                 $locations = $blocks['locations'];
-                $data['content']['wDataArray'] = array();
+                $data['wDataArray'] = array();
                 foreach ($locations as $locationinfo) {
                     $location = unserialize($locationinfo);
                     if (empty($location['city']['code']))continue;
                     $w->setLocation($location['city']['code']);
-                    $data['content']['wDataArray'][] = $w->ccData();
+                    $data['wDataArray'][] = $w->ccData();
                 }
-                if (empty($data['content']['wDataArray'])) {
-                    $data['content']['blockstoshow'] = 1;
+                if (empty($data['wDataArray'])) {
+                    $data['blockstoshow'] = 1;
                     $location = unserialize(xarModUserVars::get('weather','default_location'));
                     $w->setLocation($location['city']['code']);
-                    $data['content']['wDataArray'] = array($w->ccData());
+                    $data['wDataArray'] = array($w->ccData());
                 }
-                if (empty($data['content']['wDataArray'])) $data['content'] = "";
+                if (empty($data['wDataArray'])) $data['content'] = "";
             } catch (Exception $e) {
-                $data['content']['blockstoshow'] = 1;
+                $data['blockstoshow'] = 1;
                 $location = unserialize(xarModUserVars::get('weather','default_location'));
                 $w->setLocation($location['city']['code']);
-                $data['content']['wDataArray'] = array($w->ccData());
+                $data['wDataArray'] = array($w->ccData());
             }
             return $data;
         }
