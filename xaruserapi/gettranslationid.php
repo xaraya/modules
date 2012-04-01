8f4ccf98a759687b39e569f0c3e4ba33cfb13941
mfl <mfl@netspan.ch>
2012-04-01 06:54:43 +0000

    enhance functionality of gettranslationid

--- xaruserapi/gettranslationid.php
+++ xaruserapi/gettranslationid.php
@@ -19,24 +19,83 @@ function publications_userapi_gettranslationid($args)
     // We can check on a full locale or just a partial one (excluding charset)
     if (empty($args['partiallocale'])) $args['partiallocale'] = 0;
     // We can look for a specific translation
-    if (empty($args['locale'])) $args['locale'] = xarUserGetNavigationLocale();
+    if (empty($args['locale'])) {
+        $locale = xarUserGetNavigationLocale();
+    } else {
+        $locale = $args['locale'];
+    }
 
     sys::import('xaraya.structures.query');
     
     if ($args['partiallocale']) {
         $parts = explode('.',$locale);
-        $args['locale'] = $parts[0];
+        $locale = $parts[0];
     }
 
     $xartable = xarDB::getTables();
-    $q = new Query('SELECT',$xartable['publications']);
-    $q->eq('locale',$args['locale']);
-    $c[] = $q->peq('id',$args['id']);
-    $c[] = $q->peq('parent_id',$args['id']);
+    
+    if (empty($args['locale'])) {
+        // Return the id of the translation if it exists, or else the base document 
+        $q = new Query('SELECT',$xartable['publications']);
+        $q->addfield('id');
+        $q->eq('locale',$locale);
+        $c[] = $q->peq('id',$args['id']);
+        $c[] = $q->peq('parent_id',$args['id']);
+        $q->qor($c);
+        if (!$q->run()) return $args['id'];
+        $result = $q->row();
+        if (empty($result)) return $args['id'];
+        return $result['id']; 
+    } elseif ($args['locale'] == xarUserGetNavigationLocale()) {
+        // No need to look further
+        return $args['id']
+    } elseif ($args['locale'] == xarModVars::get('publications', 'defaultlanguage')) {
+        // Force getting the base document
+        $q = new Query('SELECT',$xartable['publications']);
+        $q->addfield('parent_id');
+        $q->eq('id',$args['id']);
+        if (!$q->run()) return $args['id'];
+        $result = $q->row();
+        if (empty($result)) return $args['id'];
+        // If this was already the base document, return its ID
+        if (empty($result['parent_id'])) return $args['id'];
+        // Else return the parent ID
+        return $result['parent_id']; 
+    } else {
+        // Force getting another translation
+        $q = new Query('SELECT');
+        $q->addtable($xartable['publications'],'p1');
+        $q->addtable($xartable['publications'],'p2');
+        $q->join('p2.parent_id','p1.parent_id');
+        $q->addfield('p2.id');
+        $q->eq('p2.locale',$locale);
+        $q->eq('p1.id',$args['id']);
+        if (!$q->run()) return $args['id'];
+        $result = $q->row();
+        if (empty($result)) return $args['id'];
+        return $result['id']; 
+    }
+
+
+if (xarUserGetVar('uname') == 'random') {
+    $xartable = xarDB::getTables();
+    $q = new Query('SELECT');
+    $q->addtable($xartable['publications'],'p1');
+    $q->addtable($xartable['publications'],'p2');
+    $q->join('p2.id','p1.parent_id');
+    $q->addfield('p1.id');
+    $c[] = $q->peq('p1.id',$args['id']);
+    $c[] = $q->peq('p1.parent_id',$args['id']);
+    $c[] = $q->peq('p2.id',$args['id']);
     $q->qor($c);
+    $d[] = $q->peq('p1.locale',$args['locale']);
+    $d[] = $q->peq('p2.locale',$args['locale']);
+    $q->qor($d);
     if (!$q->run()) return $args['id'];
+    $q->qecho();
     $result = $q->row();
     if (empty($result)) return $args['id'];
     return $result['id']; 
+    }
 }
 ?>
