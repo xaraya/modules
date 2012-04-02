b4aca8e9dbf7005ef6c3ba46ada62707f64be5cb
mfl <mfl@netspan.ch>
2012-04-02 08:52:16 +0000

    work on modify function for users

--- xaruser/modify.php
+++ xaruser/modify.php
@@ -6,8 +6,9 @@
  * @subpackage publications module
  * @category Third Party Xaraya Module
  * @version 2.0.0
+ * @copyright (C) 2011 Netspan AG
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @author mikespub
+ * @author Marc Lutolf <mfl@netspan.ch>
  */
 /**
  * modify publication
@@ -20,10 +21,12 @@ sys::import('modules.dynamicdata.class.objects.master');
 
 function publications_user_modify($args)
 {
+    if (!xarSecurityCheck('UserEditPublications')) return;
+
     extract($args);
 
     // Get parameters
-    if (!xarVarFetch('itemid',     'isset', $id, NULL, XARVAR_DONT_SET)) {return;}
+    if (!xarVarFetch('itemid',     'isset', $data['itemid'], NULL, XARVAR_DONT_SET)) {return;}
     if (!xarVarFetch('ptid',       'isset', $ptid, NULL, XARVAR_DONT_SET)) {return;}
     if (!xarVarFetch('returnurl',  'str:1', $data['returnurl'], 'view', XARVAR_NOT_REQUIRED)) {return;}
     if (!xarVarFetch('name',       'str:1', $name, '', XARVAR_NOT_REQUIRED)) {return;}
@@ -41,21 +44,35 @@ function publications_user_modify($args)
 
     // Get our object
     $data['object'] = DataObjectMaster::getObject(array('name' => $name));
-    $data['object']->getItem(array('itemid' => $id));
+    $data['object']->getItem(array('itemid' => $data['itemid']));
     $data['ptid'] = $data['object']->properties['itemtype']->value;
 
+    // Send the publication type and the object properties to the template 
+    $data['properties'] = $data['object']->getProperties();
+    
+    // Get the settings of the publication type we are using
+    $data['settings'] = xarModAPIFunc('publications','user','getsettings',array('ptid' => $data['ptid']));
+    
     // If creating a new translation get an empty copy
     if ($data['tab'] == 'newtranslation') {
-        $data['object']->properties['parent']->setValue($id);
+        $data['object']->properties['id']->setValue(0);
+        $data['object']->properties['parent']->setValue($data['itemid']);
         $data['items'][0] = $data['object']->getFieldValues(array(),1);
         $data['tab'] = '';
     } else {
         $data['items'] = array();
     }
 
-    // Get the base document
-    $data['object']->getItem(array('itemid' => $id));
-    $data['items'][$id] = $data['object']->getFieldValues(array(),1);
+    // Get the base document. If this itemid is not the base doc,
+    // then first find the correct itemid
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+    $fieldvalues = $data['object']->getFieldValues(array(),1);
+    if (!empty($fieldvalues['parent'])) {
+        $data['itemid'] = $fieldvalues['parent'];
+        $data['object']->getItem(array('itemid' => $data['itemid']));
+        $fieldvalues = $data['object']->getFieldValues(array(),1);
+    }
+    $data['items'][$data['itemid']] = $fieldvalues;
 
     // Get any translations of the base document
     $data['objectlist'] = DataObjectMaster::getObjectList(array('name' => $name));
