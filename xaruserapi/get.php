be8cf5edcca0c1370bce4cac8173e8449af51aca
mfl <mfl@netspan.ch>
2012-08-09 13:53:51 +0000

    cater to URL parameters passed

--- xaruserapi/get.php
+++ xaruserapi/get.php
@@ -23,33 +23,24 @@
 function scheduler_userapi_get($args)
 {
     extract($args);
-    $invalid = array();
-    if (empty($args['itemid']) || !is_numeric($args['itemid'])) {
-        throw new Exception(xarML('No itemid passed'));
+    if ((empty($itemid) || !is_numeric($itemid)) && (empty($module) || !is_string($module)) && (empty($type) || !is_string($type)) && (empty($func) || !is_string($func))) {
+        throw new Exception(xarML('No itemid or URL parameters passed'));
     }
-    /* Just focus on itemid for now
-        if (empty($module) || !is_string($module)) {
-            $invalid[] = 'module';
-        }
-        // CHECKME: why can't we use type instead of functype here?
-        if ((empty($type) || !is_string($type)) && (empty($type) || !is_string($type))) {
-            $invalid[] = 'type';
-        }
-        if (empty($function) || !is_string($function)) {
-            $invalid[] = 'function';
-        }
-        if (count($invalid) > 0) {
-            $msg = xarML('Invalid #(1) for #(2) function #(3)() in module #(4)',
-                         join(', ', $invalid), 'user', 'get', 'scheduler');
-            throw new BadParameterException($msg);
-        }
-
-    */
     
     sys::import('modules.dynamicdata.class.objects.master');
-    $object = DataObjectMaster::getObject(array('name' => 'scheduler_jobs'));
-    $object->getItem(array('itemid' => $args['itemid']));
-    $job = $object->getFieldValues();
+    if (!empty($itemid)) {
+        $object = DataObjectMaster::getObject(array('name' => 'scheduler_jobs'));
+        $object->getItem(array('itemid' => $args['itemid']));
+        $job = $object->getFieldValues();
+    } else {
+        $object = DataObjectMaster::getObjectList(array('name' => 'scheduler_jobs'));
+        $object->dataquery->eq('module', $module);
+        $object->dataquery->eq('type', $type);
+        $object->dataquery->eq('function', $func);
+        $items = $object->getItems();
+        if (empty($items)) return $items;
+        $job = current($items);
+    }
 
     return $job;
 }
