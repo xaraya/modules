8fcdc365ea8b0a774d09fa82d88724193cc50f00
mfl <mfl@netspan.ch>
2013-03-07 22:11:28 +0000

    add missing file

---
+++ xaradmin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Publications
+ *
+ * @package modules
+ * @subpackage publications module
+ * @category Third Party Xaraya Module
+ * @version 1.0.0
+ * @copyright (C) 2012 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+
+    define('PUBLICATIONS_STATE_DELETED',0);
+    define('PUBLICATIONS_STATE_INACTIVE',1);
+    define('PUBLICATIONS_STATE_SUBMITTED',1);
+    define('PUBLICATIONS_STATE_DRAFT',2);
+    define('PUBLICATIONS_STATE_ONHOLD',2);
+    define('PUBLICATIONS_STATE_ACTIVE',3);
+    define('PUBLICATIONS_STATE_APPROVED',3);
+    define('PUBLICATIONS_STATE_FRONTPAGE',4);
+    define('PUBLICATIONS_STATE_CHECKEDOUT',4);
+    define('PUBLICATIONS_STATE_PLACEHOLDER',5);
+
+?>
