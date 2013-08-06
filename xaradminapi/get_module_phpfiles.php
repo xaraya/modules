5fe538dfa6dca51c7cc391780cd23c7d72c770ef
Marc Lutolf <mfl@netspan.ch>
2013-08-06 15:22:21 +0200

    Update to latest local

--- xaradminapi/get_module_phpfiles.php
+++ xaradminapi/get_module_phpfiles.php
@@ -26,13 +26,8 @@ function translations_adminapi_get_module_phpfiles($args)
     if (file_exists(sys::code() . "modules/$moddir")) {
         $dd = opendir(sys::code() . "modules/$moddir");
         while ($filename = readdir($dd)) {
-//            if (is_dir(sys::code() . "modules/$moddir/$filename") && (substr($filename,0,3) == "xar")) {
-//                $names[] = mb_ereg_replace("^xar","",$filename);
-//                continue;
-//            }
             if (!preg_match('!^([a-z\-_]+)\.php$!i', $filename, $matches)) continue;
             $phpname = $matches[1];
-//            if ($phpname == 'xarversion') continue;
             if ($phpname == 'xartables') continue;
             $names[] = mb_ereg_replace("^xar","",$phpname);
         }
