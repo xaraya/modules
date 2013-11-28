3b2a8fe5fcfe068609f04f7989f1f06247abcb88
paramss <netspan@paramss.com>
2013-11-28 20:06:20 +0530

    EAV module further changes on date 28 Nov

---
+++ xaradmin/display_attribute.php
@@ -0,0 +1,18 @@
+<?php
+
+sys::import('modules.dynamicdata.class.objects.master');
+
+function eav_admin_display_attribute($args)
+{
+	if (!xarVarFetch('id' , 'int',    $data['itemid'] , 0 , XARVAR_NOT_REQUIRED)) return;
+
+    $data['object'] = DataObjectMaster::getObject(array('name' => 'eav_attributes_def'));
+    if (!isset($data['object'])) return;
+    //if (!$data['object']->checkAccess('display'))
+    //return xarResponse::Forbidden(xarML('Display #(1) is forbidden', $data['object']->label));
+
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+    
+    return $data;
+}
+?>
---
+++ xaradmin/display_entity.php
@@ -0,0 +1,19 @@
+<?php
+
+sys::import('modules.dynamicdata.class.objects.master');
+
+function eav_admin_display_entity($args)
+{
+	if (!xarVarFetch('id' , 'int',    $data['itemid'] , 0 , XARVAR_NOT_REQUIRED)) return;
+    
+    $data['object'] = DataObjectMaster::getObject(array('name' => 'eav_entities'));
+    if (!isset($data['object'])) return;
+    //if (!$data['object']->checkAccess('display'))
+    //return xarResponse::Forbidden(xarML('Display #(1) is forbidden', $data['object']->label));
+
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+    
+    return $data;
+}
+
+?>
