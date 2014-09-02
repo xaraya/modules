b0b32eee9e580993af8c78885998399745a14a19
Marc Lutolf <mfl@netspan.ch>
2014-09-02 15:20:38 +0200

    Minor fix

--- class/entity.php
+++ class/entity.php
@@ -103,6 +103,7 @@ class Entity extends DataObject
             $valuefield = 'value_' . $property->basetype;
             $q->addfield($valuefield, $property->value);
             $q->eq('attribute_id', (int)$property->id);
+            if (!$q->run()) return false;
             if (!$q->affected()) {
                 $q = new Query('INSERT', $tables['eav_data']);
                 $q->addfield('object_id', $this->parent_id);
