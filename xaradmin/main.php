7d2c6ab36e43065cb003c23fe22ab9f332e82634
Marc Lutolf <mfl@netspan.ch>
2014-11-11 15:56:41 +0100

    Another minor update

--- xaradmin/main.php
+++ xaradmin/main.php
@@ -24,13 +24,20 @@
 function keywords_admin_main()
 {
     // Security Check
-    if (!xarSecurityCheck('AdminKeywords')) return;
+    if (!xarSecurityCheck('EditKeywords')) return;
 
-    if (xarModVars::get('modules', 'disableoverview') == 0) {
+    if (xarModVars::get('modules', 'disableoverview') == 0){
         return array();
     } else {
-        xarController::redirect(xarModURL('keywords', 'admin', 'view'));
+        $redirect = xarModVars::get('keywords','defaultbackpage');
+        if (!empty($redirect)) {
+            $truecurrenturl = xarServer::getCurrentURL(array(), false);
+            $urldata = xarMod::apiFunc('roles','user','parseuserhome',array('url'=> $redirect,'truecurrenturl'=>$truecurrenturl));
+            xarController::redirect($urldata['redirecturl']);
+        } else {
+            xarController::redirect(xarModURL('keywords', 'admin', 'view'));
+        }
     }
-    return array();
+    return true;
 }
 ?>
