b5d699a19e87c7dd82fdb97df4a3a3e0afe35f8b
paramss <git@paramss.com>
2015-07-14 12:23:39 +0530

    Removed 'if invalid' condition

--- xaruser/submit_form.php
+++ xaruser/submit_form.php
@@ -19,25 +19,14 @@ function pubsub_user_submit_form($args) {
 	
 	// Get the object we are working with
 	$data['object'] = DataObjectMaster::getObject(array('name' => $name));
-	$data['object']->setFieldValues($default_values, 1);
-
-	/*
-	// Argument check
-	$invalid = array();
-	if (!isset($email) || !is_string($email)) $invalid[] = 'email';
-	if (!is_numeric($event_id)) $invalid[] = 'event_id';
-	if (!is_numeric($userid)) $invalid[] = 'userid';
-	//if (!is_numeric($action_id)) $invalid[] = 'action_id';
-	*/
+	$data['object']->setFieldValues($default_values);
 	
-	if (!empty($invalid)) {
-		return xarTpl::module('base','user','test', $data);
-	} else {		
-		// Good data: create the item
-		$itemid = $data['object']->createItem();
-		// Jump to the next page
-       	xarController::redirect(xarModURL('base','user','test'));
-	}
+		
+	// Good data: create the item
+	$itemid = $data['object']->createItem();
+	// Jump to the next page
+       	xarController::redirect(xarModURL('base','user','main', array('page'=>'test')));
+
 	return $data;
 }
 
