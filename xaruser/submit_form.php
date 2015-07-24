147675a4a6a539c36152e0c795c1d8f450f9be76
paramss <git@paramss.com>
2015-07-24 17:45:33 +0530

    Add new emails only

--- xaruser/submit_form.php
+++ xaruser/submit_form.php
@@ -2,11 +2,9 @@
 sys::import('modules.dynamicdata.class.objects.master');
 function pubsub_user_submit_form($args) {
 	extract($args);
-	
 	if (!xarVarFetch('name',        'str',    $name,            'pubsub_subscriptions', XARVAR_NOT_REQUIRED)) return;
 	if (!xarVarFetch('userid',      'int',    $userid,       0, XARVAR_NOT_REQUIRED)) return;
 	if (!xarVarFetch('event_id',    'int',    $event_id,       0, XARVAR_NOT_REQUIRED)) return;
-	//if (!xarVarFetch('action_id',   'int',    $action_id,       0, XARVAR_NOT_REQUIRED)) return;
 	if (!xarVarFetch('email',       'email', $email,'',XARVAR_NOT_REQUIRED)) return;
 	if (!xarVarFetch('returnurl', 'str',$returnurl,FALSE)) return;
 	
@@ -16,22 +14,33 @@ function pubsub_user_submit_form($args) {
 	    'user_id' => $userid,
 	    'email'   => $email,
 	);
+	
 	if(!empty($email)) {
-		// Get the object we are working with
-		$data['object'] = DataObjectMaster::getObject(array('name' => $name));
-		$data['object']->setFieldValues($default_values,1);
-				
-		// Good data: create the item
-		$itemid = $data['object']->createItem();
-	
-	    	// If this is an AJAX call, end here
-	    	xarController::$request->exitAjax();
-	
-		// Jump to the next page
-	    	//xarController::redirect(xarServer::getCurrentURL());
-	    	xarController::redirect($returnurl);
+		//check if email already available
+		sys::import('xaraya.structures.query');
+		$tables =& xarDB::getTables();
+		$q = new Query();
+		$q->addtable($tables['pubsub_subscriptions'], 'ps');
+		$q->eq('ps.email', $default_values['email']);
+		$q->run();
+		$result  = $q->output();
+
+		if(empty($result)){
+			$data['object'] = DataObjectMaster::getObject(array('name' => $name));
+			$data['object']->setFieldValues($default_values,1);
+			
+			// Good data: create the item
+			$itemid = $data['object']->createItem();
+			
+			// If this is an AJAX call, end here
+			xarController::$request->exitAjax();
+			
+			// Jump to the next page
+			xarController::redirect(xarServer::getCurrentURL());
+		} else {
+			die('Email Already Available!');
+		}
 	}
 	return true;
 }
-
 ?>
