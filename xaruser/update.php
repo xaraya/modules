81544c64e2a57a31f14206aa9ef0a15aa8b55f00
paramss <git@paramss.com>
2015-10-15 20:24:06 +0530

    redirecting to returnurl

--- xaruser/update.php
+++ xaruser/update.php
@@ -140,8 +140,12 @@ function publications_user_update()
         xarController::redirect(xarModURL('publications', 'user', 'display',
                                       array('name' => $pubtypeobject->properties['name']->value, 'itemid' => $data['itemid'])));
     } else {
-        xarController::redirect(xarModURL('publications', 'user', 'modify',
-                                      array('name' => $pubtypeobject->properties['name']->value, 'itemid' => $data['itemid'])));
+    	if (!empty($return_url)) {
+    		xarController::redirect($data['returnurl']);
+    	} else {
+    		xarController::redirect(xarModURL('publications', 'user', 'modify',
+                                   array('name' => $pubtypeobject->properties['name']->value, 'itemid' => $data['itemid'])));
+    	}
         return true;
     }
 }
