08b6d38b9d521bee9417c7c2648907f5a0f85d6d
Marc Lutolf <mfl@netspan.ch>
2015-11-08 10:11:33 +0100

    More of same
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

---
+++ xaruser/modify_extension.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Modify an ID
+ *
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Release Module
+ * @link http://xaraya.com/index.php/release/773.html
+ */
+/**
+ * Modify an ID by user
+ *
+ * @param $rid
+ *
+ * Original Author of file: John Cox via phpMailer Team
+ * @author Release module development team
+ */
+function release_user_modify_extension($args)
+{
+    if (!xarSecurityCheck('EditRelease')) return;
+
+    if (!xarVarFetch('name',       'str',      $name,            'release_extensions', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('itemid' ,    'int',      $data['itemid'] , 0 ,          XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',    'bool',     $data['confirm'], false,       XARVAR_NOT_REQUIRED)) return;
+
+    sys::import('modules.dynamicdata.class.objects.master');
+    $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+
+    $data['tplmodule'] = 'release';
+
+    if ($data['confirm']) {
+    
+        // Check for a valid confirmation key
+        if(!xarSecConfirmAuthKey()) return;
+
+        // Get the data from the form
+        $isvalid = $data['object']->checkInput();
+        
+        if (!$isvalid) {
+            // Bad data: redisplay the form with error messages
+            return xarTplModule('release','user','modify_extension', $data);        
+        } else {
+            // Good data: create the item
+            $itemid = $data['object']->updateItem(array('itemid' => $data['itemid']));
+            
+            // Jump to the next page
+            xarController::redirect(xarModURL('release','user','view_extensions'));
+            return true;
+        }
+    }
+    return $data;
+}
+?>
