31438911586cd22a1f94ac60376a671488d38ce5
Marc Lutolf <mfl@netspan.ch>
2015-11-10 23:24:47 +0100

    Add release support
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

---
+++ xaradmin/delete_release.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Delete an id
+ *
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Release Module
+ * @link http://xaraya.com/index.php/release/773.html
+ */
+/**
+ * Delete an ID
+ * 
+ * @param $rid ID
+ * 
+ * Original Author of file: John Cox via phpMailer Team
+ * @author Release module development team
+ */
+function release_admin_delete_release($args)
+{
+    if (!xarSecurityCheck('ManageRelease')) return;
+
+    if (!xarVarFetch('name',       'str:1',     $name,            'release_notes',     XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('itemid' ,    'int',       $data['itemid'] , '' ,          XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',    'checkbox',  $data['confirm'], false,     XARVAR_NOT_REQUIRED)) return;
+
+    sys::import('modules.dynamicdata.class.objects.master');
+    $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+
+    $data['tplmodule'] = 'release';
+    $data['authid'] = xarSecGenAuthKey('release');
+
+    if ($data['confirm']) {
+    
+        // Check for a valid confirmation key
+        if(!xarSecConfirmAuthKey()) return;
+
+        // Delete the item
+        $item = $data['object']->deleteItem();
+            
+        // Jump to the next page
+        xarController::redirect(xarModURL('release','admin','view_releases'));
+        return true;
+    }
+    return $data;
+}
+?>
