ef8ca15784d3923cf8383b0e54bda61158bf94a1
Marc Lutolf <mfl@netspan.ch>
2015-11-11 09:45:21 +0100

    More of same
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

---
+++ xaradmin/new_release.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Add a new release
+ *
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Release Module
+ * @link http://xaraya.com/index.php/release/773.html
+ */
+/**
+ * Add an extension and request an ID
+ *
+ * @param enum phase Phase we are at
+ *
+ * @return array
+ * @author Release module development team
+ */
+function release_admin_new_release($args)
+{
+    if (!xarSecurityCheck('AddRelease')) return;
+
+    if (!xarVarFetch('name',       'str',    $name,            'release_notes', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,     XARVAR_NOT_REQUIRED)) return;
+
+    sys::import('modules.dynamicdata.class.objects.master');
+    $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+    $data['tplmodule'] = 'release';
+
+    if ($data['confirm']) {
+    
+        // we only retrieve 'preview' from the input here - the rest is handled by checkInput()
+        if(!xarVarFetch('preview', 'str', $preview,  NULL, XARVAR_DONT_SET)) {return;}
+
+        // Check for a valid confirmation key
+        if(!xarSecConfirmAuthKey()) return;
+        
+        // Get the data from the form
+        $isvalid = $data['object']->checkInput();
+        
+        if (!$isvalid) {
+            // Bad data: redisplay the form with error messages
+            return xarTplModule('release','admin','new_release', $data);        
+        } else {
+            // Good data: create the item
+            $itemid = $data['object']->createItem();
+            
+            // Jump to the next page
+            xarController::redirect(xarModURL('release','admin','view_releases'));
+            return true;
+        }
+    }
+    return $data;
+}
+?>
