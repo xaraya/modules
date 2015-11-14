f80641374172ec61fc300a225539e5467025e382
Marc Lutolf <mfl@netspan.ch>
2015-11-14 11:06:24 +0100

    More of same
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

---
+++ xaruser/delete_documentation.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Delete a release documentation
+ *
+ * @package modules
+ * @subpackage release
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @link http://xaraya.com/index.php/release/773.html
+ */
+/**
+ * Delete a release documentation
+ * 
+ * @param $rid ID
+ * 
+ * Original Author of file: John Cox via phpMailer Team
+ * @author Release module development team
+ */
+function release_user_delete_documentation($args)
+{
+    if (!xarSecurityCheck('ManageRelease')) return;
+
+    if (!xarVarFetch('name',       'str:1',     $name,            'release_docs',     XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('itemid' ,    'int',       $data['itemid'] , '' ,          XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',    'checkbox',  $data['confirm'], false,     XARVAR_NOT_REQUIRED)) return;
+
+    sys::import('modules.dynamicdata.class.objects.master');
+    $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+
+    $data['tplmodule'] = 'release';
+    $data['authid'] = xarSecGenAuthKey('release');
+
+    if ($data['confirm']) {
+    
+        // Check for a valid confirmation key
+        if(!xarSecConfirmAuthKey()) return;
+
+        // Delete the item
+        $item = $data['object']->deleteItem();
+            
+        // Jump to the next page
+        xarController::redirect(xarModURL('release','user','view_documentation'));
+        return true;
+    }
+    return $data;
+}
+?>
---
+++ xaruser/view_documentation.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * View all release documentation
+ *
+ * @package modules
+ * @subpackage release
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @link http://xaraya.com/index.php/release/773.html
+ */
+function release_user_view_documentation()
+{
+    if (!xarSecurityCheck('EditRelease')) return;
+
+    // Get the object to be listed
+    $data['object'] = DataObjectMaster::getObjectList(array('name' => 'release_documentation'));
+
+    return $data;
+}
+
+?>
