e01f9788ca5ff2d3b20af9a722aa89f41454e132
Marc Lutolf <mfl@netspan.ch>
2015-11-14 10:55:14 +0100

    More of same
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

---
+++ xaradmin/display_documentation.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Release Module
+ *
+ * @package modules
+ * @subpackage release
+ * @category Third Party Xaraya Module
+ * @version 1.0.0
+ * @copyright (C) 2014 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+/**
+ * Display an item of the release documentation object
+ *
+ */
+    sys::import('modules.dynamicdata.class.objects.master');
+    
+    function release_admin_display_extension()
+    {
+        if (!xarSecurityCheck('ReadRelease')) return;
+
+        if (!xarVarFetch('name',       'str',    $name,            'release_docs', XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('itemid' ,    'int',    $data['itemid'] , 0 ,          XARVAR_NOT_REQUIRED)) return;
+
+        $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+        $data['object']->getItem(array('itemid' => $data['itemid']));
+
+        $data['tplmodule'] = 'release';
+
+        return $data;
+    }
+?>
---
+++ xaradmin/modify_documentation.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Modify a release documentation
+ *
+ * @package modules
+ * @subpackage release
+ * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @link http://xaraya.com/index.php/release/773.html
+ */
+/**
+ * Modify a release documentation by user
+ *
+ * @param $rid
+ *
+ * Original Author of file: John Cox via phpMailer Team
+ * @author Release module development team
+ */
+function release_admin_modify_documentation($args)
+{
+    if (!xarSecurityCheck('EditRelease')) return;
+
+    if (!xarVarFetch('name',       'str',      $name,            'release_docs', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('itemid' ,    'int',      $data['itemid'] , 0 ,          XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',    'bool',     $data['confirm'], false,       XARVAR_NOT_REQUIRED)) return;
+
+    sys::import('modules.dynamicdata.class.objects.master');
+    $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+
+    $data['tplmodule'] = 'release';
+
+    if ($data['confirm']) {
+    
+        // Check for a valid confirmation key
+        if(!xarSecConfirmAuthKey()) return;
+
+        // Get the data from the form
+        $isvalid = $data['object']->checkInput();
+        
+        if (!$isvalid) {
+            // Bad data: redisplay the form with error messages
+            return xarTplModule('release','admin','modify_documentation', $data);        
+        } else {
+            // Good data: create the item
+            $itemid = $data['object']->updateItem(array('itemid' => $data['itemid']));
+            
+            // Jump to the next page
+            xarController::redirect(xarModURL('release','admin','view_documentation'));
+            return true;
+        }
+    }
+    return $data;
+}
+?>
