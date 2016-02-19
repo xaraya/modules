116c4da824e7fd45cf15aee674356ff39ba7e467
Marc Lutolf <mfl@netspan.ch>
2016-02-19 20:05:35 +0100

    More of same
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

--- class/dta_ta827.php
+++ class/dta_ta827.php
@@ -42,38 +42,6 @@ class DTA_TA827 extends DTA{
         $this->paymentType = $paymentType;
     }
 
-    public function setPaymentAmount($amount, $currencyCode, $valuta = NULL) 
-    {
-        $paymentAmount = '';
-
-        // Check the value date
-        if ($valuta == NULL)
-            $valuta = '      ';
-        else {
-            $valuta = $this->transformDate($valuta);
-            if (!is_numeric($valuta) || (strlen($valuta) != 6 ))
-                throw new Exception(xarML("The value date must have the format DDMMYY: #(1)", $valuta));
-        }
-
-        // Check the amount
-        if (!((is_float($amount)) || (is_integer($amount))))
-            throw new Exception(xarML("The amount is not numeric: #(1)"), $amount);
-        else {
-            $this->paymentAmountNumeric = $amount;
-            $amount = str_pad(number_format($amount, 2, ',', ''), 12, $this->fillChar);
-        }
-
-        // Check the currency code
-        if (!strlen($currencyCode) == 3 )
-            throw new Exception(xarML("Invalid currency code"));
-
-        $paymentAmount = $valuta . $currencyCode . $amount;
-        if (strlen($paymentAmount) != (6 + 3 + 12 ))
-            throw new Exception(xarML("Invalid amount: #(1)", $paymentAmount));
-        else
-            $this->paymentAmount = $paymentAmount;
-    }
-
     protected function getConversionRate() 
     {
         return '';
