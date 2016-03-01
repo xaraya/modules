56af15ae9e409f4c7abd85e0cfa574b251056eb2
Marc Lutolf <mfl@netspan.ch>
2016-03-01 19:51:09 +0100

    dta_type --> payment_type
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

--- xaruser/new_dta.php
+++ xaruser/new_dta.php
@@ -19,13 +19,13 @@ function payments_user_new_dta()
 {
     if (!xarSecurityCheck('AddPayments')) return;
 
-    if (!xarVarFetch('name',       'str',    $name,            'payments_dta', XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,     XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('dta_type',   'str',    $data['dta_type'],'827',     XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('name',         'str',    $name,            'payments_dta', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',      'bool',   $data['confirm'], false,     XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('payment_type', 'str',    $data['payment_type'],'827',     XARVAR_NOT_REQUIRED)) return;
 
     sys::import('modules.dynamicdata.class.objects.master');
     $data['object'] = DataObjectMaster::getObject(array('name' => $name));
-    $data['object']->properties['dta_type']->setValue($data['dta_type']);
+    $data['object']->properties['payment_type']->setValue($data['payment_type']);
     $data['tplmodule'] = 'payments';
     $data['authid'] = xarSecGenAuthKey('payments');
 
