076fdfa377cc7cdd136d0ad49836d16220db72f5
Marc Lutolf <mfl@netspan.ch>
2016-03-05 17:36:40 +0100

    More
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

--- xaruser/new_debit_account.php
+++ xaruser/new_debit_account.php
@@ -19,14 +19,40 @@ function payments_user_new_debit_account()
 {
     if (!xarSecurityCheck('AddPayments')) return;
 
-    if (!xarVarFetch('name',       'str',    $name,            'payments_debit_account', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,     XARVAR_NOT_REQUIRED)) return;
+
+# --------------------------------------------------------
+#
+# Get the payment transactions object
+#
+    if (!xarVarFetch('name',       'str',    $name,            'payments_debit_account', XARVAR_NOT_REQUIRED)) return;
 
     sys::import('modules.dynamicdata.class.objects.master');
     $data['object'] = DataObjectMaster::getObject(array('name' => $name));
     $data['tplmodule'] = 'payments';
     $data['authid'] = xarSecGenAuthKey('payments');
 
+# --------------------------------------------------------
+#
+# Check if we are passing an object item
+#
+    if (!xarVarFetch('obj',         'str',    $object,            '', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('itemid',      'int',    $itemid,            '', XARVAR_NOT_REQUIRED)) return;
+    
+    $sourceobject = DataObjectMaster::getObject(array('name' => $object));
+    $sourceobject->getItem(array('itemid' => $itemid));
+    
+    // If we have data, transfer it to the new object
+    $sourcefields = $sourceobject->getFieldValues(array(), 1);
+    if (!empty($sourcefields)) {
+        if (isset($sourcefields['name'])) $data['object']->properties['name']->value = $sourcefields['name'];
+        if (isset($sourcefields['name'])) $data['object']->properties['account_holder']->value = $sourcefields['account_holder'];
+    }
+
+# --------------------------------------------------------
+#
+# The create button was clicked
+#
     if ($data['confirm']) {
     
         // we only retrieve 'preview' from the input here - the rest is handled by checkInput()
