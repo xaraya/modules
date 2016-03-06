a14e7f6cda1e31831140f99b6c952734c3561c2d
Marc Lutolf <mfl@netspan.ch>
2016-03-06 19:37:31 +0100

    More
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

--- xaruser/modify_transaction.php
+++ xaruser/modify_transaction.php
@@ -23,24 +23,56 @@ function payments_user_modify_transaction()
     if (!xarVarFetch('itemid' ,    'int',      $data['itemid'] , 0 ,          XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('confirm',    'checkbox', $data['confirm'], false,       XARVAR_NOT_REQUIRED)) return;
 
+# --------------------------------------------------------
+#
+# Get the payment transactions object
+#
     sys::import('modules.dynamicdata.class.objects.master');
     $data['object'] = DataObjectMaster::getObject(array('name' => $name));
     $data['object']->getItem(array('itemid' => $data['itemid']));
-
     $data['tplmodule'] = 'payments';
     $data['authid'] = xarSecGenAuthKey('payments');
 
-    // Get the debit account information
-    $data['debit_account'] = DataObjectMaster::getObject(array('name' => 'payments_debit_account'));
-    $data['debit_account']->getItem(array('itemid' => 1));
-    $debit_fields = $data['debit_account']->getFieldValues(array(), 1);
+# --------------------------------------------------------
+#
+# Check if we are passing an api item
+#
+    if (!xarVarFetch('api',          'str',    $api,            '', XARVAR_NOT_REQUIRED)) return;
+    
+    if (!empty($api)) {
+        $function = rawurldecode($api);
+        eval("\$info = $function;");
+        
+        foreach ($info as $key => $value) {
+            if (isset($data['object']->properties[$key]))
+                $data['object']->properties[$key]->value = $value;
+        }
+    }
 
-    $data['object']->properties['sender_reference']->value = $debit_fields['account_holder'];
-    $data['object']->properties['sender_line_1']->value = $debit_fields['address_1'];
-    $data['object']->properties['sender_line_2']->value = $debit_fields['address_2'];
-    $data['object']->properties['sender_line_3']->value = $debit_fields['address_3'];
-    $data['object']->properties['sender_line_4']->value = $debit_fields['address_4'];
+# --------------------------------------------------------
+#
+# Get the debit account information
+#
+    $data['debit_account'] = DataObjectMaster::getObjectList(array('name' => 'payments_debit_account'));
+    $q = $data['debit_account']->dataquery;
+    $q->eq('sender_object', $info['sender_object']);
+    $q->eq('sender_itemid', $info['sender_itemid']);
+    $items = $data['debit_account']->getItems();
 
+    if(!empty($items)) {
+        $item = current($items);
+        $data['object']->properties['sender_account']->value = $item['account_holder'];
+        $data['object']->properties['sender_line_1']->value  = $item['address_1'];
+        $data['object']->properties['sender_line_2']->value  = $item['address_2'];
+        $data['object']->properties['sender_line_3']->value  = $item['address_3'];
+        $data['object']->properties['sender_line_4']->value  = $item['address_4'];
+    }
+    
+
+# --------------------------------------------------------
+#
+# The create button was clicked
+#
     if ($data['confirm']) {
     
         // Check for a valid confirmation key
