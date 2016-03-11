5d7b6a4cbfc72181c12d502aeaddafeb340eef4c
Marc Lutolf <mfl@netspan.ch>
2016-03-11 12:18:53 +0100

    More
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

--- xaruser/new_transaction.php
+++ xaruser/new_transaction.php
@@ -129,6 +129,7 @@ function payments_user_new_transaction()
     // If we have no previous payment, chekc if a paymnt type was passed
     if (($previous_exists == false) && isset($info['payment_type'])) {
         $data['object']->properties['payment_type']->value = $info['payment_type'];
+        $data['payment_type'] = $info['payment_type'];
     }
 
 # --------------------------------------------------------
