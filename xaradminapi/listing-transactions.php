22e3aacec2c8a8b3a06c111e8aa06d5d9ed5873a
Marc Lutolf <mfl@netspan.ch>
2016-06-10 16:47:11 +0200

    Focus on the transactions listing for bulk operations
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

---
+++ xaradminapi/listing-transactions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Payments Module
+ *
+ * @package modules
+ * @subpackage payments
+ * @category Third Party Xaraya Module
+ * @version 1.0.0
+ * @copyright (C) 2016 Luetolf-Carroll GmbH
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <marc@luetolf-carroll.com>
+ */
+/**
+ * Handle calls from the listing property
+ *
+ */
+
+function payments_adminapi_listing-transactions($args)
+{
+    die("!!!");
+}
+?>
