705ff8f541e42e2da7ced0b257081132dc396809
Marc Lutolf <mfl@netspan.ch>
2017-01-09 19:27:35 +0100

    Add modules to map

--- xaradmin/modifyconfig.php
+++ xaradmin/modifyconfig.php
@@ -14,77 +14,83 @@
  *
  */
 
-    function sitemapper_admin_modifyconfig()
-    {
-        // Security Check
-        if (!xarSecurityCheck('AdminSitemapper')) return;
-        if (!xarVarFetch('phase', 'str:1:100', $phase, 'modify', XARVAR_NOT_REQUIRED, XARVAR_PREP_FOR_DISPLAY)) return;
-        if (!xarVarFetch('tab', 'str:1:100', $data['tab'], 'general', XARVAR_NOT_REQUIRED)) return;
+function sitemapper_admin_modifyconfig()
+{
+    // Security Check
+    if (!xarSecurityCheck('AdminSitemapper')) return;
+    if (!xarVarFetch('phase', 'str:1:100', $phase, 'modify', XARVAR_NOT_REQUIRED, XARVAR_PREP_FOR_DISPLAY)) return;
+    if (!xarVarFetch('tab', 'str:1:100', $data['tab'], 'general', XARVAR_NOT_REQUIRED)) return;
 
-        $data['module_settings'] = xarMod::apiFunc('base','admin','getmodulesettings',array('module' => 'sitemapper'));
-        $data['module_settings']->setFieldList('items_per_page,, use_module_alias, use_module_icons');
-        $data['module_settings']->getItem();
+    $data['module_settings'] = xarMod::apiFunc('base','admin','getmodulesettings',array('module' => 'sitemapper'));
+    $data['module_settings']->setFieldList('items_per_page,, use_module_alias, use_module_icons');
+    $data['module_settings']->getItem();
 
-        sys::import('modules.dynamicdata.class.objects.master');
-        $engines = DataObjectMaster::getObjectList(array('name' => 'sitemapper_engines'));
-        $data['engines'] = $engines->getItems(array('where' => 'state = 3'));
-        
-        switch (strtolower($phase)) {
-            case 'modify':
-            default:
-                switch ($data['tab']) {
-                    case 'general':
-                        break;
-                    case 'tab2':
-                        break;
-                    case 'tab3':
-                        break;
-                    default:
-                        break;
-                }
+    sys::import('modules.dynamicdata.class.objects.master');
+    $engines = DataObjectMaster::getObjectList(array('name' => 'sitemapper_engines'));
+    $data['engines'] = $engines->getItems(array('where' => 'state = 3'));
+    $data['modules_available'] = xarMod::apiFunc('modules', 'admin', 'getitems');
+    
+    switch (strtolower($phase)) {
+        case 'modify':
+        default:
+            switch ($data['tab']) {
+                case 'general':
+                    break;
+                case 'tab2':
+                    break;
+                case 'tab3':
+                    break;
+                default:
+                    break;
+            }
 
-                break;
+            break;
 
-            case 'update':
-                // Confirm authorisation code
-                if (!xarSecConfirmAuthKey()) {
-                    return xarTplModule('privileges','user','errors',array('layout' => 'bad_author'));
-                }        
-                switch ($data['tab']) {
-                    case 'general':
-                        $isvalid = $data['module_settings']->checkInput();
-                        if (!$isvalid) {
-                            return xarTplModule('dynamicdata','admin','modifyconfig', $data);        
-                        } else {
-                            $itemid = $data['module_settings']->updateItem();
-                        }
+        case 'update':
+            // Confirm authorisation code
+            if (!xarSecConfirmAuthKey()) {
+                return xarTplModule('privileges','user','errors',array('layout' => 'bad_author'));
+            }        
+            switch ($data['tab']) {
+                case 'general':
+                    $isvalid = $data['module_settings']->checkInput();
+                    if (!$isvalid) {
+                        return xarTplModule('dynamicdata','admin','modifyconfig', $data);        
+                    } else {
+                        $itemid = $data['module_settings']->updateItem();
+                    }
 
-                        if (!xarVarFetch('file_create_xml', 'int', $file_create_xml, 0, XARVAR_NOT_REQUIRED)) return;
-                        if (!xarVarFetch('file_create_zip', 'int', $file_create_zip, 0, XARVAR_NOT_REQUIRED)) return;
-                        if (!xarVarFetch('file_xml_filename', 'str:1:', $xml_filename, 0, XARVAR_NOT_REQUIRED)) return;
-                        if (!xarVarFetch('file_zip_filename', 'str:1:', $zip_filename, 0, XARVAR_NOT_REQUIRED)) return;
+                    if (!xarVarFetch('file_create_xml', 'int', $file_create_xml, 0, XARVAR_NOT_REQUIRED)) return;
+                    if (!xarVarFetch('file_create_zip', 'int', $file_create_zip, 0, XARVAR_NOT_REQUIRED)) return;
+                    if (!xarVarFetch('file_xml_filename', 'str:1:', $xml_filename, 0, XARVAR_NOT_REQUIRED)) return;
+                    if (!xarVarFetch('file_zip_filename', 'str:1:', $zip_filename, 0, XARVAR_NOT_REQUIRED)) return;
 
-                        xarModVars::set('sitemapper', 'file_create_xml', $file_create_xml);
-                        xarModVars::set('sitemapper', 'file_create_zip', $file_create_zip);
-                        xarModVars::set('sitemapper', 'xml_filename', $xml_filename);
-                        xarModVars::set('sitemapper', 'zip_filename', $zip_filename);
+                    xarModVars::set('sitemapper', 'file_create_xml', $file_create_xml);
+                    xarModVars::set('sitemapper', 'file_create_zip', $file_create_zip);
+                    xarModVars::set('sitemapper', 'xml_filename', $xml_filename);
+                    xarModVars::set('sitemapper', 'zip_filename', $zip_filename);
 
-                        sys::import('modules.dynamicdata.class.properties.master');
-                        $submit_engines = DataPropertyMaster::getProperty(array('name' => 'checkboxlist'));
-                        $submit_engines->checkInput('submit_engines');
-                        xarModVars::set('sitemapper', 'submit_engines', $submit_engines->value);
-                
-                        break;
-                    case 'tab2':
-                        break;
-                    case 'tab3':
-                        break;
-                    default:
-                        break;
-                }
-                break;
-        }
-        $data['submit_engines'] = xarModVars::get('sitemapper', 'submit_engines');
-        return $data;
+                    // Get a chekcbox list sys::import('modules.dynamicdata.class.properties.master');
+                    $checkbox_list = DataPropertyMaster::getProperty(array('name' => 'checkboxlist'));
+                    
+                    // Get the value for the engines to submit to
+                    $checkbox_list->checkInput('submit_engines');
+                    xarModVars::set('sitemapper', 'submit_engines', $checkbox_list->value);
+            
+                    // Get the value for the engines to submit to
+                    $checkbox_list->checkInput('modules_to_map');
+                    xarModVars::set('sitemapper', 'modules_to_map', $checkbox_list->value);
+                    break;
+                case 'tab2':
+                    break;
+                case 'tab3':
+                    break;
+                default:
+                    break;
+            }
+            break;
     }
+    $data['submit_engines'] = xarModVars::get('sitemapper', 'submit_engines');
+    return $data;
+}
 ?>
