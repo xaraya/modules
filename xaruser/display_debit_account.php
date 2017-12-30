940ad587e33579fbf371b35f16d32a3c28655b28
Marc Lutolf <mfl@netspan.ch>
2017-12-30 11:16:57 +0100

    Cosmetics

---
+++ xaruser/display_debit_account.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Payments Module
+ *
+ * @package modules
+ * @subpackage payments
+ * @category Third Party Xaraya Module
+ * @version 1.0.0
+ * @copyright (C) 2016 Luetolf-Carroll GmbH
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <marc@luetolf-carroll.com>
+ */
+/**
+ * Display an item of the payments_debit_account object
+ *
+ */
+    
+function payments_user_display_debit_account()
+{
+    if (!xarSecurityCheck('EditPayments')) return;
+
+    if (!xarVarFetch('name',       'str',      $name,            'payments_debit_account', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('itemid' ,    'int',      $data['itemid'] , 0 ,          XARVAR_NOT_REQUIRED)) return;
+
+    sys::import('modules.dynamicdata.class.objects.master');
+    $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+
+    $data['tplmodule'] = 'payments';
+    return $data;
+}
+?>
