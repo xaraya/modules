814f0611dd1abd39a39addc0b7afff2c59e20fb5
Marc Lutolf <mfl@netspan.ch>
2018-09-20 15:27:39 +0200

    More

---
+++ xaradmin/display_event.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Pubsub Module
+ *
+ * @package modules
+ * @subpackage pubsub module
+ * @category Third Party Xaraya Module
+ * @version 2.0.0
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://xaraya.com/index.php/release/181.html
+ * @author Pubsub Module Development Team
+ * @author Chris Dudley <miko@xaraya.com>
+ * @author Garrett Hunter <garrett@blacktower.com>
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+/**
+ * Display an event
+ *
+ */
+sys::import('modules.dynamicdata.class.objects.master');
+
+function pubsub_admin_display_event()
+{
+    // Xaraya security
+    if (!xarSecurityCheck('ManagePubSub')) return;
+    xarTpl::setPageTitle('Display Event');
+
+    if (!xarVarFetch('name',       'str',    $name,            'pubsub_events', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('itemid' ,    'int',    $data['itemid'] , 0 ,          XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,       XARVAR_NOT_REQUIRED)) return;
+
+    
+    $data['object'] = DataObjectMaster::getObject(array('name' => $name));
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+	
+    $data['tplmodule'] = 'pubsub';
+    
+    return $data;
+}
+?>
