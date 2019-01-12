b417daf5cb39b6b5e7f5fcc835c1bf4d69c84293
Marc Lutolf <mfl@netspan.ch>
2019-01-12 17:10:32 +0100

    Add a scheduler function

---
+++ xarschedulerapi/process_queue.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Pubsub Module
+ *
+ * @package modules
+ * @subpackage pubsub module
+ * @category Third Party Xaraya Module
+ * @version 2.0.0
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://xaraya.com/index.php/release/181.html
+ * @author Pubsub Module Development Team
+ * @author Marc Lutolf <marc@luetolf-carroll.com>
+ */
+/**
+ * Process the queue and run all pending jobs (executed by the scheduler module)
+ * nodigest - that is one email per event
+ * @returns bool
+ */
+
+function pubsub_schedulerapi_process_queue(Array $args=array())
+{
+
+    $result = xarMod::apiFunc('pubsub','admin','process_queue', $args);
+
+    return $result;
+}
+
+?>
