0b15d0f2eccdf36a20642e78d2b390ff17480067
Marc Lutolf <marc@luetolf-carroll.com>
2020-11-14 15:41:32 +0100

    mots

--- xaruser/remove.php
+++ xaruser/remove.php
@@ -21,6 +21,21 @@ function reminders_user_remove()
     if (!xarVarFetch('code',    'str',      $data['code'],       '', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('confirm', 'checkbox', $data['confirm'],    false, XARVAR_NOT_REQUIRED)) return;
 
+    sys::import('modules.dynamicdata.class.objects.master');
+    $entries = DataObjectMaster::getObjectList(array('name' => 'reminders_entries'));
+        
+    $q = $entries->dataquery;
+    
+    // Only active reminders
+    $q->eq('entries.state', 3);
+    // The reminder corresponding to this code
+    $q->eq('entries.code', $data['code']);
+
+    $items = $entries->getItems();
+
+    if ($data['confirm']) {
+    }
+
     $data['debugmode'] = xarModVars::get('reminders', 'debugmode');
 	xarTpl::setPageTemplateName('user_full');
 	return $data;
