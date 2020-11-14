740c76d41e1dcae5f32cb8c14cb84f8ca0bf244f
Marc Lutolf <marc@luetolf-carroll.com>
2020-11-14 13:52:21 +0100

    Add the remove page

---
+++ xaruser/remove.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Reminders Module
+ *
+ * @package modules
+ * @subpackage reminders
+ * @category Third Party Xaraya Module
+ * @version 1.0.0
+ * @copyright (C) 2019 Luetolf-Carroll GmbH
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <marc@luetolf-carroll.com>
+ */
+/**
+ * Call a generic test page
+ *
+ */
+function reminders_user_test()
+{
+    return array();
+}
+?>
