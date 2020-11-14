7da397f0d5916efb82343f575b35d0be26fe2b88
Marc Lutolf <marc@luetolf-carroll.com>
2020-11-14 21:42:17 +0100

    mots

--- xaruserapi/get_email_dates.php
+++ xaruserapi/get_email_dates.php
@@ -20,7 +20,11 @@ function reminders_userapi_get_email_dates($args)
     	// We have an array of item values ("an item") from a dataobject
     	$dates = array();
     	for ($i=1;$i<=10;$i++) {
-    		if ($args['array']['reminder_done_' . $i] == 1) $dates[] = $args['array']['reminder_' . $i];
+    		// Ignore dates with values 0
+    		if ($args['array']['reminder_' . $i] == 0) continue;
+    		// Ignore dates we have already done
+    		if ($args['array']['reminder_done_' . $i] == 1) continue;
+    		$dates[] = $args['array']['reminder_' . $i];
     	}
     } else {
     	// Need to add support for objects
