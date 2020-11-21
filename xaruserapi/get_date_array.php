ae71963e3f746d97b4957d589e07e5604309c4de
Marc Lutolf <marc@luetolf-carroll.com>
2020-11-21 12:51:45 +0100

    mots

--- xaruserapi/get_date_array.php
+++ xaruserapi/get_date_array.php
@@ -33,7 +33,7 @@ function reminders_userapi_get_date_array($args)
 		$this_step = $seconds[$this_step];
 		
 		$this_done = $fields['reminder_done_' . $i];
-		$steps[] = array('step' => now() - $this_step, 'done' => $this_done);
+		$steps[] = array('step' => $fields['due_date'] - $this_step, 'done' => $this_done);
     }
     
     // Sort the array by steps ASC; this means by age, oldest first
