8555659c3cce06a9da677c7a7c850f1af81024d6
Marc Lutolf <marc@luetolf-carroll.com>
2020-11-22 12:31:17 +0100

    mots

--- xarschedulerapi/process.php
+++ xarschedulerapi/process.php
@@ -23,14 +23,15 @@ function reminders_schedulerapi_process($args)
     if (empty($results)) $results = xarML('No reminders sent');
     
     // Make the result human readable for the scheduler
-	$readable_result = '';
-	foreach ($results as $result) {
-		foreach ($result as $key => $value) $readable_result .= $key . ": " . $value . " ";
-		$readable_result = trim($readable_result) . "<br/>";
-	}
-	// Remove the final <br/>
-	$results = substr($readable_result, 0, -5);
-
+    if (is_array($results)) {
+		$readable_result = '';
+		foreach ($results as $result) {
+			foreach ($result as $key => $value) $readable_result .= $key . ": " . $value . " ";
+			$readable_result = trim($readable_result) . "<br/>";
+		}
+		// Remove the final <br/>
+		$results = substr($readable_result, 0, -5);
+    }
     return $results;
 }
 
