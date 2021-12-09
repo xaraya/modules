8613320165932b9bbbb7c6df5baee1fdd4e3bf11
Marc Lutolf <marc@luetolf-carroll.com>
2021-12-09 20:06:12 +0100

    mots

--- xartables.php
+++ xartables.php
@@ -24,6 +24,7 @@ function reminders_xartables()
     $xartable['reminders_emails']          = xarDB::getPrefix() . '_reminders_emails';
     $xartable['reminders_entries']          = xarDB::getPrefix() . '_reminders_entries';
     $xartable['reminders_history']          = xarDB::getPrefix() . '_reminders_history';
+    $xartable['reminders_lookups']          = xarDB::getPrefix() . '_reminders_lookups';
 
     // Return the table information
     return $xartable;
