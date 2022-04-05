3d6d9f25e875422bd638751f908bdb611125c93e
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-05 16:45:11 +0200

    Work on generate_reandom_entry

--- xaradminapi/generate_random_entry.php
+++ xaradminapi/generate_random_entry.php
@@ -14,7 +14,15 @@
 
 function reminders_adminapi_generate_random_entry($args)
 {
+    // We need a user in order to filter for just one set of lookups
     if (empty($args['user'])) die(xarML('No user passed'));
+    
+    // We will disregard recipients of recent emails
+    if (empty($args['recent_lookups'])) {
+    	$recent_lookups = array();
+    } else {
+    	$recent_lookups = unserialize($args['recent_lookups']);
+    }
     
     // Get the number entries for this user in the lookup table
     $tables =& xarDB::getTables();
@@ -30,19 +38,21 @@ function reminders_adminapi_generate_random_entry($args)
     $result = $q->row();
     
     // Generate a random id
-    $random_id = rand(1, max((int)$result['highest']-1, 1));
-    
+    $random_id = rand(1, max((int)$result['highest'], 1));
+
     // Remove fields and conditions
     $q->clearfields();
     $q->clearconditions();
     
     // Only get lookups of this user
     $q->eq('owner', $args['user']);
+    // Ignore lookups we contacted recently
+    if (!empty($recent_lookups)) $q->notin('id', $recent_lookups);
 
     $q->setstartat($random_id);
     $q->setrowstodo(100);
     
-//    $q->qecho();
+    $q->qecho();
 
     // Get one row
     $q->run();
