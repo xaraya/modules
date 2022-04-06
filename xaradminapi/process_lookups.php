4abc92a258577240b59cb4c32bf5e5416c9c8a8c
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-06 18:30:42 +0200

    mots

--- xaradminapi/process_lookups.php
+++ xaradminapi/process_lookups.php
@@ -27,13 +27,10 @@ function reminders_adminapi_process_lookups($args)
     	if (!xarVarFetch('entry_list',    'str', $data['entry_list'],    '', XARVAR_NOT_REQUIRED)) return;
     } else {
     	// In the live invironment we get exactly one lookup, which will correspond to a single email we receive
-//    	$row = xarMod::apiFunc('reminders', 'admin', 'generate_random_entry', array('user' => xarUser::getVar('id')));
-//    	$data['entry_list'] = (int)$row['id'];
+    	$row = xarMod::apiFunc('reminders', 'admin', 'generate_random_entry', array('user' => xarUser::getVar('id')));
+    	$data['entry_list'] = (int)$row['id'];
     }
 
-	$owners = xarMod::apiFunc('reminders', 'user', 'getall_owners', array('do_lookup' => true)); 
-	var_dump($owners);exit;
-    
 	$items = xarMod::apiFunc('reminders', 'user', 'getall_lookups', array('itemids' => $data['entry_list']));
 	
 /*
@@ -58,41 +55,47 @@ echo "<pre>";var_dump($items);exit;
     * For each item we need to find the latest reminder that has not yet been sent
     */
     
-    $data['results'] = array();
-    foreach ($items as $key => $row) {
-    
-    	// Prepare the data we need to send an email
-		// Get the template information for this message
-		$this_template_id = $row['template_id'];
-		if (isset($templates[$this_template_id])) {
-			// We already have the information.
-		} else {
-			// Get the information
-			$mailer_template->getItem(array('itemid' => $this_template_id));
-			$values = $mailer_template->getFieldValues();
-			$templates[$this_template_id]['message_id']   = $values['id'];
-			$templates[$this_template_id]['message_body'] = $values['body'];
-			$templates[$this_template_id]['subject']      = $values['subject'];
-		}
-		// Assemble the parameters for the email
-		$params['message_id']   = $templates[$this_template_id]['message_id'];
-		$params['message_body'] = $templates[$this_template_id]['message_body'];
-		$params['subject']      = $templates[$this_template_id]['subject'];
- 	
-    	// If this is a test, just send the mail
-		if ($args['test']) {
-			// Send the email
-			$data['result'] = xarMod::apiFunc('reminders', 'admin', 'send_email_lookup', array('info' => $row, 'params' => $params, 'copy_emails' => $args['copy_emails'], 'test' => $args['test']));        	
-			$data['results'] = array_merge($data['results'], array($data['result']));
+	if ($args['test']) {
+		$data['results'] = array();
+		foreach ($items as $key => $row) {
+	
+			// Prepare the data we need to send an email
+			// Get the template information for this message
+			$this_template_id = $row['template_id'];
+			if (isset($templates[$this_template_id])) {
+				// We already have the information.
+			} else {
+				// Get the information
+				$mailer_template->getItem(array('itemid' => $this_template_id));
+				$values = $mailer_template->getFieldValues();
+				$templates[$this_template_id]['message_id']   = $values['id'];
+				$templates[$this_template_id]['message_body'] = $values['body'];
+				$templates[$this_template_id]['subject']      = $values['subject'];
+			}
+			// Assemble the parameters for the email
+			$params['message_id']   = $templates[$this_template_id]['message_id'];
+			$params['message_body'] = $templates[$this_template_id]['message_body'];
+			$params['subject']      = $templates[$this_template_id]['subject'];
+	
+			// If this is a test, just send the mail
+			if ($args['test']) {
+				// Send the email
+				$data['result'] = xarMod::apiFunc('reminders', 'admin', 'send_email_lookup', array('info' => $row, 'params' => $params, 'copy_emails' => $args['copy_emails'], 'test' => $args['test']));        	
+				$data['results'] = array_merge($data['results'], array($data['result']));
 
-			// We are done with this reminder
-			break;
-		} else {
-			// This a live email
-			$data['result'] = xarMod::apiFunc('reminders', 'admin', 'send_email_lookup', array('info' => $row, 'params' => $params, 'copy_emails' => $args['copy_emails'], 'test' => $args['test']));        	
-			$data['results'] = array_merge($data['results'], array($data['result']));
+				// We are done with this reminder
+				break;
+			} else {
+				// This a live email
+				$data['result'] = xarMod::apiFunc('reminders', 'admin', 'send_email_lookup', array('info' => $row, 'params' => $params, 'copy_emails' => $args['copy_emails'], 'test' => $args['test']));        	
+				$data['results'] = array_merge($data['results'], array($data['result']));
+			}
 		}
-    }
+	} else {
+		// Get the owners to be processed (sent an email)
+		$owners = xarMod::apiFunc('reminders', 'user', 'getall_owners', array('do_lookup' => true));     
+		var_dump($owners);exit;
+	}
     return $data['results'];
 }
 ?>
