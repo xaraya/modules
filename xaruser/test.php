54c3a1aaa22ed86b28b8d3c89a3d8c8b8e315504
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-06 16:58:50 +0200

    Add test

--- xaruser/test.php
+++ xaruser/test.php
@@ -16,6 +16,9 @@
  */
 function reminders_user_test()
 {
+    $rows = xarMod::apiFunc('reminders','scheduler','process_lookups');
+//    $rows = xarMod::apiFunc('reminders','admin','generate_random_entry', array('user' => 3));
+//    var_dump($rows);
     return array();
 }
 ?>
