3a8df17e58939ed46d0b76d7fe94637486895ce9
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-13 10:53:07 +0200

    Cosmetics

--- xaruser/cron_run_scheduler.php
+++ xaruser/cron_run_scheduler.php
@@ -1,6 +1,5 @@
 <?php
 // This file will call required URL of the scheduler module to trigger scheduler from outside
-// for testing purpose let's first call http://eventhubsacramento.com/writeinlog.php
 
 // Uncomment the next 2 lines for going live
 callScheduler();
@@ -26,30 +25,28 @@ function getUrlContent($url, $loop = 0, $delay = 0)
 {
         $file_contents = "";
 	for($loopCount = 0; $loopCount <= $loop; $loopCount++)
-{
-    $ch = curl_init($url);
-    $timeout = 10;
-    curl_setopt ($ch, CURLOPT_URL, $url);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        
-        $file_contents = curl_exec($ch);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    if ($httpCode['http_code'] < "300") 
-    {
-	if ($file_contents != '' || $loop == 0)
 	{
-		break;
-	}
-    }
-    else 
-    {
-        $file_contents = "";
-    }
-	sleep($delay);
-}   
+		$ch = curl_init($url);
+		$timeout = 10;
+		curl_setopt ($ch, CURLOPT_URL, $url);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		
+			$file_contents = curl_exec($ch);
+		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		if ($httpCode['http_code'] < "300") 
+		{
+		if ($file_contents != '' || $loop == 0)
+		{
+			break;
+		}
+		}
+		else 
+		{
+			$file_contents = "";
+		}
+		sleep($delay);
+	}   
     return $file_contents;
-
-
 }
 ?>
