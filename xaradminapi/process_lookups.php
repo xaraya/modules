45c53e792e37ef4dca217798532ddad8b6ab4b4e
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-15 15:56:04 +0200

    Typo

--- xaradminapi/process_lookups.php
+++ xaradminapi/process_lookups.php
@@ -81,7 +81,7 @@ function reminders_adminapi_process_lookups($args)
 		    $row['message'] = xarModVars::get('reminders', 'message');
 		    
 		    // Encode the row information so we can send that to the template
-			$row['encoded'] = 'dork' //base64_encode($row);
+			$row['encoded'] = 'dork'; //base64_encode($row);
 			
 			// Prepare the data we need to send an email
 			// Get the template information for this message
