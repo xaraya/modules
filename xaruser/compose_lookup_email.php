7a16b5e23328f8dbfcffe752619ffa8130ad60ba
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-17 12:15:34 +0200

    Mots

--- xaruser/compose_lookup_email.php
+++ xaruser/compose_lookup_email.php
@@ -19,7 +19,6 @@ function reminders_user_compose_lookup_email($args)
 
     if (!xarVarFetch('confirm',     'int', $confirm,            0, XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('code',        'str', $code,               '', XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('message_id',  'int', $data['message_id'], 0, XARVAR_NOT_REQUIRED)) return;
 
     $data['copy_emails'] = true;
     
@@ -36,6 +35,7 @@ function reminders_user_compose_lookup_email($args)
 	
 	$data['subject'] = $args['params']['subject'];
 	$data['message_body'] = unserialize($args['params']['message']);
+	$data['lookup_template'] = $args['params']['lookup_template'];
 	$name = DataPropertyMaster::getProperty(array('name' => 'name'));
 	
 	// Get the name components of the recipient to pass to the template
@@ -76,7 +76,7 @@ function reminders_user_compose_lookup_email($args)
         $bccaddress = $checkbox->value ? array(xarUser::getVar('email')) : array();
 
         // Bail if no message was chosen
-        if (empty($data['message_id']) && empty($data['message_body'])) {
+        if (empty($data['lookup_template']) && empty($data['message_body'])) {
             $data['message_warning'] = xarML('No message was defined');
             return $data;
         }
@@ -105,9 +105,9 @@ function reminders_user_compose_lookup_email($args)
                               'bccaddresses'     => $bccaddress,
                               'data'             => $emailargs,
                                             );
-                if (!empty($data['message_id']))  {
+                if (!empty($data['lookup_template']))  {
                     // We have a message ID
-                    $args['id'] = (int)$data['message_id'];
+                    $args['id'] = (int)$data['lookup_template'];
                     if (!empty($data['message_body'])) {
                     // We have a message ID (which indicates a template) and also a message body
                         $args['subject'] = $data['subject'];
