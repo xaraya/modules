9a4a31e16e2e6cae013b40087dad490c5c566cc0
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-17 22:02:35 +0200

    Mots

--- xaruser/compose_lookup_email.php
+++ xaruser/compose_lookup_email.php
@@ -137,7 +137,7 @@ function reminders_user_compose_lookup_email($args)
                 // Now record this email in the history table
                 sys::import('xaraya.structures.query');
                 $tables = xarDB::getTables();
-                $q = Query('INSERT', $tables['reminders_lookup_history']);
+                $q = new Query('INSERT', $tables['reminders_lookup_history']);
                 $q-> addfield('lookup', $args['message']);
                 $q-> addfield('owner', $args['message']);
                 $q-> addfield('subject', $args['subject']);
